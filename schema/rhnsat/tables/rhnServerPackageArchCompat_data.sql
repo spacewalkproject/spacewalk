345a0220c8da812fd61a40d27c5e969122dc38be
skarmark <skarmark@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-03-26 17:42:57 +0000

    Sat5.2: Satellite Support for Solaris Sun4v - server side database changes
    git-svn-id: svn+ssh://svn.rhndev.redhat.com/svn/rhn-svn/trunk/eng@138572 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- schema/rhnsat/tables/rhnServerPackageArchCompat_data.sql
+++ schema/rhnsat/tables/rhnServerPackageArchCompat_data.sql
@@ -408,6 +408,34 @@ insert into rhnServerPackageArchCompat
 
 insert into rhnServerPackageArchCompat
 (server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('sparc.sun4v-solaris'), 10);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('sparc-solaris'), 100);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('sparc-solaris-patch'), 210);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('sparc-solaris-patch-cluster'), 310);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('noarch-solaris'), 410);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('noarch-solaris-patch'), 510);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('sparc-sun4v-solaris'), LOOKUP_PACKAGE_ARCH('noarch-solaris-patch-cluster'), 610);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
 (LOOKUP_SERVER_ARCH('i386-i86pc-solaris'), LOOKUP_PACKAGE_ARCH('i386-solaris'), 10);
 
 insert into rhnServerPackageArchCompat
