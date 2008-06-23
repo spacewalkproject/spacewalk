69f9cc8ca1cf0a92ed00d86f3a1ae069dd5dd227
James Bowes <jbowes@redhat.com>
2008-06-23 07:40:50 -0400

    Update dummy _apache module for httpd 2+

--- backend/_apache.py
+++ backend/_apache.py
@@ -37,3 +37,21 @@ table = None
 config_tree = None
 server_root = None
 mpm_query = None
+
+# Variables for apache 2+
+exists_config_define = None
+stat = None
+
+AP_CONN_UNKNOWN = None
+AP_CONN_CLOSE = None
+AP_CONN_KEEPALIVE = None
+
+APR_NOFILE = None
+APR_REG = None
+APR_DIR = None
+APR_CHR = None
+APR_BLK = None
+APR_PIPE = None
+APR_LNK = None
+APR_SOCK = None
+APR_UNKFILE = None
