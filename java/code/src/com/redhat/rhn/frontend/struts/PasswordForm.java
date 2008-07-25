df86200dd701f705a486b150c7e620f6c0e4c61f
Partha Aji <paji@redhat.com>
2008-07-25 18:55:51 -0400

    Bugzilla: None - Fixed a checkstyle issue...

--- java/code/src/com/redhat/rhn/frontend/struts/PasswordForm.java
+++ java/code/src/com/redhat/rhn/frontend/struts/PasswordForm.java
@@ -7,20 +7,20 @@
  * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
  * along with this software; if not, see
  * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
- *
+ * 
  * Red Hat trademarks are not licensed under GPLv2. No permission is
  * granted to use or replicate Red Hat trademarks that are incorporated
- * in this software or its documentation.
+ * in this software or its documentation. 
  */
 
 package com.redhat.rhn.frontend.struts;
 
 
 /**
- * @author paji
  * This class basically scrubs all the properties
  * in the dynaform except those properties containing the word Password.
- * It is so frequently used I decided to make it uniform..
+ * @author paji
+ * @version $Rev $
  */
 public class PasswordForm extends ScrubbingDynaActionForm {
     private static final long serialVersionUID = -7565323006365800525L;
