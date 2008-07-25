ea0e636809ca95e8ef8016024ae03dd7a5d0aa08
Michael Mraka <michael.mraka@redhat.com>
2008-07-25 14:19:35 +0200

    Fixed exitcode (return sqlplus status rather than always success)

--- satellite/admin/rhn-populate-database.pl
+++ satellite/admin/rhn-populate-database.pl
@@ -90,10 +90,11 @@ local *LOGFILE;
 open(LOGFILE, ">", $log_file) or die "Error writing log file '$log_file': $OS_ERROR";
 $pid = open3(gensym, ">&LOGFILE", ">&LOGFILE", 'sqlplus', $dsn, "\@$schema_deploy_file");
 waitpid($pid, 0);
+my $retcode = $?;
 
 system('/bin/rm', $lockfile);
 
-exit 0;
+exit $retcode;
 
 sub get_next_backup_filename {
   my ($vol, $dir, $filename) = File::Spec->splitpath($log_file);
