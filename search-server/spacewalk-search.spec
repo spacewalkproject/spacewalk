spacewalkproject/spacewalk

commit ae0c379d08f7732cf469f961b6d9617381ebccaa

rename packages to spacewalk-*

search-server/spacewalk-search.spec
@@ -1,0 +1,66 @@
+%{!?__redhat_release:%define __redhat_release UNKNOWN}
+
+Name: spacewalk-search
+Summary: Spacewalk Full Text Search Server
+Group: Applications/Internet
+License: GPLv2
+Version: 0.1
+Release: 3%{?dist}
+# This src.rpm is cannonical upstream
+# You can obtain it using this set of commands
+# git clone git://git.fedorahosted.org/git/spacewalk.git/
+# cd search-server
+# make test-srpm
+URL:       https://fedorahosted.org/spacewalk 
+Source0:   %{name}-%{version}.tar.gz
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+BuildArch: noarch
+
+Summary: Java web application files for Spacewalk
+Group: Applications/Internet
+Requires: tanukiwrapper
+Requires: jpackage-utils >= 0:1.5
+BuildRequires: ant
+BuildRequires: tanukiwrapper
+%description
+This package contains the code for the Java version of the Spacewalk Web Site.
+
+%prep
+%setup
+
+%install
+ant -Djar.version=%{version} all
+rm -f lib/tanukiwrapper-3.1.2.jar
+install -d -m 755 $RPM_BUILD_ROOT/%{_sysconfdir}/rhn/search
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/indexes
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/lib
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/classes/com/redhat/satellite/search/db
+install -d -m 755 $RPM_BUILD_ROOT/etc/init.d
+install -d -m 755 $RPM_BUILD_ROOT/${_bindir}
+install -d -m 755 $RPM_BUILD_ROOT/%{_var}/log/rhn/search
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/nutch
+
+install -m 644 dist/%{name}-%{version}.jar $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/lib/
+install -m 644 lib/* $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/lib
+install -m 644 src/config/log4j.properties $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/classes/log4j.properties
+install -m 644 src/config/com/redhat/satellite/search/db/* $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/classes/com/redhat/satellite/search/db
+install -m 755 src/config/rhn-search $RPM_BUILD_ROOT/%{_sysconfdir}/init.d
+ln -s -f /usr/sbin/tanukiwrapper $RPM_BUILD_ROOT/%{_bindir}/rhnsearchd
+install -m 644 src/config/search/rhn_search.conf $RPM_BUILD_ROOT/%{_sysconfdir}/rhn/search/rhn_search.conf
+install -m 644 src/config/search/rhn_search_daemon.conf $RPM_BUILD_ROOT/%{_sysconfdir}/rhn/search/rhn_search_daemon.conf
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(644,root,root,755)
+%attr(755, root, root) %{_var}/log/rhn/search
+%{_prefix}/share/rhn/search/lib/*
+%{_prefix}/share/rhn/search/classes/log4j.properties
+%{_prefix}/share/rhn/search/classes/com/*
+%attr(755, root, root) %{_prefix}/share/rhn/search/indexes
+%attr(755, root, root) %{_sysconfdir}/init.d/rhn-search
+%attr(755, root, root) %{_bindir}/rhnsearchd
+%config(noreplace) %{_sysconfdir}/rhn/search/rhn_search.conf
+%config(noreplace) %{_sysconfdir}/rhn/search/rhn_search_daemon.conf

backend/spacewalk-server.spec
@@ -1,34 +1,46 @@
-%define rhnroot /usr/share/rhn
+%define rhnroot %{_usr}/share/rhn
 %define rhnconf /etc/rhn
 %define httpdconf /etc/rhn/satellite-httpd/conf
 
-Name: rhns
-Summary: Common programs needed to be installed on the RHN servers/proxies.
-Group: RHN/Server
+Name: spacewalk
+Summary: This top package should never be build. All is in subpackages.
+Group: Applications/Internet
 License: GPLv2
 Version: 0.1.1
-Release: 0%{?dist}
+Release: 2%{?dist}
+# This src.rpm is cannonical upstream
+# You can obtain it using this set of commands
+# git clone git://git.fedorahosted.org/git/spacewalk.git/
+# cd backend
+# make test-srpm
+URL:     https://fedorahosted.org/spacewalk 
 Source0: %{name}-%{version}.tar.gz
-BuildRoot: /var/tmp/%{name}-%{version}-root
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
 BuildArch: noarch
+
+%package common
+Summary: Common programs needed to be installed on the Spacewalk servers/proxies.
+Group: Applications/Internet
 Requires: python, rpm-python
 Requires: rhnlib >= 1.8
 BuildRequires: /usr/bin/msgfmt
 BuildRequires: /usr/bin/docbook2man
-PreReq: httpd
+Requires(pre): httpd
 # we don't really want to require this redhat-release, so we protect
 # against installations on other releases using conflicts...
-Obsoletes: rhns-common
+Obsoletes: rhns-common <= 5.2
+Obsoletes: rhns <= 5.2
 
-%description 
+%description common
 Generic program files needed by the RHN server machines.
 This package includes the common code required by all servers/proxies.
 
 %package sql
-Summary: Core functions providing SQL connectivity for the RHN backend modules
-Group: RHN/Server
-Prereq: %{name} = %{version}-%{release}
+Summary: Core functions providing SQL connectivity for the Spacewalk backend modules
+Group: Applications/Internet
+Prereq: %{name}-common = %{version}-%{release}
 Requires: python(:DBAPI:oracle)
+Obsoletes: rhns-sql <= 5.2
 
 %description sql
 This package contains the basic code that provides SQL connectivit for the RHN
@@ -36,9 +48,9 @@ backend modules.
 
 %package server
 Summary: Basic code that provides RHN Server functionality
-Group: RHN/Server
-Prereq: %{name}-sql = %{version}-%{release}
-
+Group: Applications/Internet
+Requires(pre): %{name}-sql = %{version}-%{release}
+Obsoletes: rhns-server <= 5.2
 # PyXML and sgmlop do crazy substitution stuff to get themselves into
 # python's standard namespace. apparently, they're faster than the regular
 # xml parsing stuff. Well, sgmlop is. Maybe we can get rid of PyXML.
@@ -54,9 +66,10 @@ receivers and get them enabled automatically.
 
 %package xmlrpc
 Summary: Handler for /XMLRPC
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
-Obsoletes: rhns-server-xmlrpc
+Obsoletes: rhns-server-xmlrpc <= 5.2
+Obsoletes: rhns-xmlrpc <= 5.2
 
 %description xmlrpc
 These are the files required for running the /XMLRPC handler, which
@@ -65,8 +78,9 @@ and the up2date clients.
 
 %package applet
 Summary: Handler for /APPLET
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
+Obsoletes: rhns-applet <= 5.2
 
 %description applet
 These are the files required for running the /APPLET handler, which
@@ -74,12 +88,13 @@ provides the functions for the RHN applet.
 
 %package app
 Summary: Handler for /APP
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
-Obsoletes: rhns-server-app
+Obsoletes: rhns-server-app <= 5.2
 # We really don't need specspo installed, it will do bad things when we read
 # rpms
 Conflicts: specspo
+Obsoletes: rhns-app <= 5.2
 
 %description app
 These are the files required for running the /APP handler.
@@ -87,9 +102,10 @@ Calls to /APP are used by internal maintenance tools (rhnpush).
 
 %package xp
 Summary: Handler for /XP
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
-Obsoletes: rhns-server-xp
+Obsoletes: rhns-server-xp <= 5.2
+Obsoletes: rhns-xp <= 5.2
 # We really don't need specspo installed, it will do bad things when we read
 # rpms
 Conflicts: specspo
@@ -100,67 +116,74 @@ Calls to /XP are used by tools publicly available (like rhn_package_manager).
 
 %package config-files-common
 Summary: Common files for the Configuration Management project
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
+Obsoletes: rhns-config-files-common <= 5.2
 
 %description config-files-common
 Common files required by the Configuration Management project
 
 %package config-files
 Summary: Handler for /CONFIG-MANAGEMENT
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-config-files-common = %{version}-%{release}
+Obsoletes: rhns-config-files <= 5.2
 
 %description config-files
 This package contains the server-side code for configuration management.
 
 %package config-files-tool
 Summary: Handler for /CONFIG-MANAGEMENT-TOOL
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-config-files-common = %{version}-%{release}
+Obsoletes: rhns-config-files-tool <= 5.2
 
 %description config-files-tool
 This package contains the server-side code for configuration management tool.
 
 %package upload-server
 Summary: Server-side listener for rhn-pkgupload
-Group: RHN/Server
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
+Obsoletes: rhns-upload-server <= 5.2
 
 %description upload-server
 Server-side listener for rhn-pkgupload
 
 %package package-push-server
 Summary: Listener for rhnpush (non-XMLRPC version)
-Group: RHN/SErver
+Group: Applications/Internet
 Requires: %{name}-server = %{version}-%{release}
+Obsoletes: rhns-package-push-server <= 5.2
 
 %description package-push-server
 Listener for rhnpush (non-XMLRPC version)
 
 %package satellite-tools
 Summary: Red Hat Network Services Satellite Tools
-Group: RHN/Server
-Requires: rhns-xmlrpc = %{version}-%{release}
-Requires: rhns-app = %{version}-%{release}
-Requires: rhns-certs-tools
-Requires: rhn-satellite-admin >= 3.6.0-198
+Group: Applications/Internet
+Requires: %{name}-xmlrpc = %{version}-%{release}
+Requires: %{name}-app = %{version}-%{release}
+Requires: spacewalk-certs-tools
+Requires: spacewalk-admin >= 3.6.0-198
 Requires: python-gzipstream
 Requires: PyXML
 Requires: mod_ssl
 Requires: python-optik
-Requires: rhns-xml-export-libs
+Requires: %{name}-xml-export-libs
+Obsoletes: rhns-satellite-tools <= 5.2
 
 %description satellite-tools
 Various utilities for the Red Hat Network Satellite Server.
 
 %package xml-export-libs
 Summary: Red Hat Network XML data exporter
-Group: RHN/Server
-Requires: rhns-server = %{version}-%{release}
+Group: Applications/Internet
+Requires: %{name}-server = %{version}-%{release}
 %if "%{pythongen}" == "1.5"
 Requires: python-iconv
 %endif
+Obsoletes: rhns-xml-export-libs <= 5.2
 
 %description xml-export-libs
 Libraries required by various exporting tools
@@ -210,7 +233,7 @@ fi
 echo "server.secret_key = $secret_key" >> %{rhnconf}/rhn.conf
 rm -f %{rhnconf}/rhnSecret.py*
 
-%files
+%files common
 %defattr(-,root,root)
 %dir %{rhnroot}
 %dir %{rhnroot}/common
@@ -308,7 +331,7 @@ rm -f %{rhnconf}/rhnSecret.py*
 %{rhnroot}/server/repomd/view.py*
 
 # the cache
-%attr(750,apache,root) %dir /var/cache/rhn
+%attr(750,apache,root) %dir %{_var}/cache/rhn
 # config files
 %attr(640,root,apache) %{rhnconf}/default/rhn_server.conf
 # main httpd config
@@ -478,6 +501,9 @@ rm -f %{rhnconf}/rhnSecret.py*
 
 # $Id$
 %changelog
+* Mon Aug 04 2008 Miroslav Suchý <msuchy@redhat.com>
+- rename package to spacewalk-*
+
 * Mon Jun 30 2008 Pradeep Kilambi <pkilambi@redhat.com>
 - including spacewalk-debug tool
 

branding/spacewalk-branding.spec
@@ -1,6 +1,6 @@
 Name:       spacewalk-branding
 Version:    0.2
-Release:    3%{?dist}
+Release:    4%{?dist}
 Summary:    Spacewalk branding data
 
 Group:      Applications/Internet
@@ -10,7 +10,7 @@ Source0:    %{name}-%{version}.tar.gz
 BuildRoot:  %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
 BuildArch:  noarch
 
-Requires:   rhn-html
+Requires:   spacewalk-html
 
 
 %description
@@ -44,6 +44,9 @@ rm -rf %{buildroot}
 
 
 %changelog
+* Mon Aug 04 2008  Miroslav Suchy <msuchy@redhat.com>
+- fix dependecies
+
 * Wed Jul 30 2008  Devan Goodwin <dgoodwin@redhat.com> 0.2-2
 - Adding images.
 

java/sources
@@ -1,1 +1,0 @@
-thispartignored rhn-java-sat-0.1.tar.gz

java/spacewalk-java.spec
@@ -3,18 +3,23 @@
 %define jardir          %{_localstatedir}/lib/tomcat5/webapps/rhn/WEB-INF/lib
 %define jars antlr asm bcel bouncycastle/bcprov bouncycastle/bcpg c3p0 cglib commons-beanutils commons-cli commons-codec commons-configuration commons-digester commons-discovery commons-el commons-fileupload commons-lang commons-logging commons-validator concurrent dom4j hibernate3 jaf jasper5-compiler jasper5-runtime javamail jcommon jdom jfreechart jspapi jpam log4j redstone-xmlrpc redstone-xmlrpc-client ojdbc14 oro oscache sitemesh struts taglibs-core taglibs-standard wsdl4j xalan-j2 xerces-j2 xml-commons-apis
 
-Name: rhn-java-sat
-Summary: RHN Java site packages
+Name: spacewalk-java
+Summary: Spacewalk Java site packages
 Group: Applications/Internet
 License: GPLv2
-Source1: version
-Version: %(echo `awk '{ print $1 }' %{SOURCE1}`)
-Release: %(echo `awk '{ print $2 }' %{SOURCE1}`)%{?dist}
-Source0:        %{name}-%{version}.tar.gz
+Version: 0.1
+Release: 4%{?dist}
+# This src.rpm is cannonical upstream
+# You can obtain it using this set of commands
+# git clone git://git.fedorahosted.org/git/spacewalk.git/
+# cd java
+# make test-srpm
+URL:       https://fedorahosted.org/spacewalk
+Source0:   %{name}-%{version}.tar.gz
 BuildRoot: %{_tmppath}/%{name}-root
 BuildArch: noarch
 
-Summary: Java web application files for RHN
+Summary: Java web application files for Spacewalk
 Group: Applications/Internet
 Requires: bcel
 Requires: bouncycastle-provider
@@ -85,28 +90,34 @@ BuildRequires: struts
 BuildRequires: sitemesh
 BuildRequires: velocity >= 0:1.5
 BuildRequires: wsdl4j
-Obsoletes: rhn-java
-%description -n rhn-java-sat
+Obsoletes: rhn-java <= 5.2
+Obsoletes: rhn-java-sat <= 5.2
+
+%description -n spacewalk-java
 This package contains the code for the Java version of the Red Hat
 Network Web Site.
 
-%package -n rhn-java-config-sat
-Summary: Configuration files for RHN Java
+%package -n spacewalk-java-config
+Summary: Configuration files for Spacewalk Java
 Group: Applications/Internet
-Obsoletes: rhn-java-config
-%description -n rhn-java-config-sat
-This package contains the configuration files for the RHN Java web
+Obsoletes: rhn-java-config <= 5.2
+Obsoletes: rhn-java-config-sat <= 5.2
+
+%description -n spacewalk-java-config
+This package contains the configuration files for the Spacewalk Java web
 application and taskomatic process.
 
-%package -n rhn-java-lib-sat
-Summary: Jar files for RHN Java
+%package -n spacewalk-java-lib
+Summary: Jar files for Spacewalk Java
 Group: Applications/Internet
-Obsoletes: rhn-java-lib
+Obsoletes: rhn-java-lib <= 5.2
+Obsoletes: rhn-java-lib-sat <= 5.2
+
 %description -n rhn-java-lib-sat
-This package contains the jar files for the RHN Java web application
+This package contains the jar files for the Spacewalk Java web application
 and taskomatic process.
 
-%package -n taskomatic-sat
+%package -n spacewalk-taskomatic
 Summary: Java version of taskomatic
 Group: Applications/Internet
 Requires: bcel
@@ -135,63 +146,63 @@ Requires: rhn-java-config-sat
 Requires: rhn-java-lib-sat
 Requires: concurrent
 Requires: quartz
-Obsoletes: taskomatic
-%description -n taskomatic-sat
+Obsoletes: taskomatic <= 5.2
+Obsoletes: taskomatic-sat <= 5.2
+
+%description -n spacewalk-taskomatic
 This package contains the Java version of taskomatic.
 
 %prep
-%setup -n %(echo %{main_source} | sed 's/\.tar\.gz//')
+%setup
 
 %install
 ant -Dprefix=$RPM_BUILD_ROOT install
 install -d -m 755 $RPM_BUILD_ROOT/%{_sysconfdir}/tomcat5/Catalina/localhost/
 install -d -m 755 $RPM_BUILD_ROOT/etc/init.d
-install -d -m 755 $RPM_BUILD_ROOT/usr/bin
+install -d -m 755 $RPM_BUILD_ROOT/%{_bindir}
 install -d -m 755 $RPM_BUILD_ROOT/etc/rhn
 install -d -m 755 $RPM_BUILD_ROOT/etc/rhn/default
-install -d -m 755 $RPM_BUILD_ROOT/usr/share/rhn
-install -d -m 755 $RPM_BUILD_ROOT/usr/share/rhn/lib
-install -d -m 755 $RPM_BUILD_ROOT/usr/share/rhn/classes
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/lib
+install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/classes
 install -m 755 conf/rhn.xml $RPM_BUILD_ROOT/%{_sysconfdir}/tomcat5/Catalina/localhost/rhn.xml
 install -m 644 conf/default/rhn_hibernate.conf $RPM_BUILD_ROOT/etc/rhn/default/rhn_hibernate.conf
 install -m 644 conf/default/rhn_taskomatic_daemon.conf $RPM_BUILD_ROOT/etc/rhn/default/rhn_taskomatic_daemon.conf
 install -m 644 conf/default/rhn_taskomatic.conf $RPM_BUILD_ROOT/etc/rhn/default/rhn_taskomatic.conf
 install -m 644 conf/default/rhn_org_quartz.conf $RPM_BUILD_ROOT/etc/rhn/default/rhn_org_quartz.conf
 install -m 755 scripts/taskomatic $RPM_BUILD_ROOT/etc/init.d
-install -m 644 build/webapp/rhnjava/WEB-INF/lib/rhn.jar $RPM_BUILD_ROOT/%{_usr}/share/rhn/lib
-install -m 644 build/classes/log4j.properties $RPM_BUILD_ROOT/%{_usr}/share/rhn/classes/log4j.properties
-ln -s -f /usr/sbin/tanukiwrapper $RPM_BUILD_ROOT/%{_usr}/bin/taskomaticd
+install -m 644 build/webapp/rhnjava/WEB-INF/lib/rhn.jar $RPM_BUILD_ROOT/%{_prefix}/share/rhn/lib
+install -m 644 build/classes/log4j.properties $RPM_BUILD_ROOT/%{_prefix}/share/rhn/classes/log4j.properties
+ln -s -f /usr/sbin/tanukiwrapper $RPM_BUILD_ROOT/%{_bindir}/taskomaticd
 
 %clean
 rm -rf $RPM_BUILD_ROOT
 
-#%post -n rhn-java-sat
-#/usr/bin/build-jar-repository --preserve-naming -s %{jardir} %{jars}
-#/bin/chown tomcat %{jardir}/*.jar
-#/bin/chgrp tomcat %{jardir}/*.jar
-
-%files -n rhn-java-sat
+%files -n spacewalk-java
 %defattr(644,tomcat,tomcat,775)
 %dir %{appdir}
 %{appdir}/*
 %config(noreplace) %{_sysconfdir}/tomcat5/Catalina/localhost/rhn.xml
 
-%files -n taskomatic-sat
+%files -n spacewalk-taskomatic
 %attr(755, root, root) %{_sysconfdir}/init.d/taskomatic
-%attr(755, root, root) %{_usr}/bin/taskomaticd
+%attr(755, root, root) %{_bindir}/taskomaticd
 
-%files -n rhn-java-config-sat
+%files -n spacewalk-java-config
 %defattr(644, root, root)
 %config(noreplace) %{_sysconfdir}/rhn/default/rhn_hibernate.conf
 %config(noreplace) %{_sysconfdir}/rhn/default/rhn_taskomatic_daemon.conf
 %config(noreplace) %{_sysconfdir}/rhn/default/rhn_taskomatic.conf
 %config(noreplace) %{_sysconfdir}/rhn/default/rhn_org_quartz.conf
 
-%files -n rhn-java-lib-sat
-%attr(644, root, root) %{_usr}/share/rhn/classes/log4j.properties
-%attr(644, root, root) %{_usr}/share/rhn/lib/rhn.jar
+%files -n spacewalk-java-lib
+%attr(644, root, root) %{_prefix}/share/rhn/classes/log4j.properties
+%attr(644, root, root) %{_prefix}/share/rhn/lib/rhn.jar
 
 %changelog
+* Mon Aug  4 2008 Miroslav Suchy <msuchy@redhat.com> 
+- rename package from rhn-java to spacewalk-java
+
 * Thu May 22 2008 Jan Pazdziora 5.2.0-5
 - weaken hibernate3 version requirement
 

java/version
@@ -1,1 +1,0 @@
-0.1 3

satellite/admin/admin.spec
@@ -1,21 +1,26 @@
-%define rhnroot /usr/share/rhn
+%define rhnroot /%{_prefix}/share/rhn
 Summary: Various utility scripts and data files for RHN Satellite installations
-Name: rhn-satellite-admin
-URL: http://rhn.redhat.com/
+Name: spacewalk-admin
+# This src.rpm is cannonical upstream
+# You can obtain it using this set of commands
+# git clone git://git.fedorahosted.org/git/spacewalk.git/
+# cd satellite/admin
+# make test-srpm
+URL:     https://fedorahosted.org/spacewalk 
 Version: 0.1
-Release: 1%{?dist}
-Source0: %{name}-%{version}.tar.gz
+Release: 2%{?dist}
 License: GPLv2
-Group: RHN/Server
-BuildRoot: %{_tmppath}/%{name}-root
-Requires: rhn-base
+Group: Applications/Internet
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+Requires: spacewalk-base
 Requires: perl-URI, perl(MIME::Base64)
 Requires: sudo
-Obsoletes: satellite-utils
+Obsoletes: satellite-utils <= 5.2
+Obsoletes: rhn-satellite-admin <= 5.2
 BuildArch: noarch
 
 %description
-Various utility scripts and data files for RHN Satellite installations
+Various utility scripts and data files for Spacewalk installations.
 
 %prep
 %setup
@@ -24,10 +29,10 @@ Various utility scripts and data files for RHN Satellite installations
 rm -rf $RPM_BUILD_ROOT
 make -f Makefile.admin install PREFIX=$RPM_BUILD_ROOT
 
-(cd $RPM_BUILD_ROOT/usr/bin && ln -s validate-sat-cert.pl validate-sat-cert)
+(cd $RPM_BUILD_ROOT/%{_bindir} && ln -s validate-sat-cert.pl validate-sat-cert)
 
 mkdir -p $RPM_BUILD_ROOT%{_mandir}/man3/
-/usr/bin/pod2man validate-sat-cert.pod | gzip -c - > $RPM_BUILD_ROOT%{_mandir}/man3/validate-sat-cert.3.gz
+%{_bindir}/pod2man validate-sat-cert.pod | gzip -c - > $RPM_BUILD_ROOT%{_mandir}/man3/validate-sat-cert.3.gz
 chmod 0644 $RPM_BUILD_ROOT%{_mandir}/man3/validate-sat-cert.3.gz
 
 %clean
@@ -36,22 +41,25 @@ rm -rf $RPM_BUILD_ROOT
 %files
 %defattr(-,root,root)
 %dir %{rhnroot}
-/etc/init.d/rhn-satellite
-/usr/bin/validate-sat-cert.pl
-/usr/bin/validate-sat-cert
-/usr/bin/rhn-config-satellite.pl
-/usr/bin/rhn-config-schema.pl
-/usr/bin/rhn-config-tnsnames.pl
-/usr/bin/rhn-populate-database.pl
-/usr/bin/rhn-generate-pem.pl
-/usr/bin/rhn-load-ssl-cert.pl
-/usr/bin/rhn-deploy-ca-cert.pl
-/usr/bin/rhn-install-ssl-cert.pl
+%{_sysconfdir}/init.d/rhn-satellite
+%{_bindir}/validate-sat-cert.pl
+%{_bindir}/validate-sat-cert
+%{_bindir}/rhn-config-satellite.pl
+%{_bindir}rhn-config-schema.pl
+%{_bindir}/rhn-config-tnsnames.pl
+%{_bindir}/rhn-populate-database.pl
+%{_bindir}/rhn-generate-pem.pl
+%{_bindir}/rhn-load-ssl-cert.pl
+%{_bindir}/rhn-deploy-ca-cert.pl
+%{_bindir}/rhn-install-ssl-cert.pl
 /sbin/rhn-sat-restart-silent
 %{rhnroot}/RHN-GPG-KEY
 %{_mandir}/man3/validate-sat-cert.3.gz
 
 %changelog
+* Mon Aug  4 2008 Miroslav Suchy <msuchy@redhat.com>
+- Renamed to spacewalk-admin
+
 * Mon Aug  4 2008 Jan Pazdziora 0.1-1
 - removed version and sources files
 

satellite/certs/Makefile
@@ -1,3 +1,2 @@
-
 include ../../rel-eng/Makefile
 

commit 0d491b65326a4bb3f9714f44038445ba64a8ad93

spacewalk-search: fix typo introduced by ae0c379d08f7732cf469f961b6d9617381ebccaa.

search-server/spacewalk-search.spec
@@ -38,7 +38,7 @@ install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/indexes
 install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/lib
 install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/classes/com/redhat/satellite/search/db
 install -d -m 755 $RPM_BUILD_ROOT/etc/init.d
-install -d -m 755 $RPM_BUILD_ROOT/${_bindir}
+install -d -m 755 $RPM_BUILD_ROOT/%{_bindir}
 install -d -m 755 $RPM_BUILD_ROOT/%{_var}/log/rhn/search
 install -d -m 755 $RPM_BUILD_ROOT/%{_prefix}/share/rhn/search/nutch
 