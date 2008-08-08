cb61fb1f78e6b56d6a5f8ce3f0262bbe5784f962
jesus m. rodriguez <jesusr@redhat.com>
2008-08-08 16:04:02 -0400

    bugzilla: none fix checkstyle error, unused import.

--- java/code/src/com/redhat/rhn/manager/errata/test/ErrataManagerTest.java
+++ java/code/src/com/redhat/rhn/manager/errata/test/ErrataManagerTest.java
@@ -47,7 +47,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
