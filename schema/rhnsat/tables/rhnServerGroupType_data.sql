867a7e8cc1f6aeaab4e2522f3c50436205e73b2b
shughes <shughes@ad8b2fa6-1fe3-0310-8810-ca5544d99bb5>
2008-08-21 14:12:02 +0000

    bz:none reverting due to schema change
    git-svn-id: svn+ssh://svn.rhndev.redhat.com/svn/rhn-svn/branches/eng/RELEASE-5.2/schema@176243 ad8b2fa6-1fe3-0310-8810-ca5544d99bb5

--- schema/rhnsat/tables/rhnServerGroupType_data.sql
+++ schema/rhnsat/tables/rhnServerGroupType_data.sql
@@ -19,6 +19,14 @@ insert into rhnServerGroupType (id, label, name, permanent, is_base)
                 'N', 'Y'
         );
 
+-- provisioning_entitled type ------------------------------------------------
+
+insert into rhnServerGroupType (id, label, name, permanent, is_base)
+        values (rhn_servergroup_type_seq.nextval,
+                'provisioning_entitled', 'RHN Provisioning Entitled Servers',
+                'N', 'N'
+        );
+
 -- monitoring_entitled type --------------------------------------------------
 
 insert into rhnServerGroupType (id, label, name, permanent, is_base)
@@ -27,13 +35,13 @@ insert into rhnServerGroupType (id, label, name, permanent, is_base)
         'N', 'N'
 	);
 
--- provisioning_entitled type ------------------------------------------------
+-- nonlinux_entitled type ----------------------------------------------------
 
-insert into rhnServerGroupType (id, label, name, permanent, is_base)
-        values (rhn_servergroup_type_seq.nextval,
-                'provisioning_entitled', 'RHN Provisioning Entitled Servers',
-                'N', 'N'
-        );
+insert into rhnServerGroupType ( id, label, name, permanent, is_base)
+   values ( rhn_servergroup_type_seq.nextval,
+      'nonlinux_entitled', 'Non-Linux Entitled Servers',
+      'N', 'Y'
+   );
 
 -- virtualization_* types ----------------------------------------------------
 
@@ -50,13 +58,6 @@ insert into rhnServerGroupType ( id, label, name, permanent, is_base)
       'N', 'N'
    );      
 
--- nonlinux_entitled type ----------------------------------------------------
-
-insert into rhnServerGroupType ( id, label, name, permanent, is_base)
-   values ( rhn_servergroup_type_seq.nextval,
-      'nonlinux_entitled', 'Non-Linux Entitled Servers',
-      'N', 'Y'
-   );
 
 commit;
 
