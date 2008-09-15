4660171d612adbc11b760debe7f011a2cf2bddd8
Jan Pazdziora <jpazdziora@redhat.com>
2008-09-15 14:40:08 +0200

    Schema upgrade: we need to create the column, fill it in, then set it not null and primary key.

--- schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/055-3-rhnPackageChangeLog-id-not-null.sql
+++ schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/055-3-rhnPackageChangeLog-id-not-null.sql
@@ -1,7 +1,7 @@
 
 alter table rhnPackageChangelog
-add id              number
+modify id 
 	constraint rhn_pkg_cl_id_nn not null
-	constraint rhn_pkg_cl_id_pk primary key;
+	constraint rhn_pkg_cl_id_pk primary key
 	using index tablespace [[64k_tbs]];
 
