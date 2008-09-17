e7b8c5b509a3abf0017186510fa2c1bde71335b8
John Matthews <jmatthew@redhat.com>
2008-09-17 16:36:53 -0400

    462499 -  RHEL 5.2 yum-rhn-plugin is broken after 4.2.3 update If we get an exception when getting repo xml file, refresh our up2dateAuth and retry
    This fixes an issue where a proxy and client get into an odd state
    of the client having an up2dateAuth cached, yet the proxy has no data
    cached for the client.  A call to up2dateAuth.updateLoginInfo() is required
    so the call flows through the proxy and data for the client is now available.

--- client/rhel/yum-rhn-plugin/rhnplugin.py
+++ client/rhel/yum-rhn-plugin/rhnplugin.py
@@ -439,6 +439,15 @@ class RhnRepo(YumRepository):
         """
         self.disable()
 
+    def _getRepoXML(self):
+        import yum.Errors
+        try:
+            return YumRepository._getRepoXML(self)
+        except yum.Errors.RepoError, e:
+            # Refresh our loginInfo then try again
+            # possibly it's out of date
+            up2dateAuth.updateLoginInfo()
+            return YumRepository._getRepoXML(self)
 
 class BadConfig(Exception):
     pass
