1a42716fdf95b074e1bc4c46ab009843bc327bd1
Devan Goodwin <dgoodwin@redhat.com>
2008-09-18 17:44:31 -0300

    Automatic commit of package [spacewalk-setup] release [0.3.1-1].

---
+++ satellite/install/Spacewalk-Setup/up2.sh
@@ -0,0 +1,3 @@
+scp ~/src/spacewalk/satellite/install/Spacewalk-Setup/lib/Spacewalk/Setup.pm root@newman:/usr/lib/perl5/vendor_perl/5.8.8/Spacewalk
+scp ~/src/spacewalk/satellite/install/Spacewalk-Setup/bin/spacewalk-setup root@newman:/usr/bin/spacewalk-setup
+scp ~/src/spacewalk/satellite/install/Spacewalk-Setup/share/oracle/* root@newman:/usr/share/spacewalk/setup/oracle/
