37f9034f7149e92bad56f5aa0d199462a59a5529
Justin Sherrill <jsherril@redhat.com>
2008-10-06 16:35:22 -0400

    checkstyle fix

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -992,8 +992,9 @@ public class SystemHandler extends BaseHandler {
      * @xmlrpc.param #param("string", "sessionKey")
      * @xmlrpc.param #param("int", "serverId")
      * @xmlrpc.param #param("int", "serverGroupId")
-     * @xmlrpc.param #param_desc("boolean", "member",  "'1' to assign the given server to the 
-     * given server group, '0' to remove the given server from the given server group.")
+     * @xmlrpc.param #param_desc("boolean", "member",  "'1' to assign the given server to 
+     * the given server group, '0' to remove the given server from the given server 
+     * group.")
      * @xmlrpc.returntype #return_int_success()
      */
     public int setGroupMembership(String sessionKey, Integer sid, Integer sgid, 
