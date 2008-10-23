82a8ae9d86c66e01804dd49702df0d74101fa5b3
Pradeep Kilambi <pkilambi@redhat.com>
2008-10-23 12:33:47 -0400

    fixing the constraint

--- schema/spacewalk/rhnsat/tables/rhnRegTokenPackages.sql
+++ schema/spacewalk/rhnsat/tables/rhnRegTokenPackages.sql
@@ -30,8 +30,8 @@ rhnRegTokenPackages
                                 references rhnPackageName(id)
 				on delete cascade,
         arch_id         number
-                        constraint rhn_sactionvr_paid_nn not null
-                        constraint rhn_sactionvr_paid_fk
+                        constraint rhn_reg_tok_pkg_aid_nn not null
+                        constraint rhn_reg_tok_pkg_aid_fk
                                 references rhnPackageArch(id)
                                 on delete cascade
 )
