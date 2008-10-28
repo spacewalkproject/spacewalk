170b4a2212897ebd4246dd480676e2b7bd0c6537
Mike McCune <mmccune@gmail.com>
2008-10-28 13:32:47 -0700

    461162 - addition of new spacewalk-koan package

---
+++ client/tools/spacewalk-koan/spacewalk-koan.spec
@@ -0,0 +1,50 @@
+%define rhnroot /usr/share/rhn
+
+Summary: Support package for spacewalk koan interaction.
+Name: spacewalk-koan
+Group: System Environment/Kernel
+License: GPLv2
+Source0: %{name}-%{version}.tar.gz
+Version: 0.1.0
+Release: 1%{?dist}
+BuildArch : noarch
+URL: http://rhn.redhat.com/
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-buildroot
+
+Provides: rhn.kickstart.boot_image
+Obsoletes: rhn-kickstart
+Requires: koan
+
+# If this is rhel 4 or less we need up2date.
+%if 0%{?rhel} && "%rhel" < "5"
+Requires: up2date
+%else
+Requires: rhn-check
+%endif
+
+%description
+Support package for spacewalk koan interaction.
+ 
+%prep
+%setup -q
+
+%build
+make -f Makefile.spacewalk-koan
+
+%install
+rm -rf $RPM_BUILD_ROOT
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,root,root,-)
+/usr/sbin/*
+%config(noreplace)  /etc/sysconfig/rhn/clientCaps.d/kickstart
+%{rhnroot}/spacewalkkoan/spacewalkkoan.py*
+
+#%{rhnroot}/actions/kickstart.py*
+
+%changelog
+* Tue Oct 28 2008 Mike McCune - 1.0.0-1
+- Initial creation.
---
+++ client/tools/spacewalk-koan/spacewalkkoan/spacewalkkoan.py
@@ -0,0 +1,62 @@
+#
+# Copyright (c) 2008 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+# 
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation. 
+#
+# Kickstart a system using koan.
+#
+
+import traceback
+import string
+import sys
+
+from koan.app import Koan
+
+def initiate(profile_name, extra_append, static_device=None, preserve_files=[]):
+
+    error_messages = {}
+    success = 0
+    
+    try:
+        k = Koan()
+        k.list_items          = False
+        k.server              = "172.31.1.102"
+        k.is_virt             = False
+        k.is_replace          = True
+        k.is_display          = False
+        k.profile             = "f9-x86_64"
+        k.system              = None
+        k.image               = None
+        k.live_cd             = None
+        k.virt_path           = None
+        k.virt_type           = None
+        k.virt_bridge         = None
+        k.no_gfx              = True
+        k.add_reinstall_entry = None
+        k.kopts_override      = None
+        k.run()
+
+    except Exception, e:
+        (xa, xb, tb) = sys.exc_info()
+        try:
+            getattr(e,"from_koan")
+            error_messages['from_koan'] = str(e)[1:-1]
+            print str(e)[1:-1] # nice exception, no traceback needed
+        except:
+            print xa
+            print xb
+            print string.join(traceback.format_list(traceback.extract_tb(tb)))
+            error_messages['koan'] = string.join(traceback.format_list(traceback.extract_tb(tb)))
+        return (1, "Kickstart failed. Koan error.", error_messages)
+
+    
+    return (0, "Kickstart initiate succeeded", error_messages)
