6858ab40b4b02eb2d62ed1e2e958207722909ce4
Todd B Sanders <tsanders@localhost.localdomain>
2008-10-29 08:30:35 -0400

    Updated setup.py configuration

---
+++ playpen/Telemetry/src/sample_reports/scripts/affectedSystemsReport.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+from telemetry import Report
+import sys
+import xmlrpclib
+
+class Erratum():
+    pass
+
+class System():
+    pass
+
+def processData(client, key, errata):
+    
+    try:
+        
+        for erratum in errataList:
+            
+            print erratum
+            
+            e = client.errata.getDetails(key, erratum)
+            
+            if e:
+                
+                ee = Erratum()
+                ee.advisoryName = erratum
+                ee.type = e['type']
+                ee.description = e['description']
+                ee.synopsis = e['synopsis']
+                ee.systems = []
+                
+                errata.append(ee)
+                
+                slist = client.errata.listAffectedSystems(key, erratum)
+                
+                for s in slist:
+                    
+                    system = System()
+                    system.id = s['id']
+                    system.name = s['name']
+                    
+                    ee.systems.append(system)
+                    
+    except xmlrpclib.Fault, fault:
+        print "Error in systemDetailsReport.processData():"
+        print fault.faultCode
+        print fault.faultString
+            
+    return errata
+    
+# Main Processing 
+if len(sys.argv) != 6:
+    print "Usage %s <config> <type> <username> <password> <errata>" % (sys.argv[0])
+    sys.exit(1)
+    
+# Arguments
+config = sys.argv[1]
+type = sys.argv[2]
+username = sys.argv[3]
+password = sys.argv[4]
+errataList = sys.argv[5].split(',')
+
+# Create Report Instance
+report = Report(config)
+
+# Get ServerProxies
+clients = report.connect()
+
+errata = []
+
+_count = 0
+
+for client in clients:
+    
+    # get the session key
+    try:
+        key = client.auth.login(username, password)
+    except xmlrpclib.Fault, fault:
+        print "Error in systemDetailsReport:"
+        print fault.faultCode
+        print fault.faultString
+    
+    if not (report.parameters['Aggregate']):
+        errata = []
+        
+        systems = processData(client,key,errata)
+        satellites = [report.parameters['Satellites'][_count]]
+        vars = {'errata': errata, 'satellites': satellites}
+        report.templatify(vars, type)
+        
+    else:
+        systems = processData(client,key,errata)
+        
+    _count = _count + 1
+        
+if (report.parameters['Aggregate']):
+    vars = {'errata': errata, 'satellites': report.parameters['Satellites']}
+    report.templatify(vars, type)
+        
+                                                               
+    
