eb8df97784db2aff6f764f9565fc3baae714fc92
Michael Mraka <michael.mraka@redhat.com>
2008-11-27 11:58:46 +0100

    awk lesson 2 - how to replace sed with awk

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -8,8 +8,8 @@ cd `echo $0 | sed s/$BASENAME//`/..;
 
 for package in ` \
 	rel-eng/koji-missing-builds.pl $TAG | \
-	awk '!/buildsys-macros/ { if (x==1) { print } } /Builds missing in koji/ { x=1 }' | \
-	sed '1,$s/\s*\([a-zA-Z_-]*\)-.*/\1/' | \
+	awk '!/buildsys-macros/ { if (x==1) { print gensub(" *([a-zA-Z_-]+)-.*", "\\1 ", "g")} }
+             /Builds missing in koji/ { x=1 }' | \
 	xargs -I replacestring cat rel-eng/packages/replacestring |cut -f2 -d' '`; do 
   ( cd $package && make srpm DIST='.el5' | \
     grep 'Wrote:' |cut -f2 -d' ' | \
