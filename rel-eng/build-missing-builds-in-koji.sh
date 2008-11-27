f9e051138d645f6b4117f5f989177aed3f2302bb
Michael Mraka <michael.mraka@redhat.com>
2008-11-27 13:11:36 +0100

    removed backticks - prevent errors if the list of packages is really long

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -6,13 +6,13 @@ pushd `pwd`
 BASENAME=`basename $0`
 cd `echo $0 | sed s/$BASENAME//`/..;
 
-for package in ` \
-	rel-eng/koji-missing-builds.pl $TAG | \
+rel-eng/koji-missing-builds.pl $TAG | \
 	awk '!/buildsys-macros/ {
                  if (x==1) { print gensub(" *([a-zA-Z_-]+)-.*", "\\1 ", "g")}
                  }
              /Builds missing in koji/ { x=1 }' | \
-	xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring `; do
+	xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring | \
+        while read packages ; do
   ( cd $package && make srpm DIST='.el5' | \
     awk '/Wrote:/ {print $2}' | \
     xargs -I packagepath koji -c ~/.koji/spacewalk-config build $TAG packagepath; )
