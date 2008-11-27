30855fee658571099af5af9972107a98315f3dee
Michael Mraka <michael.mraka@redhat.com>
2008-11-27 13:20:19 +0100

    fixed typos (packages -> package, "\\1 " -> "\\1")

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -8,11 +8,11 @@ cd `echo $0 | sed s/$BASENAME//`/..;
 
 rel-eng/koji-missing-builds.pl $TAG | \
 	awk '!/buildsys-macros/ {
-                 if (x==1) { print gensub(" *([a-zA-Z_-]+)-.*", "\\1 ", "g")}
+                 if (x==1) { print gensub(" *([a-zA-Z_-]+)-.*", "\\1", "g")}
                  }
              /Builds missing in koji/ { x=1 }' | \
 	xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring | \
-        while read packages ; do
+        while read package ; do
   ( cd $package && make srpm DIST='.el5' | \
     awk '/Wrote:/ {print $2}' | \
     xargs -I packagepath koji -c ~/.koji/spacewalk-config build $TAG packagepath; )
