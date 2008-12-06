10abb8fda09eacccbbf094970a9e433668ef9697
John Matthews <jmatthew@redhat.com>
2008-12-05 21:04:56 -0500

    APIs relating to system search Refactored SystemSearchHelper to allow it to be reused by API calls for search Added search by network & hardware device, search matches what webui allows API calls for search by: ip,hostname,deviceDescription,deviceId,deviceDriver,deviceVendorId

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/CommunicationErrorSearchServer.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2008 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+package com.redhat.rhn.frontend.xmlrpc;
+
+
+import com.redhat.rhn.FaultException;
+
+/**
+ * error communicating with the search server
+ *
+ * @version $Revision$
+ */
+public class CommunicationErrorSearchServer extends FaultException {
+
+    /**
+     * Constructor
+     */
+    public CommunicationErrorSearchServer() {
+        super(2901, "communicationErrorSearchServer",
+                "Error communicating with search server");
+    }
+
+    /**
+     * Constructor
+     * @param cause the cause (which is saved for later retrieval
+     * by the Throwable.getCause() method). (A null value is
+     * permitted, and indicates that the cause is nonexistent or
+     * unknown.)
+     */
+    public CommunicationErrorSearchServer(Throwable cause) {
+        super(2901, "communicationErrorSearchServer",
+                "Error communicating with search server", cause);
+    }
+
+    /**
+     * Constructor with message.
+     * @param message exception message.
+     */
+    public CommunicationErrorSearchServer(String message) {
+        super(2901 , "communicationErrorSearchServer", message);
+    }
+
+}
