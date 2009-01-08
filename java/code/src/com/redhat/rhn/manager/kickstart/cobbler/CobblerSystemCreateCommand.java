9215b4c68072b4479f0a762505a1d49bc44b145d
Mike McCune <mmccune@gmail.com>
2009-01-08 11:52:21 -0800

    461162 - agh, compile error

--- java/code/src/com/redhat/rhn/manager/kickstart/cobbler/CobblerSystemCreateCommand.java
+++ java/code/src/com/redhat/rhn/manager/kickstart/cobbler/CobblerSystemCreateCommand.java
@@ -112,7 +112,7 @@ public class CobblerSystemCreateCommand extends CobblerCommand {
      */
     public ValidatorError store() {
         String handle = null;
-        log.debug("Map null?: " + (getSystemMap != null));
+        log.debug("Map null?: " + (getSystemMap() != null));
         
         if (getSystemMap() != null) {
             handle = (String) invokeXMLRPC("get_system_handle",
