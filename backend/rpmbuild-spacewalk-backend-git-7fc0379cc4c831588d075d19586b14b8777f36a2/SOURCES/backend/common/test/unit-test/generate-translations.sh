cc71d870af43556c971565525854273cee73d9fd
Pradeep Kilambi <pkilambi@redhat.com>
2009-01-15 09:52:37 -0500

    none

--- backend/rpmbuild-spacewalk-backend-git-7fc0379cc4c831588d075d19586b14b8777f36a2/SOURCES/backend/common/test/unit-test/generate-translations.sh
+++
@@ -1,7 +0,0 @@
-TOPDIR=$(cd $(dirname $0) && pwd)
-
-domain="unit-test"
-for lang in en ro; do
-    msgfmt --statistics --verbose \
-        --output-file $TOPDIR/translations/$lang/LC_MESSAGES/$domain.mo $TOPDIR/$domain.$lang
-done
