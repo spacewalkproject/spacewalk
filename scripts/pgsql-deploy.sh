aeecfee3c8e66e46c8d282b178628dc26b78949d
Devan Goodwin <dgoodwin@redhat.com>
2009-01-28 15:40:00 -0400

    Modify pgsql-deploy.sh for new files, and copying via scp.
    Call with:
    
        SW_USER_HOST=root@yourspacewalk scripts/pgsql-deploy.sh

--- scripts/pgsql-deploy.sh
+++ scripts/pgsql-deploy.sh
@@ -13,16 +13,19 @@ echo ""
 
 # Watchout for the vendor_perl dir here, this is ok for RHEL 5 and thus probably CentOS 5, 
 # but this is beyond fragile:
-cp -v ./spacewalk/setup/lib/Spacewalk/Setup.pm /usr/lib/perl5/vendor_perl/5.8.8/Spacewalk/Setup.pm
-cp -v ./spacewalk/setup/bin/spacewalk-setup /usr/bin/spacewalk-setup
+scp ./spacewalk/setup/lib/Spacewalk/Setup.pm $SW_USER_HOST:/usr/lib/perl5/vendor_perl/5.8.8/Spacewalk/Setup.pm
+scp ./spacewalk/setup/bin/spacewalk-setup $SW_USER_HOST:/usr/bin/spacewalk-setup
 
-cp -v ./schema/spacewalk/postgresql/postgresql.universe.satellite.sql /etc/sysconfig/rhn/postgresql.universe.satellite.sql
+scp ./schema/spacewalk/postgresql/postgresql.universe.satellite.sql $SW_USER_HOST:/etc/sysconfig/rhn/postgresql.universe.satellite.sql
 
-cp -R -v ./backend/server/rhnSQL /usr/share/rhn/server/
+scp -r ./backend/server/rhnSQL $SW_USER_HOST:/usr/share/rhn/server/
+scp -r ./backend/satellite_tools $SW_USER_HOST:/usr/share/rhn/
 
-cp -v ./spacewalk/admin/rhn-populate-database.pl /usr/bin/rhn-populate-database.pl
+scp ./spacewalk/admin/rhn-populate-database.pl $SW_USER_HOST:/usr/bin/rhn-populate-database.pl
+
+scp ./spacewalk/config/etc/sysconfig/rhn-satellite-prep/etc/rhn/rhn.conf $SW_USER_HOST:/etc/sysconfig/rhn-satellite-prep/etc/rhn/rhn.conf
 
 echo ""
 echo "To undo these changes remove and re-install the following packages:"
-echo "    rpm -e --nodeps spacewalk-setup spacewalk-admin spacewalk-backend-sql"
-echo "    yum install spacewalk-setup spacewalk-admin spacewalk-backend-sql"
+echo "    rpm -e --nodeps spacewalk-setup spacewalk-admin spacewalk-backend-sql spacewalk-backend-tools spacewalk-config"
+echo "    yum install spacewalk-setup spacewalk-admin spacewalk-backend-sql spacewalk-backend-tools spacewalk-config"
