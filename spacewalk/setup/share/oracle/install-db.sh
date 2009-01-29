d151e2dd552ef3275d823cb35775b184ecc2016c
Jan Pazdziora <jpazdziora@redhat.com>
2009-01-29 09:57:09 +0100

    Revert "install-db.sh: Call create-db-dirs.sh before create-db.sh, to get directories created, and labeled."
    We can actually call restorecon from inside of create-db.sh.
    
    This reverts commit aafb1a3935f0d2714b71af9db6d3a2e119fa3994.

--- spacewalk/setup/share/oracle/install-db.sh
+++ spacewalk/setup/share/oracle/install-db.sh
@@ -37,8 +37,6 @@ usermod -G oracle,dba,apache oracle
 ORACLE_ADMIN_DIR=/opt/apps/oracle/admin/10.2.0
 export ORACLE_ADMIN_DIR
 
-$ORACLE_ADMIN_DIR/create-db-dirs.sh --datadir /rhnsat/data/rhnsat --admindir /rhnsat/admin/rhnsat
-
 runuser - oracle -c "$ORACLE_ADMIN_DIR/create-db.sh --db rhnsat --user rhnsat --password rhnsat --datadir /rhnsat/data/rhnsat --admindir /rhnsat/admin/rhnsat --template $ORACLE_ADMIN_DIR/embedded-createdb.tmpl"
 
 LISTFILE=$ORACLE_HOME/network/admin/listener.ora
