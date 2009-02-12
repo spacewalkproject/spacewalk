f818d951960cd7817cd24b21ddbda2a6cd3f1fb1
Pradeep Kilambi <pkilambi@redhat.com>
2009-02-12 10:35:58 -0500

    more checkstyle fixes

--- java/code/src/com/redhat/rhn/domain/errata/Cve.java
+++ java/code/src/com/redhat/rhn/domain/errata/Cve.java
@@ -1,32 +1,52 @@
+/**
+ * Copyright (c) 2008 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ * 
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation. 
+ */
 package com.redhat.rhn.domain.errata;
-
+/**
+ * 
+ * @version $Rev $
+ *
+ */
 public class Cve {
-	
-	private Long id;
-	private String name;
-    
-	/**
+
+    private Long id;
+    private String name;
+    /**
      * @return Returns the id.
      */
     public Long getId() {
         return id;
     }
-    
+   
     /**
      * @param i The id to set.
      */
     public void setId(Long i) {
         this.id = i;
     }
-    
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	
-
+    /**
+     * 
+     * @param name The name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+    /**
+     * 
+     * @return Returns name
+     */
+    public String getName() {
+        return name;
+    }
 }
