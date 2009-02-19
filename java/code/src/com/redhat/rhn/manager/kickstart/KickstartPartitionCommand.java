aee6ecbe50d8f6959987cf06eb9b763791c8eb16
Justin Sherrill <jsherril@redhat.com>
2009-02-19 16:11:41 -0500

    checkstyle fix

--- java/code/src/com/redhat/rhn/manager/kickstart/KickstartPartitionCommand.java
+++ java/code/src/com/redhat/rhn/manager/kickstart/KickstartPartitionCommand.java
@@ -34,7 +34,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 /**
  * KickstartPrePostCommand - for editing the pre and post steps
