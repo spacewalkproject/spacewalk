04753bec338552a351895d93799ababc90a573ec
John Matthews <jmatthew@redhat.com>
2009-02-19 23:14:54 -0500

    checkstyle fix

--- java/code/src/com/redhat/rhn/domain/kickstart/test/KickstartDataTest.java
+++ java/code/src/com/redhat/rhn/domain/kickstart/test/KickstartDataTest.java
@@ -65,7 +65,6 @@ import java.util.Collection;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
