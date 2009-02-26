d2fa833029c73c912c21eccda18221e749cade3b
Devan Goodwin <dgoodwin@rm-rf.ca>
2009-02-26 14:06:31 -0400

    build.py: Stop using --nodeps when building rpms locally.

--- rel-eng/lib/spacewalk/releng/builder.py
+++ rel-eng/lib/spacewalk/releng/builder.py
@@ -190,7 +190,7 @@ class Builder(object):
         define_dist = ""
         if self.dist:
             define_dist = "--define 'dist %s'" % self.dist
-        cmd = "rpmbuild %s %s --nodeps --clean -ba %s" % \
+        cmd = "rpmbuild %s %s --clean -ba %s" % \
                 (self._get_rpmbuild_dir_options(), define_dist, self.spec_file)
         output = run_command(cmd)
         print output
