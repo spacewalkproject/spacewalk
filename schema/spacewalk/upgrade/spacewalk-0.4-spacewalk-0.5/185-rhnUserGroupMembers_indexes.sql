ab24df841bc8e998ac1be3313b4d98003665e2a6
Milan Zazrivec <mzazrivec@redhat.com>
2009-03-09 17:49:58 +0100

    we're actually dropping parallel query

--- schema/spacewalk/upgrade/spacewalk-0.4-spacewalk-0.5/185-rhnUserGroupMembers_indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-0.4-spacewalk-0.5/185-rhnUserGroupMembers_indexes.sql
@@ -1,4 +1,4 @@
 
-alter index rhn_ugmembers_uid_ugid_uq nologging;
-alter index rhn_ugmembers_ugid_uid_idx nologging;
+alter index rhn_ugmembers_uid_ugid_uq noparallel;
+alter index rhn_ugmembers_ugid_uid_idx noparallel;
 
