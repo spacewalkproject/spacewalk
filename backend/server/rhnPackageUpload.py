3e1bc16744a3d816dbedd7d91939ab75618c5de1
Pradeep Kilambi <pkilambi@redhat.com>
2009-03-23 12:38:06 -0400

    487621 - applying jbowe's patch to fix seg faults rhnpush has been causing server-side on F10

--- backend/server/rhnPackageUpload.py
+++ backend/server/rhnPackageUpload.py
@@ -16,11 +16,11 @@
 # $Id$
 
 import os
-import struct
 import tempfile
 from mod_python import apache
 
 from common import CFG, log_debug, rhnFault, rhn_mpm, rhnLib, UserDictCase
+from common.rhn_rpm import get_header_byte_range
 
 from server import rhnSQL
 from server.importlib import importLib, userAuth, mpmSource, backendOracle, \
@@ -375,59 +375,6 @@ def load_package(package_stream):
         payload_stream.seek(0,0)
 
     return header, payload_stream, md5sum, header_start, header_end
-
-def get_header_byte_range(package_file):
-    """ 
-    Return the start and end bytes of the rpm header object. 
-    
-    For details of the rpm file format, see:
-    http://www.rpm.org/max-rpm/s1-rpm-file-format-rpm-file-format.html
-    """
-
-    lead_size = 96
-
-    # Move past the rpm lead
-    package_file.seek(lead_size)
-
-    sig_size = get_header_struct_size(package_file)
-
-    # Now we can find the start of the actual header.
-    header_start = lead_size + sig_size
-
-    package_file.seek(header_start)
-
-    header_size = get_header_struct_size(package_file)
-
-    header_end = header_start + header_size
-
-    return (header_start, header_end)
-
-def get_header_struct_size(package_file):
-    """
-    Compute the size in bytes of the rpm header struct starting at the current
-    position in package_file. 
-    """
-    # Move past the header preamble
-    package_file.seek(8, 1)
-
-    # Read the number of index entries
-    header_index = package_file.read(4)
-    (header_index_value, ) = struct.unpack('>I', header_index)
-    
-    # Read the the size of the header data store 
-    header_store = package_file.read(4)
-    (header_store_value, ) = struct.unpack('>I', header_store)
-
-    # The total size of the header. Each index entry is 16 bytes long.
-    header_size = 8 + 4 + 4 + header_index_value * 16 + header_store_value
-
-    # Headers end on an 8-byte boundary. Round out the extra data. 
-    round_out = header_size % 8
-    if round_out != 0:
-        header_size = header_size + (8 - round_out)
-
-    return header_size
-
 
 class AlreadyUploadedError(Exception):
     pass
