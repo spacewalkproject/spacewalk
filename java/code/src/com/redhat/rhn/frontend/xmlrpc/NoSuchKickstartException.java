fe5154b6a09d2af1a5dde8651512c9d314a0e1da
Justin Sherrill <jsherril@redhat.com>
2009-03-31 17:09:44 -0400

    458838 - adding new files for kickstart exception 404

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/NoSuchKickstartException.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+
+/*
+ * AUTOMATICALLY GENERATED FILE, DO NOT EDIT.
+ */
+package com.redhat.rhn.frontend.xmlrpc;
+
+import com.redhat.rhn.FaultException;
+
+/**
+ * no such package
+ * <p>
+
+ *
+ * @version definition($Rev: 76724 $)/template($Rev: 67725 $)
+ */
+public class NoSuchKickstartException extends FaultException  {
+
+
+    /////////////////////////
+    // Constructors
+    /////////////////////////
+        /**
+     * Constructor
+     */
+    public NoSuchKickstartException() {
+        super(2764 , "noSuchKickstart" , "The kickstart could not be found");
+        // begin member variable initialization
+    }
+
+        /**
+     * Constructor
+     * @param cause the cause (which is saved for later retrieval
+     * by the Throwable.getCause() method). (A null value is
+     * permitted, and indicates that the cause is nonexistent or
+     * unknown.)
+     */
+    public NoSuchKickstartException(Throwable cause) {
+        super(2764 , "noSuchKickstart" , "The kickstart could not be found" , cause);
+        // begin member variable initialization
+    }
+
+}
