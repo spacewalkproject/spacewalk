a9e43a7c3288684390e11a04a2a13589d064b7e0
Milan Zazrivec <mzazrivec@redhat.com>
2009-04-01 13:43:10 +0200

    Revert "484702 - remove dead function generate_server_pem from RHN::SatInstall."
    This reverts commit 128f4ee11c4bbd18690b4974f51e97db07849c32.
    
    Conflicts:
    
    	web/modules/rhn/RHN/SatInstall.pm
    
    generate_server_pem() is still needed by rhn-enable-push.pl
    (rhn-upgrade package).
    
    This cannot be replaced by Spacewalk::Setup::generate_server_pem(),
    because we need to be able to install rhn-upgrade on all Satellite
    versions, even those that do not have Spacewalk::Setup module.

--- web/modules/rhn/RHN/SatInstall.pm
+++ web/modules/rhn/RHN/SatInstall.pm
@@ -475,6 +475,37 @@ sub get_db_population_errors {
   return @errors;
 }
 
+sub generate_server_pem {
+  my $class = shift;
+  my %params = validate(@_, { ssl_dir => 1,
+			      system => 1,
+			      out_file => 0 });
+
+  my @opts;
+
+  push @opts, '--ssl-dir=' . File::Spec->catfile($params{ssl_dir}, $params{system});
+
+  if ($params{out_file}) {
+    push @opts, '--out-file=' . $params{out_file};
+  }
+  my $opts = join(' ', @opts);
+
+  my $content;
+
+  open(FH, "/usr/bin/sudo /usr/bin/rhn-generate-pem.pl $opts |")
+    or throw "(satinstall:generate_pem_error) Could not generate server.pem file: $OS_ERROR";
+
+  my @content = <FH>;
+
+  close(FH);
+
+  if (not $params{out_file}) {
+    $content = join('', @content);
+  }
+
+  return $content;
+}
+
 sub is_rpm_installed {
   my $class = shift;
   my $rpmname = shift;
