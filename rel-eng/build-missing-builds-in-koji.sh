d965aedbb52adb46a8de4755f4f9f10bb35b13c4
Miroslav Suchý <msuchy@redhat.com>
2009-04-03 09:28:24 +0200

    build using tito

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 TAGS=([0]="dist-5E-sw-0.6-candidate" [1]="dist-f10-sw-0.6-candidate")
-DISTS=([0]='.el5' [1]='.fc10')
 
 pushd `pwd`
 
@@ -9,6 +8,8 @@ cd `dirname $0`/..
 
 for tag in 0 1; do
   rel-eng/koji-missing-builds.py ${TAGS[$tag]} | \
+    grep -v oracle-server-admin | \
+    grep -v oracle-server-scripts | \
 	  awk '!/buildsys-macros/ {
                  if (x==1) { print gensub(" *([a-zA-Z_-]+)-.*", "\\1", "g")}
                  }
@@ -16,11 +17,10 @@ for tag in 0 1; do
 	  xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring | \
         while read package ; do
           (
-            echo Building $package for ${TAGS[$tag]}
-            cd $package && make srpm DIST=${DISTS[$tag]} | \
-            awk '/Wrote:/ {print $2}' | \
-            xargs -I packagepath koji -c ~/.koji/spacewalk-config build ${TAGS[$tag]} packagepath
-            make upload-tgz
+            echo Building package in path $package 
+            cd $package && ${TITO_PATH}tito build --koji-release | \
+            awk '/Wrote:.*tar.gz/ {print $2}' | \
+            xargs -I packagepath scp packagepath fedorahosted.org:spacewalk
           )
         done
 done
