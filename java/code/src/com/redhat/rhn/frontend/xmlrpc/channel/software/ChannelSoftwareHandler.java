eeca275cf391c5aa43a775382574482c382bb99f
Justin Sherrill <jsherril@redhat.com>
2009-04-03 18:14:43 -0400

    489792 - fixing documentation issues with various listErrata api calls

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/channel/software/ChannelSoftwareHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/channel/software/ChannelSoftwareHandler.java
@@ -1098,17 +1098,7 @@ public class ChannelSoftwareHandler extends BaseHandler {
      * @xmlrpc.param #param($date, "startDate")
      * @xmlrpc.returntype
      *      #array()
-     *          #struct("errata")
-     *              #prop_desc("string","advisory", "name of the advisory")
-     *              #prop_desc("string","issue_date",
-     *                         "date format follows YYYY-MM-DD HH24:MI:SS")
-     *              #prop_desc("string","update_date",
-     *                         "date format follows YYYY-MM-DD HH24:MI:SS")
-     *              #prop("string","synopsis")
-     *              #prop("string","advisory_type")
-     *              #prop_desc("string","last_modified_date",
-     *                         "date format follows YYYY-MM-DD HH24:MI:SS")
-     *          #struct_end()
+     *          $ErrataOverviewSerializer
      *      #array_end()
      */
     public List listErrata(String sessionKey, String channelLabel,
@@ -1138,17 +1128,7 @@ public class ChannelSoftwareHandler extends BaseHandler {
      * @xmlrpc.param #param($date, "endDate")
      * @xmlrpc.returntype
      *      #array()
-     *          #struct("errata")
-     *              #prop_desc("string","advisory", "name of the advisory")
-     *              #prop_desc("string","issue_date",
-     *                         "date format follows YYYY-MM-DD HH24:MI:SS")
-     *              #prop_desc("string","update_date",
-     *                         "date format follows YYYY-MM-DD HH24:MI:SS")
-     *              #prop("string","synopsis")
-     *              #prop("string","advisory_type")
-     *              #prop_desc("string","last_modified_date",
-     *                         "date format follows YYYY-MM-DD HH24:MI:SS")
-     *          #struct_end()
+     *          $ErrataOverviewSerializer
      *      #array_end()
      */
 
@@ -1189,7 +1169,6 @@ public class ChannelSoftwareHandler extends BaseHandler {
      *    #array()
      *      #struct("errata")
      *        #prop_desc("int", "id", "Errata Id")
-     *        #prop("string","advisory_type")
      *        #prop_desc("string", "date", "Date erratum was created.")
      *        #prop_desc("string", "advisory_synopsis", "Summary of the erratum.")
      *        #prop_desc("string", "advisory_type", "Type label such as Security, Bug Fix")
