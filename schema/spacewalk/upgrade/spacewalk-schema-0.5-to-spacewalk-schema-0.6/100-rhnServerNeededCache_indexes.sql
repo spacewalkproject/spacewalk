757fe0529864abc022f514fb46103ec8eb049167
Justin Sherrill <jsherril@redhat.com>
2009-04-06 18:21:55 -0400

    fixing some index mixups, nologging was left off the base schema for a few indexes for 0.5

--- schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/100-rhnServerNeededCache_indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/100-rhnServerNeededCache_indexes.sql
@@ -1,5 +1,10 @@
 create index rhn_snc_speid_idx
         on rhnServerNeededCache(server_id, package_id, errata_id)
-        parallel
+        noparallel
         tablespace [[128m_tbs]]
         nologging;
+
+alter index rhn_snc_pid_idx noparallel;
+alter index rhn_snc_sid_idx noparallel;
+alter index rhn_snc_eid_idx noparallel;
+
