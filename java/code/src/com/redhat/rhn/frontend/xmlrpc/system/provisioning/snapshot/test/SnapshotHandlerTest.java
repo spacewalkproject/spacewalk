c55ad4bf0952843b458b33579544338069cb442e
Brad Buckingham <bbuckingham@redhat.com>
2009-04-07 16:09:24 -0400

    487566 - api/script - initial commit of snapshot script and mods to snapshot apis
    This commit includes several changes:
    - modifications to the snapshot APIs as well as move of those APIs from
      the system namespace to system.provisioning.snapshot
    - creation of a 'system-snapshot' script that uses the APIs and provides a user
      an easier way to perform operations like list/delete of system snapshots
      (note: man page included)
    - initial creation of a 'spacewalk-utils' package that contains the
      new script as well as future similar scripts.. the intent is that
      users can install this package on a client and run then scripts remotely
    
    - note: the spec file still needs to be tested; however, this commit
      should have no impact on spacewalk... once it is verified,
      will create any necessary tickets to get the package included in future
      deliveries
    
    Squashed commit of the following:
    
    commit 42e352fa43755b8841f009d766016a097c73fecb
    Author: Brad Buckingham <bbuckingham@redhat.com>
    Date:   Tue Apr 7 16:00:14 2009 -0400
    
        checkstyle fix
    
    commit cf2065f653fbbec81dd47ff9f46e83fb5eda53b3
    Author: Brad Buckingham <bbuckingham@redhat.com>
    Date:   Tue Apr 7 15:22:23 2009 -0400
    
        adding the initial snapshot script, man page, spec file and makefile
    
    commit 11e99a7980aa9d2effd28ff70b23a93884e49165
    Author: Brad Buckingham <bbuckingham@redhat.com>
    Date:   Mon Apr 6 16:41:25 2009 -0400
    
        api - snapshot - add some additional error handling for snapshot apis
    
    commit 42022cf9433392d9e12985ad56bc7510f4fa5508
    Author: Brad Buckingham <bbuckingham@redhat.com>
    Date:   Mon Mar 30 12:30:30 2009 -0400
    
        487566 - modifications to snapshot APIs
    
        - moved apis from system handler to separate snapshot handler
        - made delete and list APIs more flexible to be able to delete and list
          by date as well as global or per system
    
    commit 087603f0f7f6022359e045df4ccba41164a9b6cf
    Author: Brad Buckingham <bbuckingham@redhat.com>
    Date:   Tue Mar 17 12:41:45 2009 -0400
    
        487566 - add system.listSystems, add snapshot apis in to new snapshot namespace

---
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/provisioning/snapshot/test/SnapshotHandlerTest.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.xmlrpc.system.provisioning.snapshot.test;
+
+import com.redhat.rhn.domain.rhnpackage.Package;
+import com.redhat.rhn.domain.rhnpackage.PackageNevra;
+import com.redhat.rhn.domain.rhnpackage.test.PackageTest;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.ServerGroup;
+import com.redhat.rhn.domain.server.ServerSnapshot;
+import com.redhat.rhn.domain.server.test.ServerFactoryTest;
+import com.redhat.rhn.frontend.xmlrpc.system.provisioning.snapshot.SnapshotHandler;
+import com.redhat.rhn.frontend.xmlrpc.test.BaseHandlerTestCase;
+import com.redhat.rhn.testing.ServerGroupTestUtils;
+import com.redhat.rhn.testing.TestUtils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * SnapshotHandlerTest
+ * @version $Rev$
+ */
+public class SnapshotHandlerTest extends BaseHandlerTestCase {
+
+    private SnapshotHandler handler = new SnapshotHandler();
+
+    private ServerSnapshot generateSnapshot(Server server) {
+        ServerSnapshot snap = new ServerSnapshot();
+        snap.setServer(server);
+        snap.setOrg(server.getOrg());
+        snap.setReason("blah");
+        return snap;
+    }
+
+    public void testListSnapshots() throws Exception {
+        Server server = ServerFactoryTest.createTestServer(admin, true);
+        ServerSnapshot snap = generateSnapshot(server);
+        ServerGroup grp = ServerGroupTestUtils.createEntitled(server.getOrg());
+        snap.addGroup(grp);
+
+        TestUtils.saveAndFlush(snap);
+        Map dateInfo = new HashMap();
+        List<ServerSnapshot> list = handler.listSnapshots(adminKey,
+                server.getId().intValue(), dateInfo);
+        assertContains(list, snap);
+        assertContains(snap.getGroups(), grp);
+
+    }
+
+    public  void testListSnapshotPackages() throws Exception {
+        Server server = ServerFactoryTest.createTestServer(admin, true);
+        ServerSnapshot snap = generateSnapshot(server);
+        Package pack = PackageTest.createTestPackage();
+        PackageNevra packN = new PackageNevra();
+        packN.setArch(pack.getPackageArch());
+        packN.setEvr(pack.getPackageEvr());
+        packN.setName(pack.getPackageName());
+        snap.getPackages().add(packN);
+        TestUtils.saveAndFlush(packN);
+        TestUtils.saveAndFlush(snap);
+        Set<PackageNevra> list = handler.listSnapshotPackages(adminKey,
+                snap.getId().intValue());
+         assertContains(list, packN);
+    }
+
+    public void testDeleteSnapshot() throws Exception {
+        Server server = ServerFactoryTest.createTestServer(admin, true);
+        ServerSnapshot snap = generateSnapshot(server);
+        TestUtils.saveAndFlush(snap);
+
+        handler.deleteSnapshot(adminKey, snap.getId().intValue());
+        Map dateInfo = new HashMap();
+        List<ServerSnapshot> list = handler.listSnapshots(adminKey,
+                server.getId().intValue(), dateInfo);
+        assertTrue(list.size() == 0);
+
+    }
+
+    public void testDeleteSnapshots() throws Exception {
+        Server server = ServerFactoryTest.createTestServer(admin, true);
+        ServerSnapshot snap = generateSnapshot(server);
+        generateSnapshot(server);
+        generateSnapshot(server);
+        generateSnapshot(server);
+        generateSnapshot(server);
+        TestUtils.saveAndFlush(snap);
+
+        Map dateInfo = new HashMap();
+        handler.deleteSnapshots(adminKey, server.getId().intValue(), dateInfo);
+        List<ServerSnapshot> list = handler.listSnapshots(adminKey,
+                server.getId().intValue(), dateInfo);
+        assertTrue(list.size() == 0);
+    }
+}
