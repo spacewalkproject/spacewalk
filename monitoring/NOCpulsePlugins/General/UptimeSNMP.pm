31e26ad0c9c0f85f6a3d070ec67382e0deb6d099
Tomas Lestach <tlestach@redhat.com>
2009-04-16 13:23:05 +0200

    449919 - fixing SNMP uptime information
    probe queried uptime of SNMP service, now it queries uptime of the host machine

--- monitoring/NOCpulsePlugins/General/UptimeSNMP.pm
+++ monitoring/NOCpulsePlugins/General/UptimeSNMP.pm
@@ -7,7 +7,7 @@ use NOCpulse::Probe::SNMP::MibEntryList;
 my @entries = 
   (
    { name      => 'Uptime',
-     oid       => '1.3.6.1.2.1.1.3.0',
+     oid       => '1.3.6.1.2.1.25.1.1.0',
      data_type => 'OCTET_STRING',
    },
   );
