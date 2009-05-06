2d584302c1ec2c23d9a358464539e1e7a587be0f
Jason Dobies <jason.dobies@redhat.com>
2009-05-06 12:19:16 -0400

    437361 - Added all orgs (except default org) to the entitlement's org subtab.
    Added a new call into OrgManager for returning entitlements with empty orgs. There is currently an XMLRPC API call that uses the existing method. I'll file a new BZ to address that (similar to the issue seen in the software entitlement calls) in an effort to deprecate the existing call.

--- java/code/src/com/redhat/rhn/frontend/action/multiorg/SystemEntitlementOrgsAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/multiorg/SystemEntitlementOrgsAction.java
@@ -14,6 +14,17 @@
  */
 package com.redhat.rhn.frontend.action.multiorg;
 
+import java.util.Iterator;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.DynaActionForm;
 import com.redhat.rhn.common.db.datasource.DataList;
 import com.redhat.rhn.common.validator.ValidatorError;
 import com.redhat.rhn.domain.entitlement.Entitlement;
@@ -27,38 +38,25 @@ import com.redhat.rhn.manager.entitlement.EntitlementManager;
 import com.redhat.rhn.manager.org.OrgManager;
 import com.redhat.rhn.manager.org.UpdateOrgSystemEntitlementsCommand;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
-import org.apache.struts.action.ActionErrors;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.action.DynaActionForm;
-
-import java.util.Iterator;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * SystemEntitlementOrgsAction
- * @version $Rev$
+/** 
+ * SystemEntitlementOrgsAction 
  */
 public class SystemEntitlementOrgsAction extends RhnAction {
 
+    private Logger log = Logger.getLogger(this.getClass());
 
-    private static Logger log = Logger.getLogger(SystemEntitlementOrgsAction.class);
-    /**
-     * ${@inheritDoc}
-     */
-    public ActionForward execute(ActionMapping mapping, ActionForm form, 
-            HttpServletRequest request, HttpServletResponse response) throws Exception {
+    /** ${@inheritDoc} */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm form,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response)
+        throws Exception {
+        
         DynaActionForm dynaForm = (DynaActionForm) form;
         RequestContext ctx = new RequestContext(request);
-        
+
         String entitlementLabel = request.getParameter("label");
-        Entitlement e = EntitlementManager.getByName(entitlementLabel);        
+        Entitlement e = EntitlementManager.getByName(entitlementLabel);
         User user = ctx.getLoggedInUser();
         request.setAttribute("entname", e.getHumanReadableLabel());
         //Get and store values submitted
@@ -68,26 +66,27 @@ public class SystemEntitlementOrgsAction extends RhnAction {
             Org org = OrgFactory.lookupById(orgId);
             String newCount = request.getParameter("newCount_" + orgClickedStr);
 
-            if (newCount != null && !StringUtils.isEmpty(newCount)) {                
+            if (newCount != null && !StringUtils.isEmpty(newCount)) {
                 ActionErrors errors = updateSubscriptions(
-                             org, dynaForm, request, e,
-                             newCount); 
+                    org, request, e,
+                    newCount);
 
-                if (errors != null && errors.size() > 0) {                    
+                if (errors != null && errors.size() > 0) {
                     getStrutsDelegate().saveMessages(request, errors);
                 }
-                else {                    
+                else {
                     createSuccessMessage(request,
-                           "softwareEntitlementSubs.successMessage", 
-                           org.getName());
+                        "softwareEntitlementSubs.successMessage",
+                        org.getName());
                 }
-            }            
+            }
         }
 
         //Render the data from database            
-        DataList <Map> result = OrgManager.allOrgsSingleEntitlement(entitlementLabel);
+        DataList<Map> result =
+            OrgManager.allOrgsSingleEntitlementWithEmptyOrgs(entitlementLabel);
         Org satelliteOrg = OrgFactory.getSatelliteOrg();
-        for (Iterator <Map> itr = result.iterator(); itr.hasNext();) {
+        for (Iterator<Map> itr = result.iterator(); itr.hasNext();) {
             Map row = itr.next();
             if (satelliteOrg.getId().equals(row.get("orgid"))) {
                 itr.remove();
@@ -97,71 +96,73 @@ public class SystemEntitlementOrgsAction extends RhnAction {
         request.setAttribute("egntname", entitlementLabel);
         request.setAttribute("enthuman", e.getHumanReadableLabel());
         request.setAttribute("pageList", result);
-        request.setAttribute("parentUrl", request.getRequestURI() + 
-                "?label=" + entitlementLabel);
+        request.setAttribute("parentUrl", request.getRequestURI() +
+            "?label=" + entitlementLabel);
 
         //Calculate System Wide Sat usage 
         Long orgCount = OrgManager.getTotalOrgCount(user);
         DataList satEntCounts = OrgManager.getSatEntitlementUsage(entitlementLabel);
         Map row = (Map) satEntCounts.get(0);
-        Long maxEnt = ((Long)row.get("total")).longValue();
-        Long curEnt = ((Long)row.get("curr")).longValue();
-        Long alloc =  ((Long)row.get("alloc")).longValue();
+        Long maxEnt = ((Long) row.get("total")).longValue();
+        Long curEnt = ((Long) row.get("curr")).longValue();
+        Long alloc = ((Long) row.get("alloc")).longValue();
         Long ratio = alloc * 100 / orgCount;
-        
+
         request.setAttribute("maxEnt", maxEnt);
         request.setAttribute("curEnt", curEnt);
         request.setAttribute("alloc", alloc);
         request.setAttribute("orgsnum", orgCount);
         request.setAttribute("ratio", ratio);
-        
-        
+
+
         return mapping.findForward("default");
     }
 
-    private ActionErrors updateSubscriptions(Org org, DynaActionForm dynaForm, 
-        HttpServletRequest request, Entitlement ent, String newCount) {        
+    private ActionErrors updateSubscriptions(Org org,
+                                             HttpServletRequest request,
+                                             Entitlement ent,
+                                             String newCount) {
 
-        if (org.getId().equals(OrgFactory.getSatelliteOrg().getId())) {            
+        if (org.getId().equals(OrgFactory.getSatelliteOrg().getId())) {
             createErrorMessage(request, "org.entitlements.system.defaultorg", null);
             return null;
         }
 
-        ActionErrors errors =  new ActionErrors();
-         
+        ActionErrors errors = new ActionErrors();
+
         //Validate if its a numeric value
         if (!StringUtils.isNumeric(newCount)) {
             ValidatorError error = new ValidatorError(
-                   "softwareEntitlementSubs.invalidInput");
+                "softwareEntitlementSubs.invalidInput");
             errors.add(
-                    RhnValidationHelper.validatorErrorToActionErrors(
+                RhnValidationHelper.validatorErrorToActionErrors(
                     error));
             return errors;
         }
-        
-        Long count = null;
+
+        Long count;
         try {
             count = Long.parseLong(newCount);
             if (count < 0) {
                 throw new NumberFormatException();
             }
-        } 
-        catch (NumberFormatException numEx) {            
+        }
+        catch (NumberFormatException numEx) {
             ValidatorError error = new ValidatorError(
-            "softwareEntitlementSubs.invalidInput");
+                "softwareEntitlementSubs.invalidInput");
             errors.add(RhnValidationHelper.validatorErrorToActionErrors(error));
-            return errors;            
-        }        
+            return errors;
+        }
 
         // Store/update db 
         UpdateOrgSystemEntitlementsCommand updateCmd = new
-                          UpdateOrgSystemEntitlementsCommand(ent, org, count);
+            UpdateOrgSystemEntitlementsCommand(ent, org, count);
         ValidatorError ve = updateCmd.store();
         if (ve != null) {
             errors.add(RhnValidationHelper.
-                       validatorErrorToActionErrors(ve));
-        }    
-        
+                validatorErrorToActionErrors(ve));
+        }
+
         return errors;
-     }
+    }
 }
