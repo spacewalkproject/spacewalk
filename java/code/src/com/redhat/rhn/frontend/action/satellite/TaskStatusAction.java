21b9d58cd488a7affb9a3d469891beb00a025042
Justin Sherrill <jsherril@redhat.com>
2009-05-14 18:19:19 -0400

    500499 - fixed issue where task engine times were not displayed, the old perl code had been ripped out, so i converted it to java

---
+++ java/code/src/com/redhat/rhn/frontend/action/satellite/TaskStatusAction.java
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.action.satellite;
+
+import com.redhat.rhn.frontend.struts.RhnAction;
+import com.redhat.rhn.manager.task.TaskManager;
+import com.redhat.rhn.taskomatic.task.CleanCurrentAlerts;
+import com.redhat.rhn.taskomatic.task.DailySummary;
+import com.redhat.rhn.taskomatic.task.ErrataQueue;
+import com.redhat.rhn.taskomatic.task.SessionCleanup;
+import com.redhat.rhn.taskomatic.task.SummaryPopulation;
+import com.redhat.rhn.taskomatic.task.SynchProbeState;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Task Status Action
+ *
+ * @version $Rev: 1 $
+ */
+public class TaskStatusAction extends RhnAction {
+
+    private static final String KEY = "key";
+    private static final String DATE = "date";
+    private static final String LABEL = "label";
+
+    private static final String CURRENT_DB_TIME = "current_db";
+
+    private static final String LAST_COMPLETED = "last_task_completed";
+
+
+    /** {@inheritDoc} */
+    public ActionForward execute(ActionMapping mapping,
+                                 ActionForm formIn,
+                                 HttpServletRequest request,
+                                 HttpServletResponse response) {
+
+
+        List<Map> toDisplay = getDisplayList();
+
+
+        for (Map map : toDisplay) {
+            Date date = TaskManager.getTaskExecutionTime((String) map.get(LABEL));
+            map.put(DATE, date);
+        }
+
+
+        request.setAttribute(CURRENT_DB_TIME, TaskManager.getCurrentDBTime());
+        request.setAttribute("list", toDisplay);
+        return mapping.findForward("default");
+    }
+
+
+    private List<Map> getDisplayList() {
+        List<Map> list = new ArrayList<Map>();
+
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.last.completed");
+                put(LABEL, LAST_COMPLETED);
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.session.cleanup");
+                put(LABEL, SessionCleanup.DISPLAY_NAME);
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.errata.queue.notif");
+                put(LABEL, ErrataQueue.DISPLAY_NAME);
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.errata.mail");
+                put(LABEL, "errata_engine");
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.daily.summary");
+                put(LABEL, SummaryPopulation.DISPLAY_NAME);
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.daily.summary.mail");
+                put(LABEL, DailySummary.DISPLAY_NAME);
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.clean.alerts");
+                put(LABEL, CleanCurrentAlerts.DISPLAY_NAME);
+            }
+        });
+
+        list.add(new HashMap(){
+            {
+                put(KEY, "task.status.sync.probe");
+                put(LABEL, SynchProbeState.DISPLAY_NAME);
+            }
+        });
+
+        return list;
+    }
+
+}
+
---
+++ java/code/src/com/redhat/rhn/manager/task/TaskManager.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.manager.task;
+
+import com.redhat.rhn.common.db.datasource.CallableMode;
+import com.redhat.rhn.common.db.datasource.DataResult;
+import com.redhat.rhn.common.db.datasource.ModeFactory;
+import com.redhat.rhn.common.db.datasource.SelectMode;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class TaskManager {
+
+    public static Date getTaskExecutionTime(String label) {
+
+        SelectMode m = ModeFactory.getMode("Task_queries", "get_task_stats");
+        Map in = new HashMap();
+        in.put("label", label);
+        DataResult<Map> list = m.execute(in);
+
+        if (!list.isEmpty()) {
+            return (Date) list.get(0).get("last_poll");
+        }
+        return null;
+    }
+
+
+
+    public static Date getCurrentDBTime() {
+        SelectMode m = ModeFactory.getMode("Task_queries", "get_current_time");
+        DataResult<Map> list = m.execute();
+        if (!list.isEmpty()) {
+            return (Date) list.get(0).get("sysdate");
+        }
+        return null;
+    }
+
+}
