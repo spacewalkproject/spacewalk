25cae0c660aaf2ffd1b0c967469f94d7035c9788
jesus m. rodriguez <jesusr@redhat.com>
2009-06-01 16:13:13 -0400

    store pid file locally.

--- search-server/run.sh
+++ search-server/run.sh
@@ -14,4 +14,4 @@ fi
 echo "starting server"
 export CLASSPATH=`pwd`/build
 #java -Djava.library.path=/usr/lib -classpath `build-classpath-directory lib dist` com.redhat.satellite.search.Main
-/usr/sbin/tanukiwrapper `pwd`/rhn_search_daemon_dev.conf wrapper.pidfile=rhn-search.pid wrapper.daemonize=FALSE
+/usr/sbin/tanukiwrapper `pwd`/rhn_search_daemon_dev.conf wrapper.pidfile=`pwd`/rhn-search.pid wrapper.daemonize=FALSE
