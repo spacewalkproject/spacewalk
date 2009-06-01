c5b7ce6dfc5fe0a29b67dc029040fd6a0b2b9bef
jesus m. rodriguez <jesusr@redhat.com>
2009-06-01 16:20:52 -0400

    496933 - update the errata index when publishing an errata.

--- java/code/src/com/redhat/rhn/manager/errata/ErrataManager.java
+++ java/code/src/com/redhat/rhn/manager/errata/ErrataManager.java
@@ -63,6 +63,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import redstone.xmlrpc.XmlRpcClient;
+import redstone.xmlrpc.XmlRpcFault;
+
 /**
  * ErrataManager is the singleton class used to provide business operations
  * on Errata, where those operations interact with other top tier business
@@ -125,7 +128,7 @@ public class ErrataManager extends BaseManager {
     /**
      * Takes an unpublished errata and returns a published errata into the 
      * channels we pass in. NOTE:  This method does NOT update the errata cache for
-     * the channels.  THat is done when packages are pushed as part of the errata 
+     * the channels.  That is done when packages are pushed as part of the errata
      * publication process (which is not done here)
      * 
      * @param unpublished The errata to publish
@@ -140,6 +143,9 @@ public class ErrataManager extends BaseManager {
         
         retval = addChannelsToErrata(retval, channelIds, user);
         log.debug("publish - updateErrataCacheForChannelsAsync called");
+
+        // update the search server
+        updateSearchIndex();
         return retval;
     }
     
@@ -153,6 +159,9 @@ public class ErrataManager extends BaseManager {
         //pass on to the factory
         Errata retval = ErrataFactory.publish(unpublished);
         log.debug("publish - errata published");
+
+        // update the search server
+        updateSearchIndex();
         return retval;
     }
     
@@ -1197,5 +1206,35 @@ public class ErrataManager extends BaseManager {
        return m.execute(params);       
    }
 
-    
+   /**
+    * update the errata search index.
+    * @return true if index was updated, false otherwise.
+    */
+   private static boolean updateSearchIndex() {
+       boolean flag = false;
+
+       try {
+           XmlRpcClient client = new XmlRpcClient(Config.get().getSearchServerUrl(), true);
+           List args = new ArrayList();
+           args.add("errata");
+           Boolean rc = (Boolean)client.invoke("admin.updateIndex", args);
+           flag =  rc.booleanValue();
+       }
+       catch (XmlRpcFault e) {
+           // right now updateIndex doesn't throw any faults.
+           log.error("Errata index not updated. Search server unavailable." +
+                   "ErrorCode = " + e.getErrorCode(), e);
+           e.printStackTrace();
+       }
+       catch (Exception e) {
+           // if the search server is down, folks will know when they
+           // attempt to search. If this call failed the errata in
+           // question won't be searchable immediately, but will get picked
+           // up the next time the search server runs the job (after being
+           // restarted.
+           log.error("Errata index not updated. Search server unavailable.", e);
+       }
+
+       return flag;
+   }
 }
