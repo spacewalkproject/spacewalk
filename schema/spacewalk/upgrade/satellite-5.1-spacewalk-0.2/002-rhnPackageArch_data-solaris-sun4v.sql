250e5a1536e0e62df0732145d216b56cbc7d21e9
Milan Zazrivec <mzazrivec@redhat.com>
2009-06-17 11:59:14 +0200

    506272 - insert row only if it does not exist

--- schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/002-rhnPackageArch_data-solaris-sun4v.sql
+++ schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/002-rhnPackageArch_data-solaris-sun4v.sql
@@ -1,2 +1,13 @@
-insert into rhnPackageArch (id, label, name, arch_type_id) values
-(rhn_package_arch_id_seq.nextval, 'sparc.sun4v-solaris', 'Sparc Solaris sun4v', lookup_arch_type('sysv-solaris'));
+insert into rhnPackageArch (id, label, name, arch_type_id) (
+    select rhn_package_arch_id_seq.nextval,
+           'sparc.sun4v-solaris',
+           'Sparc Solaris sun4v',
+           lookup_arch_type('sysv-solaris')
+    from dual
+    where not exists (
+        select id, label, name, arch_type_id
+        from rhnPackageArch
+        where label = 'sparc.sun4v-solaris' and
+              name = 'Sparc Solaris sun4v'and
+              arch_type_id = lookup_arch_type('sysv-solaris'))
+);
