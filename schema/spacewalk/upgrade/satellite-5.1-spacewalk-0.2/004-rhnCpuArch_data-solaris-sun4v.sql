e80744f3c0d0e69ef1555becc5ce22c5f0b8e71e
Milan Zazrivec <mzazrivec@redhat.com>
2009-06-17 12:09:15 +0200

    506272 - insert row only if it does not exist

--- schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/004-rhnCpuArch_data-solaris-sun4v.sql
+++ schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/004-rhnCpuArch_data-solaris-sun4v.sql
@@ -1,2 +1,11 @@
-insert into rhnCpuArch (id, label, name) values
-(rhn_cpu_arch_id_seq.nextval, 'sun4v', 'sun4v');
+insert into rhnCpuArch (id, label, name) (
+    select rhn_cpu_arch_id_seq.nextval,
+           'sun4v',
+           'sun4v'
+    from dual
+    where not exists (
+        select label, name
+        from rhnCpuArch
+        where label = 'sun4v' and
+              name = 'sun4v')
+);
