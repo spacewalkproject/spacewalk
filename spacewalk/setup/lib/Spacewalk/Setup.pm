3dbad2fbda7e5c3358f9d41105c13147f91816a4
Milan Zazrivec <mzazrivec@redhat.com>
2009-06-24 14:15:35 +0200

    remove extraneous backslash characters

--- spacewalk/setup/lib/Spacewalk/Setup.pm
+++ spacewalk/setup/lib/Spacewalk/Setup.pm
@@ -728,9 +728,9 @@ EOQ
                    -err_message => "Could not upgrade database.\n",
                    -err_code => 15,
                    -system_opts => ['/sbin/runuser', 'oracle', '-c',
-                                    SHARED_DIR . '/oracle/' . $upgrade_script . \
-                                    " --db " . $answers->{'db-sid'} . \
-                                    " --user " . $answers->{'db-user'}]);
+                                    SHARED_DIR . '/oracle/' . $upgrade_script .
+                                    " --db $answers->{'db-sid'}" .
+                                    " --user $answers->{'db-user'}"]);
 
         system_or_exit(['service', 'oracle', 'restart'], 41,
                        'Could not restart oracle service');
