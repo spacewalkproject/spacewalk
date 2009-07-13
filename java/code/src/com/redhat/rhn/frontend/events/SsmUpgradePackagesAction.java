43aa54f9a6e2d58fc8146dac61cbc9f33d33d728
Jason Dobies <jason.dobies@redhat.com>
2009-07-13 09:25:35 -0400

    510329 - Chunked processing of SSM package upgrades. Performance isn't much better just yet, but checking it in for sharing purposes.

--- java/code/src/com/redhat/rhn/frontend/events/SsmUpgradePackagesAction.java
+++ java/code/src/com/redhat/rhn/frontend/events/SsmUpgradePackagesAction.java
@@ -14,34 +14,24 @@
  */
 package com.redhat.rhn.frontend.events;
 
-import com.redhat.rhn.common.db.datasource.DataResult;
-
-import com.redhat.rhn.common.localization.LocalizationService;
-
-import com.redhat.rhn.common.messaging.EventMessage;
-
-import com.redhat.rhn.domain.server.Server;
-import com.redhat.rhn.domain.server.ServerFactory;
-
-import com.redhat.rhn.domain.user.User;
-import com.redhat.rhn.domain.user.UserFactory;
-
-import com.redhat.rhn.frontend.dto.PackageListItem;
-
-import com.redhat.rhn.manager.action.ActionManager;
-
-import com.redhat.rhn.manager.ssm.SsmOperationManager;
-
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-
 import org.apache.log4j.Logger;
+import com.redhat.rhn.common.db.datasource.DataResult;
+import com.redhat.rhn.common.hibernate.HibernateFactory;
+import com.redhat.rhn.common.localization.LocalizationService;
+import com.redhat.rhn.common.messaging.EventMessage;
+import com.redhat.rhn.domain.server.Server;
+import com.redhat.rhn.domain.server.ServerFactory;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.domain.user.UserFactory;
+import com.redhat.rhn.frontend.dto.PackageListItem;
+import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.ssm.SsmOperationManager;
 
 /**
  * Handles removing packages from servers in the SSM.
@@ -49,61 +39,165 @@ import org.apache.log4j.Logger;
  * @see com.redhat.rhn.frontend.events.SsmUpgradePackagesEvent
  */
 public class SsmUpgradePackagesAction extends AbstractDatabaseAction {
+
+    /** Number of servers to process at once. */
+    private static final int SERVER_CHUNK_SIZE = 1000;
+
     private static Logger log = Logger.getLogger(SsmUpgradePackagesAction.class);
 
     /** {@inheritDoc} */
     protected void doExecute(EventMessage msg) {
         log.debug("Executing package upgrades.");
+
         SsmUpgradePackagesEvent event = (SsmUpgradePackagesEvent) msg;
 
-        DataResult result = event.getResult();
         User user = UserFactory.lookupById(event.getUserId());
-        Date earliest = event.getEarliest();
 
         LocalizationService ls = LocalizationService.getInstance();
         long operationId = SsmOperationManager.createOperation(user,
             ls.getMessage("ssm.package.upgrade.operationname"), null);
-        int numPackages = 0;
 
-        // The package collection is a set to prevent duplciates when keeping a running
-        // total of all packages selected
-        Set<PackageListItem> allPackages = new HashSet<PackageListItem>();
+        // Explicitly call handle transactions here so the operation creation above
+        // is persisted before the potentially long running logic below
+        handleTransactions();
 
-        // Looking up all the server objects in Hibernate was *brutally* slow here:
-        List<Long> allServerIds = new LinkedList<Long>();
-
-        // Iterate the data, which is essentially each unique package/server combination
-        // to upgrade. Note that this is only for servers that we have marked as having the
-        // package installed.
-        log.debug("Iterating data.");
-        for (Iterator it = result.iterator(); it.hasNext();) {
-
-            // Add action for each package found in the elaborator
-            Map data = (Map) it.next();
-
-            // Load the server
-            Long sid = (Long)data.get("id");
-            Server server = ServerFactory.lookupByIdAndOrg(sid, user.getOrg());
-
-            // Get the packages out of the elaborator
-            List elabList = (List) data.get("elaborator0");
-
-            List<PackageListItem> items = new ArrayList<PackageListItem>(elabList.size());
-            for (Iterator elabIt = elabList.iterator(); elabIt.hasNext();) {
-                Map elabData = (Map) elabIt.next();
-                String idCombo = (String) elabData.get("id_combo");
-                PackageListItem item = PackageListItem.parse(idCombo);
-                items.add(item);
-            }
-
-            // Convert to list of maps
-            List<Map<String, Long>> packageListData = PackageListItem.toKeyMaps(items);
-
-            // Create the action
-            ActionManager.schedulePackageUpgrade(user, server, packageListData, earliest);
+        try {
+            scheduleUpgrades(user, event);
         }
+        catch (Exception e) {
+            log.error("Error while scheduling package upgrades for event: " + event, e);
+        }
+        finally {
+            SsmOperationManager.completeOperation(user, operationId);
+        }
+
         log.debug("Done.");
     }
 
+    private void scheduleUpgrades(User user, SsmUpgradePackagesEvent event) {
+
+        /* We already have the data result which contains the server IDs that will
+           have package upgrades installed as well as the list of each package to
+           apply (in the elaborator). Until we can correct the ActionManager code to
+           not require the full sever load, we do the following:
+
+           - Load the servers in batches of 1K (batched to avoid database limitation)
+           - Create the package lists (munging the IDs) for each of the 1K servers
+           - Schedule the actions for these 1K systems
+           - Repeat as much as necessary
+
+           The big savings here is the reduction of calls to load the servers by a factor
+           of 1000. We need to be careful to finish work on the 1K system batches and
+           releasing any references to the objects involved *before* moving on to the
+           next batch for memory reasons. If we were to hold on to all system/package
+           references until the end we'd end up smashing our memory footprint.
+
+           It should be noted that this solution should not be considered the final (best)
+           answer. This is being implemented under the upcoming Satellite 5.3 deadline.
+           Ideally the ActionManager APIs should be revisited to better support working
+           with groups of servers. So when revisiting this in the future, realize the
+           outside influences before proclaiming that this code is insane. :)
+
+           jdobies, Jul 10, 2009
+         */
+
+        long actionStart = System.currentTimeMillis();
+
+        DataResult result = event.getResult();
+        Date earliest = event.getEarliest();
+
+        List<Long> chunkServerIds = new ArrayList<Long>(1000);
+        List<Server> chunkServers = new ArrayList<Server>(1000);
+        Map<Long, List<Map<String, Long>>> chunkServerToPackages =
+            new HashMap<Long, List<Map<String, Long>>>(1000);
+
+        List<DataResult> allServerChunks = split(result);
+        for (DataResult chunk : allServerChunks) {
+
+            long chunkStart = System.currentTimeMillis();
+
+            // Loop over each server in the chunk
+            for (Iterator it = chunk.iterator(); it.hasNext();) {
+
+                Map data = (Map) it.next();
+
+                // Keep a running list of what servers we've processed in this chunk
+                Long sid = (Long) data.get("id");
+                chunkServerIds.add(sid);
+
+                // Get the packages out of the elaborator for this server
+                List elabList = (List) data.get("elaborator0");
+
+                // Handle each package for the server
+                List<PackageListItem> packages =
+                    new ArrayList<PackageListItem>(elabList.size());
+
+                for (Iterator elabIt = elabList.iterator(); elabIt.hasNext();) {
+                    Map elabData = (Map) elabIt.next();
+                    String idCombo = (String) elabData.get("id_combo");
+                    PackageListItem item = PackageListItem.parse(idCombo);
+                    packages.add(item);
+                }
+
+                // Convert to data type needed for API and associate with this server
+                List<Map<String, Long>> packageListData =
+                    PackageListItem.toKeyMaps(packages);
+                chunkServerToPackages.put(sid, packageListData);
+            }
+
+            // Batch load all servers found in this chunk
+            chunkServers.addAll(ServerFactory.lookupByIdsAndUser(chunkServerIds, user));
+
+            // For each server we just loaded, look up its packages and make the action
+            for (Server server : chunkServers) {
+                List<Map<String, Long>> packageListData =
+                    chunkServerToPackages.get(server.getId());
+
+                ActionManager.schedulePackageUpgrade(user, server,
+                    packageListData, earliest);
+            }
+
+            // Clean up before the next chunk
+            chunkServerIds.clear();
+            chunkServers.clear();
+            chunkServerToPackages.clear();
+
+            // Close out the transactions
+            HibernateFactory.commitTransaction();
+            HibernateFactory.getSession().flush();
+
+            if (log.isDebugEnabled()) {
+                log.debug("Time to schedule chunk: " +
+                    (System.currentTimeMillis() - chunkStart));
+            }
+        } // end chunk loop
+
+        if (log.isDebugEnabled()) {
+            log.debug("Time to schedule all actions: " +
+                (System.currentTimeMillis() - actionStart));
+        }
+    }
+
+    private List<DataResult> split(DataResult result) {
+        List<DataResult> listOfSubLists = new ArrayList<DataResult>();
+
+        int chunkNumber = 1;
+        while (chunkNumber * SERVER_CHUNK_SIZE < result.size()) {
+            int start = (chunkNumber - 1) * SERVER_CHUNK_SIZE;
+            int end = chunkNumber * SERVER_CHUNK_SIZE; // subList is exclusive on end
+
+            DataResult subList = result.subList(start, end);
+            listOfSubLists.add(subList);
+
+            chunkNumber++;
+        }
+
+        // Pick up extras
+        int start = (chunkNumber - 1) * SERVER_CHUNK_SIZE;
+        DataResult lastSubList = result.subList(start, result.size());
+        listOfSubLists.add(lastSubList);
+
+        return listOfSubLists;
+    }
 }
 
