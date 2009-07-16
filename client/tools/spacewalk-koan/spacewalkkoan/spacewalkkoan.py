62c27aca1285a8f0a89c896da1566f4bba3ddec5
Partha Aji <paji@redhat.com>
2009-07-16 15:27:06 -0400

    510299 - static ks fix.
    Basically spacewalk koan did
    ifconfig $DEVICE | perl -lne '/Mask:([\d.]+)/ and print $1'"
    instead of
     "ifconfig %s | perl -lne '/Mask:([\d.]+)/ and print $1'" % device
    to determine the interface of the network we are interested ..
    This may give us wrong information on the ip address of
    the network interface we are interested on.

--- client/tools/spacewalk-koan/spacewalkkoan/spacewalkkoan.py
+++ client/tools/spacewalk-koan/spacewalkkoan/spacewalkkoan.py
@@ -47,10 +47,10 @@ def find_host_name():
     return execute("hostname")[0]
 
 def find_netmask(device):
-    return execute("ifconfig $DEVICE | perl -lne '/Mask:([\d.]+)/ and print $1'")[0]
+    return execute("ifconfig %s | perl -lne '/Mask:([\d.]+)/ and print $1'" % device)[0]
 
 def find_ip(device):
-    return execute("ifconfig $DEVICE | perl -lne '/inet addr:([\d.]+)/ and print $1'")[0]
+    return execute("ifconfig %s | perl -lne '/inet addr:([\d.]+)/ and print $1'" % device)[0]
 
 def find_name_servers():
     servers = execute("cat /etc/resolv.conf | perl -lne '/^nameserver\s+(\S+)$/ and print $1'")
