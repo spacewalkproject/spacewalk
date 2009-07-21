e4145e6d4fe548e7471dbfd78d189761312f79ab
Milan Zazrivec <mzazrivec@redhat.com>
2009-07-21 15:30:41 +0200

    512929 - skip rhn_rcm_pva_def_uniq constraint creation
    We don't need to create this constraint, since we'll drop it
    later in the schema upgrade process anyway. Moreover, depending
    on the current status of sat-synced content, creation of this
    constraint may raise
    ORA-02299: cannot validate RHN_RCM_PVA_DEF_UNIQ - duplicate keys found
    error.

--- schema/spacewalk/upgrade/spacewalk-0.4-spacewalk-0.5/189-rhnReleaseChannelMap-unique.sql
+++ schema/spacewalk/upgrade/spacewalk-0.4-spacewalk-0.5/189-rhnReleaseChannelMap-unique.sql
@@ -1,3 +1,8 @@
-ALTER TABLE rhnReleaseChannelMap
-ADD CONSTRAINT rhn_rcm_pva_def_uniq
-UNIQUE (product, version, channel_arch_id, is_default);
+-- This change is not needed in VADER, since we drop the constraint later
+-- in the schema upgrade course. Additionaly, depending on the status of
+-- currently synced content, this constraint may cause ORA-02299: cannot
+-- validate RHN_RCM_PVA_DEF_UNIQ - duplicate keys found
+
+-- ALTER TABLE rhnReleaseChannelMap
+-- ADD CONSTRAINT rhn_rcm_pva_def_uniq
+-- UNIQUE (product, version, channel_arch_id, is_default);
--- schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/121-rhnReleaseChannelMap.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/121-rhnReleaseChannelMap.sql
@@ -1,4 +1,8 @@
-ALTER TABLE rhnReleaseChannelMap DROP CONSTRAINT rhn_rcm_pva_def_uniq;
+-- Unneccessary to drop the rhn_rcm_pva_def_uniq constraint, since we've
+-- decided not to create it earlier in the schema upgrade course.
+
+-- ALTER TABLE rhnReleaseChannelMap DROP CONSTRAINT rhn_rcm_pva_def_uniq;
+
 ALTER TABLE rhnReleaseChannelMap DROP COLUMN is_default;
 ALTER TABLE rhnReleaseChannelMap 
 ADD CONSTRAINT rhn_rcm_pvar_uniq
