0b64232669032fbe2c2682661c2a60f42dab341c
Jan Pazdziora <jpazdziora@redhat.com>
2009-07-23 14:45:55 +0200

    reporting: when report name is not specified on the command line, show list of available reports.

--- backend/satellite_tools/reports.py
+++ backend/satellite_tools/reports.py
@@ -3,6 +3,9 @@ import os
 
 REPORT_DEFINITIONS = "/usr/share/spacewalk/reports/data"
 
+def available_reports():
+	return os.listdir(REPORT_DEFINITIONS)
+
 class report:
 	def __init__(self, name):
 		full_path = os.path.join(REPORT_DEFINITIONS, name)
