792cb55228151f56d1846bffffb22ddaa83270ad
Jan Pazdziora <jpazdziora@redhat.com>
2009-07-23 15:19:47 +0200

    reporting: change structure of the report definition file to also include column names.
    This is also used to check that the report definition matches
    with column names returned by the query.

--- backend/satellite_tools/reports.py
+++ backend/satellite_tools/reports.py
@@ -1,5 +1,6 @@
 
 import os
+import re
 
 REPORT_DEFINITIONS = "/usr/share/spacewalk/reports/data"
 
@@ -9,6 +10,44 @@ def available_reports():
 class report:
 	def __init__(self, name):
 		full_path = os.path.join(REPORT_DEFINITIONS, name)
+		self.sql = None
+		self.columns = None
+		self._load(full_path)
+
+	def _load(self, full_path):
 		fd = open(full_path, 'r')
-		self.sql = fd.read()
+		tag = None
+		value = ''
+		re_comment = re.compile('^\s*#')
+		re_tag_name = re.compile('^(\S+):\s*$')
+
+		for line in fd:
+			result = re_comment.match(line)
+			if result != None:
+				continue
+
+			result = re_tag_name.match(line)
+			if result != None:
+				if tag != None:
+					self._set(tag, value)
+					tag = None
+					value = ''
+				tag = result.group(1)
+			else:
+				value += line
+
+		if tag != None:
+			self._set(tag, value)
+
+	def _set(self, tag, value):
+		if tag == 'columns':
+			self.columns = filter(lambda x: x != '', re.split('\s+', value))
+		elif tag == 'sql':
+			self.sql = value
+		else:
+			raise spacewalk_report_unknown_tag_exception(tag)
+
+
+class spacewalk_report_unknown_tag_exception(Exception):
+	pass
 
