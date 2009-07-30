7a846f41e44fee82f9f37ca39a5ac9f3a23d85c0
Devan Goodwin <dgoodwin@redhat.com>
2009-07-30 09:44:31 -0300

    Re-order Oracle upgrade script.

--- schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/998-postgresql-compatability-procs.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/998-postgresql-compatability-procs.sql
@@ -2,34 +2,6 @@
 --- Add stored procedures for PostgreSQL compatability.
 ---
 
-CREATE OR REPLACE VIEW
-rhnServerOutdatedPackages
-(
-    server_id,
-    package_name_id,
-    package_evr_id,    
-    package_nvre,
-    errata_id,
-    errata_advisory
-)
-AS
-SELECT DISTINCT SNPC.server_id,
-       P.name_id, 
-       P.evr_id, 
-       PN.name || '-' || evr_t_as_vre_simple( PE.evr ),
-       E.id,
-       E.advisory
-  FROM rhnPackageName PN,
-       rhnPackageEVR PE,
-       rhnPackage P,
-       rhnServerNeededPackageCache SNPC
-         left outer join
-        rhnErrata E
-          on SNPC.errata_id = E.id
- WHERE SNPC.package_id = P.id
-   AND P.name_id = PN.id
-   AND P.evr_id = PE.id;
-
 create or replace function evr_t_as_vre( a evr_t )
   return varchar2
 is
@@ -60,6 +32,34 @@ begin
        return ret;
 end;
 
+
+CREATE OR REPLACE VIEW
+rhnServerOutdatedPackages
+(
+    server_id,
+    package_name_id,
+    package_evr_id,    
+    package_nvre,
+    errata_id,
+    errata_advisory
+)
+AS
+SELECT DISTINCT SNPC.server_id,
+       P.name_id, 
+       P.evr_id, 
+       PN.name || '-' || evr_t_as_vre_simple( PE.evr ),
+       E.id,
+       E.advisory
+  FROM rhnPackageName PN,
+       rhnPackageEVR PE,
+       rhnPackage P,
+       rhnServerNeededPackageCache SNPC
+         left outer join
+        rhnErrata E
+          on SNPC.errata_id = E.id
+ WHERE SNPC.package_id = P.id
+   AND P.name_id = PN.id
+   AND P.evr_id = PE.id;
 
 
 
