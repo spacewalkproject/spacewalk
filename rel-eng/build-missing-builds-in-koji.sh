d6276ea4ab79bd2a92a10876ce3f2c91b4083f66
Miroslav Suchý <msuchy@redhat.com>
2009-08-04 10:30:14 +0200

    run builds only once - tito build it for all arch, no need to run it for every tag

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAGS=([0]="dist-5E-sw-0.6-candidate" [1]="dist-f10-sw-0.6-candidate")
+TAGS=([0]="dist-5E-sw-0.6-candidate" [1]="dist-f10-sw-0.6-candidate" [2]="dist-f11-sw-0.6-candidate")
 
 pushd `pwd`
 
@@ -9,22 +9,22 @@ cd `dirname $0`/..
 # say python to be nice to pipe
 export PYTHONUNBUFFERED=1
 
-for tag in 0 1; do
+for tag in 0 1 2; do
   rel-eng/koji-missing-builds.py --no-extra ${TAGS[$tag]} | \
     awk '!(/buildsys-macros/ || /oracle-server-admin/ || /oracle-server-scripts/) {
                  if (x==1) { print gensub(" *([a-zA-Z_-]+)-.*", "\\1", "g")}
                  }
              /Builds missing in koji/ { x=1 }' | \
 	  xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring | \
-        grep -v solaris | \
-        while read package ; do
-          (
-            echo Building package in path $package 
-            cd $package && ${TITO_PATH}tito build --koji-release | \
-            awk '/Wrote:.*tar.gz/ {print $2}' | \
-            xargs -I packagepath scp packagepath fedorahosted.org:spacewalk
-          )
-        done
+        grep -v solaris
+done | sort | uniq | \
+while read package ; do
+    (
+      echo Building package in path $package 
+      cd $package && ${TITO_PATH}tito build --koji-release | \
+      awk '/Wrote:.*tar.gz/ {print $2}' | \
+      xargs -I packagepath scp packagepath fedorahosted.org:spacewalk
+    )
 done
 
 popd
