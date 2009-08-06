2124f62920321c39f82519c8e7be55cbb370ec47
Justin Sherrill <jsherril@redhat.com>
2009-08-06 14:10:27 -0400

    adding upgrade for new fedora gpg key

---
+++ schema/spacewalk/upgrade/spacewalk-schema-0.5-to-spacewalk-schema-0.6/193-rhn-PackageKey.sql
@@ -0,0 +1,2 @@
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values 
+   (rhn_pkey_id_seq.nextval, '1dc5c758d22e77f2', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
