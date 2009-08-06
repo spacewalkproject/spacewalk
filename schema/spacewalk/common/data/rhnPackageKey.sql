fb74d44a1400c64e1b97c0d42b9facc1453bf974
Justin Sherrill <jsherril@redhat.com>
2009-08-06 14:07:27 -0400

    adding newest fedora gpg key, and moving _data sql file to the right place

--- schema/spacewalk/common/data/rhnPackageKey.sql
+++ schema/spacewalk/common/data/rhnPackageKey.sql
@@ -47,6 +47,8 @@ insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 (rhn_pkey_id_seq.nextval, 'a84edae89c800aca', lookup_package_key_type('gpg'), lookup_package_provider('Suse'));
 
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
+(rhn_pkey_id_seq.nextval, '1dc5c758d22e77f2', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
 
 
 
