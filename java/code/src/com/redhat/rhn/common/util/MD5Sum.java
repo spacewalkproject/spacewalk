1f617dc25a0bd3551fc112e366d9a24e6ee05345
Mike McCune <mmccune@redhat.com>
2009-08-12 11:40:02 -0700

    514305 - switching the algorithm for reading the file into memory.
    New algorithm ditches the ByteArrayOutputStream which has its own
    internal byte array buffer which means we are maintaining 2x
    copies of the file in memory which doubles the heap required.

--- java/code/src/com/redhat/rhn/common/util/MD5Sum.java
+++ java/code/src/com/redhat/rhn/common/util/MD5Sum.java
@@ -142,28 +142,11 @@ public class MD5Sum {
      */
     public static String getFileMD5Sum(File f) throws IOException {
         String sum = null;
-        FileInputStream in = new FileInputStream(f.getAbsolutePath());
 
-        byte[] b = new byte[1024];
-        int num = 0;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-
-        while ((num = in.read(b)) != -1) {
-            out.write(b, 0, num);
-
-            if (out.size() > SCOUR_MD5_BYTE_LIMIT) {
-                sum = md5Sum(out.toByteArray(), SCOUR_MD5_BYTE_LIMIT);
-                break;
-            }
-        }
-
-        if (sum == null) {
-            sum = md5Sum(out.toByteArray(), SCOUR_MD5_BYTE_LIMIT);
-        }
-
-        in.close();
-        out.close();
+        byte[] barray = FileUtils.readByteArrayFromFile(f, 0, f.length());
+        sum = md5Sum(barray, SCOUR_MD5_BYTE_LIMIT);
 
         return sum;
     }
+
 }
--- java/code/src/com/redhat/rhn/common/util/test/MD5SumTest.java
+++ java/code/src/com/redhat/rhn/common/util/test/MD5SumTest.java
@@ -15,21 +15,57 @@
 package com.redhat.rhn.common.util.test;
 
 import com.redhat.rhn.common.util.MD5Sum;
-import com.redhat.rhn.testing.RhnBaseTestCase;
 import com.redhat.rhn.testing.TestUtils;
 
 import java.io.File;
+import java.util.Random;
+
+import junit.framework.TestCase;
 
 
 /**
  * @author mmccune
  *
  */
-public class MD5SumTest extends RhnBaseTestCase {
+public class MD5SumTest extends TestCase {
 
     public void testMD5Sum() throws Exception {
         File testFile = new File(TestUtils.findTestData("test.file").getFile());
         String sum = MD5Sum.getFileMD5Sum(testFile);
         assertEquals("ab0aa62f30d67085cd07ea9004a1437f", sum);
+    }
+    
+    /**
+     * Note that this test creates a large 100MB file in /tmp
+     * and then does an md5sum on the file.
+     * 
+     * With a previous implementation of MD5Sum.getFileMD5Sum() this would
+     * cause an OOME with a max-heap size in junit of 256m.  This is configured in:
+     * spacewalk/buildconf/build-utils.xml:
+     * 
+     *       <junit forkmode="once" fork="yes" printsummary="off" showoutput="yes"
+     *        haltonfailure="${halt-tests-on-failure}" 
+     *        failureproperty="junit_test_failure" maxmemory="256m">
+     *
+     * The new implementation uses less memory and thus passes the test below when ran
+     * from Junit CLI.
+     * 
+     * @throws Exception
+     */
+    public void testOOMEMD5Sum() throws Exception {
+        File large = new File("/tmp/large-file.dat");
+        // Create a large 100mb file
+        large.createNewFile();
+        writeRandomLargeBytesToFile(large);
+        String sum = MD5Sum.getFileMD5Sum(large);
+        assertNotNull(sum);
+        large.delete();
+    }
+    
+    private static void writeRandomLargeBytesToFile(File f) {
+        byte[] ba = new byte[101326592];
+        Random r = new Random();
+        r.nextBytes(ba);
+        TestUtils.writeByteArrayToFile(f, ba);
     }
 }
