93503e2c8a90153e6cc9bd4ac72c36c92086fd05
Milan Zazrivec <mzazrivec@redhat.com>
2009-08-17 19:20:54 +0200

    517867 - insert solaris data only if these do not exist

--- schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/006-rhnServerPackageArchCompat_data-solaris-sun4v.sql
+++ schema/spacewalk/upgrade/satellite-5.1-spacewalk-0.2/006-rhnServerPackageArchCompat_data-solaris-sun4v.sql
@@ -28,6 +28,15 @@ set preference = 210
 where server_arch_id = LOOKUP_SERVER_ARCH('sparc-sun4m-solaris')
 	and package_arch_id = LOOKUP_PACKAGE_ARCH('sparc-solaris');
 
-insert into rhnServerPackageArchCompat
-(server_arch_id, package_arch_id, preference) values
-(LOOKUP_SERVER_ARCH('sparc-sun4m-solaris'), LOOKUP_PACKAGE_ARCH('sparc.sun4v-solaris'), 100);
+insert into rhnServerPackageArchCompat (server_arch_id, package_arch_id, preference)
+    select lookup_server_arch('sparc-sun4m-solaris'),
+           lookup_package_arch('sparc.sun4v-solaris'),
+           100
+    from dual
+    where not exists (
+        select 1
+        from rhnServerPackageArchCompat
+        where server_arch_id = lookup_server_arch('sparc-sun4m-solaris') and
+		      package_arch_id = lookup_package_arch('sparc.sun4v-solaris') and
+		      preference = 100
+	);
