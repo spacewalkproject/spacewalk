d6c844b22916e09f5ed07ffe8f3a05f0888c7a1a
Michael Mraka <michael.mraka@redhat.com>
2009-09-22 13:53:27 +0200

    523389 - can't add NOT NULL column into non-empty table
            so we have to add nullable one and then modify it

--- schema/spacewalk/upgrade/rhn-satellite-schema-5.1-to-spacewalk-schema-0.2/055-1-rhnPackageChangeLog-id.sql
+++ schema/spacewalk/upgrade/rhn-satellite-schema-5.1-to-spacewalk-schema-0.2/055-1-rhnPackageChangeLog-id.sql
@@ -1,6 +1,8 @@
 
 alter table rhnPackageChangelog
-add id              number
+add id              number;
+alter table rhnPackageChangelog
+modify id
         constraint rhn_pkg_cl_id_nn not null novalidate
         constraint rhn_pkg_cl_id_pk primary key
                 using index tablespace [[64k_tbs]]
