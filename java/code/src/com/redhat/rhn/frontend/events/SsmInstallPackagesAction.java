95596ce0a9de626a25f99a4556e017abd7af68ac
Justin Sherrill <jsherril@redhat.com>
2009-09-28 20:23:18 -0400

    525549 - fixing issue where SSM package operations would run out of memory

--- java/code/src/com/redhat/rhn/frontend/events/SsmInstallPackagesAction.java
+++ java/code/src/com/redhat/rhn/frontend/events/SsmInstallPackagesAction.java
@@ -14,28 +14,27 @@
  */
 package com.redhat.rhn.frontend.events;
 
-import com.redhat.rhn.domain.user.UserFactory;
-
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Date;
-import java.util.LinkedList;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import com.redhat.rhn.common.messaging.EventMessage;
 import com.redhat.rhn.common.localization.LocalizationService;
-import com.redhat.rhn.manager.ssm.SsmOperationManager;
+import com.redhat.rhn.common.messaging.EventMessage;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.domain.user.UserFactory;
+import com.redhat.rhn.frontend.action.SetLabels;
+import com.redhat.rhn.frontend.dto.EssentialServerDto;
+import com.redhat.rhn.frontend.dto.PackageListItem;
 import com.redhat.rhn.manager.action.ActionManager;
 import com.redhat.rhn.manager.rhnset.RhnSetDecl;
+import com.redhat.rhn.manager.ssm.SsmOperationManager;
 import com.redhat.rhn.manager.system.SystemManager;
-import com.redhat.rhn.domain.user.User;
-import com.redhat.rhn.domain.server.Server;
-import com.redhat.rhn.domain.server.ServerFactory;
-import com.redhat.rhn.frontend.dto.PackageListItem;
-import com.redhat.rhn.frontend.dto.EssentialServerDto;
-import com.redhat.rhn.frontend.action.SetLabels;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Schedules package installations on systems in the SSM.
@@ -92,17 +91,13 @@ public class SsmInstallPackagesAction extends AbstractDatabaseAction {
             PackageListItem.toKeyMaps(packageListItems);
 
         // Create one action for all servers to which the packages are installed
-        List<Server> actionServers = new ArrayList<Server>(servers.size());
         List<Long> serverIds = new LinkedList<Long>();
         for (EssentialServerDto dto : servers) {
             serverIds.add(dto.getId());
         }
-        log.debug("Looking up server objects.");
-        actionServers.addAll(ServerFactory.lookupByIdsAndUser(serverIds, user));
+
         log.debug("Scheduling actions.");
-
-
-        ActionManager.schedulePackageInstall(user, actionServers,
+        ActionManager.schedulePackageInstall(user, serverIds,
             packageListData, earliest);
         log.debug("Done scheduling package installations.");
 
