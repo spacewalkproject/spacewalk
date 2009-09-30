d7856b103c43690d2d1ee197dc40f2d54f07f682
Justin Sherrill <jsherril@redhat.com>
2009-09-30 11:31:21 -0400

    Revert "adding workaround for hibernate oddity/bug"
    This reverts commit 7e51e6b06314e5a01e1c028f6d1221017ff27297.

--- java/code/src/com/redhat/rhn/domain/server/ServerFactory.java
+++ java/code/src/com/redhat/rhn/domain/server/ServerFactory.java
@@ -40,7 +40,6 @@ import org.hibernate.Session;
 import java.sql.Types;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -771,10 +770,6 @@ public class ServerFactory extends HibernateFactory {
      * @return list of system ids that are solaris systems
      */
     public static List<Long> listSolarisSystems(Collection<Long> systemIds) {
-        //Hibernate can't handle empty lists for in clauses, silly hibernate
-        if (systemIds.isEmpty()) {
-            return Collections.EMPTY_LIST;
-        }
         return ServerFactory.getSession().getNamedQuery("Server.listSolarisSystems").
                 setParameterList("sids", systemIds).list();
     }
@@ -786,10 +781,6 @@ public class ServerFactory extends HibernateFactory {
      * @return list of system ids that are linux systems
      */
     public static List<Long> listLinuxSystems(Collection<Long> systemIds) {
-       //Hibernate can't handle empty lists for in clauses, silly hibernate
-        if (systemIds.isEmpty()) {
-            return Collections.EMPTY_LIST;
-        }
         return ServerFactory.getSession().getNamedQuery("Server.listRedHatSystems").
                                                 setParameterList("sids", systemIds).list();
         
