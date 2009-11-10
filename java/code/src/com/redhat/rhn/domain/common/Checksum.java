7180ef0eeed4fdc09276e608d9b15b71636f631a
Miroslav Suchý <msuchy@redhat.com>
2009-11-10 13:02:12 +0100

    Creating new domain class for table rhnChecksum

---
+++ java/code/src/com/redhat/rhn/domain/common/Checksum.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2009 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.domain.common;
+
+import com.redhat.rhn.domain.BaseDomainHelper;
+import com.redhat.rhn.domain.common.ChecksumType;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+/**
+ * Checksum
+ * Class for checksums of files, packages and erratas.
+ * @version $Rev$
+ */
+public class Checksum extends BaseDomainHelper {
+
+    private Long id;
+    private String checksum;
+    private ChecksumType checksumType;
+
+    /**
+     * @return Returns the id.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param idIn The id to set.
+     */
+    public void setId(Long idIn) {
+        this.id = idIn;
+    }
+
+    /**
+     * @return Returns the checksum.
+     */
+    public String getChecksum() {
+        return checksum;
+    }
+
+    /**
+     * @param checksumIn The checksum to set.
+     */
+    public void setChecksum(String checksumIn) {
+        this.checksum = checksumIn;
+    }
+
+    /**
+     * @return Returns the type of checksum.
+     */
+    public String getChecksumType() {
+        return checksumType;
+    }
+
+    /**
+     * @param checksumTypeIn The checksum type to set.
+     */
+    public void setDescription(ChecksumType checksumTypeIn) {
+        this.checksumType = checksumTypeIn;
+    }
+
+    /** {@inheritDoc} */
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Checksum checksum = (Checksum) o;
+
+        if (id != null ? !id.equals(checksum.id) : checksum.id != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    /** {@inheritDoc} */
+    public String toString() {
+        ToStringBuilder builder = new ToStringBuilder(this);
+
+        builder.append("id", id);
+        builder.append("checksum", checksum);
+
+        return builder.toString();
+    }
+}
