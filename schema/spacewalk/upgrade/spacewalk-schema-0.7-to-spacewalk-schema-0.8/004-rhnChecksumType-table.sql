9af2b26ef0981688dfd0e659de32cecea02d04c6
Michael Mraka <michael.mraka@redhat.com>
2009-12-04 16:25:21 +0100

    moved sha256 related upgrade scripts to appropriate directory

--- schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/004-rhnChecksumType-table.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/004-rhnChecksumType-table.sql
@@ -7,10 +7,10 @@
 -- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
--- 
+--
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation. 
+-- in this software or its documentation.
 --
 
 CREATE TABLE rhnChecksumType
--- schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/007-rhnChecksumView.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/007-rhnChecksumView.sql
@@ -7,13 +7,13 @@
 -- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
--- 
+--
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation. 
+-- in this software or its documentation.
 --
 
-create or replace view rhnChecksumView 
+create or replace view rhnChecksumView
 as
 select c.id,
        ct.label checksum_type,
