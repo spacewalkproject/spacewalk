ee95841a1277d1999a7cc79acc0a6009c9baf4ae
Jan Pazdziora <jpazdziora@redhat.com>
2009-12-15 09:36:10 +0100

    rhn-compat-profile-select not used anywhere, removing, as well as compat_profile_select.

--- web/modules/sniglets/Sniglets/Profiles.pm
+++ web/modules/sniglets/Sniglets/Profiles.pm
@@ -30,7 +30,6 @@ sub register_tags {
   my $class = shift;
   my $pxt = shift;
 
-  $pxt->register_tag('rhn-compat-profile-select' => \&compat_profile_select);
   $pxt->register_tag('rhn-compat-system-select' => \&compat_system_select);
   $pxt->register_tag('rhn-profile-name' => \&profile_name, -2);
   $pxt->register_tag('rhn-profile-edit' => \&profile_edit);
@@ -200,30 +199,6 @@ sub profile_delete_cb {
   throw "param 'delete_success_page' needed but not provided." unless $redir;
   $pxt->push_message(site_info => "Profile <strong>$name</strong> deleted.");
   $pxt->redirect($redir, ($sid ? (sid => $sid) : ()));
-}
-
-sub compat_profile_select {
-  my $pxt = shift;
-  my %params = @_;
-
-  my $sid = $pxt->param('sid');
-  my $ret = $params{__block__};
-
-  my $empty_msg = $params{empty_message} || '';
-  throw 'param empty_message needed but not provided.'
-    unless $empty_msg;
-
-  my @profiles = RHN::Profile->compatible_with_server($sid, $pxt->user->org_id);
-  my @options = map { [ PXT::Utils->escapeHTML($_->[1] || ''), $_->[0], 0 ] } @profiles;
-
-  if (@options) {
-    $ret =~ s/\{profile_select\}/PXT::HTML->select(-name => 'prid', -options => \@options)/e;
-  }
-  else {
-    $ret = $empty_msg;
-  }
-
-  return $ret;
 }
 
 sub compat_system_select {
