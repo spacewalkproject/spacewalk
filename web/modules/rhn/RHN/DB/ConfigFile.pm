5c171de052a3b6fc2b5f25c46937e47f1d2eb89a
Jan Pazdziora <jpazdziora@redhat.com>
2010-01-05 16:00:45 +0100

    Removing bunch of unused methods from RHN::DB::ConfigFile:
    id_to_latest_revision_crid
    file_name_id_to_path
    delete_file_path
    file_id_to_path

--- web/modules/rhn/RHN/DB/ConfigFile.pm
+++ web/modules/rhn/RHN/DB/ConfigFile.pm
@@ -18,65 +18,12 @@ use strict;
 package RHN::DB::ConfigFile;
 use RHN::DB;
 
-# given a config file id, return the crid of the latest revision, if any
-sub id_to_latest_revision_crid {
-  my $class = shift;
-  my $cf_id = shift;
-
-  my $dbh = RHN::DB->connect;
-
-  my $sth = $dbh->prepare('SELECT CFt.latest_config_revision_id FROM rhnConfigFile CFt WHERE CFT.id = :cf_id');
-  $sth->execute_h(cf_id => $cf_id);
-
-  my ($ret) = $sth->fetchrow;
-  $sth->finish;
-
-  return $ret;
-}
-
 # given a path, return the rhnConfigFileName id
 sub path_to_id {
   my $class = shift;
 
   my $dbh = RHN::DB->connect;
   return $dbh->call_function('lookup_config_filename', @_);
-}
-
-# given an id, return the rhnConfigFileName path
-sub file_name_id_to_path {
-  my $class = shift;
-
-  my $dbh = RHN::DB->connect;
-  my $sth = $dbh->prepare("SELECT path FROM rhnConfigFileName WHERE id = ?");
-  $sth->execute(@_);
-
-  my ($ret) = $sth->fetchrow;
-  $sth->finish;
-
-  return $ret;
-}
-
-# given an rhnConfigFile id, delete it
-sub delete_file_path {
-  my $self = shift;
-  my $cfid = shift;
-
-  my $dbh = RHN::DB->connect;
-  my $sth = $dbh->call_procedure('rhn_config.delete_file', $cfid);
-  $dbh->commit;
-}
-
-# given a config file id, find the path
-sub file_id_to_path {
-  my $class = shift;
-
-  my $dbh = RHN::DB->connect;
-  my $sth = $dbh->prepare("SELECT config_file_name_id FROM rhnConfigFile WHERE id = ?");
-  $sth->execute(@_);
-
-  my ($ret) = $sth->fetchrow;
-  $sth->finish;
-  return $class->file_name_id_to_path($ret);
 }
 
 1;
