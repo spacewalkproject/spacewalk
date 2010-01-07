73a06842c0019d09b6b428831a5c8596cf113a49
Michael Mraka <michael.mraka@redhat.com>
2010-01-07 09:51:21 +0100

    Update copyright in backend subtree for files changed in 2010.

--- backend/satellite_exporter/constants.py
+++ backend/satellite_exporter/constants.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2008 Red Hat, Inc.
+# Copyright (c) 2008--2010 Red Hat, Inc.
 #
 # This software is licensed to you under the GNU General Public License,
 # version 2 (GPLv2). There is NO WARRANTY for this software, express or
