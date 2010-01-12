a7bb3a24db3b4ebcfd6d963d084e24d09eece6af
Michael Mraka <michael.mraka@redhat.com>
2010-01-12 10:16:15 +0100

    fixed ISS error
    Exception Handler Information
    Traceback (most recent call last):
      File "/usr/share/rhn/satellite_exporter/satexport.py", line 103, in _wrapper
        ret = function(req)
      File "/usr/share/rhn/satellite_exporter/satexport.py", line 169, in _handler
        result = f(*params)
      File "/usr/share/rhn/satellite_exporter/handlers/non_auth_dumper.py", line 381, in
    +errata
        return self.dump_errata(errata=errata)
      File "/usr/share/rhn/satellite_tools/disk_dumper/dumper.py", line 833, in dump_errata
        dumper.dump()
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 91, in dump
        self.timer(6, "dump_subelement", self.dump_subelement, data)
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 52, in timer
        result = apply(function, args, kwargs)
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 101, in
    +dump_subelement
        data.dump()
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 91, in dump
        self.timer(6, "dump_subelement", self.dump_subelement, data)
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 52, in timer
        result = apply(function, args, kwargs)
      File "/usr/share/rhn/satellite_tools/disk_dumper/dumper.py", line 1047, in
    +dump_subelement
        self._dump_subelement(row)
      File "/usr/share/rhn/satellite_tools/disk_dumper/dumper.py", line 1492, in
    +_dump_subelement
        return exportLib.ErrataSynopsisDumper.dump_subelement(self, data)
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 888, in
    +dump_subelement
        d.dump()
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 69, in dump
        self._attributes = self.set_attributes() or {}
      File "/usr/share/rhn/satellite_tools/exporter/exportLib.py", line 921, in
    +set_attributes
        return {
    KeyError: 'org_id'

--- backend/satellite_tools/disk_dumper/dumper.py
+++ backend/satellite_tools/disk_dumper/dumper.py
@@ -1450,6 +1450,7 @@ class ErrataDumperEx(CachedDumper, exportLib.ErrataSynopsisDumper):
         h = rhnSQL.prepare("""
             select
                 e.id,
+                e.org_id,
                 e.advisory_name,
                 e.advisory,
                 e.advisory_type,
