2dabdc268e97fbde25cb5b200a1a02fab322a56d
Tomas Lestach <tlestach@redhat.com>
2010-01-13 18:06:01 +0100

    unused import removed

--- java/code/src/com/redhat/rhn/frontend/action/channel/manage/ChannelPackagesRemoveAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/channel/manage/ChannelPackagesRemoveAction.java
@@ -19,7 +19,6 @@ import com.redhat.rhn.common.localization.LocalizationService;
 import com.redhat.rhn.common.security.PermissionException;
 import com.redhat.rhn.domain.channel.Channel;
 import com.redhat.rhn.domain.channel.ChannelFactory;
-import com.redhat.rhn.domain.rhnpackage.PackageFactory;
 import com.redhat.rhn.domain.rhnset.RhnSet;
 import com.redhat.rhn.domain.role.RoleFactory;
 import com.redhat.rhn.domain.user.User;
