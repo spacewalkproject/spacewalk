9d2debe93fa7a99eca60f05bb8c668553d51cedb
Miroslav Suchý <msuchy@redhat.com>
2010-02-03 13:16:09 +0100

    531454 - provide architecture even for downgrade/upgrade

--- java/code/src/com/redhat/rhn/manager/action/ActionManager.java
+++ java/code/src/com/redhat/rhn/manager/action/ActionManager.java
@@ -1020,7 +1020,8 @@ public class ActionManager extends BaseManager {
                       params.put("r", pm.getSystem().getRelease());
                       epoch = pm.getSystem().getEpoch();
                       params.put("e", epoch != null ? epoch : "");
-                      params.put("a", "");
+                      params.put("a", pm.getSystem().getArch() != null ?
+                          pm.getOther().getArch() : "");
                       m.executeUpdate(params);
                   }
                   
@@ -1030,7 +1031,8 @@ public class ActionManager extends BaseManager {
                   params.put("r", pm.getOther().getRelease());
                   epoch = pm.getOther().getEpoch();
                   params.put("e", epoch != null ? epoch : "");
-                  params.put("a", "");
+                  params.put("a", pm.getOther().getArch() != null ?
+                          pm.getOther().getArch() : "");
                   m.executeUpdate(params);
               }
           }
