97a9f4e277edf306d828362408465e65f2482d10
Michael Mraka <michael.mraka@redhat.com>
2010-02-09 16:46:14 +0100

    changed data upgrades to direct inserts/updates (performace needs)

--- schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/012-2-rhnPackageFile-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/012-2-rhnPackageFile-data.sql
@@ -1,5 +1,42 @@
-update rhnPackageFile p
-   set checksum_id = lookup_checksum('md5', md5);
+declare
+ md5_id number;
+ min_pid number;
+ max_pid number;
+ lmin number;
+ lmax number;
+begin
+  select id
+    into md5_id
+    from rhnChecksumType
+   where label = 'md5';
 
-commit;
+  select min(package_id), max(package_id)
+    into min_pid, max_pid
+    from rhnPackageFile;
+
+  lmin := min_pid;
+  lmax := lmin + 99999;
+  while lmin < max_pid loop
+    insert into rhnChecksum (id, checksum_type_id, checksum)
+           (select rhnChecksum_seq.nextval, md5_id, csum
+              from (select distinct md5 as csum
+                      from rhnPackageFile
+                     where package_id between lmin and lmax
+                     minus
+                    select checksum as csum
+                      from rhnChecksum
+                     where checksum_type_id = md5_id));
+    commit;
+    update rhnPackageFile p
+       set checksum_id = (select id
+                            from rhnChecksum c
+                           where checksum_type_id = md5_id
+                             and p.md5 =  c.checksum)
+     where package_id between lmin and lmax;
+    commit;
+    lmin := lmax + 1;
+    lmax := lmin + 99999;
+  end loop;
+end;
+/
 
--- schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/018-2-rhnAppInstallSession-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/018-2-rhnAppInstallSession-data.sql
@@ -1,5 +1,28 @@
-update rhnAppInstallSession s
-   set checksum_id = lookup_checksum('md5', md5sum);
+declare
+ md5_id number;
+begin
+  select id
+    into md5_id
+    from rhnChecksumType
+   where label = 'md5';
 
-commit;
+  insert into rhnChecksum (id, checksum_type_id, checksum)
+         (select rhnChecksum_seq.nextval, md5_id, csum
+            from (select distinct md5sum as csum
+                    from rhnAppInstallSession
+                   where md5sum is not null
+                   minus
+                  select checksum as csum
+                    from rhnChecksum
+                   where checksum_type_id = md5_id));
+  commit;
+  update rhnAppInstallSession p
+     set checksum_id = (select id
+                          from rhnChecksum c
+                         where checksum_type_id = md5_id
+                           and p.md5sum =  c.checksum)
+   where md5sum is not null;
+  commit;
+end;
+/
 
