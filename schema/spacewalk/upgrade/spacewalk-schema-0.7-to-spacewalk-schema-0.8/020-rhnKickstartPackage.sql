a2c33bd4064655a4a77a1a929b866488b51ebfad
Michael Mraka <michael.mraka@redhat.com>
2010-02-09 19:24:09 +0100

    making position column unique in given kickstart
    fixes commit de3e61bd40178507c2884069e6f6e626371fd6a0
    
    making position column unique in the rhnKickstartPackage table
    
    this commit refers to BZ#527724 and its fix 01141f852a53f3b3f4e2d6e55ccd91478cb12209
    (the bug was introduced, because of missing unique constraint)

--- schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/020-rhnKickstartPackage.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/020-rhnKickstartPackage.sql
@@ -1,1 +1,5 @@
-ALTER TABLE rhnKickstartPackage ADD CONSTRAINT rhn_kspackage_pos_uq UNIQUE ( position );
+DROP INDEX rhn_kspackage_id_idx;
+
+ALTER TABLE rhnKickstartPackage
+    ADD CONSTRAINT rhn_kspackage_pos_uq UNIQUE (kickstart_id, position)
+    USING INDEX TABLESPACE [[4m_tbs]];
