279a80e00a1d50ffd1fbb8828fcfe002d7ffc1a2
Partha Aji <paji@redhat.com>
2010-03-11 16:17:29 -0500

    Fixed a unit test that required an additional parameter

--- java/code/src/com/redhat/rhn/frontend/action/rhnpackage/profile/test/SyncActionsTest.java
+++ java/code/src/com/redhat/rhn/frontend/action/rhnpackage/profile/test/SyncActionsTest.java
@@ -91,6 +91,7 @@ public class SyncActionsTest extends RhnMockStrutsTestCase {
         
         addRequestParameter(RequestContext.SID, s1.getId().toString());
         addRequestParameter(RequestContext.SID + "_1", s2.getId().toString());
+        addRequestParameter("time", String.valueOf(System.currentTimeMillis()));
         addRequestParameter("use_date", Boolean.FALSE.toString());
         setRequestPathInfo("/systems/details/packages/profiles/SyncSystemsSubmit");
 
