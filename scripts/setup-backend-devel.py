3c93fefece0139876aec742d8037952bb05adb66
Justin Sherrill <jsherril@redhat.com>
2010-03-11 14:36:17 -0500

    adding utilitiy to setup the backend for development purposes

---
+++ scripts/setup-backend-devel.py
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script symlinks everything needed from you git directory into /usr/share/rhn in order
+# to run the spacewalk backend directly from git.  NOTE: your spacewalk git directory must be in /spacewalk
+
+
+if [ ! -d /spacewalk ]; then
+   echo "/spacewalk does not exist"
+   exit -1
+fi
+
+cd /usr/share/rhn/
+
+DIRS="common satellite_exporter satellite_tools server spacewalk upload_server"
+
+for dir in $DIRS; do
+
+     if [ -d $dir ]; then
+        mv $dir $dir.bak
+     fi
+     ln -s /spacewalk/backend/$dir
+done;
+
+echo """
+
+Please add:
+
+<Directory "/usr/share/rhn/*">
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Order allow,deny
+    Allow from all
+</Directory>
+
+to /etc/httpd/conf.d/zz-spacewalk-server.conf within the virtual host definition and restart httpd
+
+"""
