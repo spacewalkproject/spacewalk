32f607647e4c0214233d6de15c05a5f5f41ec542
Michael Mraka <michael.mraka@redhat.com>
2010-03-11 16:31:47 +0100

    563902 - tuned sql upgrade script
    we have fit into 1GB TEMP and 1GB UNDO here

--- schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/012-2-rhnPackageFile-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-0.7-to-spacewalk-schema-0.8/012-2-rhnPackageFile-data.sql
@@ -4,18 +4,23 @@ declare
  max_pid number;
  lmin number;
  lmax number;
+ incr number;
 begin
   select id
     into md5_id
     from rhnChecksumType
    where label = 'md5';
 
-  select min(package_id), max(package_id)
-    into min_pid, max_pid
+  select min(package_id)
+    into min_pid
+    from rhnPackageFile;
+  select max(package_id)
+    into max_pid
     from rhnPackageFile;
 
+  incr := 80000;
   lmin := min_pid;
-  lmax := lmin + 1999;
+  lmax := lmin + incr - 1;
   while lmin < max_pid loop
     insert into rhnChecksum (id, checksum_type_id, checksum)
            (select rhnChecksum_seq.nextval, md5_id, csum
@@ -24,13 +29,21 @@ begin
                       from rhnPackageFile p
                      where package_id between lmin and lmax
                        and md5 is not null
-                       and not exists (select /*+ index(c rhnchecksum_chsum_idx) */ 1
+                       and not exists (select /*+ index(c rhnChecksum_chsum_uq) */ 1
                                          from rhnChecksum c
                                         where p.md5 = c.checksum
                                           and c.checksum_type_id = md5_id)
                    )
            );
     commit;
+    lmin := lmax + 1;
+    lmax := lmin + incr -1;
+  end loop;
+
+  incr := 10000;
+  lmin := min_pid;
+  lmax := lmin + incr - 1;
+  while lmin < max_pid loop
     update /*+ index(p rhn_package_file_pid_cid_uq) */ rhnPackageFile p
        set checksum_id = (select id
                             from rhnChecksum c
@@ -40,7 +53,7 @@ begin
        and md5 is not null;
     commit;
     lmin := lmax + 1;
-    lmax := lmin + 1999;
+    lmax := lmin + incr -1;
   end loop;
 end;
 /
