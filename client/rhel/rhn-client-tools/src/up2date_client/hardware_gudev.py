a06aae19db08f97cad92da36a1c57ad789fd6fc8
Miroslav Suchý <msuchy@redhat.com>
2010-03-16 10:29:57 +0100

    564352 - basic framework for using python-gudev
    a lot of FIXME for now, will fill in later one by one

--- client/rhel/rhn-client-tools/src/up2date_client/hardware_gudev.py
+++ client/rhel/rhn-client-tools/src/up2date_client/hardware_gudev.py
@@ -25,7 +25,25 @@ def get_devices:
 		'class' : 'OTHER'           
 		'desc' : 'Intel Corporation|5000 Series Chipset PCI Express x4 Port 2'
 	"""
-	pass
+	# listen to uevents on all subsystems
+	client = gudev.Client([""])
+	# FIX ME - change to None to list all devices once it is fixed in gudev
+	devices = client.query_by_subsystem("pci")
+	result = []
+	for device in devices:
+		subsystem = device.get_subsystem()
+		result_item = {
+			'bus': 		subsystem,
+			'driver':	device.get_driver(),
+			'pciType':  _clasify_pci_type(subsystem),
+			'detached':	'0', # always zero?
+			'class':	_clasify_class(device)
+			'desc':		_get_device_desc(device),
+		}
+		if subsystem == 'block':
+			result_item['device'] = device.get_property('DEVNAME')
+		result.append(result_item)
+	return result_item
 
 def get_computer_info:
 	""" Return dictionaries with keys (values are provided as example):
@@ -34,4 +52,36 @@ def get_computer_info:
            	'system.kernel.machine': 'i686'
 		'system.kernel.name': 'Linux'
 	"""
-	pass
+	pass #FIXME
+
+
+def _clasify_pci_type(subsystem):
+	""" return 1 if device is PCI, otherwise -1 """
+	if subsystem == 'pci':
+		return '1'
+	else:
+		return '-1'
+
+def _clasify_class(device):
+	""" Clasify type of device. Returned value is one of following string:
+		NETWORK, KEYBOARD, MOUSE, VIDEO, USB, IDE, SCSI, RAID, MODEM, SCANNER
+		CAPTURE, AUDIO, FIREWIRE, SOCKET, CDROM, HD, FLOPPY, TAPE, PRINTER, OTHER
+		or None if it is neither PCI nor USB device.
+	"""
+	return #FIXME
+
+def _get_device_desc(device):
+	""" Return human readable description of device. """
+	subsystem = device.get_subsystem()
+	command = None
+	if subsystem == 'pci':
+		command = "lspci -d %s" % device.get_property('PCI_ID')
+	elif subsystem == 'usb':
+		command = "lsusb -d %s:%s" % ( device.get_property('ID_VENDOR_ID'),
+				device.get_property('ID_MODEL_ID') )
+	from subprocess import PIPE, Popen
+	if command:
+	    return subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).stdout.read()
+	else:
+		return ''
+
