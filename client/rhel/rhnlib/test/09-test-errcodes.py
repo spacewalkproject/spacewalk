3fb6d517229c2741a6de1904731f1e405485a997
Simon Lukasik <slukasik@redhat.com>
2010-04-02 09:21:20 +0200

    Cleaning up, preparing for automatization

--- client/rhel/rhnlib/test/09-test-errcodes.py
+++ client/rhel/rhnlib/test/09-test-errcodes.py
@@ -3,17 +3,28 @@
 # munge the headers to produce an error message
 #
 # $Id$
+#
+# USAGE:  $0 SERVER SYSTEMID
 
 import sys
 sys.path.append('..')
 from rhn.rpclib import Server, GETServer, ProtocolError, reportError
 
-s = Server("http://xmlrpc.rhn.redhat.com/XMLRPC")
-if len(sys.argv) > 1:
-    system_id_file = sys.argv[1]
-else:
-    system_id_file = '/etc/sysconfig/rhn/systemid'
+SERVER = "xmlrpc.rhn.redhat.com"
+HANDLER = "/XMLRPC"
+system_id_file = '/etc/sysconfig/rhn/systemid'
+try:
+    SERVER = sys.argv[1]
+    system_id_file = sys.argv[2]
+except:
+    pass
 
+def get_test_server_https():
+    global SERVER, HANDLER
+    return Server("https://%s%s" % (SERVER, HANDLER))
+
+
+s = get_test_server_https()
 sysid = open(system_id_file).read()
 
 dict = s.up2date.login(sysid)
@@ -29,3 +40,7 @@ try:
     l = sg.listPackages(channel_name, channel_version)
 except ProtocolError, e:
     print reportError(e.headers)
+    print("OK (error above expected)");
+    sys.exit(0);
+print("ERROR: Exception didn't occured!");
+sys.exit(-1);
