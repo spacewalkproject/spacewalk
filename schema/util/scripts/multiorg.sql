fb3587ec64e209837595d0a36273dd9eea105972
Michael Mraka <michael.mraka@redhat.com>
2010-04-13 11:37:02 +0200

    cleaning old stuff
    scripts are no more used/referenced from git or wiki

--- schema/util/scripts/multiorg.sql
+++
@@ -1,9 +0,0 @@
--- sql statements for multiorg work
-alter table rhnChannel drop column parent_channel;
-alter table web_contact drop column oracle_contact_id;
-alter table web_customer drop column oracle_customer_number;
-alter table web_customer drop column oracle_customer_id;
-alter table web_customer drop column customer_type;
-alter table web_customer drop column credit_application_completed;
---commit;
-
--- schema/util/show-proc.sql
+++
@@ -1,4 +0,0 @@
-set pagesize 50000
-select line||' '||text from all_source where name = upper('&name')
-  and type in ('FUNCTION', 'PROCEDURE','PACKAGE BODY')
-  ORDER by line;
