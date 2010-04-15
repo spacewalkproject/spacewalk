29e58294f7676cf1617a524d4514a913e625386f
Simon Lukasik <slukasik@redhat.com>
2010-04-15 13:08:14 +0200

    Cleaning up, preparing for automatization

--- client/rhel/rhnlib/test/21-proxy-user-agent.py
+++ client/rhel/rhnlib/test/21-proxy-user-agent.py
@@ -1,8 +1,37 @@
+#!/usr/bin/python
+#
+# tests rhn.rpclib.Server(), connection through proxy
+#
+# $Id$
+#
+# USAGE:  $0 SERVER PROXY [SYSTEMID]
+
 import sys
 sys.path.append('..')
 from rhn import rpclib
 
-s = rpclib.Server("https://xmlrpc.rhn.redhat.com/XMLRPC", 
-    proxy="localhost:1234")
+SERVER = "xmlrpc.rhn.redhat.com"
+HANDLER = "/XMLRPC"
+PROXY = "proxy.example.com:8080"
+system_id_file = '/etc/sysconfig/rhn/systemid'
 
-print s.a.b()
+if len(sys.argv) < 3:
+    print "Non efficient cmd-line arguments! Provide at least server & proxy!"
+    sys.exit(1);
+
+try:
+    SERVER = sys.argv[1]
+    PROXY = sys.argv[2]
+    system_id_file = sys.argv[3]
+except:
+    pass
+
+SERVER_URL = "https://" + SERVER + HANDLER
+
+systemid = open(system_id_file).read()
+
+s = rpclib.Server(SERVER_URL, proxy = PROXY)
+
+dict = s.up2date.login(systemid);
+
+print "Test PASSES"
