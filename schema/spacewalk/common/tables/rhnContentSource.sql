a38e77959a76a20754c7c8b9da19d9085025fc7a
Shannon Hughes <shughes@redhat.com>
2010-05-05 14:20:07 -0400

    renaming to contentsource since we are moving channel out to a new mapping table named rhnChannelContentSource

--- schema/spacewalk/common/tables/rhnContentSource.sql
+++ schema/spacewalk/common/tables/rhnContentSource.sql
@@ -7,29 +7,26 @@
 -- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
--- 
+--
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation. 
+-- in this software or its documentation.
 --
 --
 --
 --
 
 create table
-rhnChannelContentSource
+rhnContentSource
 (
         id		number NOT NULL
 			constraint rhn_ccs_id_pk primary key,
-        channel_id      number NOT NULL
-                        constraint rhn_ccs_c_fk
-                                references rhnChannel(id) on delete cascade,
         type_id         number NOT NULL
                         constraint rhn_ccs_type_fk
                                 references rhnContentSourceType(id),
         source_url      varchar2(512) NOT NULL,
         label           varchar2(64) NOT NULL,
-        last_synced     date, 
+        last_synced     date,
         created         date default(sysdate) NOT NULL,
         modified        date default(sysdate) NOT NULL
 )
@@ -41,7 +38,7 @@ create sequence rhn_chan_content_src_id_seq start with 500;
 
 
 create unique index rhn_ccs_uq
-	on rhnChannelContentSource(channel_id, type_id, source_url)
+	on rhnContentSource(channel_id, type_id, source_url)
 	tablespace [[64k_tbs]]
   ;
 
