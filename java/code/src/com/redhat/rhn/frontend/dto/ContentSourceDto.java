8be368ab7177faeeb67ab289db17d91cdf0096fe
Shannon Hughes <shughes@redhat.com>
2010-05-13 11:25:35 -0400

    adding ContentSource DTO object for repo listtags

---
+++ java/code/src/com/redhat/rhn/frontend/dto/ContentSourceDto.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2009--2010 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.dto;
+
+import java.util.Date;
+
+
+/**
+ * ContentSourceDto
+ * @version $Rev$
+ */
+public class ContentSourceDto extends BaseDto {
+
+    private Long id;    
+    private String label;
+    private Date lastSynced;
+    
+    /**
+     * 
+     * @return laste date repo was synced
+     */
+    public Date getLastSynced() {
+        return lastSynced;
+    }
+    
+    /**
+     * 
+     * @param lastSyncedIn date to set of last sync
+     */
+    public void setLastSynced(Date lastSyncedIn) {
+        this.lastSynced = lastSyncedIn;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public Long getId() {
+        return id;
+    }
+    
+    /**
+     * @param idIn The id to set.
+     */
+    public void setId(Long idIn) {
+        this.id = idIn;
+    }    
+    
+    /**
+     * @return the label
+     */
+    public String getLabel() {
+        return label;
+    }
+
+    
+    /**
+     * @param labelIn The label to set.
+     */
+    public void setLabel(String labelIn) {
+        this.label = labelIn;
+    }
+
+
+}
