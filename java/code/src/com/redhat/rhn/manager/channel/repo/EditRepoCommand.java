1ddd420815cb6b4b8a5869149a240a12d68fd9e5
Shannon Hughes <shughes@redhat.com>
2010-05-25 17:04:29 -0400

    adding repo edit commands

---
+++ java/code/src/com/redhat/rhn/manager/channel/repo/EditRepoCommand.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2009--2010 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.manager.channel.repo;
+
+import com.redhat.rhn.domain.channel.ChannelFactory;
+import com.redhat.rhn.domain.user.User;
+
+/**
+ * CreateRepoCommand - Command to create a repo
+ * @version $Rev: 119601 $
+ */
+public class EditRepoCommand extends BaseRepoCommand {
+
+ public EditRepoCommand(User currentUser, Long repoId) {
+     super();
+     this.repo = ChannelFactory.lookupContentSource(repoId, currentUser.getOrg());
+ }
+
+}
