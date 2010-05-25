28344151fac686c0e2bfaa212a2911d5bf191bc6
Shannon Hughes <shughes@redhat.com>
2010-05-25 16:43:07 -0400

    refactoring repo commands to use base class

--- java/code/src/com/redhat/rhn/manager/channel/repo/CreateRepoCommand.java
+++ java/code/src/com/redhat/rhn/manager/channel/repo/CreateRepoCommand.java
@@ -14,125 +14,19 @@
  */
 package com.redhat.rhn.manager.channel.repo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.redhat.rhn.common.validator.ValidatorError;
 import com.redhat.rhn.domain.channel.ChannelFactory;
-import com.redhat.rhn.domain.channel.ContentSource;
 import com.redhat.rhn.domain.org.Org;
 
 /**
  * CreateRepoCommand - Command to create a repo
  * @version $Rev: 119601 $
  */
-public class CreateRepoCommand {
+public class CreateRepoCommand extends BaseRepoCommand {
 
-    private String label;
-    private String url;
-    private Org org;
-    private ContentSource newRepo;
-    private List<ValidatorError> errors;        
-
-    /** 
-     * Constructor to create an org
-     * @param nameIn to set on the org
-     * @param loginIn to use for 1st user in org
-     * @param passwordIn to set for first user
-     * @param emailIn to set for first user
-     */
-    public CreateRepoCommand(String labelIn, String urlIn, Org orgIn) {
-        this.label = labelIn;
-        this.url = urlIn;
-        this.org = orgIn;
-    }
-    
-    /**
-     * 
-     * @return Org of repo
-     */
-    public Org getOrg() {
-        return org;
-    }
-    
-    /**
-     * 
-     * @param org to set for repo
-     */
-    public void setOrg(Org org) {
-        this.org = org;
-    }
-
-    /**
-     * 
-     * @return label for repo
-     */
-    public String getLabel() {
-        return label;
-    }
-
-    /**
-     * 
-     * @param labelIn to set for repo
-     */
-    public void setLabel(String LabelIn) {
-        this.label = LabelIn;
-    }
-    
-    /**
-     * 
-     * @return url for repo
-     */
-    public String getUrl() {
-        return url;
-    }
-
-    /**
-     * 
-     * @param urlIn to set for repo
-     */
-    public void setUrl(String urlIn) {
-        this.url = urlIn;
-    }
-
-    /**
-     * Check for errors and store Org to db.
-     * @return ValidatorError[] array if there are errors
-     */
-    public ValidatorError[] store() {
-        ValidatorError[] errors = validate();
-        if (errors != null && errors.length > 0) {
-            return errors;
-        }
-        else {
-            // Create repo
-            ContentSource repo = ChannelFactory.createRepo();
-            repo.setLabel(this.label);
-            repo.setSourceUrl(this.url);
-            repo.setOrg(this.org);            
-            repo.setType(ChannelFactory.CONTENT_SOURCE_TYPE_YUM);
-            ChannelFactory.save(repo);
-            this.newRepo = repo;
-        }
-        return null;
-    }
-
-    /**
-     * Get the newly created org.
-     * @return Org that was stored to DB
-     */
-    public ContentSource getNewRepo() {
-        return this.newRepo;
-    }
-
-    /**
-     * Validates the repo object. 
-     * @return an Object array of ValidatorErrors.
-     */
-    public ValidatorError[] validate() {
-        errors = new ArrayList(); //clear validation errors                               
-        return (ValidatorError[]) errors.toArray(new ValidatorError[0]);
-    }
-    
+ public CreateRepoCommand(Org orgIn) {
+     super();
+     this.repo = ChannelFactory.createRepo();
+     this.repo.setOrg(orgIn);
+ }
 
 }
