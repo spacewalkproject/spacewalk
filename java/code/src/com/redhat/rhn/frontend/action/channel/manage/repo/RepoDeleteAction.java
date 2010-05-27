46c06c83675cf814483b696b48680a5cdddc0a28
Shannon Hughes <shughes@redhat.com>
2010-05-27 14:19:20 -0400

    initial files to support Repo delete

---
+++ java/code/src/com/redhat/rhn/frontend/action/channel/manage/repo/RepoDeleteAction.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2009--2010 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.action.channel.manage.repo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import com.redhat.rhn.common.validator.ValidatorException;
+import com.redhat.rhn.domain.channel.ContentSource;
+import com.redhat.rhn.frontend.struts.RequestContext;
+import com.redhat.rhn.frontend.struts.RhnAction;
+import com.redhat.rhn.frontend.struts.RhnHelper;
+import com.redhat.rhn.frontend.struts.RhnValidationHelper;
+import com.redhat.rhn.manager.channel.repo.EditRepoCommand;
+
+/**
+ * RepoDeleteAction
+ * @version $Rev: 1 $
+ */
+public class RepoDeleteAction extends RhnAction {
+    /** {@inheritDoc} */
+    public ActionForward execute(ActionMapping mapping,
+                                  ActionForm formIn,
+                                  HttpServletRequest request,
+                                  HttpServletResponse response) {
+        RequestContext context = new RequestContext(request);
+        EditRepoCommand cmd = new EditRepoCommand(context.getLoggedInUser(),
+                context.getParamAsLong("id"));
+        ContentSource src = cmd.getNewRepo();
+
+        request.setAttribute(RepoDetailsAction.LABEL, src.getLabel() );
+        request.setAttribute(RepoDetailsAction.URL, src.getSourceUrl() );
+        request.setAttribute("id", src.getId());
+
+        if (context.isSubmitted()) {
+            try {
+                //delete here
+                createSuccessMessage(request,
+                            "repos.delete.success", cmd.getLabel());
+                return mapping.findForward("success");
+            }
+            catch (ValidatorException ve) {
+                getStrutsDelegate().saveMessages(request, ve.getResult());
+                RhnValidationHelper.setFailedValidation(request);
+            }
+        }
+
+        return mapping.findForward(RhnHelper.DEFAULT_FORWARD);
+    }
+}
---
+++ java/code/webapp/WEB-INF/pages/channel/manage/repo/deleteconfirm.jsp
@@ -0,0 +1,52 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ taglib uri="http://rhn.redhat.com/rhn" prefix="rhn" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-bean" prefix="bean" %>
+<%@ taglib uri="http://jakarta.apache.org/struts/tags-html" prefix="html" %>
+
+<html:xhtml/>
+<html>
+<body>
+<rhn:toolbar base="h1" img="/img/rhn-icon-info.gif" imgAlt="info.alt.img">
+	<c:out value="${requestScope.label}"/>
+</rhn:toolbar>
+
+<h2><bean:message key="repos.jsp.delete.header2"/></h2>
+<p><bean:message key="repos.jsp.delete.summary"/></p>
+<p><rhn:warning key= "repos.jsp.delete.warning"/></p>
+<div>
+	<form method="post" action="/rhn/channels/cobbler/manage/repos/RepoDelete.do">
+<h2><bean:message key="repos.jsp.delete.info.header"/></h2>
+    <table class="details">
+    <tr>
+        <th>
+         <bean:message key="repos.jsp.create.label"/>
+        </th>
+        <td>
+			<c:out value="${label}"/></pre>
+       </td>
+    </tr>
+    <tr>
+      <th>
+       <bean:message key="repos.jsp.create.url"/>
+      </th>
+      <td>
+	    <c:out value="${url}"/></pre>
+      </td>
+    </tr>
+
+    </table>
+    <hr />
+    <table align="right">
+    <tr>
+      <td></td>
+      <rhn:submitted/>
+      <input type="hidden" name="id" value="${requestScope.id}"/>
+      <td align="right"><input type=submit name="dispatch"
+       value="${rhn:localize('repos.jsp.delete.submit')}"/></td>
+    </tr>
+        </table>
+    </form>
+</div>
+
+</body>
+</html>
