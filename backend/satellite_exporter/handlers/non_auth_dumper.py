72534170117c3a73e89a588f168a95f70a182a0c
Michael Mraka <michael.mraka@redhat.com>
2010-05-31 14:55:29 +0200

    fixed typo
    fixing build time error:
    non_auth_dumper.py: Error compiling
      File "non_auth_dumper.py", line 493
          def __init__(self, writer, channels)

--- backend/satellite_exporter/handlers/non_auth_dumper.py
+++ backend/satellite_exporter/handlers/non_auth_dumper.py
@@ -490,7 +490,7 @@ class ContainerWriter:
 # based on the creation date
 # XXX No caching for now
 class ChannelsDumper(exportLib.ChannelsDumper):
-    def __init__(self, writer, channels)
+    def __init__(self, writer, channels):
         # if snapshot is None, then all the objects from the channel are
         # returned - this is useful for snapshotting
         exportLib.ChannelsDumper.__init__(self, writer, channels)
