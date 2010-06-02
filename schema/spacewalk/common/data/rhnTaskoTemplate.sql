079185f49e2d80d4fb81cb8f2f8e241a91866ba2
Tomas Lestach <tlestach@redhat.com>
2010-06-02 14:57:25 +0200

    add test DB data

---
+++ schema/spacewalk/common/data/rhnTaskoTemplate.sql
@@ -0,0 +1,31 @@
+--
+-- Copyright (c) 2010 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
+
+
+INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
+             VALUES (rhn_tasko_template_id_seq.nextval,
+                        (SELECT id FROM rhnTaskoBunch WHERE name='daily-status'),
+                        (SELECT id FROM rhnTaskoTask WHERE name='summary-population'),
+                        0,
+                        '');
+
+INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
+             VALUES (rhn_tasko_template_id_seq.nextval,
+                         (SELECT id FROM rhnTaskoBunch WHERE name='daily-status'),
+                         (SELECT id FROM rhnTaskoTask WHERE name='daily-summary'),
+                         1,
+                         '');
+
+commit;
