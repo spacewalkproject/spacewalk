74e906730b83464af5ec49322e2aa7d18713ede0
Partha Aji <paji@redhat.com>
2010-06-04 18:04:04 -0400

    Updated some views using hosted changes

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/007-rhnOrgChannelFamilyPermissions.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/007-rhnOrgChannelFamilyPermissions.sql
@@ -18,9 +18,11 @@
 
 create or replace view rhnOrgChannelFamilyPermissions as
 	select	pcf.channel_family_id,
-		u.org_id as org_id,
-		to_number(null, null) as max_members,
-		0 as current_members,
+		u.org_id org_id,
+		to_number(null) max_members,
+		0 current_members,
+		to_number(null) fve_max_members,
+		0 fve_current_members,
 		pcf.created,
 		pcf.modified
 	from	rhnPublicChannelFamily pcf,
@@ -30,7 +32,19 @@ create or replace view rhnOrgChannelFamilyPermissions as
 		org_id,
 		max_members,
 		current_members,
+		fve_max_members,
+		fve_current_members,
 		created,
 		modified
 	from	rhnPrivateChannelFamily;
 
+--
+--
+-- Revision 1.2  2004/04/16 16:07:12  pjones
+-- bugzilla: none -- 8.1.7 won't let you use "null foo" as a column in a view
+-- that gets unioned with a typed column.  you have to use "to_number(null) foo".  What a load of crap.
+--
+-- Revision 1.1  2004/04/14 00:09:24  pjones
+-- bugzilla: 120761 -- split rhnChannelPermissions into two tables, eliminating
+-- a frequent full table scan
+--
