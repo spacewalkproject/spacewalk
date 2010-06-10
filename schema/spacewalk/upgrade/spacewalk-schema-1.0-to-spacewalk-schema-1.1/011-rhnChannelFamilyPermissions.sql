23c6644f3584b74ec5ebc5a39db4fb18b2d9636b
Partha Aji <paji@redhat.com>
2010-06-10 12:47:38 -0400

    Added channel family permissions to upgrade

---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/011-rhnChannelFamilyPermissions.sql
@@ -0,0 +1,39 @@
+--
+-- Copyright (c) 2008 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+-- 
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation. 
+--
+--
+--
+--
+
+create or replace view rhnChannelFamilyPermissions as
+	select	channel_family_id,
+		to_number(null, null) as org_id,
+		to_number(null, null) as max_members,
+		0 as current_members,
+                0 as fve_max_members,
+                0 as fve_current_members,
+		created,
+		modified
+	from	rhnPublicChannelFamily
+	union
+	select	channel_family_id,
+		org_id,
+		max_members,
+		current_members,
+                fve_max_members,
+                fve_current_members,
+		created,
+		modified
+	from	rhnPrivateChannelFamily;
+
