e8da858236a93a41cf2f7d645addb04a4e8e71f9
Partha Aji <paji@redhat.com>
2010-06-17 17:55:12 -0400

    Forgot to commit EligibleFlexGuestAction

---
+++ java/code/src/com/redhat/rhn/frontend/action/systems/entitlements/EligibleFlexGuestAction.java
@@ -0,0 +1,89 @@
+/**
+ * Copyright (c) 2010 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.action.systems.entitlements;
+
+import com.redhat.rhn.frontend.dto.ChannelFamilySystem;
+import com.redhat.rhn.frontend.struts.RequestContext;
+import com.redhat.rhn.frontend.struts.RhnAction;
+import com.redhat.rhn.frontend.taglibs.list.helper.ListSessionSetHelper;
+import com.redhat.rhn.frontend.taglibs.list.helper.Listable;
+import com.redhat.rhn.manager.system.VirtualizationEntitlementsManager;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * EligibleFlexGuestAction
+ * @version $Rev$
+ */
+public class EligibleFlexGuestAction extends RhnAction implements Listable {
+    
+    private static final String SELECTABLE = "selectable";
+    /**
+     * 
+     * {@inheritDoc}
+     */
+    public ActionForward execute(ActionMapping mapping,
+            ActionForm formIn,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+        request.setAttribute(SELECTABLE, Boolean.TRUE);
+        ListSessionSetHelper helper = new ListSessionSetHelper(this, request);
+        helper.execute();
+        if (helper.isDispatched()) {
+            RequestContext context = new RequestContext(request);
+            return handleConfirm(helper, context, mapping);
+        }
+        
+        return mapping.findForward("default");
+    }
+
+
+    private ActionForward handleConfirm(ListSessionSetHelper helper, RequestContext context,
+            ActionMapping mapping) {
+        Set <Long> serverIds = new HashSet<Long>();
+        for (String selectionKey : helper.getSet()) {
+            Long sid = ChannelFamilySystem.parseServerId(selectionKey);
+            Long cfid = ChannelFamilySystem.parseChannelFamilyId(selectionKey);
+            VirtualizationEntitlementsManager.getInstance().convertToFlex(sid, cfid,
+                                                               context.getLoggedInUser());
+            serverIds.add(sid);
+        }
+        helper.destroy();
+        
+        getStrutsDelegate().saveMessage("eligible.flexguest.systems.confirm.message",
+                                    new String [] { String.valueOf(serverIds.size())}, 
+                                context.getRequest());
+        return  mapping.findForward("success");
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    public List getResult(RequestContext contextIn) {
+        return VirtualizationEntitlementsManager.getInstance().
+                    listEligibleFlexGuests(contextIn.getLoggedInUser());
+    }
+}
