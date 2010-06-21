74a8c923432ffcdc440cae1f5dcccee55c43834e
Aron Parsons <aparsons@redhat.com>
2010-06-21 13:25:57 -0400

    added functions to rename and delete Kickstart profiles added functions to create and delete crypto keys added functions to manipulate Kickstart trees (distributions) fixed a couple of errors in the editor() function

--- SpacewalkShell.py
+++ SpacewalkShell.py
@@ -231,7 +231,7 @@ For help for a specific command try 'help <cmd>'.
         return matches
 
 
-    def editor(self, template):
+    def editor(self, template = '', delete = False):
         # create a temporary file
         (descriptor, file_name) = mkstemp(prefix='spacecmd.')
 
@@ -245,7 +245,7 @@ For help for a specific command try 'help <cmd>'.
                 pass
 
         # use the user's specified editor
-        if os.environ['EDITOR']:
+        if 'EDITOR' in os.environ:
             if self.EDITORS[0] != os.environ['EDITOR']:
                 self.EDITORS.insert(0, os.environ['EDITOR'])
 
@@ -274,6 +274,13 @@ For help for a specific command try 'help <cmd>'.
                 file = open(file_name, 'r')
                 contents = file.read()
                 file.close()
+
+                if delete:
+                    try:
+                        os.remove(file_name)
+                        file_name = ''
+                    except:
+                        logging.error('Could not remove %s' % file_name)
 
                 return (contents, file_name)
             except:
@@ -1529,6 +1536,59 @@ For help for a specific command try 'help <cmd>'.
 
 ####################
 
+    def help_cryptokey_create(self):
+        print 'cryptokey_create: Create a cryptographic key'
+        print 'usage: cryptokey_create'
+
+    def do_cryptokey_create(self, args):
+        key_type = ''
+        while not re.match('GPG|SSL', key_type):
+            key_type = self.prompt_user('GPG or SSL [G/S]:')
+           
+            if re.match('G', key_type, re.I):
+                key_type = 'GPG'
+            elif re.match('S', key_type, re.I):
+                key_type = 'SSL'
+            else:
+                logging.warning('Invalid key type')
+                key_type = '' 
+
+        description = ''
+        while description == '':
+            description = self.prompt_user('Description:')
+
+        content = self.editor(delete=True)
+
+        self.client.kickstart.keys.create(self.session,
+                                          description,
+                                          key_type,
+                                          content)
+
+####################
+
+    def help_cryptokey_delete(self):
+        print 'cryptokey_delete: Delete a cryptographic key'
+        print 'usage: cryptokey_delete NAME'
+
+    def complete_cryptokey_delete(self, text, line, begidx, endidx):
+        if len(line.split(' ')) <= 2:
+            return self.tab_completer(self.do_cryptokey_list('', True), 
+                                      text)
+
+    def do_cryptokey_delete(self, args):
+        args = self.parse_arguments(args)
+
+        if len(args) != 1:
+            self.help_cryptokey_delete()
+            return
+
+        name = args[0]
+
+        if self.user_confirm('Delete this key [y/N]:'):
+            self.client.kickstart.keys.delete(self.session, name)
+
+####################
+
     def help_cryptokey_list(self):
         print 'cryptokey_list: List all cryptographic keys (SSL, GPG)'
         print 'usage: cryptokey_list'
@@ -1578,6 +1638,184 @@ For help for a specific command try 'help <cmd>'.
 
             print
             print details.get('content')
+
+####################
+
+    def help_distribution_create(self):
+        print 'distribution_create: Create a Kickstart tree'
+        print 'usage: distribution_create'
+
+    def do_distribution_create(self, args):
+        name = self.prompt_user('Label:')
+
+        base_path = self.prompt_user('Path to Kickstart Tree:')
+
+        base_channel = ''
+        while base_channel == '':
+            print
+            print 'Base Channels:'
+            for c in self.list_base_channels():
+                print '  %s' % c
+
+            base_channel = self.prompt_user('Base Channel:')
+
+            if base_channel not in self.list_base_channels():
+                logging.warning('Invalid channel label')
+                base_channel = ''
+
+        install_types = \
+            self.client.kickstart.tree.listInstallTypes(self.session)
+       
+        install_types = [ t.get('label') for t in install_types ]
+ 
+        install_type = ''
+        while install_type == '':
+            print
+            print 'Install Types:'
+            for t in install_types:
+                print '  %s' % t
+
+            install_type = self.prompt_user('Install Type:')
+    
+            if install_type not in install_types:
+                logging.warning('Invalid install type')
+                install_type = '' 
+
+        self.client.kickstart.tree.create(self.session,
+                                          name,
+                                          base_path,
+                                          base_channel,
+                                          install_type)
+
+####################
+
+    def help_distribution_list(self):
+        print 'distribution_list: List the available Kickstart trees'
+        print 'usage: distribution_list'
+
+    def do_distribution_list(self, args, doreturn=False):
+        channels = self.client.kickstart.listKickstartableChannels(self.session)
+
+        avail_trees = []
+        for c in channels:
+            trees = self.client.kickstart.tree.list(self.session,
+                                                    c.get('label'))
+
+            for t in trees:
+                label = t.get('label')
+                if label not in avail_trees:
+                    avail_trees.append(label)
+
+        if doreturn:
+            return avail_trees
+        else:
+            if len(avail_trees):
+                print '\n'.join(sorted(avail_trees))
+
+####################
+
+    def help_distribution_delete(self):
+        print 'distribution_delete: Delete a Kickstart tree'
+        print 'usage: distribution_delete LABEL'
+
+    def complete_distribution_delete(self, text, line, begidx, endidx):
+        if len(line.split(' ')) <= 2:
+            return self.tab_completer(self.do_distribution_list('', True), 
+                                      text)
+
+    def do_distribution_delete(self, args):
+        args = self.parse_arguments(args)
+
+        if len(args) != 1:
+            self.help_distribution_delete()
+            return
+
+        label = args[0]
+
+        if self.user_confirm('Delete this tree [y/N]:'):
+            self.client.kickstart.tree.delete(self.session, label)
+
+####################
+
+    def help_distribution_rename(self):
+        print 'distribution_rename: Rename a Kickstart tree'
+        print 'usage: distribution_rename OLDNAME NEWNAME'
+
+    def complete_distribution_rename(self, text, line, begidx, endidx):
+        if len(line.split(' ')) <= 2:
+            return self.tab_completer(self.do_distribution_list('', True), 
+                                      text)
+
+    def do_distribution_rename(self, args):
+        args = self.parse_arguments(args)
+
+        if len(args) != 2:
+            self.help_distribution_rename()
+            return
+
+        oldname = args[0]
+        newname = args[1]
+
+        self.client.kickstart.tree.rename(self.session, oldname, newname)
+
+####################
+
+    def help_distribution_update(self):
+        print 'distribution_update: Update the path of a Kickstart tree'
+        print 'usage: distribution_update LABEL'
+    
+    def complete_distribution_update(self, text, line, begidx, endidx):
+        if len(line.split(' ')) <= 2:
+            return self.tab_completer(self.do_distribution_list('', True), 
+                                      text)
+
+    def do_distribution_update(self, args):
+        args = self.parse_arguments(args)
+
+        if len(args) != 1:
+            self.help_distribution_update()
+            return
+
+        label = args[0]
+
+        base_path = self.prompt_user('Path to Kickstart Tree:')
+
+        base_channel = ''
+        while base_channel == '':
+            print
+            print 'Base Channels:'
+            for c in self.list_base_channels():
+                print '  %s' % c
+
+            base_channel = self.prompt_user('Base Channel:')
+
+            if base_channel not in self.list_base_channels():
+                logging.warning('Invalid channel label')
+                base_channel = ''
+
+        install_types = \
+            self.client.kickstart.tree.listInstallTypes(self.session)
+       
+        install_types = [ t.get('label') for t in install_types ]
+ 
+        install_type = ''
+        while install_type == '':
+            print
+            print 'Install Types:'
+            for t in install_types:
+                print '  %s' % t
+
+            install_type = self.prompt_user('Install Type:')
+    
+            if install_type not in install_types:
+                logging.warning('Invalid install type')
+                install_type = '' 
+
+        self.client.kickstart.tree.update(self.session,
+                                          label,
+                                          base_path,
+                                          base_channel,
+                                          install_type)
 
 ####################
 
@@ -1961,7 +2199,7 @@ For help for a specific command try 'help <cmd>'.
 
     def help_group_delete(self):
         print 'group_delete: Delete a system group'
-        print 'usage: group_create NAME ...'
+        print 'usage: group_delete NAME ...'
 
     def complete_group_delete(self, text, line, begidx, endidx):
         return self.tab_completer(self.do_group_list('', True), text)
@@ -2107,6 +2345,28 @@ For help for a specific command try 'help <cmd>'.
         else:
             if len(kickstarts):
                 print '\n'.join(sorted(kickstarts))
+
+####################
+
+    def help_kickstart_delete(self):
+        print 'kickstart_delete: Delete a Kickstart profile'
+        print 'usage: kickstart_delete PROFILE'
+
+    def complete_kickstart_delete(self, text, line, begidx, endidx):
+        if len(line.split(' ')) <= 2:
+            return self.tab_completer(self.do_kickstart_list('', True), text)
+
+    def do_kickstart_delete(self, args):
+        args = self.parse_arguments(args)
+
+        if len(args) != 1:
+            self.help_kickstart_delete()
+            return
+
+        label = args[0]
+
+        if self.user_confirm('Delete this profile [y/N]:'):
+            self.client.kickstart.deleteProfile(self.session, label)
 
 ####################
 
@@ -2318,6 +2578,28 @@ For help for a specific command try 'help <cmd>'.
         #                                                    self.server)
 
         print kickstart
+
+####################
+
+    def help_kickstart_rename(self):
+        print 'kickstart_rename: Rename a Kickstart profile'
+        print 'usage: kickstart_rename OLDNAME NEWNAME'
+
+    def complete_kickstart_rename(self, text, line, begidx, endidx):
+        if len(line.split(' ')) <= 2:
+            return self.tab_completer(self.do_kickstart_list('', True), text)
+
+    def do_kickstart_rename(self, args):
+        args = self.parse_arguments(args)
+
+        if len(args) != 2:
+            self.help_kickstart_rename()
+            return
+
+        oldname = args[0]
+        newname = args[1]
+
+        self.client.kickstart.renameProfile(self.session, oldname, newname)
 
 ####################
 
