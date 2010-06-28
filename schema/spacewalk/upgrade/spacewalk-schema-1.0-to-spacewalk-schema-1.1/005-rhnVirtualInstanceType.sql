1d3f87532d1f02059221c6af3f526c07528cb4ba
Partha Aji <paji@redhat.com>
2010-06-28 12:19:11 -0400

    Consolidate virtual instance type changes to 1 file

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/005-rhnVirtualInstanceType.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/005-rhnVirtualInstanceType.sql
@@ -1,2 +1,30 @@
+--
+-- Copyright (c) 2010 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+-- 
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation. 
+--
+--
+--
+--
 
 INSERT INTO rhnVirtualInstanceType (ID, NAME, LABEL) values (rhn_vit_id_seq.nextval, 'KVM/QEMU', 'qemu');
+
+insert into rhnVirtualInstanceType (id, name, label) 
+    values (rhn_vit_id_seq.nextval, 'VMware', 'vmware');
+
+insert into rhnVirtualInstanceType (id, name, label)
+    values (rhn_vit_id_seq.nextval, 'Hyper-V', 'hyperv');
+
+insert into rhnVirtualInstanceType (id, name, label)
+    values (rhn_vit_id_seq.nextval, 'Virtage', 'virtage');
+
+
