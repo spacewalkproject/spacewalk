3715e8f4a7f118ff67009093f22391b73de0b6e0
Tomas Lestach <tlestach@redhat.com>
2010-07-02 13:41:20 +0200

    migrate CleanCurrentAlerts

--- schema/spacewalk/common/data/rhnTaskoTemplate.sql
+++ schema/spacewalk/common/data/rhnTaskoTemplate.sql
@@ -55,4 +55,12 @@ INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
                         (SELECT id FROM rhnTaskoTask WHERE name='compare-config-files'),
                         0,
                         '');
+
+INSERT INTO rhnTaskoTemplate (id, bunch_id, task_id, ordering, start_if)
+             VALUES (rhn_tasko_template_id_seq.nextval,
+                        (SELECT id FROM rhnTaskoBunch WHERE name='clean-alerts-bunch'),
+                        (SELECT id FROM rhnTaskoTask WHERE name='clean-current-alerts'),
+                        0,
+                        '');
+
 commit;
