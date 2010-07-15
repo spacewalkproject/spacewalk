spacewalkproject/spacewalk

commit 48df10b3ac4804976efb8689322b202662370816

define new parameter dry_run for all actions
dry_run = 1 has following meaning: return all data for this actions, but do not write anything new to db (like subscribe to channels or trigger new actions during kickstarts).
default is dry_run = 0 i.e. normal run

backend/server/handlers/xmlrpc/queue.py
@@ -70,7 +70,7 @@ class Queue(rhnHandler):
         # all good
         return {'id': actionId, 'version': 1, 'action': xml}
 
-    def __getV2(self, action):
+    def __getV2(self, action, dry_run=0):
         """ Fetches queued actions for the clients version 2+. """
         log_debug(3, self.server_id)
         # Get the root dir of this install
@@ -86,7 +86,7 @@ class Queue(rhnHandler):
             raise EmptyAction("Could not get a valid method for %s" % (
                 action['method'],))
         # Call the method
-        result = method(self.server_id, action['id'])
+        result = method(self.server_id, action['id'], dry_run)
         if result is None:
             # None are mapped to the empty list
             result = ()
@@ -186,11 +186,32 @@ class Queue(rhnHandler):
 
             self._invalidate_child_actions(action_id)
 
+    _query_queue_future = rhnSQL.Statement("""
+                    select sa.action_id id, a.version,
+                           sa.remaining_tries, at.label method,
+                           at.unlocked_only,
+                           a.prerequisite
+                      from rhnServerAction sa,
+                           rhnAction a,
+                           rhnActionType at
+                     where sa.server_id = :server_id
+                       and sa.action_id = a.id
+                       and a.action_type = at.id
+                       and sa.status in (0, 1) -- Queued or picked up
+                       and a.earliest_action <= sysdate + (:time_window/24)  -- Check earliest_action
+                      order by a.earliest_action, a.prerequisite nulls first, a.id
+    """)
+
     def get_future_actions(self, system_id, time_window):
         """ return actions which are scheduled within next /time_window/ hours """
         self.auth_system(system_id)
         log_debug(3, "Checking for future actions within %d hours" % time_window)
-        ## TODO
+        h = rhnSQL.prepare(self._query_queue_future)
+        h.execute(server_id=self.server_id, time_window=time_window)
+        result = []
+        while action = h.fetchone_dict():
+            result.append(self.__getV2(action, dry_run=1))
+        return result
 
     _query_queue_get = rhnSQL.Statement("""
                     select sa.action_id id, a.version, 

backend/server/action/activation.py
@@ -40,7 +40,7 @@ _query_copy_revs_from_shadow_action = rhnSQL.Statement("""
        and server_id = :server_id
 """)
 
-def schedule_deploy(server_id, action_id):
+def schedule_deploy(server_id, action_id, dry_run=0):
     log_debug(2, server_id, action_id)
     s = rhnServer.search(server_id)
 
@@ -57,9 +57,12 @@ def schedule_deploy(server_id, action_id):
         prerequisite=new_action_id,
         )
 
-    h = rhnSQL.prepare(_query_copy_revs_from_shadow_action)
-    h.execute(action_id=action_id, new_action_id=new_action_id_2, 
-        server_id=server_id)
+    if (!dry_run):
+        h = rhnSQL.prepare(_query_copy_revs_from_shadow_action)
+        h.execute(action_id=action_id, new_action_id=new_action_id_2,
+            server_id=server_id)
+    else:
+        log_debug(4, "dry run requested")
 
     log_debug(4, "scheduled config deploy for activation key")
 
@@ -67,7 +70,7 @@ def schedule_deploy(server_id, action_id):
 
 
 # XXX this duplicates rhnAction.schedule_server_packages_update. fix that.
-def schedule_pkg_install(server_id, action_id):
+def schedule_pkg_install(server_id, action_id, dry_run=0):
     s = rhnServer.search(server_id)
 
     new_action_id = rhnAction.schedule_server_action(
@@ -78,8 +81,11 @@ def schedule_pkg_install(server_id, action_id):
         org_id=s.server['org_id'],
         )
 
-    h = rhnSQL.prepare(_query_copy_pkgs_from_shadow_action)
-    h.execute(action_id=action_id, new_action_id=new_action_id)
+    if (!dry_run):
+        h = rhnSQL.prepare(_query_copy_pkgs_from_shadow_action)
+        h.execute(action_id=action_id, new_action_id=new_action_id)
+    else:
+        log_debug(4, "dry run requested")
 
     log_debug(4, "scheduled pkg install for activation key")
 

backend/server/action/configfiles.py
@@ -49,7 +49,7 @@ _query_mtime_upload_paths = rhnSQL.Statement("""
      where action_id = :action_id
 """)
 
-def mtime_upload(server_id, action_id):
+def mtime_upload(server_id, action_id, dry_run=0):
     log_debug(3)
 
     data = {}
@@ -90,7 +90,7 @@ def mtime_upload(server_id, action_id):
     return action_id, data
 
 
-def upload(server_id, action_id):
+def upload(server_id, action_id, dry_run=0):
     log_debug(3)
     h = rhnSQL.prepare(_query_upload_files)
     h.execute(action_id=action_id, server_id=server_id)
@@ -98,15 +98,15 @@ def upload(server_id, action_id):
 
     return action_id, files
 
-def deploy(server_id, action_id):
+def deploy(server_id, action_id, dry_run=0):
     log_debug(3)
     return _get_files(server_id, action_id)
 
-def verify(server_id, action_id):
+def verify(server_id, action_id, dry_run=0):
     log_debug(3)
     return _get_files(server_id, action_id)
 
-def diff(server_id, action_id):
+def diff(server_id, action_id, dry_run=0):
     log_debug(3)
     return _get_files(server_id, action_id)
 

backend/server/action/errata.py
@@ -22,7 +22,7 @@ from server.rhnLib import InvalidAction
 __rhnexport__ = ['update']
 
 # returns a list of errata scheduled for this action
-def update(serverId, actionId):
+def update(serverId, actionId, dry_run=0):
     log_debug(3)
     statement = """
         select errata_id

backend/server/action/hardware.py
@@ -18,6 +18,6 @@ from common import log_debug
 
 __rhnexport__ = ['refresh_list']
 
-def refresh_list(serverId, actionId):
+def refresh_list(serverId, actionId, dry_run=0):
     log_debug(3)
     return None

backend/server/action/kickstart.py
@@ -40,7 +40,7 @@ _query_file_list_initiate= rhnSQL.Statement("""
        and ak.action_id = :action_id
 """)
 
-def initiate(server_id, action_id):
+def initiate(server_id, action_id, dry_run=0):
     log_debug(3)
     h = rhnSQL.prepare(_query_initiate)
     h.execute(action_id=action_id)
@@ -64,8 +64,11 @@ def initiate(server_id, action_id):
     
     return (kickstart_host, boot_image, append_string, static_device, system_record, files)
 
-def schedule_sync(server_id, action_id):
+def schedule_sync(server_id, action_id, dry_run=0):
     log_debug(3, server_id, action_id)
+    if dry_run:
+        raise ShadowAction("dry run requested - skipping")
+
     kickstart_session_id = server_kickstart.get_kickstart_session_id(server_id, 
         action_id)
     

backend/server/action/kickstart_guest.py
@@ -39,7 +39,7 @@ _query_initiate_guest = rhnSQL.Statement("""
        and ksdef.virtualization_type = kvt.id       
 """)
 
-def schedule_virt_guest_pkg_install(server_id, action_id):
+def schedule_virt_guest_pkg_install(server_id, action_id, dry_run=0):
     """
         ShadowAction that schedules a package installation action for the
         rhn-virtualization-guest package.
@@ -60,7 +60,10 @@ def schedule_virt_guest_pkg_install(server_id, action_id):
 
     try:
         install_scheduler = PackageInstallScheduler(server_id, action_id, rhn_v12n_package)
-        install_scheduler.schedule_package_install()
+        if (!dry_run):
+            install_scheduler.schedule_package_install()
+        else:
+            log_debug(4, "dry run requested")
     except NoActionInfo, nai:
         raise InvalidAction(str(nai))
     except PackageNotFound, pnf:
@@ -71,7 +74,7 @@ def schedule_virt_guest_pkg_install(server_id, action_id):
     log_debug(3, "Completed scheduling install of rhn-virtualization-guest!")
     raise ShadowAction("Scheduled installation of RHN Virtualization Guest packages.")
 
-def initiate(server_id, action_id):
+def initiate(server_id, action_id, dry_run=0):
     log_debug(3)
     h = rhnSQL.prepare(_query_initiate_guest)
     h.execute(action_id=action_id)
@@ -99,6 +102,10 @@ def initiate(server_id, action_id):
     return (kickstart_host, cobbler_system_name, virt_type, ks_session_id, name,
                 mem_kb, vcpus, disk_gb, virt_bridge, disk_path, append_string)
 
-def add_tools_channel(server_id, action_id):
-    subscribe_to_tools_channel(server_id)
+def add_tools_channel(server_id, action_id, dry_run=0):
+    log_debug(3)
+    if (!dry_run):
+        subscribe_to_tools_channel(server_id)
+    else:
+        log_debug(4, "dry run requested")
     raise ShadowAction("Subscribed guest to tools channel.")

backend/server/action/kickstart_host.py
@@ -25,11 +25,14 @@ from server.rhnChannel import subscribe_to_tools_channel
 
 __rhnexport__ = ['schedule_virt_host_pkg_install', 'add_tools_channel']
 
-def add_tools_channel(server_id, action_id):
-    subscribe_to_tools_channel(server_id)
+def add_tools_channel(server_id, action_id, dry_run=0):
+    if (!dry_run):
+        subscribe_to_tools_channel(server_id)
+    else:
+        log_debug(4, "dry run requested")
     raise ShadowAction("Subscribed server to tools channel.")
 
-def schedule_virt_host_pkg_install(server_id, action_id):
+def schedule_virt_host_pkg_install(server_id, action_id, dry_run=0):
     """
         ShadowAction that schedules a package installation action for the
         rhn-virtualization-host and osad packages.
@@ -57,10 +60,12 @@ def schedule_virt_host_pkg_install(server_id, action_id):
 
     try:
         rhn_v12n_install_scheduler = PackageInstallScheduler(server_id, action_id, rhn_v12n_package)
-        rhn_v12n_install_scheduler.schedule_package_install()
-
         messaging_package = PackageInstallScheduler(server_id, action_id, messaging_package)
-        messaging_package.schedule_package_install()
+        if (!dry_run):
+            rhn_v12n_install_scheduler.schedule_package_install()
+            messaging_package.schedule_package_install()
+        else:
+            log_debug(4, "dry run requested")
     except NoActionInfo, nai:
         raise InvalidAction(str(nai))
     except PackageNotFound, pnf:

commit 8a0f3957b72835583c7f2672d4643d5fe199b9df

in python, assignment is not an expression, and thus has no value
Problem introduced in 48df10b3ac4804976efb8689322b202662370816

backend/server/handlers/xmlrpc/queue.py
@@ -209,7 +209,12 @@ class Queue(rhnHandler):
         h = rhnSQL.prepare(self._query_queue_future)
         h.execute(server_id=self.server_id, time_window=time_window)
         result = []
-        while action = h.fetchone_dict():
+        while 1:
+            action = h.fetchone_dict() or []
+
+            if not action:
+                break
+
             result.append(self.__getV2(action, dry_run=1))
         return result
 