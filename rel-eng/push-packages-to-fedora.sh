41c11760605aa7657edd1fce65c138c435f06a28
Miroslav Suchý <msuchy@redhat.com>
2010-08-13 18:48:13 +0200

    update push-packages-to-fedora.sh with new packages and to use fedpkg

--- rel-eng/push-packages-to-fedora.sh
+++ rel-eng/push-packages-to-fedora.sh
@@ -7,11 +7,11 @@ TOP_DIR_FEDORA=$1
 # top dir of spacewalk git checkout
 TOP_DIR_GIT=$2
 
-PACKAGE_LIST="rhnlib rhn-client-tools rhnsd yum-rhn-plugin rhncfg spacewalk-koan spacewalk-certs-tools rhnpush nocpulse-common perl-Satcon perl-NOCpulse-CLAC perl-NOCpulse-Debug perl-NOCpulse-Gritch perl-NOCpulse-Object perl-NOCpulse-SetID perl-NOCpulse-Utils spacewalk-proxy-html spacewalk-proxy-docs"
+PACKAGE_LIST="rhnlib rhn-client-tools rhnsd yum-rhn-plugin rhncfg spacewalk-koan spacewalk-certs-tools rhnpush nocpulse-common perl-Satcon perl-NOCpulse-CLAC perl-NOCpulse-Debug perl-NOCpulse-Gritch perl-NOCpulse-Object perl-NOCpulse-SetID perl-NOCpulse-Utils spacewalk-proxy-html spacewalk-proxy-docs spacecmd"
 
 
 if [ -z "$TOP_DIR_FEDORA" -o ! -d "$TOP_DIR_FEDORA" ]; then
-    echo "Error: Fedora CVS directory $TOP_DIR_FEDORA do not exist"
+    echo "Error: Fedora git directory $TOP_DIR_FEDORA do not exist"
     exit 1
 fi
 if [ -z "$TOP_DIR_GIT" -o ! -d "$TOP_DIR_GIT" ]; then
@@ -25,7 +25,7 @@ for package in $PACKAGE_LIST; do
 		cd "$TOP_DIR_GIT"
 		cd `cat rel-eng/packages/$package | cut -f2 -d" "`
 		SRC_RPM=`tito build --srpm | tail -n1 | cut -f2 -d" "`
-		BASENAME=`basename $SRC_RPM`
+		BASENAME=`basename $SRC_RPM .src.rpm`
 		NVR_GIT=`rpm -qp --queryformat '%{name}-%{version}' "$SRC_RPM"`
 		cd "$TOP_DIR_FEDORA"
 		if [ ! -d "$package" ]; then
@@ -35,22 +35,25 @@ for package in $PACKAGE_LIST; do
 		cd "$package"
 
 		echo "Updating $TOP_DIR_FEDORA/$package"
-		cvs -fq update -d -P >/dev/null
+		fedpkg switch-branch master || ( echo 'Error: could not switch to master' && exit 1 )
+        git pull || ( echo 'Error: could not update git' && exit 1 )
 
-		# find version of package in fedora CVS and strip release from it
-		cd devel
-		NVR_CVS=`make verrel | perl -an -F- -e 'pop @F; print join("-", @F)'`
-		cd ..
+		# find version of package in fedora git and strip release from it
+		NVR_DISTGIT=`fedpkg verrel | perl -an -F- -e 'pop @F; print join("-", @F)'`
 
-		if [ "$NVR_CVS" != "$NVR_GIT" ]; then
-			echo "Version in CVS is: $NVR_CVS"
+		if [ "$NVR_DISTGIT" != "$NVR_GIT" ]; then
+			echo "Version in dist-git is: $NVR_DISTGIT"
 			echo "Importing version: $NVR_GIT"
-			./common/cvs-import.sh -m "Rebase to $BASENAME in rawhide." $SRC_RPM || exit $?
-			cvs -fq update -d -P >/dev/null
-			cd devel
-			make build
+			fedpkg import $SRC_RPM || exit $?
+            fedpkg commit -m "Rebase to $BASENAME in rawhide."
+			git tag $BASENAME
+			echo "Review your changes and hit ENTER to continue or Ctrl+C to stop"
+            read
+			git push || ( echo 'Error: could not push changes' && exit 1 )
+            git push --tags
+			fedpkg build --nowait
 		else
-			echo "$NVR_CVS already imported - skipping."
+			echo "$NVR_DISTGIT already imported - skipping."
 		fi
 done
 
