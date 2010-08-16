e00b0da3412ac942ada61d5fe3efff9d720d3632
Michael Mraka <michael.mraka@redhat.com>
2010-08-16 14:27:39 +0200

    fixed 1.1 schema upgrade
    SQL> CREATE UNIQUE INDEX rhn_sp_snep_uq
        ON rhnServerPackage (server_id, name_id, evr_id, package_arch_id)
        TABLESPACE USERS
        NOLOGGING;
      2    3    4      ON rhnServerPackage (server_id, name_id, evr_id,
    package_arch_id)
           *
    ERROR at line 2:
    ORA-01452: cannot CREATE UNIQUE INDEX; duplicate keys found

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/019-rhnServerPackage-index.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/019-rhnServerPackage-index.sql
@@ -4,7 +4,7 @@ DELETE FROM rhnServerPackage A
                           name_id,
                           evr_id,
                           package_arch_id,
-                          min(created) min_created
+                          min(rowid) min_rowid
                      FROM rhnServerPackage
                     GROUP BY server_id, name_id, evr_id, package_arch_id
                    HAVING count(*) > 1) N
@@ -12,7 +12,7 @@ DELETE FROM rhnServerPackage A
               AND a.name_id = n.name_id
               AND a.evr_id = n.evr_id
               AND a.package_arch_id = n.package_arch_id
-              AND a.created > n.min_created);
+              AND a.rowid > n.min_rowid);
 
 DROP INDEX rhn_sp_snep_idx;
 CREATE UNIQUE INDEX rhn_sp_snep_uq
