8517d9c0ef2ffe2dcd5f716771ce024fc389d4ca
Tomas Lestach <tlestach@redhat.com>
2010-08-17 12:16:18 +0200

    upgrades for taskomatic

---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/014-rhnTaskoRun.sql
@@ -0,0 +1,40 @@
+--
+-- Copyright (c) 2010 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
+
+
+CREATE TABLE rhnTaskoRun
+(
+    id              NUMBER NOT NULL
+                        CONSTRAINT rhn_tasko_run_id_pk PRIMARY KEY,
+    template_id     NUMBER NOT NULL
+                        CONSTRAINT rhn_tasko_run_template_fk
+                        REFERENCES rhnTaskoTemplate (id),
+    schedule_id     NUMBER NOT NULL
+                        CONSTRAINT rhn_tasko_run_schedule_fk
+                        REFERENCES rhnTaskoSchedule (id),
+    org_id          NUMBER,
+    start_time      DATE,
+    end_time        DATE,
+    std_output_path VARCHAR2(100),
+    std_error_path  VARCHAR2(100),
+    status          VARCHAR2(10),
+    created         DATE
+                        DEFAULT (sysdate) NOT NULL,
+    modified        DATE
+                        DEFAULT (sysdate) NOT NULL
+)
+;
+
+CREATE SEQUENCE rhn_tasko_run_id_seq;
---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/020-rhnTaskoBunch_data.sql
@@ -0,0 +1,71 @@
+--
+-- Copyright (c) 2010 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+--
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation.
+--
+
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'daily-status-bunch', 'Sends daily report', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'sat-sync-bunch', 'Runs satellite-sync\nParameters: list parameter lists channels\nchannel parameter specifies channel to be synced\nwithout parameter runs satellite-sync without parameters', 'Y');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'clear-taskologs-bunch', 'Clears taskomatic run log history', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'cobbler-sync-bunch', 'Applies any cobbler configuration changes', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'compare-configs-bunch', 'Schedules a comparison of config files on all systems', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'clean-alerts-bunch', 'Clears current monitoring alerts', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'sync-probe-bunch', 'Calls the synch probe state proc', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'session-cleanup-bunch', 'Deletes expired rows from the PXTSessions table to keep it from growing too large', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'cert-check-bunch', 'Satellite certificate check', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'sandbox-cleanup-bunch', 'Clean up sandbox', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'repo-sync-bunch', 'Used for syncing repos to a channel', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'package-cleanup-bunch', 'Cleans up orphaned packages', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'kickstartfile-sync-bunch', 'Syncs kickstart profiles that were generated using the wizard', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'kickstart-cleanup-bunch', 'Cleans up stale Kickstarts', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'errata-queue-bunch', 'Processes errata', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'errata-cache-bunch', 'Performs errata cache recalc for a given server or channel', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'channel-repodata-bunch', 'Generates channel repodata', '');
+
+INSERT INTO rhnTaskoBunch (id, name, description, org_bunch)
+             VALUES (rhn_tasko_bunch_id_seq.nextval, 'satcert-check-bunch', 'Checks whether satellite certificate has not expired', '');
+
+commit;
