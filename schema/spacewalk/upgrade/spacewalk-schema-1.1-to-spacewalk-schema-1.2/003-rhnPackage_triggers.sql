365b4983a43268a8515058eb3493d6c72c5e9a51
Justin Sherrill <jsherril@redhat.com>
2010-08-17 12:12:47 -0400

    619337 - adding trigger to update last_modified date on channel table if the checksum of a package changes

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/003-rhnPackage_triggers.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/003-rhnPackage_triggers.sql
@@ -8,10 +8,10 @@
 -- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
--- 
+--
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation. 
+-- in this software or its documentation.
 --
 
 create or replace trigger
@@ -29,7 +29,7 @@ begin
 	-- might want to use this to keep webdev in sync.
 	if :new.last_modified = :old.last_modified then
 		:new.last_modified := sysdate;
-	end if;       
+	end if;
 	:new.modified := sysdate;
 
         -- bz 619337 if we are updating the checksum, we need to
