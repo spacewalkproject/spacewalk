3ee019bf93bdd80315ab3f165f729272e284cde7
Tomas Lestach <tlestach@redhat.com>
2010-08-26 15:05:16 +0200

    fixed definition of rhnChannelNewestPackageView
    when fixing 591291 (commit e1569a9a6a3e27c99f1125b6f5c12c47836947fd) we found out, rhnChannelNewestPackageView doesn't take architecture into account

--- schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
+++ schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
@@ -7,10 +7,10 @@
 -- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
 -- along with this software; if not, see
 -- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
--- 
+--
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
--- in this software or its documentation. 
+-- in this software or its documentation.
 --
 --
 --
@@ -22,23 +22,25 @@ rhnChannelNewestPackageView
 as
 SELECT m.channel_id          as channel_id,
        p.name_id             as name_id,
-       pe.id                 as evr_id,
-       p.package_arch_id     as package_arch_id,
+       p.evr_id              as evr_id,
+       m.package_arch_id     as package_arch_id,
        p.id                  as package_id
 FROM
     (select max(pe.evr) AS max_evr,
          cp.channel_id,
-         p.name_id
+         p.name_id,
+         p.package_arch_id
         from rhnPackageEVR       pe,
          rhnPackage          p,
          rhnChannelPackage   cp
         where p.evr_id = pe.id
          and cp.package_id = p.id
-        group by cp.channel_id, p.name_id) m,
+        group by cp.channel_id, p.name_id, p.package_arch_id) m,
     rhnPackageEVR       pe,
     rhnPackage          p
 WHERE m.max_evr = pe.evr
 AND m.name_id = p.name_id
+AND m.package_arch_id = p.package_arch_id
 AND p.evr_id = pe.id
 ;
 
--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/051-rhnChannelNewestPackageView.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/051-rhnChannelNewestPackageView.sql
@@ -3,22 +3,24 @@ rhnChannelNewestPackageView
 as
 SELECT m.channel_id          as channel_id,
        p.name_id             as name_id,
-       pe.id                 as evr_id,
-       p.package_arch_id     as package_arch_id,
+       p.evr_id              as evr_id,
+       m.package_arch_id     as package_arch_id,
        p.id                  as package_id
 FROM
     (select max(pe.evr) AS max_evr,
          cp.channel_id,
-         p.name_id
+         p.name_id,
+         p.package_arch_id
         from rhnPackageEVR       pe,
          rhnPackage          p,
          rhnChannelPackage   cp
         where p.evr_id = pe.id
          and cp.package_id = p.id
-        group by cp.channel_id, p.name_id) m,
+        group by cp.channel_id, p.name_id, p.package_arch_id) m,
     rhnPackageEVR       pe,
     rhnPackage          p
 WHERE m.max_evr = pe.evr
 AND m.name_id = p.name_id
+AND m.package_arch_id = p.package_arch_id
 AND p.evr_id = pe.id
 ;
