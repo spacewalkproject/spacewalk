e1569a9a6a3e27c99f1125b6f5c12c47836947fd
Tomas Lestach <tlestach@redhat.com>
2010-08-26 08:39:12 +0200

    591291 - rewrite rhnChannelNewestPackageView
    stored procedure refresh_newest_package gets 32 times faster for rhel-x86_64-server-5

--- schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
+++ schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
@@ -20,26 +20,25 @@
 create or replace view
 rhnChannelNewestPackageView
 as
-select  cp.channel_id		as channel_id,
-		p.name_id			as name_id,
-		p.evr_id			as evr_id,
-		p.package_arch_id	as package_arch_id,
-		p.id				as package_id
-from	rhnPackageEVR		pe,
-		rhnPackage			p,
-		rhnChannelPackage	cp
-where	cp.package_id = p.id
-		and p.evr_id = pe.id
-		and pe.evr = (
-			select	max(sq_pe.evr)
-			from	rhnChannelPackage sq_cp,
-					rhnPackage sq_p,
-					rhnPackageEVR sq_pe
-			where	1=1
-				and sq_cp.channel_id = cp.channel_id
-				and sq_cp.package_id = sq_p.id
-				and sq_p.name_id = p.name_id
-				and sq_pe.id = sq_p.evr_id
-		)
+SELECT m.channel_id          as channel_id,
+       p.name_id             as name_id,
+       pe.id                 as evr_id,
+       p.package_arch_id     as package_arch_id,
+       p.id                  as package_id
+FROM
+    (select max(pe.evr) AS max_evr,
+         cp.channel_id,
+         p.name_id
+        from rhnPackageEVR       pe,
+         rhnPackage          p,
+         rhnChannelPackage   cp
+        where p.evr_id = pe.id
+         and cp.package_id = p.id
+        group by cp.channel_id, p.name_id) m,
+    rhnPackageEVR       pe,
+    rhnPackage          p
+WHERE m.max_evr = pe.evr
+AND m.name_id = p.name_id
+AND p.evr_id = pe.id
 ;
 
