8c6d61e1c5889a20cabe2c3a7637300eef9188ac
Jan Pazdziora <jpazdziora@redhat.com>
2010-09-02 16:53:19 +0200

    Reapply rhn_cache and rhn_org package and rhn_user package and package body, to make for clean schema upgrades from 0.2.

---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/502-rhn_org.sql
@@ -0,0 +1,55 @@
+--
+-- Copyright (c) 2008 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+-- 
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation. 
+--
+--
+--
+--
+
+CREATE OR REPLACE
+PACKAGE rhn_org
+IS
+	version varchar2(100) := '';
+
+    CURSOR server_group_by_label(org_id_in NUMBER, group_label_in VARCHAR2) IS
+    	   SELECT SG.*
+	     FROM rhnServerGroupType SGT,
+	     	  rhnServerGroup SG
+	    WHERE SG.group_type = SGT.id
+	      AND SGT.label = group_label_in
+	      AND SG.org_id = org_id_in;
+	    
+    FUNCTION find_server_group_by_type(org_id_in NUMBER, 
+                                       group_label_in VARCHAR2) 
+    RETURN NUMBER;
+
+    procedure delete_org(org_id_in in number);
+    procedure delete_user(user_id_in in number, deleting_org in number := 0);
+
+END rhn_org;
+/
+SHOW ERRORS
+
+--
+-- Revision 1.7  2004/07/13 22:46:04  pjones
+-- bugzilla: 125938 -- nothing uses update_errata_cache() any more, remove it
+--
+-- Revision 1.6  2004/02/10 15:05:01  pjones
+-- bugzilla: none -- add version tag here
+--
+-- Revision 1.5  2003/02/18 16:35:45  pjones
+-- delete_user
+--
+-- Revision 1.4  2002/05/10 22:08:23  pjones
+-- id/log
+--
---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/503-rhn_user.sql
@@ -0,0 +1,64 @@
+--
+-- Copyright (c) 2008 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+-- 
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation. 
+--
+--
+--
+--
+
+create or replace
+package rhn_user
+is
+	version varchar2(100) := '';
+
+    function check_role(user_id_in in number, role_in in varchar2) return number;
+    PRAGMA RESTRICT_REFERENCES(check_role, WNDS, RNPS, WNPS);
+
+    function check_role_implied(user_id_in in number, role_in in varchar2) return number;
+    PRAGMA RESTRICT_REFERENCES(check_role_implied, WNDS, RNPS, WNPS);
+
+    function get_org_id(user_id_in in number) return number;
+    PRAGMA RESTRICT_REFERENCES(get_org_id, WNDS, RNPS, WNPS);
+    
+	function find_mailable_address(user_id_in in number) return varchar2;
+
+	procedure add_servergroup_perm(
+		user_id_in in number,
+		server_group_id_in in number
+	);
+
+	procedure remove_servergroup_perm(
+		user_id_in in number,
+		server_group_id_in in number
+	);
+
+	procedure add_to_usergroup(
+		user_id_in in number,
+		user_group_id_in in number
+	);
+
+	procedure add_users_to_usergroups(
+		user_id_in in number
+	);
+
+	procedure remove_from_usergroup(
+		user_id_in in number,
+		user_group_id_in in number
+	);
+
+	procedure remove_users_from_servergroups(
+		user_id_in in number
+	);
+end rhn_user;
+/
+SHOW ERRORS
