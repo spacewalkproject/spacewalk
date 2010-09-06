52384ab2b0a886ec2415641a15223e5c9af004a9
Michael Mraka <michael.mraka@redhat.com>
2010-09-06 09:48:38 +0200

    removed outdated log comments

--- schema/spacewalk/oracle/data/rhn_schedule_days.sql
+++ schema/spacewalk/oracle/data/rhn_schedule_days.sql
@@ -72,20 +72,3 @@ end_2,start_3,end_3,start_4,end_4,last_update_user,last_update_date)
     null,null,null,null,null,null,'system',null);
 commit;
 
---
---Revision 1.5  2004/06/17 20:48:59  kja
---bugzilla 124970 -- _data is in for 350.
---
---Revision 1.4  2004/05/29 21:51:49  pjones
---bugzilla: none -- _data is not for 340, so says kja.
---
---Revision 1.3  2004/05/07 23:30:22  kja
---Shortened constraint/other names as needed.  Fixed minor syntax errors.
---
---Revision 1.2  2004/05/04 20:03:38  kja
---Added commits.
---
---Revision 1.1  2004/04/22 19:05:45  kja
---Added the 24 x 7 schedule data.  Corrected logic for skipping sequence numbers
---in rhn_notification_formats_data.sql and rhn_strategies_data.sql.
---
