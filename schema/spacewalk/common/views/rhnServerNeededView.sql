6e717160dcddef1225f18af6b98d96b9a625888d
Michael Mraka <michael.mraka@redhat.com>
2010-09-06 11:23:55 +0200

    585965 - rhnServerNeededView rewritten from scratch
    it utilize rhnChannelNewestPackage cache table and new table for package upgrade
    compatibility

--- schema/spacewalk/common/views/rhnServerNeededView.sql
+++ schema/spacewalk/common/views/rhnServerNeededView.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2008 Red Hat, Inc.
+-- Copyright (c) 2010 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -14,9 +14,10 @@
 --
 
 
--- A view that displays an uncached version of rhnServerNeededCache
+-- A view that displays packages which can be updated on selected client.
+-- This is an uncached version of rhnServerNeededCache.
 
-CREATE OR REPLACE VIEW
+create or replace view
 rhnServerNeededView
 (
     org_id,
@@ -26,56 +27,60 @@ rhnServerNeededView
     package_name_id,
     channel_id
 )
-AS
-SELECT DISTINCT S.org_id,
-     S.id as server_id,
-     ep.errata_id as errata_id,
-     P.id as package_id,
-     P.name_id as package_name_id,
-     CP.channel_id as channel_id
-FROM
-    rhnPackage P
-    inner join rhnPackageEVR P_EVR on P_EVR.id = P.evr_id
-    inner join rhnPackageEVR SP_EVR on SP_EVR.evr < P_EVR.evr  
-    inner join rhnServerPackage SP on SP.name_id = P.name_id 
-               and SP.evr_id = SP_EVR.id
-               AND SP.evr_id != P.evr_id
-    inner join rhnServer S on SP.server_id = S.id
-    inner join rhnServerPackageArchCompat SPAC on spac.server_arch_id = s.server_arch_id 
-               AND p.package_arch_id = spac.package_arch_id
-    inner join rhnServerChannel SC on SC.server_id = S.id 
-    inner join rhnChannelPackage CP on CP.package_id = P.id 
-               and SC.channel_id = CP.channel_id
-    left outer join rhnErrataPackage EP on EP.package_id = P.id
-                   AND EXISTS 
-                   (SELECT 1 from rhnChannelErrata CE where ce.channel_id = SC.channel_id
-                    AND CE.errata_id = EP.errata_id) 
-    where
-           --- If the channel has more than 1 package with the same NVRE but different arches
-           ---  Then we need to add an additional join condition (the server's package arch id) 
-           P.package_arch_id = COALESCE(
-                                   (select distinct 1
-                                       from rhnPackage P2 inner join rhnChannelPackage CP2
-                                            on P2.id = CP2.package_id
-                                         where CP2.channel_id = SC.channel_id and
-                                               P2.id = CP2.package_id and
-                                               P2.name_id = P.name_id
-                                          group by P2.evr_id having count(*) > 1),
-                                    COALESCE(SP.package_arch_id, P.package_arch_id),
-                                    P.package_arch_id
-                               )
-           AND
-           ---  If we can use arch to find the MAX EVR, use that 
-           ---  Otherwise just use whatever they have 
-           SP_EVR.evr =
-                  COALESCE(
-                     (SELECT MAX(PE.evr) FROM rhnServerPackage SP2, rhnPackageEvr PE
-                       WHERE PE.id = SP2.evr_id AND SP2.server_id = SP.server_id AND
-                         SP2.name_id = SP.name_id
-                         AND COALESCE(SP2.package_arch_id, P.package_arch_id) = P.package_arch_id
-                     ),
-                     (SELECT MAX(PE.evr) FROM rhnServerPackage SP2, rhnPackageEvr PE
-                       WHERE PE.id = SP2.evr_id AND SP2.server_id = SP.server_id AND
-                      SP2.name_id = SP.name_id)
-                    )
+as
+select s.org_id,
+       s.id as server_id,
+       pce.errata_id,
+       pkg.id as package_id,
+       neededpkg.name_id as package_name_id,
+       scp.min_channel_id as channel_id
+  from (select sc.server_id, np.name_id, np.package_arch_id, max(np.evr) max_evr
+          from (-- list of newest packages in channels with EVR
+                select np_np.*, np_pe.evr
+                  from rhnchannelnewestpackage np_np
+                  join rhnpackageEVR np_pe
+                    on np_pe.id = np_np.evr_id) np
+          join (-- list of packages on the server with EVR
+                select sp_sp.server_id, sp_sp.name_id, sp_sp.package_arch_id, max(sp_pe.evr) as max_evr
+                  from rhnserverpackage sp_sp
+                  join rhnpackageEVR sp_pe
+                    on sp_pe.id = sp_sp.evr_id
+                 group by sp_sp.server_id, sp_sp.name_id, sp_sp.package_arch_id) sp
+            on -- at first - we want only newer (=higher EVR) packages than there are on the server
+               sp.name_id = np.name_id and sp.max_evr < np.evr
+          join -- secondly - packages must be upgrade compatible
+               rhnpackageupgradearchcompat puac
+            on puac.package_arch_id = sp.package_arch_id and puac.package_upgrade_arch_id = np.package_arch_id
+          join -- thirdly - packages must be in channel where server is subscribed to
+               rhnserverchannel sc
+            on sc.server_id = sp.server_id and sc.channel_id = np.channel_id
+        group by sc.server_id, np.name_id, np.package_arch_id
+        ) neededpkg
+  join -- lookup org_id by server
+       rhnServer s
+    on neededpkg.server_id = s.id
+  join (--lookup package_id by max_evr, name and package_arch
+        select p.*, p_evr.evr
+               --
+               , p_evr.release, p_evr.version, p_evr.epoch
+          from rhnPackage p
+          join rhnpackageevr p_evr
+            on p_evr.id = p.evr_id) pkg
+    on pkg.evr = neededpkg.max_evr
+   and pkg.name_id = neededpkg.name_id
+   and pkg.package_arch_id = neededpkg.package_arch_id
+  join (-- lookup channel_id - we want only one id eve if package is in more channels
+        -- so pick lowest one
+        select cp.package_id, csc.server_id, min(cp.channel_id) as min_channel_id
+          from rhnChannelPackage cp
+          join rhnServerChannel csc
+            on csc.channel_id = cp.channel_id
+         group by cp.package_id, csc.server_id) scp
+    on scp.package_id = pkg.id and scp.server_id = neededpkg.server_id
+  left join (-- lookup errata id (can be NULL)
+             select ep.package_id, ce.errata_id, ce.channel_id
+               from rhnErrataPackage ep
+               join rhnChannelErrata ce
+                 on ce.errata_id = ep.errata_id) pce
+    on pce.package_id = pkg.id and pce.channel_id = scp.min_channel_id
 ;
