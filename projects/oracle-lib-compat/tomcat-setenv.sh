3e5fd863b076ab611d0beb53a295b2d8e08f3ae0
Jan Pazdziora <jpazdziora@redhat.com>
2010-09-23 08:59:29 +0200

    623115 - file lookup using just the linker name (libldap.so) fails if openldap-devel is not installed.
    We shall find the soname path using the dynamic linker.

--- projects/oracle-lib-compat/tomcat-setenv.sh
+++ projects/oracle-lib-compat/tomcat-setenv.sh
@@ -1,10 +1,10 @@
 
 # To address bug 623115/627859, we need to override the ldap_* symbols
 # in libclntsh.so* upon tomcat5 startup with those from standard library.
-if [ -f /usr/lib64/libldap.so ] ; then
-	LD_PRELOAD=/usr/lib64/libldap.so
-else
-	LD_PRELOAD=/usr/lib/libldap.so
-fi
-export LD_PRELOAD
+for i in /lib64/security/pam_ldap.so /lib/security/pam_ldap.so ; do
+	if [ -f $i ] ; then
+		export LD_PRELOAD=`ldd $i | perl -lne '/^\s+libldap\S+\s+=>\s+(\S+)/ and print $1 and exit'`
+		break
+	fi
+done
 
