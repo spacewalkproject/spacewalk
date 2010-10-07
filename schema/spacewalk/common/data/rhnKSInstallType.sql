29233cb8d6b1d968a5eebbb240c5959de4a53d8f
Partha Aji <paji@redhat.com>
2010-10-07 17:04:35 -0400

    641145: Updated kickstart timezone and install information needed for Rhel 6 profiles

--- schema/spacewalk/common/data/rhnKSInstallType.sql
+++ schema/spacewalk/common/data/rhnKSInstallType.sql
@@ -14,6 +14,11 @@
 --
 insert into rhnKSInstallType (id, label, name)
         values (rhn_ksinstalltype_id_seq.nextval,
+                'rhel_6','Red Hat Enterprise Linux 6'
+        );
+
+insert into rhnKSInstallType (id, label, name)
+        values (rhn_ksinstalltype_id_seq.nextval,
                 'rhel_5','Red Hat Enterprise Linux 5'
         );
 
@@ -41,5 +46,4 @@ insert into rhnKSInstallType (id, label, name)
         values (rhn_ksinstalltype_id_seq.nextval,
                 'generic_rpm','Generic RPM'
         );
-
 commit;
