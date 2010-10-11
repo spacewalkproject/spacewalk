e81db82da535a81c7ff65c925bbeb436bf368824
Milan Zazrivec <mzazrivec@redhat.com>
2010-10-11 18:01:57 +0200

    571608 - delete duplicate kickstart_id, package_name_id pairs
    This is to avoid
    
    ORA-02299: cannot validate (RHNSAT.RHN_KSPACKAGE_NAME_UQ) - duplicate keys found
    
    error when creating RHN_KSPACKAGE_NAME_UQ during schema upgrade.

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/002-rhnKickstartPackage.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/002-rhnKickstartPackage.sql
@@ -1,3 +1,14 @@
+-- first, delete duplicate kickstart_id, package_name_id pairs if any
+
+delete
+    from rhnKickstartPackage
+   where rowid not in
+       (select min(rowid)
+          from rhnKickstartPackage
+      group by kickstart_id, package_name_id);
+
+commit;
+
 ALTER TABLE rhnKickstartPackage
     ADD CONSTRAINT rhn_kspackage_name_uq UNIQUE (kickstart_id, package_name_id)
     USING INDEX TABLESPACE [[4m_tbs]];
