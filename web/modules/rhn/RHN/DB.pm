spacewalkproject/spacewalk

commit 612f390ae06bfaa291f24a2d05ac26c3841bbf23

If host or port is not specified, we do not want to put those empty strings to the dbi:Pg: connect string.

web/modules/rhn/RHN/DB.pm
@@ -55,7 +55,14 @@ sub get_dbi_connection_string {
         return "dbi:Oracle:$dbname";
     }
     elsif ($backend eq "postgresql") {
-        return "DBI:Pg:dbname=$dbname;host=$host;port=$port";
+        my $q = "dbi:Pg:dbname=$dbname";
+        if (defined $host and $host ne '') {
+            $q .= ";host=$host";
+            if (defined $port and $port ne '') {
+                $q .= ";port=$port";
+            }
+        }
+	return $q;
     }
 }
 

commit c5443e50014b060bf66824bea0fc7664a2660355

Fix indentation -- use spaces.
Spaces are ugly but for the sake of consistency we will use them.
Problem was introduced by me in 612f390ae06bfaa291f24a2d05ac26c3841bbf23.

web/modules/rhn/RHN/DB.pm
@@ -62,7 +62,7 @@ sub get_dbi_connection_string {
                 $q .= ";port=$port";
             }
         }
-	return $q;
+        return $q;
     }
 }
 