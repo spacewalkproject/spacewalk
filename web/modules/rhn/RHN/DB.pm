spacewalkproject/spacewalk

commit fd42e1b6e0ff968b12a78566c7930b67d0125141

Move use DBD::Oracle to eval so that we do not get rpm dependencies populated.
In the future we shall add a test for driver being Oracle but I was
not able to find reproducer where encode_blob / RHN::DB::Type::BLOB
would be used, so this is only a rpm-fixing change.

web/modules/rhn/RHN/DB.pm
@@ -528,12 +528,12 @@ sub execute_h {
   $self->set_err("Odd number of params to execute_h") if @params % 2;
 
   while (my ($k, $v) = (splice @params, 0, 2, ())) {
-    use DBD::Oracle qw(:ora_types);
 
     my $attr = {};
     use Scalar::Util qw/blessed/;
 
     if (ref $v and blessed($v) and $v->isa("RHN::DB::Type::BLOB")) {
+      eval 'use DBD::Oracle qw(:ora_types)' or die $@;
       $attr->{ora_type} = ORA_BLOB;
       if (defined $v->{ora_field}) {
         $attr->{ora_field} = $v->{ora_field};

commit a22d8c0fdfa2f8903b49eaa4e758f0a3b5e94de1

Fix the ORA_BLOB issue which prevents spacewalk-schema from starting.
The bug was introduced by me in fd42e1b6e0ff968b12a78566c7930b67d0125141.

web/modules/rhn/RHN/DB.pm
@@ -540,8 +540,8 @@ sub execute_h {
     use Scalar::Util qw/blessed/;
 
     if (ref $v and blessed($v) and $v->isa("RHN::DB::Type::BLOB")) {
-      eval 'use DBD::Oracle qw(:ora_types)' or die $@;
-      $attr->{ora_type} = ORA_BLOB;
+      eval 'use DBD::Oracle ()' or die $@;
+      $attr->{ora_type} = DBD::Oracle::ORA_BLOB();
       if (defined $v->{ora_field}) {
         $attr->{ora_field} = $v->{ora_field};
       }