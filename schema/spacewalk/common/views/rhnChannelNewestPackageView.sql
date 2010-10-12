c6baad03714602863d7bcd6633a55022b45b315e
Milan Zazrivec <mzazrivec@redhat.com>
2010-10-12 17:01:44 +0200

    642287 - restrict rhnChannelNewestPackageView to packages in channels

--- schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
+++ schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
@@ -37,10 +37,13 @@ FROM
          and cp.package_id = p.id
         group by cp.channel_id, p.name_id, p.package_arch_id) m,
     rhnPackageEVR       pe,
-    rhnPackage          p
+    rhnPackage          p,
+    rhnChannelPackage   chp
 WHERE m.max_evr = pe.evr
 AND m.name_id = p.name_id
 AND m.package_arch_id = p.package_arch_id
 AND p.evr_id = pe.id
+AND chp.package_id = p.id
+AND chp.channel_id = m.channel_id
 ;
 
