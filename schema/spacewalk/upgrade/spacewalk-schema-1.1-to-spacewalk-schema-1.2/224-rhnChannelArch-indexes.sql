b54a79161a08e562e08ca38855a2490b1d32cacc
Michael Mraka <michael.mraka@redhat.com>
2010-10-13 15:49:37 +0200

    Revert "index can't be dropped before constraint"
    This reverts commit e640111863322121253625d08d99b1c3a75af210.

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/224-rhnChannelArch-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/224-rhnChannelArch-indexes.sql
@@ -1,6 +1,6 @@
+drop index rhn_carch_id_l_n_idx;
+
 alter table rhnChannelArch drop CONSTRAINT rhn_carch_label_uq;
 ALTER TABLE rhnChannelArch
     ADD CONSTRAINT rhn_carch_label_uq UNIQUE (label)
     USING INDEX TABLESPACE [[2m_tbs]];
-
-drop index rhn_carch_id_l_n_idx;
