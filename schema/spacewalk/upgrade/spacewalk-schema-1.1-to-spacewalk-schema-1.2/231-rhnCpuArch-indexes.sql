821e6c24dfd5ea1f9f3e32d43c2cdb79b9cf35c3
Michael Mraka <michael.mraka@redhat.com>
2010-10-13 16:36:35 +0200

    old indexes must be dropped before constraint re-creation

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/231-rhnCpuArch-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/231-rhnCpuArch-indexes.sql
@@ -1,7 +1,7 @@
 ALTER TABLE rhnCpuArch DROP CONSTRAINT rhn_cpuarch_label_uq;
+DROP INDEX rhn_cpuarch_l_id_n_idx;
 
 ALTER TABLE rhnCpuArch
     ADD CONSTRAINT rhn_cpuarch_label_uq UNIQUE (label)
     USING INDEX TABLESPACE [[2m_tbs]];
 
-DROP INDEX rhn_cpuarch_l_id_n_idx;
--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/234-rhnEmailAddressState-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/234-rhnEmailAddressState-indexes.sql
@@ -1,7 +1,7 @@
 ALTER TABLE rhnEmailAddressState DROP CONSTRAINT rhn_eastate_label_uq;
+DROP INDEX rhn_eastate_label_id_idx;
 
 ALTER TABLE rhnEmailAddressState
     ADD CONSTRAINT rhn_eastate_label_uq UNIQUE (label)
     USING INDEX TABLESPACE [[64k_tbs]];
 
-DROP INDEX rhn_eastate_label_id_idx;
