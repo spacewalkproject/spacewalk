83f2ed27befbcbabee1a706ca76849ff9aea4327
Michael Mraka <michael.mraka@redhat.com>
2010-10-14 11:29:22 +0200

    642962 - fixed issue where some of group by columns are null

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/019-rhnServerPackage-index.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/019-rhnServerPackage-index.sql
@@ -1,18 +1,7 @@
 DELETE FROM rhnServerPackage A
- WHERE EXISTS
-   (SELECT 1 FROM (SELECT server_id,
-                          name_id,
-                          evr_id,
-                          package_arch_id,
-                          min(rowid) min_rowid
-                     FROM rhnServerPackage
-                    GROUP BY server_id, name_id, evr_id, package_arch_id
-                   HAVING count(*) > 1) N
-            WHERE a.server_id = n.server_id
-              AND a.name_id = n.name_id
-              AND a.evr_id = n.evr_id
-              AND a.package_arch_id = n.package_arch_id
-              AND a.rowid > n.min_rowid);
+WHERE rowid NOT IN (SELECT min(rowid) min_rowid
+                      FROM rhnServerPackage
+                     GROUP BY server_id, name_id, evr_id, package_arch_id);
 
 DROP INDEX rhn_sp_snep_idx;
 CREATE UNIQUE INDEX rhn_sp_snep_uq
