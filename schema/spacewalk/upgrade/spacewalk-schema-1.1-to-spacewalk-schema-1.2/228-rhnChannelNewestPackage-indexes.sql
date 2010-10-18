5f82714beb96055d59ba426314185a1174ce5232
Michael Mraka <michael.mraka@redhat.com>
2010-10-18 11:23:08 +0200

    fixed ORA-02429: cannot drop index used for enforcement of unique/primary key

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/228-rhnChannelNewestPackage-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/228-rhnChannelNewestPackage-indexes.sql
@@ -1,4 +1,7 @@
+alter table rhnChannelNewestPackage disable constraint rhn_cnp_cid_nid_uq;
 drop index rhn_cnp_cnep_idx;
+alter table rhnChannelNewestPackage enable constraint rhn_cnp_cid_nid_uq;
+
 drop index rhn_cnp_necp_idx;
 drop index rhn_cnp_pid_idx;
 
