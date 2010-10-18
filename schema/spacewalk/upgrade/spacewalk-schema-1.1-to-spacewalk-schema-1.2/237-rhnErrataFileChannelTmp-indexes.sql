cf3722c7aba5cb1cf8c1782e08aae38cf7805617
Michael Mraka <michael.mraka@redhat.com>
2010-10-18 11:36:14 +0200

    fixed index name

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/237-rhnErrataFileChannelTmp-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/237-rhnErrataFileChannelTmp-indexes.sql
@@ -2,4 +2,4 @@ CREATE INDEX rhn_efilectmp_cid_idx
     ON rhnErrataFileChannelTmp (channel_id)
     TABLESPACE [[64k_tbs]];
 
-DROP INDEX rhn_efilec_cid_efid_idx;
+DROP INDEX rhn_efilectmp_cid_efid_idx;
