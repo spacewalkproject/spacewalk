53f01338ed8838cd4aa7d8880130e90f1fe0d9cc
Michael Mraka <michael.mraka@redhat.com>
2010-10-22 11:48:41 +0200

    reviewed rhnServerServerGroupArchCompat indexes

--- schema/spacewalk/common/tables/rhnServerServerGroupArchCompat.sql
+++ schema/spacewalk/common/tables/rhnServerServerGroupArchCompat.sql
@@ -34,8 +34,8 @@ CREATE UNIQUE INDEX rhn_ssg_ac_said_sgt_uq
     ON rhnServerServerGroupArchCompat (server_arch_id, server_group_type)
     TABLESPACE [[64k_tbs]];
 
-CREATE INDEX rhn_ssg_ac_sgt_said_idx
-    ON rhnServerServerGroupArchCompat (server_group_type, server_arch_id)
+CREATE INDEX rhn_ssg_ac_sgt_idx
+    ON rhnServerServerGroupArchCompat (server_group_type)
     TABLESPACE [[64k_tbs]]
     NOLOGGING;
 
