5ae12379a85c6e3d050edd228e5ea06b992d55a0
Lukas Zapletal <lzap+git@redhat.com>
2010-10-25 13:22:34 +0200

    Taskomatic schedule schema correction (sysdate)

--- schema/spacewalk/postgres/data/rhnTaskoSchedule.sql
+++ schema/spacewalk/postgres/data/rhnTaskoSchedule.sql
@@ -17,84 +17,84 @@
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'sync-probe-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='sync-probe-bunch'),
-        sysdate, '0 * * * * ?');
+        current_timestamp, '0 * * * * ?');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'errata-queue-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='errata-queue-bunch'),
-        sysdate, '0 * * * * ?');
+        current_timestamp, '0 * * * * ?');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'cobbler-sync-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='cobbler-sync-bunch'),
-        sysdate, '0 * * * * ?');
+        current_timestamp, '0 * * * * ?');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'channel-repodata-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='channel-repodata-bunch'),
-        sysdate, '0 * * * * ?');
+        current_timestamp, '0 * * * * ?');
 
 -- Every 10 minutes
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'package-cleanup-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='package-cleanup-bunch'),
-        sysdate, '0 0/10 * * * ?');
+        current_timestamp, '0 0/10 * * * ?');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'kickstart-cleanup-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='kickstart-cleanup-bunch'),
-        sysdate, '0 0/10 * * * ?');
+        current_timestamp, '0 0/10 * * * ?');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'errata-cache-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='errata-cache-bunch'),
-        sysdate, '0 0/10 * * * ?');
+        current_timestamp, '0 0/10 * * * ?');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'kickstartfile-sync-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='kickstartfile-sync-bunch'),
-        sysdate, '0 0/10 * * * ?');
+        current_timestamp, '0 0/10 * * * ?');
 
 -- Every 15 minutes
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'session-cleanup-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='session-cleanup-bunch'),
-        sysdate, '0 0/15 * * * ?');
+        current_timestamp, '0 0/15 * * * ?');
 
 -- Once a day at 4:05:00 AM (beware of 2AM cronjobs)
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'sandbox-cleanup-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='sandbox-cleanup-bunch'),
-        sysdate, '0 5 4 ? * *');
+        current_timestamp, '0 5 4 ? * *');
 
 -- Once a day at 4:35:00 AM (beware of 2AM cronjobs)
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'clean-alerts-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='clean-alerts-bunch'),
-        sysdate, '0 35 4 ? * *');
+        current_timestamp, '0 35 4 ? * *');
 
 -- Once a day at 11:00 PM
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'daily-status-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='daily-status-bunch'),
-        sysdate, '0 0 23 ? * *');
+        current_timestamp, '0 0 23 ? * *');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'compare-configs-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='compare-configs-bunch'),
-        sysdate, '0 0 23 ? * *');
+        current_timestamp, '0 0 23 ? * *');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'satcert-check-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='satcert-check-bunch'),
-        sysdate, '0 0 23 ? * *');
+        current_timestamp, '0 0 23 ? * *');
 
 INSERT INTO rhnTaskoSchedule (id, job_label, bunch_id, active_from, cron_expr)
     VALUES(nextval('rhn_tasko_schedule_id_seq'), 'clear-taskologs-default',
         (SELECT id FROM rhnTaskoBunch WHERE name='clear-taskologs-bunch'),
-        sysdate, '0 0 23 ? * *');
+        current_timestamp, '0 0 23 ? * *');
