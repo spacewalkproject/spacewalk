dac1880e406246e53fe7baf9c70ef85ebc3e9200
Michael Mraka <michael.mraka@redhat.com>
2010-11-11 16:00:05 +0100

    removed unnecessary double assigning

--- backend/server/handlers/app/channel.py
+++ backend/server/handlers/app/channel.py
@@ -129,7 +129,7 @@ class Channel(RPC_Base):
         log_debug(3)
         self._auth(username, password)
 
-        kstree = kstree = rhnKickstart.lookup_tree(ks_label)
+        kstree = rhnKickstart.lookup_tree(ks_label)
 
         if not isinstance(kstree, rhnKickstart.Kickstart):
             message = 'Kickstart tree not found'
