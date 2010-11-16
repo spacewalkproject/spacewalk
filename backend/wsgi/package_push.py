spacewalkproject/spacewalk

commit c7abc29bb1c8ba32a13ea22a2f5b050db26178a3

612581 - move python modules from /usr/share/rhn to python site-packages

backend/wsgi/wsgiHandler.py
@@ -17,7 +17,7 @@
 
 from wsgi import wsgiRequest
 
-from common import log_debug
+from spacewalk.common import log_debug
 
 def handle(environ, start_response, server, component_type, type="server.apacheServer"):
     #wsgi seems to capitalize incoming headers and add HTTP- to the front :/

backend/upload_server/handlers/package_push/package_push.py
@@ -21,11 +21,10 @@
 import os
 import base64
 import string
-from common import apache
 from rhn import rpclib
 
-from common import CFG, log_debug, log_error, rhnFault, rhnFlags
-from server import rhnPackageUpload, rhnSQL, basePackageUpload
+from spacewalk.common import apache, CFG, log_debug, log_error, rhnFault, rhnFlags
+from spacewalk.server import rhnPackageUpload, rhnSQL, basePackageUpload
 from spacewalk.common.checksum import getFileChecksum
 
 class PackagePush(basePackageUpload.BasePackageUpload):

commit 00d6abf256a5c9502129711cd2eb20209908456d

fixed mod_wsgi configuration
broken by c7abc29bb1c8ba32a13ea22a2f5b050db26178a3
612581 - move python modules from /usr/share/rhn to python site-packages

backend/wsgi/package_push.py
@@ -19,4 +19,4 @@ from wsgi import wsgiHandler
 def application(environ, start_response):
     return wsgiHandler.handle(environ, start_response,
                               "package_push", "server.upload.package-push",
-                              "server.apacheUploadServer")
+                              "spacewalk.server.apacheUploadServer")

backend/wsgi/sat_dump.py
@@ -19,4 +19,4 @@ from wsgi import wsgiHandler
 def application(environ, start_response):
     return wsgiHandler.handle(environ, start_response,
                               "non_auth_dumper", "server.iss.internal",
-                              "satellite_exporter.satexport")
+                              "spacewalk.satellite_exporter.satexport")

backend/wsgi/wsgiHandler.py
@@ -54,7 +54,7 @@ def handle(environ, start_response, server, component_type, type="spacewalk.serv
         req.send_http_header(status=ret)
 
     #exporter doesn't have a logHandler
-    if type != 'satellite_exporter.satexport':
+    if type != 'spacewalk.satellite_exporter.satexport':
         logServ = get_handle(type, "LogHandler")
         logServ(req)
     cleanServ = get_handle(type, "CleanupHandler")