f3b87b069a2d51fb36cc40e5fac13f101cff1bd1
Michael Mraka <michael.mraka@redhat.com>
2010-11-28 20:56:50 +0100

    python 2.4+ (RHEL5+) has hasattr(gettext, 'GNUTranslations') == True; removing dead code

--- backend/common/rhnTranslate.py
+++ backend/common/rhnTranslate.py
@@ -18,28 +18,27 @@ import types
 import gettext
 import string
 
-if hasattr(gettext, 'GNUTranslations'):
-    class RHN_Translations(gettext.GNUTranslations):
-        # Defining our own class, since we'd like to save the language we use
-        # Determining the language is not very pretty - we parse the file name
-        # which is supposed to be something like 
-        # .../<lang>/LC_MESSAGES/<domain>.po
-        def _parse(self, fp):
-            gettext.GNUTranslations._parse(self, fp)
-            filename = fp.name
-            filename = os.path.normpath(filename)
-            # Extract the language
-            self.lang = string.split(filename, '/')[-3]
+class RHN_Translations(gettext.GNUTranslations):
+    # Defining our own class, since we'd like to save the language we use
+    # Determining the language is not very pretty - we parse the file name
+    # which is supposed to be something like
+    # .../<lang>/LC_MESSAGES/<domain>.po
+    def _parse(self, fp):
+        gettext.GNUTranslations._parse(self, fp)
+        filename = fp.name
+        filename = os.path.normpath(filename)
+        # Extract the language
+        self.lang = string.split(filename, '/')[-3]
 
-        def getlangs(self):
-            # Return all languages
-            #pkilambi:bug#158561,170819,170821: the gettext object in python 2.2.3 has no attribute
-            #_fallback so add a check if __dict__ has key
-            #if not self._fallback or not hasattr(self._fallback, 'getlangs'):
-            if not self.__dict__.has_key("_fallback") or not self._fallback or not hasattr(self._fallback, 'getlangs'):
-                return [ self.lang, 'C' ]
-            # Recursive call
-            return [ self.lang ] + self._fallback.getlangs()
+    def getlangs(self):
+        # Return all languages
+        #pkilambi:bug#158561,170819,170821: the gettext object in python 2.2.3 has no attribute
+        #_fallback so add a check if __dict__ has key
+        #if not self._fallback or not hasattr(self._fallback, 'getlangs'):
+        if not self.__dict__.has_key("_fallback") or not self._fallback or not hasattr(self._fallback, 'getlangs'):
+            return [ self.lang, 'C' ]
+        # Recursive call
+        return [ self.lang ] + self._fallback.getlangs()
 
 class i18n:
     _default_langs = ['en', 'en_US', 'C']
@@ -79,47 +78,9 @@ class i18n:
         if localedir: self.localedir = localedir
         self._set_catalog()
 
-# Old-style gettext
-class i18n_old(i18n):
-    def _set_catalog(self):
-        self.cat = gettext.Catalog(self.domain, localedir=self.localedir)
-
-    def getlangs(self):
-        return gettext.lang
-
-    def setlangs(self, langs):
-        if isinstance(langs, types.StringType):
-            langs = [ langs ]
-        # Build the list of languages
-        _added_langs = []
-        # Aargh - on python 1.5, gettext removes os and string - so subsequent
-        # calls to _expandLang fail - so add the string module back
-        gettext.string = string
-        for l in langs:
-            _added_langs.extend(gettext._expandLang(l))
-        # Adding some defaults
-        _added_langs.extend(self._default_langs)
-        # Remove duplicates
-        h = {}
-        del gettext.lang[:]
-        for l in _added_langs:
-            if l == 'C':
-                # We'll add it back later
-                continue
-            if h.has_key(l):
-                # Already loaded it
-                continue
-            gettext.lang.append(l)
-            h[l] = None
-        if "C" not in gettext.lang:
-            gettext.lang.append("C")
-        self._set_catalog()
 
 def N_(str):
     return str
 
-if hasattr(gettext, 'GNUTranslations'):
-    cat = i18n()
-else:
-    cat = i18n_old()
+cat = i18n()
 _ = cat.gettext
