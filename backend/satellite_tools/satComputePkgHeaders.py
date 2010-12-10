spacewalkproject/spacewalk

commit c7abc29bb1c8ba32a13ea22a2f5b050db26178a3

612581 - move python modules from /usr/share/rhn to python site-packages

backend/satellite_tools/satComputePkgHeaders.py
@@ -20,11 +20,7 @@
 import sys
 from optparse import OptionParser, Option
 
-_path = "/usr/share/rhn"
-if _path not in sys.path:
-    sys.path.append(_path)
-
-from common import initCFG
+from spacewalk.common import initCFG
 
 SERVER_RETURN = 0
 
@@ -54,8 +50,8 @@ OK = 1
 sys.modules["_apache"] = sys.modules["__main__"]
 
 
-from server import rhnSQL
-from spacewalk.common import rhn_rpm
+from spacewalk.server import rhnSQL
+from spacewalk.spacewalk.common import rhn_rpm
 
 options_table = [
     Option("-v", "--verbose",       action="count", 

commit 4954cb74cccfa09000eb18ed8ce61226ef37a13c

fixed rhn_rpm import
error introduced in c7abc29bb1c8ba32a13ea22a2f5b050db26178a3

backend/satellite_tools/satComputePkgHeaders.py
@@ -51,7 +51,7 @@ sys.modules["_apache"] = sys.modules["__main__"]
 
 
 from spacewalk.server import rhnSQL
-from spacewalk.spacewalk.common import rhn_rpm
+from spacewalk.common import rhn_rpm
 
 options_table = [
     Option("-v", "--verbose",       action="count", 