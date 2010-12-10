spacewalkproject/spacewalk

commit c7abc29bb1c8ba32a13ea22a2f5b050db26178a3

612581 - move python modules from /usr/share/rhn to python site-packages

backend/satellite_tools/updatePackages.py
@@ -21,16 +21,14 @@ import os
 import time
 import shutil
 
-sys.path.append('/usr/share/rhn')
-
 from optparse import Option, OptionParser
-from common import rhnLib, rhnLog, initLOG, CFG, initCFG
+from spacewalk.common import rhnLib, rhnLog, initLOG, CFG, initCFG
 from spacewalk.common import rhn_rpm
-from server.rhnLib import parseRPMFilename, get_package_path
-from server import rhnSQL, rhnPackageUpload
-from server.rhnServer import server_packages
-from satellite_tools.progress_bar import ProgressBar
-from spacewalk.common.checksum import getFileChecksum
+from spacewalk.server.rhnLib import parseRPMFilename, get_package_path
+from spacewalk.server import rhnSQL, rhnPackageUpload
+from spacewalk.server.rhnServer import server_packages
+from spacewalk.satellite_tools.progress_bar import ProgressBar
+from spacewalk.spacewalk.common.checksum import getFileChecksum
 
 initCFG('server.satellite')
 initLOG(CFG.LOG_FILE, CFG.DEBUG)

commit fd9e684d1c72ec8e0074879d2393fe905f31c610

fixed getFileChecksum import
error introduced in c7abc29bb1c8ba32a13ea22a2f5b050db26178a3

backend/satellite_tools/updatePackages.py
@@ -27,7 +27,7 @@ from spacewalk.server.rhnLib import parseRPMFilename, get_package_path
 from spacewalk.server import rhnSQL, rhnPackageUpload
 from spacewalk.server.rhnServer import server_packages
 from spacewalk.satellite_tools.progress_bar import ProgressBar
-from spacewalk.spacewalk.common.checksum import getFileChecksum
+from spacewalk.common.checksum import getFileChecksum
 from spacewalk.server.importlib import mpmSource
 
 initCFG('server.satellite')