spacewalkproject/spacewalk

commit b4261f748940a3a34d2747162cda348fe0589be1

fixing rhnpush error: ORA-00904: "MD5SUM": invalid identifier

backend/server/importlib/backend.py
@@ -1977,12 +1977,12 @@ class Backend:
             apply(h.executemany, (), params)
 
     def validate_pks(self):
-        # If nevra is enabled use md5sum as primary key
+        # If nevra is enabled use checksum as primary key
         tbs = self.tables['rhnPackage']
         if CFG.ENABLE_NVREA:
-            # Add md5sum as a primarykey if nevra is enabled
-            if 'md5sum' not in tbs.pk:
-                tbs.pk.append('md5sum')
+            # Add checksum as a primarykey if nevra is enabled
+            if 'checksum_id' not in tbs.pk:
+                tbs.pk.append('checksum_id')
             
 # Returns a tuple for the hash's values
 def build_key(hash, fields):

backend/server/importlib/backendOracle.py
@@ -131,7 +131,8 @@ class OracleBackend(Backend):
                 'header_end'    : DBint(),
                 'last_modified' : DBdateTime(),
             },
-            pk          = ['org_id', 'name_id', 'evr_id', 'package_arch_id'],
+            pk          = ['org_id', 'name_id', 'evr_id', 'package_arch_id',
+                           'checksum_id'],
             nullable    = ['org_id'],
             severityHash = {
                 'path'          : 1,

commit d1a9e12d8b5409ab54c029a6a9b7e9f72d157a89

624092 - update package if pushing using --force and package with same NVREA already exist
this regression has been caused by commit b4261f748940a3a34d2747162cda348fe0589be1
we do not want to have checksum_id as PK if NVREA is disabled

backend/server/importlib/backend.py
@@ -1969,10 +1969,10 @@ class Backend:
     def validate_pks(self):
         # If nevra is enabled use checksum as primary key
         tbs = self.tables['rhnPackage']
-        if CFG.ENABLE_NVREA:
-            # Add checksum as a primarykey if nevra is enabled
-            if 'checksum_id' not in tbs.pk:
-                tbs.pk.append('checksum_id')
+        if not CFG.ENABLE_NVREA:
+            # remove checksum from a primary key if nevra is disabled.
+            if 'checksum_id' in tbs.pk:
+                tbs.pk.remove('checksum_id')
             
 # Returns a tuple for the hash's values
 def build_key(hash, fields):