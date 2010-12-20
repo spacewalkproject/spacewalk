f19676151d0e83a64016a68d3c8a903f74b4af29
Tomas Lestach <tlestach@redhat.com>
2010-12-20 19:31:39 +0100

    662981 - change warnings to errors, if required channels not found when adding virt entitlement

--- java/code/src/com/redhat/rhn/manager/system/SystemManager.java
+++ java/code/src/com/redhat/rhn/manager/system/SystemManager.java
@@ -1607,10 +1607,10 @@ public class SystemManager extends BaseManager {
                     user, server, ChannelManager.RHN_VIRT_HOST_PACKAGE_NAME);
 
             // If this is a Satellite and no RHN Tools channel is available
-            // report the error, but continue:
+            // report the error
             if (!ConfigDefaults.get().isSpacewalk() && toolsChannel == null) {
                 log.warn("no tools channel found");
-                result.addWarning(new ValidatorWarning("system.entitle.notoolschannel"));
+                result.addError(new ValidatorError("system.entitle.notoolschannel"));
             }
             // If Spacewalk and no channel has the rhn-virtualization-host package,
             // warn but allow the operation to proceed.
@@ -1630,7 +1630,7 @@ public class SystemManager extends BaseManager {
                             user, server, nameId, evrId, archId);
                 }
                 else {
-                    result.addWarning(new ValidatorWarning("system.entitle.novirtpackage",
+                    result.addError(new ValidatorError("system.entitle.novirtpackage",
                                         ChannelManager.RHN_VIRT_HOST_PACKAGE_NAME));
                 }
             }
@@ -1669,7 +1669,7 @@ public class SystemManager extends BaseManager {
                 // If we couldn't find a virt channel, warn the user but continue:
                 if (virtChannel == null) {
                     log.warn("no virt channel");
-                    result.addWarning(new ValidatorWarning(
+                    result.addError(new ValidatorError(
                             "system.entitle.novirtchannel"));
                 }
             }
