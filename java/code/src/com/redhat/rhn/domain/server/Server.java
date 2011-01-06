fedf9076caf9d6e8c269239c4ccb4fb4cd3b40f0
Tomas Lestach <tlestach@redhat.com>
2011-01-06 18:02:12 +0100

    628755 - fix searching for the primary network interface

--- java/code/src/com/redhat/rhn/domain/server/Server.java
+++ java/code/src/com/redhat/rhn/domain/server/Server.java
@@ -837,25 +837,23 @@ public class Server extends BaseDomainHelper implements Identifiable {
         if (!networkInterfaces.isEmpty()) {
             Iterator i = networkInterfaces.iterator();
             // First pass look for names
-            while (i.hasNext()) {
-                NetworkInterface n = (NetworkInterface) i.next();
-                boolean notEmpty = !n.isDisabled();
-                if (n.getName().equals("eth0") && notEmpty) {
-                    log.debug("Found eth0");
-                    return n;
-                }
-                if (n.getName().startsWith("eth0") && notEmpty) {
-                    log.debug("Found eth0*");
-                    return n;
-                }
-                if (n.getName().equals("eth1") && notEmpty) {
-                    log.debug("Found eth1");
-                    return n;
-                }
-                if (n.getName().startsWith("eth1") && notEmpty) {
-                    log.debug("Found eth1*");
-                    return n;
-                }
+            NetworkInterface ni = null;
+
+            ni = findActiveIfaceWithName("eth0", false);
+            if (ni != null) {
+                return ni;
+            }
+            ni = findActiveIfaceWithName("eth0", true);
+            if (ni != null) {
+                return ni;
+            }
+            ni = findActiveIfaceWithName("eth1", false);
+            if (ni != null) {
+                return ni;
+            }
+            ni = findActiveIfaceWithName("eth1", true);
+            if (ni != null) {
+                return ni;
             }
             // Second pass look for localhost
             i = networkInterfaces.iterator();
@@ -876,6 +874,30 @@ public class Server extends BaseDomainHelper implements Identifiable {
         return null;
     }
 
+    private NetworkInterface findActiveIfaceWithName(String pattern, boolean startsWith) {
+        if (networkInterfaces.isEmpty()) {
+            return null;
+        }
+        for (Iterator i = networkInterfaces.iterator(); i.hasNext();) {
+            NetworkInterface ni = (NetworkInterface) i.next();
+            if (ni.isDisabled()) {
+                continue;
+            }
+            if (startsWith) {
+                if (ni.getName().startsWith(pattern)) {
+                    log.debug("Found " + pattern + "*");
+                    return ni;
+                }
+            }
+            else {
+                if (ni.getName().equals(pattern)) {
+                    log.debug("Found " + pattern);
+                    return ni;
+                }
+            }
+        }
+        return null;
+    }
 
     // Sometimes java really annoys me
     private Network findPrimaryNetwork() {
