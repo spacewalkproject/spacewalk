339c842924037eff31b50ec8ea490e4ed59ea3fe
Michael Mraka <michael.mraka@redhat.com>
2011-01-07 12:16:37 +0100

    662563 - in 0.7 rhn_sndpb_pid_ptype_idx was used for rhn_sndpb_probe_id_pk enforcement
    we need to disable and re-enable primary key so it will
    create it's own index instead of dropped one

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/221-rhn_sat_node_probe-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/221-rhn_sat_node_probe-indexes.sql
@@ -1,3 +1,5 @@
 
+alter table rhn_sat_node_probe disable constraint rhn_sndpb_probe_id_pk;
 drop index rhn_sndpb_pid_ptype_idx;
+alter table rhn_sat_node_probe enable constraint rhn_sndpb_probe_id_pk;
 
