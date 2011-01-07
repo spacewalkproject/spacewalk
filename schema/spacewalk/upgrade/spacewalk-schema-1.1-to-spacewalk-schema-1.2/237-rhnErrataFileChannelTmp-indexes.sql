676f5dbf2cae684a0a2a819a3c563cb8c1282603
Michael Mraka <michael.mraka@redhat.com>
2011-01-07 13:21:25 +0100

    662563 - in 0.7 rhn_efilectmp_cid_efid_idx was used for rhn_efilectmp_efid_cid_uq enforcement
    we need to disable and re-enable unique key so it will
    use a different index instead of dropped one

--- schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/237-rhnErrataFileChannelTmp-indexes.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.1-to-spacewalk-schema-1.2/237-rhnErrataFileChannelTmp-indexes.sql
@@ -2,4 +2,6 @@ CREATE INDEX rhn_efilectmp_cid_idx
     ON rhnErrataFileChannelTmp (channel_id)
     TABLESPACE [[64k_tbs]];
 
+alter table rhnErrataFileChannelTmp disable constraint RHN_EFILECTMP_EFID_CID_UQ;
 DROP INDEX rhn_efilectmp_cid_efid_idx;
+alter table rhnErrataFileChannelTmp enable constraint RHN_EFILECTMP_EFID_CID_UQ;
