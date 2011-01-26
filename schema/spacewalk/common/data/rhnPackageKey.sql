80d746ed93ba1b4f5a4efd867a01e24dba5e220d
Michael Mraka <michael.mraka@redhat.com>
2011-01-26 17:03:37 +0100

    added new Spacewalk key

--- schema/spacewalk/common/data/rhnPackageKey.sql
+++ schema/spacewalk/common/data/rhnPackageKey.sql
@@ -54,8 +54,11 @@ insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 (sequence_nextval('rhn_pkey_id_seq'), '9d1cc34857bbccba', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
 
+-- Spacewalk
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 (sequence_nextval('rhn_pkey_id_seq'), '95423d4e430a1c35', lookup_package_key_type('gpg'), lookup_package_provider('Spacewalk'));
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
+(sequence_nextval('rhn_pkey_id_seq'), 'ed635379b3892132', lookup_package_key_type('gpg'), lookup_package_provider('Spacewalk'));
 
 -- RHEL 6
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
