spacewalkproject/spacewalk

commit 6aa980ce46e59e4a7de40bcfe6a5c2dfa87b92d7

670746 - fix malformed query _query_action_verify_packages
This line was lost by mistake in commit 482a3442fb8609d76eef6a5ed7e3c2a4633db63f

backend/server/action/packages.py
@@ -36,6 +36,7 @@ _query_action_verify_packages = rhnSQL.Statement("""
            pe.release as release,
            pe.epoch as epoch,
            pa.label as arch
+      from rhnActionPackage ap,
  left join rhnPackageArch pa
         on ap.package_arch_id = pa.id,
            rhnPackageName pn,

commit 002d4d9e172334d3e791c9469a52801871349072

675912 - fixed typo
introduced in 6aa980ce46e59e4a7de40bcfe6a5c2dfa87b92d7

backend/server/action/packages.py
@@ -36,7 +36,7 @@ _query_action_verify_packages = rhnSQL.Statement("""
            pe.release as release,
            pe.epoch as epoch,
            pa.label as arch
-      from rhnActionPackage ap,
+      from rhnActionPackage ap
  left join rhnPackageArch pa
         on ap.package_arch_id = pa.id,
            rhnPackageName pn,