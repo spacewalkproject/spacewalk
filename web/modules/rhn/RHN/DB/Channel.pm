14d8c86aeda1b3d680bd55c8caebe1ba07ebfb5a
Miroslav Suchý <msuchy@redhat.com>
2011-02-16 17:12:59 +0100

    enable RHN Proxy 5.4 on RHEL6

--- web/modules/rhn/RHN/DB/Channel.pm
+++ web/modules/rhn/RHN/DB/Channel.pm
@@ -304,6 +304,9 @@ my %proxy_chans_by_version = ('1.1' => ['redhat-rhn-proxy-as-i386-2.1', 'redhat-
                    '5.4' => [ 'redhat-rhn-proxy-5.4-server-i386-5',
                          'redhat-rhn-proxy-5.4-server-x86_64-5',
                          'redhat-rhn-proxy-5.4-server-s390x-5',
+                         'redhat-rhn-proxy-5.4-server-i386-6',
+                         'redhat-rhn-proxy-5.4-server-x86_64-6',
+                         'redhat-rhn-proxy-5.4-server-s390x-6',
                             ],
 
 			     );
