93d09aa0f827faf2c6a93a8109c1ef2cdebc2deb
Tomas Lestach <tlestach@redhat.com>
2011-02-21 18:09:50 +0100

    removing dead code
    selectedVersionStr is always not null at this point

--- java/code/src/com/redhat/rhn/frontend/action/channel/manage/AddRedHatErrataAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/channel/manage/AddRedHatErrataAction.java
@@ -169,10 +169,6 @@ public class AddRedHatErrataAction extends RhnListAction {
                 sortChannelsAndChildify(channelSet, channelList, user, selectedChannelStr);
                 request.setAttribute(CHANNEL_LIST, channelList);
             }
-            if (channelList.size() > 0 && selectedVersionStr == null) {
-                selectedChannelStr = channelList.get(0).getId().toString();
-            }
-
         }
 
 
