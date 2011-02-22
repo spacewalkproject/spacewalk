spacewalkproject/spacewalk

commit cb638ba72a893e872da251668a9963c9d87d6f59

Localize the FILE; also use three-parameter opens.

monitoring/SatConfig/general/SysVStep.pm
@@ -172,7 +172,8 @@ sub installSysVLinks
 		foreach $level (@klevels) {
 			symlink('/etc/rc.d/init.d/'.ref($self),'/etc/rc.d/rc'.$level.'.d/K'.$stopSeq.ref($self));
 		}
-		open(FILE,">".SysVStep->ConfigValue('installed').'/'.ref($self));
+		local * FILE;
+		open(FILE, '>', SysVStep->ConfigValue('installed').'/'.ref($self));
 		print FILE join(',',@levels)."\n";
 		close(FILE);
 	}
@@ -293,7 +294,8 @@ sub startStep
 		}
 		if ($is_not_subsystem)  {
 			# This is for RH "rc" script - won't kill if this isn't here
-			open(FILE,'>/var/lock/subsys/'.ref($self));
+			local * FILE;
+			open(FILE, '>', /var/lock/subsys/'.ref($self));
 			print FILE 'running';
 			close(FILE);
 		}
@@ -549,19 +551,20 @@ sub registerForInstall
 	if ($registrant) {
 		my $regdir = SysVStep->ConfigValue('registry');
 		my $filename = $regdir.'/'.ref($self);
+		local * FILE;
 		if ( ! -f $filename ) {
-			open(FILE,">$filename");
+			open(FILE, '>', $filename);
 			print FILE $registrant."\n";
 			close(FILE);
 		} else {
-			open(FILE, $filename);
+			open(FILE, '<', $filename);
 			chomp(my @keys = <FILE>);
 			close(FILE);
 			my %ary;
 			@ary{@keys} = (1 .. scalar(@keys));
 			print ref($self)." keys = ".join(',',keys(%ary))."\n";
 			if ( ! defined($ary{$registrant})) {
-				open(FILE,">>$filename");
+				open(FILE, '>>', $filename);
 				print FILE $registrant."\n";
 				close(FILE);
 			}
@@ -581,7 +584,8 @@ sub registrationList
 	my $regdir = SysVStep->ConfigValue('registry');
 	my $filename = $regdir.'/'.ref($self);
 	if ( -f $filename ) {
-		open(FILE,$filename);
+		local * FILE;
+		open(FILE, '<', $filename);
 		my $item;
 		foreach  $item (<FILE>) {
 			chomp($item);

commit e14719237f4dd26db56588fefc9f8d1801e19d84

Fixing typo (missing single-quote).
Bug introduced by cb638ba72a893e872da251668a9963c9d87d6f59.

monitoring/SatConfig/general/SysVStep.pm
@@ -295,7 +295,7 @@ sub startStep
 		if ($is_not_subsystem)  {
 			# This is for RH "rc" script - won't kill if this isn't here
 			local * FILE;
-			open(FILE, '>', /var/lock/subsys/'.ref($self));
+			open(FILE, '>', '/var/lock/subsys/'.ref($self));
 			print FILE 'running';
 			close(FILE);
 		}