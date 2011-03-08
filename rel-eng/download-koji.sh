1b830599b6d5cce83281cae6a250815c4f213f9b
Michael Mraka <michael.mraka@redhat.com>
2011-03-08 14:04:07 +0100

    download-koji.sh - script for downloading (not only scratch) builds from koji

---
+++ rel-eng/download-koji.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+KOJI=$(echo "$1" | sed 's,\(https*://.*\)/[^/]*$,\1/,')
+WGET='wget --no-check-certificate -nv'
+
+$WGET -O - $1 \
+    | awk -F\" 'function basename(f) {
+                        gsub(".*[/=]", "", f)
+                        return f}
+                />download</ && ! /src\.rpm/ {
+                        print $4 " -O " basename($4)}
+                /"getfile?.*\.rpm"/ && ! /src\.rpm/ {
+                        print "'"$KOJI"'" $2 " -O " basename($2)} ' \
+    | xargs -n 3 $WGET
