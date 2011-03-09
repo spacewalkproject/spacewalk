a515bea229f3d103507403443a3f5935aa221215
Jan Pazdziora <jpazdziora@redhat.com>
2011-03-09 15:13:34 +0100

    Fixing system group admin granting (PostgreSQL).
    Addressing
    DBD::Pg::st execute failed: ERROR:  syntax error at or near "usgps"
    LINE 2:     cursor usgps is

--- web/modules/rhn/RHN/DB/User.pm
+++ web/modules/rhn/RHN/DB/User.pm
@@ -1653,33 +1653,21 @@ sub grant_servergroup_permission {
   my $sgid = shift;
 
   my $dbh = RHN::DB->connect;
-  my $sth = $dbh->prepare(<<EOS);
-DECLARE
-    cursor usgps is
-        select  wc.id user_id,
-                sg.id server_group_id
-        from    rhnServerGroup sg,
-                web_contact wc
-        where   wc.id = :user_id
-            and sg.id = :server_group_id
+  if ($dbh->selectrow_array(<<EOS, {}, $uid, $sgid)) {
+        select  web_contact.id, rhnServerGroup.id
+        from    web_contact, rhnServerGroup
+        where   web_contact.id = ?
+            and rhnServerGroup.id = ?
             and not exists (
                 select  1
                 from    rhnUserServerGroupPerms usgp
-                where   usgp.user_id = :user_id
-                    and usgp.server_group_id = :server_group_id
-            );
-BEGIN
-    for usgp in usgps loop
-        begin
-            rhn_user.add_servergroup_perm(:user_id, :server_group_id);
-        exception
-            when others then null;
-        end;
-    end loop;
-END;
+                where   web_contact.id = usgp.user_id
+                    and rhnServerGroup.id = usgp.server_group_id
+            )
 EOS
+    $dbh->call_procedure('rhn_user.add_servergroup_perm', $uid, $sgid);
+  }
 
-  $sth->execute_h(user_id => $uid, server_group_id => $sgid);
   $dbh->commit;
 }
 
