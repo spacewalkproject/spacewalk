efea95d61e87246ce5138792ce0b1cb3cbf3f6f4
Jan Pazdziora <jpazdziora@redhat.com>
2011-03-10 16:00:37 +0100

    Using sequence_nextval instead of the .nextval.

--- schema/spacewalk/upgrade/spacewalk-schema-1.3-to-spacewalk-schema-1.4/021-rhnKsInstallType.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.3-to-spacewalk-schema-1.4/021-rhnKsInstallType.sql
@@ -1,1 +1,1 @@
-insert into rhnKSInstallType (id,label,name) VALUES (rhn_ksinstalltype_id_seq.nextval, 'suse', 'SUSE Linux');
+insert into rhnKSInstallType (id,label,name) VALUES (sequence_nextval('rhn_ksinstalltype_id_seq'), 'suse', 'SUSE Linux');
