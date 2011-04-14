294eb5607159db16b5f3f6625bfe5b1812142800
Miroslav Suchý <msuchy@redhat.com>
2011-04-14 16:57:41 +0200

    683200 - set the protocol correctly

--- client/tools/rhncfg/config_common/rhn_main.py
+++ client/tools/rhncfg/config_common/rhn_main.py
@@ -132,7 +132,7 @@ class BaseMain:
         up2date_cfg = dict(cfg.items())
 
         server_name = config.getServerlURL()
-        up2date_cfg['proto'] = 'http'
+        up2date_cfg['proto'] = urlsplit(server_name[0])[0]
         up2date_cfg['server_list'] = map(lambda x: urlsplit(x)[1], server_name)
         if server_name:
             server_name = urlsplit(server_name[0])[1]
