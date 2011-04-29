a50a4746bfd801e5765dfcff4577261cf63fdd60
Miroslav Suchý <msuchy@redhat.com>
2011-04-29 10:12:15 +0200

    691283 - create persistdir in _repos_persistdir instead of PWD

--- client/rhel/yum-rhn-plugin/rhnplugin.py
+++ client/rhel/yum-rhn-plugin/rhnplugin.py
@@ -75,6 +75,8 @@ def init_hook(conduit):
                repo.baseurl = ['file:///' + repodir ]
                repo.urls = repo.baseurl
                repo.name = reponame
+               if hasattr(conduit.getConf(), '_repos_persistdir'):
+                   repo.base_persistdir = conduit.getConf()._repos_persistdir
                repo.enable()
                if not repos.findRepos(repo.id):
                    repos.add(repo)
