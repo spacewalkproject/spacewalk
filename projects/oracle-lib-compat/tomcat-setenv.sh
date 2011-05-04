42eba5234a9c2ce5f724c78819507493f6a87c9d
Jan Pazdziora <jpazdziora@redhat.com>
2011-05-04 15:58:48 +0200

    We unset LD_PRELOAD to force ldd to show the libldap line with => even if LD_PRELOAD was already set.

--- projects/oracle-lib-compat/tomcat-setenv.sh
+++ projects/oracle-lib-compat/tomcat-setenv.sh
@@ -3,6 +3,8 @@
 # in libclntsh.so* upon tomcat5 startup with those from standard library.
 for i in /lib64/security/pam_ldap.so /lib/security/pam_ldap.so ; do
 	if [ -f $i ] ; then
+		# to force ldd to show the libldap line with => even if LD_PRELOAD was already set
+		unset LD_PRELOAD
 		export LD_PRELOAD=`ldd $i | perl -lne '/^\s+libldap\S+\s+=>\s+(\S+)/ and print $1 and exit'`
 		break
 	fi
