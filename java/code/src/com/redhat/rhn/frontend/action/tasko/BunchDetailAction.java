eed3e5c9fa86b834b0b88cc096731c232b4fb2a6
Tomas Lestach <tlestach@redhat.com>
2011-05-06 18:17:57 +0200

    659138 - update single run action message

--- java/code/src/com/redhat/rhn/frontend/action/tasko/BunchDetailAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/tasko/BunchDetailAction.java
@@ -27,8 +27,11 @@ import com.redhat.rhn.taskomatic.TaskomaticApiException;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -62,8 +65,13 @@ public class BunchDetailAction extends RhnAction implements Listable {
 
         if (ctx.isSubmitted()) {
             try {
-                new TaskomaticApi().scheduleSingleSatBunch(loggedInUser, bunchLabel);
-                createSuccessMessage(request, "message.bunch.singlescheduled", bunchLabel);
+                Date date = new TaskomaticApi().scheduleSingleSatBunch(loggedInUser,
+                        bunchLabel);
+                ActionMessages msgs = new ActionMessages();
+                msgs.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("message.bunch.singlescheduled", bunchLabel,
+                        LocalizationService.getInstance().formatCustomDate(date)));
+                saveMessages(request, msgs);
             }
             catch (TaskomaticApiException e) {
                 createErrorMessage(request,
