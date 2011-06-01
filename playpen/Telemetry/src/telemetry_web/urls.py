0634639b583f01b6cc307ad1a901cf5fdc191403
Jan Pazdziora <jpazdziora@redhat.com>
2011-06-01 09:30:42 +0200

    The playpen/ content is dead, removing.

--- playpen/Telemetry/src/telemetry_web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from telemetry_web.views import *
-
-urlpatterns = patterns('',
-    (r'^telemetry/reports[/]$', list_reports),
-    (r'^telemetry/reports/reportdetails[/]$', report_details),
-    (r'^telemetry/reports/reportresults[/]$', report_results),
-    (r'^telemetry[/]$', index_view)
-)
