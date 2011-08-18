061d90567aadd89541764ac8a97068e893113c81
Miroslav Suchý <msuchy@redhat.com>
2011-08-18 16:02:39 +0200

    if we use 1024 as base, we should use kiB, MiB, GiB

--- backend/satellite_tools/satsync.py
+++ backend/satellite_tools/satsync.py
@@ -1812,13 +1812,13 @@ Please contact your RHN representative""") % (generation, sat_cert.generation))
         if (bytes/kilo == 0):
             return "%d bytes" %(bytes)
         elif (bytes/mega == 0):
-            return "%d kB" %(bytes / 1024)
+            return "%d kiB" %(bytes / 1024)
         elif (bytes/giga == 0):
             mega_value=bytes / mega
-            return "%d.%03d MB" %(mega_value, (bytes % mega) / kilo)
+            return "%d.%03d MiB" %(mega_value, (bytes % mega) / kilo)
         else:
             giga_value=bytes / giga
-            return "%d.%03d GB" %(giga_value, (bytes % giga) / mega)
+            return "%d.%03d GiB" %(giga_value, (bytes % giga) / mega)
 
     def _get_package_stream(self, channel, package_id, nvrea, sources):
         """ returns (filepath, stream), so in the case of a "wire source",
