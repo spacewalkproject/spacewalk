spacewalkproject/spacewalk

commit 35b7a37c4093b09ac6310c1b1876906ccf347e7d

Fixing system group admin revocation (which is also used while granting) (PostgreSQL).
Addressing
DBD::Pg::st execute failed: ERROR:  syntax error at or near "usgps"
LINE 2:   cursor usgps is

web/modules/rhn/RHN/DB/User.pm
@@ -1684,32 +1684,23 @@ sub revoke_servergroup_permission {
   my $sgid = shift;
 
   my $dbh = RHN::DB->connect;
-  my $sth = $dbh->prepare(<<EOS);
-DECLARE
-  cursor usgps is
+  for my $row (@{ $dbh->selectall_arrayref(<<EOS, {}, $uid, $sgid) }) {
     select  usgp.server_group_id, usgp.user_id
     from    rhnUserServerGroupPerms usgp
-    where   usgp.server_group_id = :server_group_id
+    where   usgp.user_id = ?
+        and usgp.server_group_id = ?
         and exists (
             select  1
             from    rhnServerGroup  sg,
                     web_contact     wc
-            where   wc.id = :user_id
-                and sg.id = :server_group_id
+            where   wc.id = usgp.user_id
+                and sg.id = usgp.server_group_id
                 and sg.org_id = wc.org_id
             );
-BEGIN
-    for usgp in usgps loop
-        begin
-            rhn_user.remove_servergroup_perm(:user_id, :server_group_id);
-        exception
-            when others then null;
-        end;
-    end loop;
-END;
 EOS
+    $dbh->call_procedure('rhn_user.remove_servergroup_perm', $uid, $sgid);
+  }
 
-  $sth->execute_h(server_group_id => $sgid, user_id => $uid);
   $dbh->commit;
 }
 

commit cde1c64c0a0fc62615347109699c4a798d9d8a9f

fixed typo in sql query
(introduced in 35b7a37c4093b09ac6310c1b1876906ccf347e7d)

web/modules/rhn/RHN/DB/User.pm
@@ -1696,7 +1696,7 @@ sub revoke_servergroup_permission {
             where   wc.id = usgp.user_id
                 and sg.id = usgp.server_group_id
                 and sg.org_id = wc.org_id
-            );
+            )
 EOS
     $dbh->call_procedure('rhn_user.remove_servergroup_perm', $uid, $sgid);
   }