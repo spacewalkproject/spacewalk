spacewalkproject/spacewalk

commit 86ec92b1c0221a9bb51e0f032f1800b349972036

do not mask original error by raise in execption
From Jason email:
In a nutshell, NEVER NEVER NEVER DO THIS:

try:
    .... # some work
except:
    ... # possibly something else that doesn't include logging the traceback
    raise MyOwnExceptionClass('new message here')

We've pointed this out before, and I'm pointing it out again. THIS MASKS
THE ORIGINAL ERROR! When trying to debug these, I usually feel like
strangling someone.

We should either (1) log the current exception and traceback if we're
actually handling the error (e.g. at the top level of the web services
or in the pulp client), (2) re-raise the original error (e.g. useful for
doing some local cleanup), or (3) concatenate the existing traceback to
our new exception (e.g. when differentiating the error condition from an
underlying standard library or third party exception).

Let me demonstrate the last two.

Re-raising the original error (2):

try:
    .... # some work
except:
    .... # local clean up, logging, etc.
    raise

The last empty 'raise' statement will conveniently re-raise the caught
exception.

Concatenating the existing traceback (3):

import sys

try:
    ... # some work
except:
    ... # optional clean up, etc.
    raise PulpException('more meaningful error message'), None, sys.exc_info()[2]

The ', None, sys.exc_info()[2]' portion at the end of the new 'raise'
statement will concatenate the exiting traceback to the traceback
generated by the raise. This allows us to see the original point where
something went wrong instead of hiding it beneath the new PulpException.

Note: it can be any exception, PulpException is just used as an example.

backend/server/importlib/backend.py
@@ -18,6 +18,7 @@
 
 import copy
 import string
+import sys
 
 from spacewalk.common import rhn_rpm
 from spacewalk.common.rhnConfig import CFG
@@ -479,7 +480,6 @@ class Backend:
                 raise TypeError("Expected an IncompletePackage instance, found %s" % \
                                 str(type(package)))
         for package in packages:
-            exception = None
             for type, chksum  in package['checksums'].iteritems():
                 package['checksum_type'] = type
                 package['checksum']      = chksum
@@ -489,10 +489,10 @@ class Backend:
                     exception = None
                     break
                 except InvalidPackageError, e:
-                    exception = e
-            if exception and not ignore_missing:
-                raise exception 
-                    
+                    if ignore_missing:
+                        pass
+                    else:
+                        raise
 
     def lookupSolarisPackages(self, packages, ignore_missing=0):
         for pkg in packages:
@@ -993,8 +993,8 @@ class Backend:
 	        activate_channel_entitlements(cfp['org_id'], 
 		               cfp['channel_family'], cfp['max_members'], cfp['max_flex'])
             except rhnSQL.SQLError, e:
-		raise rhnFault(23, str(e[1]) + ": org_id [%s] family [%s] max [%s]" % \
-		    (cfp['org_id'], cfp['channel_family'], cfp['max_members']), explain=0)
+                raise rhnFault(23, str(e[1]) + ": org_id [%s] family [%s] max [%s]" % \
+                    (cfp['org_id'], cfp['channel_family'], cfp['max_members']), explain=0), None, sys.exc_info()[2]
             
         # The way we constructed the list of channel family permissions, we
         # should have (at least) all of the permissions from the database in
@@ -1255,7 +1255,7 @@ class Backend:
             try:
                 refresh_newest_package(channel_id, caller)
             except rhnSQL.SQLError, e:
-                raise rhnFault(23, str(e[1]), explain=0)
+                raise rhnFault(23, str(e[1]), explain=0), None, sys.exc_info()[2]
             if deleted_packages_list:
                 invalidate_ss = 1
             else:
@@ -1316,9 +1316,9 @@ class Backend:
                 h.executemany(id=ids, value=values)
             except rhnSQL.SQLSchemaError, e:
                 if e.errno == 01401:
-                    raise ValueError, e.errmsg
+                    raise ValueError, e.errmsg, sys.exc_info()[2]
                 else:
-                    raise rhnFault(30, e.errmsg, explain=0)
+                    raise rhnFault(30, e.errmsg, explain=0), None, sys.exc_info()[2]
 
     def __buildQueries(self, childTables):
         childTableLookups = {}
@@ -1629,7 +1629,7 @@ class Backend:
             try:
                 self.__doInsertTable(tname, dict)
             except rhnSQL.SQLError, e:
-                raise rhnFault(54, str(e[1]), explain=0)
+                raise rhnFault(54, str(e[1]), explain=0), None, sys.exc_info()[2]
 
     def __doInsertTable(self, table, hash):
         if not hash:

commit 0285415f8403d2714b72e3534138fbd92230f6c1

fixed package lookup
regression caused by 86ec92b1c0221a9bb51e0f032f1800b349972036

fixing
Traceback (most recent call last):
  File "/usr/bin/satellite-sync", line 139, in main
    return satsync.Runner().main()
...
  File "/usr/lib/python2.4/site-packages/spacewalk/server/importlib/backend.py", line 1701, in __lookupObjectCollection
    raise InvalidPackageError(object, "Could not find object %s in table %s" % (object, tableName))

backend/server/importlib/backend.py
@@ -480,19 +480,21 @@ class Backend:
                 raise TypeError("Expected an IncompletePackage instance, found %s" % \
                                 str(type(package)))
         for package in packages:
+            # here we need to figure out which checksum we have in the database
+            not_found = None
             for type, chksum  in package['checksums'].iteritems():
                 package['checksum_type'] = type
                 package['checksum']      = chksum
                 package['checksum_id']  = checksums[(type, chksum)]
                 try:
                     self.__lookupObjectCollection([package], 'rhnPackage')
-                    exception = None
+                    not_found = None
                     break
                 except InvalidPackageError, e:
-                    if ignore_missing:
-                        pass
-                    else:
-                        raise
+                    not_found = (e, sys.exc_info()[2])
+            if not_found and not ignore_missing:
+                # package is not in database at all
+                raise not_found[0], None, not_found[1]
 
     def lookupSolarisPackages(self, packages, ignore_missing=0):
         for pkg in packages: