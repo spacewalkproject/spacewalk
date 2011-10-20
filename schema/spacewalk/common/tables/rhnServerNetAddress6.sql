7d3b8d0ec1627c84d73d4383cd9f3d31cd2f1784
Milan Zazrivec <mzazrivec@redhat.com>
2011-10-20 17:38:13 +0200

    delete from rhnServerNetInterface: add on delete cascade

--- schema/spacewalk/common/tables/rhnServerNetAddress6.sql
+++ schema/spacewalk/common/tables/rhnServerNetAddress6.sql
@@ -18,7 +18,8 @@ CREATE TABLE rhnServerNetAddress6
 (
     interface_id  NUMBER NOT NULL
                    CONSTRAINT rhn_srv_net_iaddress6_iid_fk
-                       REFERENCES rhnServerNetInterface (id),
+                       REFERENCES rhnServerNetInterface (id)
+                       ON DELETE CASCADE,
     address    VARCHAR2(45),
     netmask    VARCHAR2(49),
     scope      VARCHAR2(64),
