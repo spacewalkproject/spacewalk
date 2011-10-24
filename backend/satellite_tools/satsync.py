17655a026da3c251857ed2da575f5b137ee5aee0
Michael Mraka <michael.mraka@redhat.com>
2011-10-24 13:56:18 +0200

    731692 - make number formating consistent across all units

--- backend/satellite_tools/satsync.py
+++ backend/satellite_tools/satsync.py
@@ -1807,19 +1807,17 @@ Please contact your RHN representative""") % (generation, sat_cert.generation))
 
     # Translate x bytes to string "x MB", "x GB" or "x kB"
     def _bytes_to_fuzzy(self, bytes):
-        kilo=1024
-        mega=kilo*1024
-        giga=mega*1024
-        if (bytes/kilo == 0):
-            return "%d bytes" %(bytes)
-        elif (bytes/mega == 0):
-            return "%d kiB" %(bytes / 1024)
-        elif (bytes/giga == 0):
-            mega_value=float(bytes) / mega
-            return "%.3f MiB" %(mega_value)
-        else:
-            giga_value=float(bytes) / giga
-            return "%.3f GiB" %(giga_value)
+        units = [ 'bytes', 'kiB', 'MiB', 'GiB', 'TiB', 'PiB']
+        base = 1024
+        fuzzy = bytes
+        for unit in units:
+            if fuzzy >= base:
+                fuzzy = float(fuzzy)/base
+            else:
+                break
+        int_len = len("%d" % fuzzy)
+        fract_len = 3 - int_len
+        return "%*.*f %s" % (int_len, fract_len, fuzzy, unit)
 
     def _get_package_stream(self, channel, package_id, nvrea, sources):
         """ returns (filepath, stream), so in the case of a "wire source",
