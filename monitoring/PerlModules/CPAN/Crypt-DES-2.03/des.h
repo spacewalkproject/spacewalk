f7119227dbb956bfe080c70d138827497ad58784
Jan Pazdziora <jpazdziora@redhat.com>
2011-10-24 15:24:15 +0200

    Removing Perl modulex that are (in newer versions) on CPAN.

--- monitoring/PerlModules/CPAN/Crypt-DES-2.03/des.h
+++
@@ -1,7 +0,0 @@
-typedef unsigned char des_user_key[8];
-typedef unsigned char des_cblock[8];
-typedef unsigned long des_ks[32];
-
-void des_crypt( des_cblock in, des_cblock out, des_ks key, int encrypt );
-void des_expand_key( des_user_key userKey, des_ks key );
-
