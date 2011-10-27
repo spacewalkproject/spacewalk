ece6447ff83de66444d7e939135c231c37419ce4
Milan Zazrivec <mzazrivec@redhat.com>
2011-10-27 10:59:06 +0200

    Revert "schema upgrade"
    This reverts commit cee165a5cdd26112f2cc2cbb9774f894c10675b2.

--- schema/spacewalk/upgrade/spacewalk-schema-1.5-to-spacewalk-schema-1.6/111-add_rhnServerNetAddress4_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO rhnServerNetAddress4
-    (interface_id, address, netmask, broadcast, created)
-    SELECT id, ip_addr, netmask, broadcast, created
-    FROM rhnServerNetInterface;
-
-COMMIT;
