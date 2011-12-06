f78a2f5a672ee8023e59b0d7290782b03b128d10
Miroslav Suchý <msuchy@redhat.com>
2011-12-06 16:07:26 +0100

    change build-missing-builds-in-koji.sh for tito 0.4 and upload tar.gz files only once and build only to those tags which are only missing the build

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
 
-if [ "$(basename $0)" == "build-missing-builds-in-brew.sh" ] ; then
-    TAGS="satellite-5.4-rhel-5-candidate"
-    KOJI_MISSING_BUILD_BREW_ARG="--brew"
-    TITO_BUILD_ARG="--cvs-release"
-else
-    TAGS="dist-5E-sw-1.6-candidate dist-6E-sw-1.6-candidate dist-f14-sw-1.6-candidate dist-f15-sw-1.6-candidate dist-f16-sw-1.6-candidate"
-    TITO_BUILD_ARG="--release"
-    FEDORA_UPLOAD=1
-fi
+TAGS="dist-5E-sw-1.6-candidate dist-6E-sw-1.6-candidate dist-f14-sw-1.6-candidate dist-f15-sw-1.6-candidate dist-f16-sw-1.6-candidate"
+FEDORA_UPLOAD=1
 
 pushd . >/dev/null
 pushd `dirname $0`/.. >/dev/null
@@ -17,21 +10,34 @@ pushd `dirname $0`/.. >/dev/null
 # say python to be nice to pipe
 export PYTHONUNBUFFERED=1
 
+declare -a PACKAGES
+
 for tag in $TAGS; do
   rel-eng/koji-missing-builds.py $KOJI_MISSING_BUILD_BREW_ARG --no-extra $tag | \
     perl -lne '/^\s+(.+)-.+-.+$/ and print $1' | \
-    xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring
-done | sort | uniq | \
-while read package ; do
+    xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring \
+    | \
+    while read package ; do
     (
-      echo Building package in path $package
+      echo Building package in path $package for $tag
       cd $package && \
-          echo y | ${TITO_PATH}tito build $TITO_BUILD_ARG | \
-          awk '/Wrote:.*tar.gz/ {print $2}' | \
-          if [ "0$FEDORA_UPLOAD" -eq 1 ] ; then
-              xargs -I packagepath scp packagepath fedorahosted.org:spacewalk
-          fi
+          ${TITO_PATH}tito release $tag && \
+          PACKAGES[${#PACKAGES[@]}]=$package
     )
+    done
 done
+if [ "0$FEDORA_UPLOAD" -eq 1 ] ; then
+  for package in "${PACKAGES[@]}"; do
+    echo $package
+  done | sort | uniq |\
+  while read package ; do
+  (
+      echo Uploading tgz for path $package
+      cd $package && LC_ALL=C ${TITO_PATH}tito build --tgz | \
+      awk '/Wrote:.*tar.gz/ {print $2}' | \
+      xargs -I packagepath scp packagepath fedorahosted.org:spacewalk
+  )
+  done
+fi
 
-popd
+popd >/dev/null
