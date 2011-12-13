07f293b59eac29a24b15ab82c3014725c7379b68
Miroslav Suchý <msuchy@redhat.com>
2011-12-13 12:27:38 +0100

    build package foo for tags el5, el6.. and then package bar for tags el5, el6
    currently it build everything for el5, then everything el6.
    Adeltonek does not like it.

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -11,21 +11,36 @@ pushd `dirname $0`/.. >/dev/null
 export PYTHONUNBUFFERED=1
 
 PACKAGES=$(mktemp)
+PACKAGES_TAG=$(mktemp)
 
+#gather data
+echo -n Gathering data:
 for tag in $TAGS; do
   rel-eng/koji-missing-builds.py $KOJI_MISSING_BUILD_BREW_ARG --no-extra $tag | \
     perl -lne '/^\s+(.+)-.+-.+$/ and print $1' | \
     xargs -I replacestring awk '{print $2}' rel-eng/packages/replacestring \
     | \
     while read package ; do
-    (
+      echo $package>$PACKAGES && \
+      echo $package $tag>$PACKAGES_TAG
+      echo -n .
+    done
+done
+echo
+
+#build packages
+cat $PACKAGES_TAG | sort | \
+while read package tag; do
+  (
       echo Building package in path $package for $tag
       cd $package && \
           ONLY_TAGS=$tag ${TITO_PATH}tito release koji && \
-          echo $package>$PACKAGES
-    )
-    done
+          echo $package>$PACKAGES && \
+          echo $package $tag>$PACKAGES_TAG
+  )
 done
+
+#upload tgz
 if [ "0$FEDORA_UPLOAD" -eq 1 ] ; then
   for package in $(cat $PACKAGES | sort | uniq); do
   (
