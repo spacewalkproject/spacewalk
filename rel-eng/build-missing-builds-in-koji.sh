3754e9bc8ed4437f71b0c13c48fadca8b1dd6420
Milan Zazrivec <mzazrivec@redhat.com>
2011-12-22 13:11:14 +0100

    use 1.7 koji tags

--- rel-eng/build-missing-builds-in-koji.sh
+++ rel-eng/build-missing-builds-in-koji.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-TAGS="dist-5E-sw-1.6-candidate dist-6E-sw-1.6-candidate dist-f14-sw-1.6-candidate dist-f15-sw-1.6-candidate dist-f16-sw-1.6-candidate"
+TAGS="dist-5E-sw-1.7-candidate dist-6E-sw-1.7-candidate dist-f15-sw-1.7-candidate dist-f16-sw-1.7-candidate"
 FEDORA_UPLOAD=1
 
 pushd . >/dev/null
