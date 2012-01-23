e8e976df476bc51ca95325f00c8d433428060dd7
Michael Mraka <michael.mraka@redhat.com>
2012-01-23 11:12:58 +0100

    756918 - fix data for package_group

--- schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/007-rhnPackageGroup-trim.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/007-rhnPackageGroup-trim.sql
@@ -1,3 +1,21 @@
+-- relink reference to group with trailing \n to correct one
+update rhnpackage set package_group = (select n.id
+                                         from rhnpackagegroup n
+                                         join rhnpackagegroup o
+                                           on substr(o.name, 1, length(o.name)-1) = n.name
+                                          and o.id <> n.id
+                                        where o.id = package_group)
+                where package_group in (select o.id
+                                          from rhnpackagegroup n
+                                          join rhnpackagegroup o
+                                            on substr(o.name, 1, length(o.name)-1) = n.name
+                                           and o.id <> n.id);
+
+-- delete unused groups
+delete from rhnpackagegroup pg
+      where not exists (select 1 from rhnpackage p where p.package_group = pg.id)
+        and id > 1; -- don't remove 'NoGroup'
+
 -- remove trailing \n
 update rhnpackagegroup set name = substr(name, 1, length(name)-1)
                      where name like '%' || chr(10);
