3d092050bcc07f852acdd7e93bed053dbbcc5115
Miroslav Suchý <msuchy@redhat.com>
2012-01-31 15:42:06 +0100

    rewrite rhn_wcon_disabled_seq.nextval to sequence_nextval('rhn_wcon_disabled_seq')

--- schema/util/enable-user.sql
+++ schema/util/enable-user.sql
@@ -1,7 +1,7 @@
 insert into rhnWebContactChangeLog 
 (id, web_contact_id, web_contact_from_id, change_state_id, date_completed)
 values
-(rhn_wcon_disabled_seq.nextval, 
+(sequence_nextval('rhn_wcon_disabled_seq'),
  (select id from web_contact where login_uc = upper('&login')),
  null,
  (select id from rhnWebContactChangeState where label = 'enabled'),
