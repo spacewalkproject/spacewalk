31f731fb4e93c7b52879fab04e853fb05ea50ce3
Michael Mraka <michael.mraka@redhat.com>
2012-02-08 10:43:59 +0100

    locally disabled W0702: 67:HandlerWrap.__call__: No exception type(s) specified

--- proxy/proxy/apacheServer.py
+++ proxy/proxy/apacheServer.py
@@ -64,6 +64,7 @@ class HandlerWrap:
             else:
                 raise Exception("Class has no attribute %s" % self.__name)
         except:
+            # pylint: disable=W0702
             Traceback(self.__name, req, extra = "Unhandled exception type",
                       severity="unhandled")
             return apache.HTTP_INTERNAL_SERVER_ERROR
