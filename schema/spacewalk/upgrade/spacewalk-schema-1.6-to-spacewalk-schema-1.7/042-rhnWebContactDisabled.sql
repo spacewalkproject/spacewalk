d84b1a66b4b3e3193d4118616db1787919504c24
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-14 17:12:59 +0100

    Regenerate rhnWebContactDisabled (we missed to update the view syntax in the past).

---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/042-rhnWebContactDisabled.sql
@@ -0,0 +1,43 @@
+--
+-- Copyright (c) 2008 Red Hat, Inc.
+--
+-- This software is licensed to you under the GNU General Public License,
+-- version 2 (GPLv2). There is NO WARRANTY for this software, express or
+-- implied, including the implied warranties of MERCHANTABILITY or FITNESS
+-- FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+-- along with this software; if not, see
+-- http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+-- 
+-- Red Hat trademarks are not licensed under GPLv2. No permission is
+-- granted to use or replicate Red Hat trademarks that are incorporated
+-- in this software or its documentation. 
+--
+--
+--
+--
+create or replace view
+rhnWebContactDisabled
+as
+select
+   wcon.id,
+   wcon.org_id,
+   wcon.login,
+   wcon.login_uc,
+   wcon.password,
+   wcon.old_password,
+   wcon.oracle_contact_id,
+   wcon.created,
+   wcon.modified,
+   wcon.ignore_flag
+from
+   rhnWebContactChangeLog   wccl,
+   rhnWebContactChangeState wccs,
+   web_contact              wcon
+where wccl.change_state_id = wccs.id
+   and wccs.label = 'disabled'
+   and wccl.web_contact_id = wcon.id
+   and wccl.date_completed =
+              (select max(wccl_exists.date_completed)
+                 from rhnWebContactChangeLog   wccl_exists
+                where wccl.web_contact_id = wccl_exists.web_contact_id);
+
