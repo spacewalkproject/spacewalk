eec8dff415ab40f06233127914712d4851a9e227
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-14 13:44:03 +0100

    Fixing typo in schema upgrade script; also, rhnServerOverview is not database specific.

--- schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/024-rhnServerOverview.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/024-rhnServerOverview.sql
@@ -1,3 +1,4 @@
+-- oracle equivalent source sha1 6ad08bc269a271e810c7e4942f282985536e7f07
 --
 -- Copyright (c) 2008--2010 Red Hat, Inc.
 --
@@ -31,7 +32,7 @@ rhnServerOverview
     bug_errata, 
     enhancement_errata,
     outdated_packages,
-    config_files_with_differences,
+	config_files_with_differences,
     last_checkin_days_ago,
     last_checkin,
     pending_updates,
@@ -42,7 +43,7 @@ rhnServerOverview
 )
 as
 select
-    s.org_id, s.id, s.name, 0, s.modified, 
+    s.org_id, s.id, s.name, 0, s.modified,
     ( select count(user_id) from rhnUserServerPerms ap 
       where server_id = s.id ), 
     ( select count(server_group_id) from rhnVisibleServerGroupMembers
@@ -53,7 +54,7 @@ select
        where SC.server_id = S.id
          and SC.channel_id = C.id
 	 and C.parent_channel IS NULL),
-    NVL(( select C.name
+    coalesce(( select C.name
         from rhnChannel C,
 	     rhnServerChannel SC
        where SC.server_id = S.id
@@ -96,7 +97,7 @@ select
          and ACR.failure_id is null
          and ACRR.result is not null
         ),
-    ( select sysdate - checkin from rhnServerInfo where server_id = S.id ),
+    ( select date_diff_in_days(checkin, current_timestamp) from rhnServerInfo where server_id = S.id ),
     ( select TO_CHAR(checkin, 'YYYY-MM-DD HH24:MI:SS') from rhnServerInfo where server_id = S.id ),
     ( select count(1) 
         from rhnServerAction
@@ -105,7 +106,7 @@ select
     os,
     release,
     ( select name from rhnServerArch where id = s.server_arch_id),
-    NVL((select 1 from rhnServerLock SL WHERE SL.server_id = S.id), 0)
+    coalesce((select 1 from rhnServerLock SL WHERE SL.server_id = S.id), 0)
 from 
     rhnServer S
-/
+;
