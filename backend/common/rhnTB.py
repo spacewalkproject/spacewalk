cf53fd032ce4bcd235d5cfaddd6cd82e7f0e5cac
Michael Mraka <michael.mraka@redhat.com>
2012-02-14 13:58:09 +0100

    fixed C0301:176: Line too long (123/120)

--- backend/common/rhnTB.py
+++ backend/common/rhnTB.py
@@ -173,7 +173,8 @@ def Traceback(method = None, req = None, mail = 1, ostream = sys.stderr,
         
         outstring = exc.getvalue()
         
-        #5/18/05 wregglej - 151158 Go through every string in the security list and censor it out of the debug information.
+        #5/18/05 wregglej - 151158 Go through every string in the security list
+        # and censor it out of the debug information.
         outstring = censor_string(outstring)
  
         rhnMail.send(headers, outstring)
