b0ce00f060844eb2054e46d6b5ba95dd90a0fb20
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-16 16:09:48 +0100

    The view rhnUserChannelFamilyPerms is the same on both database backends.

--- schema/spacewalk/common/views/rhnUserChannelFamilyPerms.sql
+++ schema/spacewalk/common/views/rhnUserChannelFamilyPerms.sql
@@ -1,7 +1,5 @@
--- oracle equivalent source sha1 e0428e62a46d0974943981499bdf16c9f5471b8f
--- retrieved from ./1235066623/21f37df477f4c9a372b85916798c9ad2ff734e58/schema/spacewalk/rhnsat/views/rhnUserChannelFamilyPerms.sql
 --
--- Copyright (c) 2008--2010 Red Hat, Inc.
+-- Copyright (c) 2008--2012 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -13,9 +11,6 @@
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
 -- in this software or its documentation. 
---
---
---
 --
 
 create or replace view rhnUserChannelFamilyPerms as
--- schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/050-rhnUserChannelFamilyPerms.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.6-to-spacewalk-schema-1.7/050-rhnUserChannelFamilyPerms.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2008 Red Hat, Inc.
+-- Copyright (c) 2008--2012 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -12,16 +12,13 @@
 -- granted to use or replicate Red Hat trademarks that are incorporated
 -- in this software or its documentation. 
 --
---
---
---
 
 create or replace view rhnUserChannelFamilyPerms as
 	select	pcf.channel_family_id,
-		u.org_id org_id,
-		u.id user_id,
-		to_number(null) max_members,
-		0 current_members,
+		u.org_id as org_id,
+		u.id as user_id,
+		to_number(null, null) as max_members,
+		0 as current_members,
 		pcf.created,
 		pcf.modified
 	from	rhnPublicChannelFamily pcf,
@@ -29,7 +26,7 @@ create or replace view rhnUserChannelFamilyPerms as
 	union
 	select	pcf.channel_family_id,
 		u.org_id,
-		u.id user_id,
+		u.id as user_id,
 		pcf.max_members,
 		pcf.current_members,
 		pcf.created,
@@ -38,12 +35,3 @@ create or replace view rhnUserChannelFamilyPerms as
 		web_contact u
 	where	u.org_id = pcf.org_id;
 
---
---
--- Revision 1.2  2004/04/16 16:07:12  pjones
--- bugzilla: none -- 8.1.7 won't let you use "null foo" as a column in a view
--- that gets unioned with a typed column.  you have to use "to_number(null) foo".  What a load of crap.
---
--- Revision 1.1  2004/04/14 15:58:39  pjones
--- bugzilla: none -- make rhnUserChannel work without org_id... (duh...)
---
