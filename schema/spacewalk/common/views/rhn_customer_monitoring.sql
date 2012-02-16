7223b609b82351db254a996b207765c311c5482d
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-16 16:33:34 +0100

    The view rhn_customer_monitoring is the same on both database backends.

--- schema/spacewalk/common/views/rhn_customer_monitoring.sql
+++ schema/spacewalk/common/views/rhn_customer_monitoring.sql
@@ -1,7 +1,5 @@
--- oracle equivalent source sha1 09ad5a18270ce086e9d71e895d1daf1952aa1e01
--- retrieved from ./1240273396/cea26e10fb65409287d4579c2409403b45e5e838/schema/spacewalk/oracle/views/rhn_customer_monitoring.sql
 --
--- Copyright (c) 2008--2010 Red Hat, Inc.
+-- Copyright (c) 2008--2012 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -14,10 +12,6 @@
 -- granted to use or replicate Red Hat trademarks that are incorporated
 -- in this software or its documentation. 
 --
---
---
---
---
 
 create or replace view rhn_customer_monitoring as
 select	org.id			as recid,
@@ -25,11 +19,9 @@ select	org.id			as recid,
 	1			as schedule_id,	--24 x 7
 	0			as def_ack_wait,
 	1			as def_strategy,	--Broadcast, No Ack
-	'GMT'::varchar		as preferred_time_zone,
+	'GMT'			as preferred_time_zone,
 	0			as auto_update	--Windows only
 from
 	web_customer org
---where	1=1
---	and some logic here to check for entitlements?
 ;
 
