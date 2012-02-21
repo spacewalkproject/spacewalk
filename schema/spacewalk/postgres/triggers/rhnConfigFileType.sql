7da10a07c70214e865310f190068abae15b98236
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-21 14:42:34 +0100

    Create the trigger rhn_conffiletype_mod_trig correctly on the rhnConfigFileType table.

--- schema/spacewalk/postgres/triggers/rhnConfigFileType.sql
+++ schema/spacewalk/postgres/triggers/rhnConfigFileType.sql
@@ -1,5 +1,5 @@
--- oracle equivalent source sha1 7782ed77c594224c04da703665fa32ca3cafaf92
--- retrieved from ./1241102873/cdc6d42049bf86fbc9f1d3a5c54275eeacbd641d/schema/spacewalk/oracle/triggers/rhnConfigFileType.sql
+-- oracle equivalent source sha1 9d9ff77590d40a1a4336fb49425f3f8804ff7e85
+
 create or replace function rhn_conffiletype_mod_trig_fun() returns trigger as
 $$
 begin
@@ -11,6 +11,6 @@ $$ language plpgsql;
 
 create trigger
 rhn_conffiletype_mod_trig
-before insert or update on rhnConfigFile
+before insert or update on rhnConfigFileType
 for each row
 execute procedure rhn_conffiletype_mod_trig_fun();
