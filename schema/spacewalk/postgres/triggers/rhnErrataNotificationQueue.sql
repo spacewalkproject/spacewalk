5e2a95f9baed9a4d86f3328bcf3aede68f122fff
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-21 13:54:46 +0100

    Create the trigger rhn_enqueue_mod_trig correctly on the rhnErrataNotificationQueue table.

--- schema/spacewalk/postgres/triggers/rhnErrataNotificationQueue.sql
+++ schema/spacewalk/postgres/triggers/rhnErrataNotificationQueue.sql
@@ -1,5 +1,5 @@
 -- oracle equivalent source sha1 71c579aa03b247561d70ba84001a8141c3c1f353
--- retrieved from ./1240273396/cea26e10fb65409287d4579c2409403b45e5e838/schema/spacewalk/oracle/triggers/rhnErrataNotificationQueue.sql
+
 create or replace function rhn_enqueue_mod_trig_fun() returns trigger as
 $$
 begin
@@ -10,6 +10,6 @@ $$ language plpgsql;
 
 create trigger
 rhn_enqueue_mod_trig
-before insert or update on rhnErrataKeywordTmp
+before insert or update on rhnErrataNotificationQueue
 for each row
 execute procedure rhn_enqueue_mod_trig_fun();
