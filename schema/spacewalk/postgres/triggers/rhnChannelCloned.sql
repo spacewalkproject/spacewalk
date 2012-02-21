c27a9534f34b52e1e20175a91176996b38320f3d
Jan Pazdziora <jpazdziora@redhat.com>
2012-02-21 13:27:53 +0100

    Consolidate both rhnChannelCloned triggers to one, add the new.modified initialization to the PostgreSQL variant.

--- schema/spacewalk/postgres/triggers/rhnChannelCloned.sql
+++ schema/spacewalk/postgres/triggers/rhnChannelCloned.sql
@@ -1,7 +1,6 @@
--- oracle equivalent source sha1 4b530d0069b9a4bf25f0512b2e7179217f599208
--- retrieved from ./1240273396/cea26e10fb65409287d4579c2409403b45e5e838/schema/spacewalk/oracle/triggers/rhnChannelCloned.sql
+-- oracle equivalent source sha1 61f46d4aaf2cd5709ab688458f29316b91764e58
 --
--- Copyright (c) 2008--2010 Red Hat, Inc.
+-- Copyright (c) 2008--2012 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -14,30 +13,29 @@
 -- granted to use or replicate Red Hat trademarks that are incorporated
 -- in this software or its documentation. 
 --
---
---
---
-
 
 create or replace function rhn_channel_cloned_comps_trig_fun() returns trigger
 as
 $$
 begin
-        -- if there are not comps in the cloned channel by now,
-        -- we shall clone comps from the original channel
-        insert into rhnChannelComps
-                ( id, channel_id, relative_filename,
-                        last_modified, created, modified )
-        select nextval('rhn_channelcomps_id_seq'), new.id, relative_filename,
-                        current_timestamp, current_timestamp, current_timestamp
-        from rhnChannelComps
-        where channel_id = new.original_id
-                and not exists (
-                        select 1
-                        from rhnChannelComps x
-                        where x.channel_id = new.id
-                );
+	new.modified := current_timestamp;
 
+	if tg_op = 'INSERT' then
+		-- if there are not comps in the cloned channel by now,
+		-- we shall clone comps from the original channel
+		insert into rhnChannelComps
+			( id, channel_id, relative_filename,
+				last_modified, created, modified )
+		select nextval('rhn_channelcomps_id_seq'), new.id, relative_filename,
+				current_timestamp, current_timestamp, current_timestamp
+		from rhnChannelComps
+		where channel_id = new.original_id
+			and not exists (
+				select 1
+				from rhnChannelComps x
+				where x.channel_id = new.id
+			);
+	end if;
         return new;
 end;
 $$
@@ -46,6 +44,6 @@ language plpgsql;
 
 create trigger
 rhn_channel_cloned_comps_trig
-before insert on rhnChannelCloned
+before insert or update on rhnChannelCloned
 for each row
 execute procedure rhn_channel_cloned_comps_trig_fun();
