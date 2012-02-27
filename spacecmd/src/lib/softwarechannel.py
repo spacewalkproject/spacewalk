2f20a2b3033963704d942be7189542e973b6f285
Steven Hardy <shardy@redhat.com>
2012-02-27 16:14:41 +0000

    spacecmd bugfix : softwarechannel_adderrata use cloneAsOriginal
    softwarechannel_adderrata should use errata.cloneAsOriginal when the api version
    supports it, or adding an errata can result in packages from an unrelated
    release version being added to the channel. Also explicitly adding packages to
    the destination channel is not required, since both errata.clone and
    errata.cloneAsOriginal add the packages themselves.

--- spacecmd/src/lib/softwarechannel.py
+++ spacecmd/src/lib/softwarechannel.py
@@ -1241,13 +1241,17 @@ def do_softwarechannel_adderrata(self, args):
         # lead to timeouts on the server
         for erratum in errata:
             logging.debug('Cloning %s' % erratum)
-            self.client.errata.clone(self.session, dest_channel, [erratum])
-
-    # add the affected packages to the channel
-    logging.info('Adding required packages to %s' % dest_channel)
-    self.client.channel.software.addPackages(self.session,
-                                             dest_channel,
-                                             package_ids)
+            if self.check_api_version('10.11'):
+                # This call is poorly documented, but it stops errata.clone
+                # pushing EL6 packages into EL5 channels when the errata 
+                # package list contains both versions, ref bz678721
+                self.client.errata.cloneAsOriginal(self.session, dest_channel,\
+                    [erratum])
+            else:
+                logging.warning("Using the old errata.clone function")
+                logging.warning("If you have base channels for multiple OS" +\
+                    " versions, check no unexpected packages have been added")
+                self.client.errata.clone(self.session, dest_channel, [erratum])
 
     # regenerate the errata cache since we just cloned errata
     self.generate_errata_cache(True)
