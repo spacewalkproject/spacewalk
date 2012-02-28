bcdad17441738f54c8ead6451e9c620f3558bd43
Milan Zazrivec <mzazrivec@redhat.com>
2012-02-28 17:35:33 +0100

    use pg_dblink_exec to execute insert inside lookup_package_nevra

--- schema/spacewalk/postgres/procs/lookup_package_nevra.sql
+++ schema/spacewalk/postgres/procs/lookup_package_nevra.sql
@@ -1,7 +1,6 @@
 -- oracle equivalent source sha1 9f3d83ca7b15edbc9013285a8303d725dcd4f006
--- retrieved from ./1241057068/d2f16725f65bddae85cd4782cd82e0c84c0a776d/schema/spacewalk/oracle/procs/lookup_package_nevra.sql
 --
--- Copyright (c) 2008--2010 Red Hat, Inc.
+-- Copyright (c) 2008--2012 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -13,12 +12,6 @@
 -- Red Hat trademarks are not licensed under GPLv2. No permission is
 -- granted to use or replicate Red Hat trademarks that are incorporated
 -- in this software or its documentation. 
---
---
---
---
-
-
 
 create or replace function
 lookup_package_nevra(
@@ -27,34 +20,44 @@ lookup_package_nevra(
         package_arch_id_in in numeric,
         ignore_null_name in numeric default 0
 ) returns numeric
-AS
+as
 $$
-DECLARE
-        nevra_id numeric;
-BEGIN
-        if ignore_null_name = 1 and name_id_in is null then
-                return null;
-        end if;
+declare
+    nevra_id numeric;
+begin
+    if ignore_null_name = 1 and name_id_in is null then
+        return null;
+    end if;
 
-        select  id
-        into    nevra_id
-        from    rhnPackageNEVRA
-        where   name_id = name_id_in
-                and evr_id = evr_id_in
-                and (package_arch_id = package_arch_id_in or
-                        (package_arch_id is null
-                         and package_arch_id_in is null));
+    select id
+      into nevra_id
+      from rhnPackageNEVRA
+     where name_id = name_id_in and
+           evr_id = evr_id_in and
+           (package_arch_id = package_arch_id_in or
+            (package_arch_id is null and package_arch_id_in is null));
 
-        IF NOT FOUND THEN
-		insert into rhnPackageNEVRA
-                        (id, name_id, evr_id, package_arch_id)
-                        values
-                        (       nextval('rhn_pkgnevra_id_seq'),
-                                name_id_in, evr_id_in, package_arch_id_in
-                        );
+    if not found then
+        nevra_id := nextval('rhn_pkgnevra_id_seq');
+        begin
+            perform pg_dblink_exec(
+                'insert into rhnPackageNEVRA(id, name_id, evr_id, package_arch_id) values (' ||
+                    nevra_id || ', ' ||
+                    coalesce(quote_literal(name_id_in), 'NULL') || ', ' ||
+                    coalesce(quote_literal(evr_id_in), 'NULL') || ', ' ||
+                    coalesce(quote_literal(package_arch_id_in), 'NULL') || ')');
                 nevra_id := currval('rhn_pkgnevra_id_seq');
-        END IF;
+        exception when unique_violation then
+            select id
+              into strict nevra_id
+              from rhnPackageNEVRA
+             where name_id = name_id_in and
+                   evr_id = evr_id_in and
+                   (package_arch_id = package_arch_id_in or
+                    (package_arch_id is null and package_arch_id_in is null));
+        end;
+    end if;
 
-        return nevra_id;
+    return nevra_id;
 end;
-$$ LANGUAGE PLPGSQL;
+$$ language plpgsql immutable;
