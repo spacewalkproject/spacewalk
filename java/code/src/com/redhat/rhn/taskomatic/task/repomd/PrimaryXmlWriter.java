7bd94e2cce0caa6d76ca29ee3933f9423f431002
Tomas Lestach <tlestach@redhat.com>
2012-02-28 10:57:49 +0100

    remove unused private static log

--- java/code/src/com/redhat/rhn/taskomatic/task/repomd/PrimaryXmlWriter.java
+++ java/code/src/com/redhat/rhn/taskomatic/task/repomd/PrimaryXmlWriter.java
@@ -24,7 +24,6 @@ import com.redhat.rhn.manager.task.TaskManager;
 import com.redhat.rhn.taskomatic.task.TaskConstants;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
 
 import java.io.ByteArrayOutputStream;
@@ -45,8 +44,6 @@ public class PrimaryXmlWriter extends RepomdWriter {
     private PackageCapabilityIterator recommendsIterator;
     private PackageCapabilityIterator suggestsIterator;
     private PackageCapabilityIterator supplementsIterator;
-    private static Logger log = Logger.getLogger(PrimaryXmlWriter.class);
-
     /**
      *
      * @param writer The writer object for primary xml
