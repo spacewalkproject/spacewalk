7dd85913124fabbda20af4c2c716f1c688681eb4
Tomas Lestach <tlestach@redhat.com>
2012-02-28 11:01:22 +0100

    remove unused private static final attributes

--- java/code/src/com/redhat/rhn/common/util/RecurringEventPicker.java
+++ java/code/src/com/redhat/rhn/common/util/RecurringEventPicker.java
@@ -53,13 +53,6 @@ public class RecurringEventPicker {
      */
     public static final String USE_DATE = "use_date";
 
-
-
-    private static final String MONTH = "month";
-    private static final String DAY = "day";
-    private static final String HOUR = "hour";
-    private static final String MINUTE = "minute";
-
     private static final String STATUS_DISABLED = "disabled";
 
 
