70974d5081e791d19f25782aecc0da18a899ff84
Milan Zazrivec <mzazrivec@redhat.com>
2012-02-29 17:26:55 +0100

    use pg_dblink_exec to execute insert inside lookup_xccdf_benchmark

--- schema/spacewalk/postgres/procs/lookup_xccdf_benchmark.sql
+++ schema/spacewalk/postgres/procs/lookup_xccdf_benchmark.sql
@@ -14,27 +14,35 @@
 -- in this software or its documentation.
 --
 
-CREATE OR REPLACE FUNCTION
-lookup_xccdf_benchmark(identifier_in IN VARCHAR, version_in IN VARCHAR)
-RETURNS NUMERIC
-AS
+create or replace function
+lookup_xccdf_benchmark(identifier_in in varchar, version_in in varchar)
+returns numeric
+as
 $$
-DECLARE
-    benchmark_id NUMERIC;
-BEGIN
-    SELECT id
-        INTO benchmark_id
-        FROM rhnXccdfBenchmark
-        WHERE identifier = identifier_in
-            AND version = version_in;
+declare
+    benchmark_id numeric;
+begin
+    select id
+      into benchmark_id
+      from rhnXccdfBenchmark
+     where identifier = identifier_in and version = version_in;
 
-    IF NOT FOUND THEN
-        INSERT INTO rhnXccdfBenchmark (id, identifier, version)
-            VALUES (nextval('rhn_xccdf_benchmark_id_seq'),
-                identifier_in, version_in)
-            RETURNING id INTO benchmark_id;
-    END IF;
+    if not found then
+        benchmark_id := nextval('rhn_xccdf_benchmark_id_seq');
+        begin
+            perform pg_dblink_exec(
+                'insert into rhnXccdfBenchmark (id, identifier, version) values (' ||
+                benchmark_id || ', ' ||
+                coalesce(quote_literal(identifier_in), 'NULL') || ', ' ||
+                coalesce(quote_literal(version_in), 'NULL') || ')');
+        exception when unique_violation then
+            select id
+              into strict benchmark_id
+              from rhnXccdfBenchmark
+             where identifier = identifier_in and version = version_in;
+        end;
+    end if;
 
-    RETURN benchmark_id;
-END;
-$$ LANGUAGE PLPGSQL;
+    return benchmark_id;
+end;
+$$ language plpgsql immutable;
