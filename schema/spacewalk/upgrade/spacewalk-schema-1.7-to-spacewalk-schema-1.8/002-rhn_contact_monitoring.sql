0af8b2f4f4123f71d6bb3113dba00827ade7d236
Milan Zazrivec <mzazrivec@redhat.com>
2012-03-07 18:24:13 +0100

    rhn_contact_monitoring: schema upgrade

---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/002-rhn_contact_monitoring.sql
@@ -0,0 +1,15 @@
+create or replace view rhn_contact_monitoring as
+select	u.id			as recid,
+	u.org_id		as customer_id,
+	wupi.last_name		as contact_last_name,
+	wupi.first_names	as contact_first_name,
+	wupi.email          as email_address,
+	u.login			as username,
+	u.password		as password,
+	1			as schedule_id,
+	'GMT'			as preferred_time_zone
+from
+	web_user_personal_info wupi,
+	web_contact u
+where	u.id = wupi.web_user_id
+;
