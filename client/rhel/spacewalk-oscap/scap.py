2ba74f1eefa093a1b6a78cc3f7d9bf95b9395657
Simon Lukasik <slukasik@redhat.com>
2012-03-29 15:50:42 +0200

    When errors occur submit them back to the server.
    On the web user interface user will see the following:
    Client execution returned "oscap tool did not produce valid xml.
    Profile "server" was not found. xccdf_eval: oscap tool returned 1" (code 1)

--- client/rhel/spacewalk-oscap/scap.py
+++ client/rhel/spacewalk-oscap/scap.py
@@ -19,7 +19,7 @@ def xccdf_eval(args, cache_only=None):
     oscap_err = _run_oscap(['xccdf', 'eval', '--results', results_file.name]
         + args['params'].split(' ') + [args['path']])
     if not _assert_xml(results_file.name):
-        return (1, 'oscap tool did not produce valid xml.', {})
+        return (1, 'oscap tool did not produce valid xml.\n' + oscap_err, {})
 
     ret, resume, xslt_err = _xccdf_resume(results_file.name)
     del(results_file)
