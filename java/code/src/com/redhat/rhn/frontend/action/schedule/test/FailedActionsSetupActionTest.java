f1ac6e32bb7abe7c105992ad9734d79530e75eb2
Tomas Lestach <tlestach@redhat.com>
2012-04-11 10:55:47 +0200

    fix FailedActionsSetupActionTest

--- java/code/src/com/redhat/rhn/frontend/action/schedule/test/FailedActionsSetupActionTest.java
+++ java/code/src/com/redhat/rhn/frontend/action/schedule/test/FailedActionsSetupActionTest.java
@@ -24,14 +24,14 @@ import com.redhat.rhn.domain.server.test.ServerFactoryTest;
 import com.redhat.rhn.frontend.struts.RhnAction;
 import com.redhat.rhn.manager.rhnset.RhnSetDecl;
 import com.redhat.rhn.manager.rhnset.RhnSetManager;
-import com.redhat.rhn.testing.RhnMockStrutsTestCase;
+import com.redhat.rhn.testing.RhnPostMockStrutsTestCase;
 import com.redhat.rhn.testing.TestUtils;
 
 /**
  * FailedActionsSetupActionTest
  * @version $Rev$
  */
-public class FailedActionsSetupActionTest extends RhnMockStrutsTestCase {
+public class FailedActionsSetupActionTest extends RhnPostMockStrutsTestCase {
 
 
     public void setUp() throws Exception {
