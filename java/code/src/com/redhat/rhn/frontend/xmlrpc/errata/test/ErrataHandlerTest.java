efcb99288904f7b02ca689075200560e6ebb528a
Tomas Lestach <tlestach@redhat.com>
2012-04-16 17:13:07 +0200

    fix ErrataHandlerTest

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/errata/test/ErrataHandlerTest.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/errata/test/ErrataHandlerTest.java
@@ -444,7 +444,7 @@ public class ErrataHandlerTest extends BaseHandlerTestCase {
         Map errataInfo = new HashMap();
 
 
-        String advisoryName = RandomStringUtils.random(37);
+        String advisoryName = RandomStringUtils.random(101);
         populateErrataInfo(errataInfo);
         errataInfo.put("advisory_name", advisoryName);
 
