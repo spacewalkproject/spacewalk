4b620e3578d4307ded234cc2533f47c32954dad2
Miroslav Suchý <msuchy@redhat.com>
2012-05-22 14:25:02 +0200

    719609 - set priorities between server and package arch

---
+++ schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/050-rhnServerPackageArchCompat.sql
@@ -0,0 +1,32 @@
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv7l-redhat-linux'), LOOKUP_PACKAGE_ARCH('armv7hnl'), 0);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv7l-redhat-linux'), LOOKUP_PACKAGE_ARCH('armv7hl'), 10);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv7l-redhat-linux'), LOOKUP_PACKAGE_ARCH('armv7l'), 20);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv7l-redhat-linux'), LOOKUP_PACKAGE_ARCH('armv6l'), 30);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv7l-redhat-linux'), LOOKUP_PACKAGE_ARCH('armv5tel'), 40);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv7-redhat-linux'), LOOKUP_PACKAGE_ARCH('noarch'), 1000);
+
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv5tejl-redhat-linux'), LOOKUP_PACKAGE_ARCH('armv5tel'), 0);
+
+insert into rhnServerPackageArchCompat
+(server_arch_id, package_arch_id, preference) values
+(LOOKUP_SERVER_ARCH('armv5tejl-redhat-linux'), LOOKUP_PACKAGE_ARCH('noarch'), 1000);
