a35ebe422a16df70487524bff28fcc43fa99312f
Jan Pazdziora <jpazdziora@redhat.com>
2012-05-29 11:21:47 +0200

    Disable rhn_server_channel_mod_trig while updating rhnServerChannel, so that modified does not get modified.

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/004-rhnServerChannel.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/004-rhnServerChannel.sql
@@ -1,4 +1,6 @@
 
+alter trigger rhn_server_channel_mod_trig disable;
 alter TABLE rhnServerChannel add is_fve  char default 'N' 
    CONSTRAINT rhn_server_channel_is_fve_nn NOT NULL
    CONSTRAINT rhn_server_channel_is_fve_ck CHECK (IS_FVE IN ('Y', 'N'));
+alter trigger rhn_server_channel_mod_trig enable;
