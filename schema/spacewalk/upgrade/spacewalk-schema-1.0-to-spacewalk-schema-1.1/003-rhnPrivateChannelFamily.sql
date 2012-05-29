0ca989826e19bde678162930d73cf9855f28c709
Jan Pazdziora <jpazdziora@redhat.com>
2012-05-29 11:48:31 +0200

    Disable rhn_privcf_mod_trig while updating rhnPrivateChannelFamily, so that modified does not get modified.

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/003-rhnPrivateChannelFamily.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/003-rhnPrivateChannelFamily.sql
@@ -1,3 +1,6 @@
+
+alter trigger rhn_privcf_mod_trig disable;
 alter table rhnPrivateChannelFamily add FVE_MAX_MEMBERS NUMBER default (0);
 alter table rhnPrivateChannelFamily add FVE_CURRENT_MEMBERS NUMBER
         DEFAULT (0) NOT NULL;
+alter trigger rhn_privcf_mod_trig enable;
