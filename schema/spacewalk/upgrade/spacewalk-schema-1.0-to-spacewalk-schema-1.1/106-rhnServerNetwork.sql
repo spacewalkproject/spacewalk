8b30385ccd7e0c9ee2b372fda84b7b1986bc995c
Jan Pazdziora <jpazdziora@redhat.com>
2012-05-29 11:14:58 +0200

    Disable rhn_servernetwork_mod_trig while updating rhnServerNetwork, so that modified does not get modified.

--- schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/106-rhnServerNetwork.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.0-to-spacewalk-schema-1.1/106-rhnServerNetwork.sql
@@ -1,2 +1,4 @@
 alter table rhnServerNetwork modify (ipaddr varchar(16));
+alter trigger rhn_servernetwork_mod_trig disable;
 update rhnServerNetwork set ipaddr=trim(ipaddr);
+alter trigger rhn_servernetwork_mod_trig enable;
