f926cc13ce62e70bfd221492cd3233773939bd9d
Michael Mraka <michael.mraka@redhat.com>
2012-06-15 16:38:53 +0200

    check_package_spec() was dead after up2date.package() removal

--- backend/server/handlers/xmlrpc/up2date.py
+++ backend/server/handlers/xmlrpc/up2date.py
@@ -304,50 +304,6 @@ class Up2date(rhnHandler):
                                                   deps, clientVersion, all, limit_operator, limit)
         return result
 
-def check_package_spec(package):
-    """ Check for a package spec correctness
-        Each package should be a list or a tuple of three or four members,
-        name, version, release, [epoch]
-        in case of lack of epoch we assume "" string
-        WARNING: we need to make sure we bound ALL values as strings because
-        the lack of epoch is suggested by the empty string (''), which is going
-        to cause problems if epoch gets bound as an integer.
-    """
-    # This one checks for sane values for name, version, release
-    def __check_Int_String(name, value, package = package):
-        if type(value) not in (StringType, IntType):
-            log_error("Field %s (%s)=`%s' in %s does not pass type checks" % (
-                name, type(value), str(value), str(package)))
-            raise rhnFault(30, _("Invalid value for %s in package tuple: "
-                                 "%s (%s)") % (name, value, type(value)))
-        value = str(value)
-        if not len(value):
-            log_error("Field %s has an EMPTY value in %s" % (value, package))
-        return value
-
-    log_debug(4, package)
-    # Checks if package is a proper package spec
-    if type(package) not in (ListType, TupleType) or len(package) < 3:
-        log_error("Package argument %s (len = %d) does not "
-                  "pass type checks" % (str(package), len(package)))
-        raise rhnFault(30, _("Invalid package parameter %s (%s)") %
-                       (package, type(package)))
-    name, version, release = package[:3]
-    # figure out the epoch
-    if len(package) > 3:
-        epoch = package[3]
-        if epoch in ["(none)", "None", None]:
-            epoch = ""
-        epoch = str(epoch)
-    else:
-        epoch = ""
-    # impose some validity checks on name, version, release
-    name = __check_Int_String("name", name)
-    version = __check_Int_String("version", version)
-    release = __check_Int_String("release", release)
-    # Fix up for safety
-    return [name, version, release, epoch]
-
 
 class Servers(rhnHandler):
     """ A class to handle the site selection... """
