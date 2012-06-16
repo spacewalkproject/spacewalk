9b930abf494222ccf4c086bb354db73950c15217
Miroslav Suchý <msuchy@redhat.com>
2012-06-16 13:38:47 +0200

    allow linking against openssl
    http://people.gnome.org/~markmc/openssl-and-the-gpl.html

--- client/rhel/rhn-client-tools/src/up2date_client/rhnregGui.py
+++ client/rhel/rhn-client-tools/src/up2date_client/rhnregGui.py
@@ -12,6 +12,20 @@
 # granted to use or replicate Red Hat trademarks that are incorporated
 # in this software or its documentation.
 #
+# In addition, as a special exception, the copyright holders give
+# permission to link the code of portions of this program with the
+# OpenSSL library under certain conditions as described in each
+# individual source file, and distribute linked combinations
+# including the two.
+# You must obey the GNU General Public License in all respects
+# for all of the code used other than OpenSSL.  If you modify
+# file(s) with this exception, you may extend this exception to your
+# version of the file(s), but you are not obligated to do so.  If you
+# do not wish to do so, delete this exception statement from your
+# version.  If you delete this exception statement from all source
+# files in the program, then also delete it here.
+
+
 
 # this is a module containing classes for the registration related windows in
 # gui.py. The code is split up so we can reuse it in the firstboot modules
