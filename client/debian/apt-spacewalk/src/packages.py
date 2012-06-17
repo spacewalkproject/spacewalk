77d82e9be387ddb3abddb250693e0e04848840a1
Miroslav Suchý <msuchy@redhat.com>
2012-06-17 12:23:37 +0200

    add copyright information to header of .py files

--- client/debian/apt-spacewalk/src/packages.py
+++ client/debian/apt-spacewalk/src/packages.py
@@ -3,10 +3,19 @@
 #
 # Author: Simon Lukasik
 #         Lukas Durfina
+# License: GPLv2
 #
 # TODO:   Be strict on architectures and package versions
 #         Staging content
 #
+# Copyright (c) 2012 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
 
 import os
 import sys
--- client/debian/apt-spacewalk/src/post_invoke.py
+++ client/debian/apt-spacewalk/src/post_invoke.py
@@ -6,7 +6,15 @@
 # Date:    2011-03-14
 # License: GPLv2
 #
+# Copyright (c) 1999--2012 Red Hat, Inc.
 #
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+
 
 import sys
 
