e8cb44dfbf3d9f5cd6d673335cc932894d5cabf3
Michael Mraka <michael.mraka@redhat.com>
2012-08-10 13:55:49 +0200

    843374 - make list of newest packages unique
    there can be two packages with the same NEVRA but different checksum

--- schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
+++ schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
@@ -20,6 +20,12 @@
 create or replace view
 rhnChannelNewestPackageView
 as
+SELECT channel_id,
+       name_id,
+       evr_id,
+       package_arch_id,
+       max(package_id) as package_id
+FROM (
 SELECT m.channel_id          as channel_id,
        p.name_id             as name_id,
        p.evr_id              as evr_id,
@@ -45,5 +51,7 @@ AND m.package_arch_id = p.package_arch_id
 AND p.evr_id = pe.id
 AND chp.package_id = p.id
 AND chp.channel_id = m.channel_id
+)
+group by channel_id, name_id, evr_id, package_arch_id
 ;
 
