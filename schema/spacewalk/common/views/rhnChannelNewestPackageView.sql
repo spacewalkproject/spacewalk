a4505264511ff975fa64599ed04789300d47fdf2
Michael Mraka <michael.mraka@redhat.com>
2012-08-10 14:00:06 +0200

    fixed indentation

--- schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
+++ schema/spacewalk/common/views/rhnChannelNewestPackageView.sql
@@ -26,31 +26,31 @@ SELECT channel_id,
        package_arch_id,
        max(package_id) as package_id
 FROM (
-SELECT m.channel_id          as channel_id,
-       p.name_id             as name_id,
-       p.evr_id              as evr_id,
-       m.package_arch_id     as package_arch_id,
-       p.id                  as package_id
-FROM
-    (select max(pe.evr) AS max_evr,
-         cp.channel_id,
-         p.name_id,
-         p.package_arch_id
-        from rhnPackageEVR       pe,
-         rhnPackage          p,
-         rhnChannelPackage   cp
-        where p.evr_id = pe.id
-         and cp.package_id = p.id
-        group by cp.channel_id, p.name_id, p.package_arch_id) m,
-    rhnPackageEVR       pe,
-    rhnPackage          p,
-    rhnChannelPackage   chp
-WHERE m.max_evr = pe.evr
-AND m.name_id = p.name_id
-AND m.package_arch_id = p.package_arch_id
-AND p.evr_id = pe.id
-AND chp.package_id = p.id
-AND chp.channel_id = m.channel_id
+       SELECT m.channel_id          as channel_id,
+              p.name_id             as name_id,
+              p.evr_id              as evr_id,
+              m.package_arch_id     as package_arch_id,
+              p.id                  as package_id
+         FROM
+              (select max(pe.evr) AS max_evr,
+                      cp.channel_id,
+                      p.name_id,
+                      p.package_arch_id
+                 from rhnPackageEVR       pe,
+                      rhnPackage          p,
+                      rhnChannelPackage   cp
+                where p.evr_id = pe.id
+                  and cp.package_id = p.id
+                group by cp.channel_id, p.name_id, p.package_arch_id) m,
+              rhnPackageEVR       pe,
+              rhnPackage          p,
+              rhnChannelPackage   chp
+        WHERE m.max_evr = pe.evr
+          AND m.name_id = p.name_id
+          AND m.package_arch_id = p.package_arch_id
+          AND p.evr_id = pe.id
+          AND chp.package_id = p.id
+          AND chp.channel_id = m.channel_id
 )
 group by channel_id, name_id, evr_id, package_arch_id
 ;
