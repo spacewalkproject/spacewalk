f89a70e86fa7344c1b0cc323a8f8173c99051800
Jan Pazdziora <jpazdziora@redhat.com>
2012-09-05 16:54:29 +0200

    849018 - extending rhnPackageCapability.version.

--- schema/spacewalk/common/tables/rhnPackageCapability.sql
+++ schema/spacewalk/common/tables/rhnPackageCapability.sql
@@ -1,5 +1,5 @@
 --
--- Copyright (c) 2008 Red Hat, Inc.
+-- Copyright (c) 2008--2012 Red Hat, Inc.
 --
 -- This software is licensed to you under the GNU General Public License,
 -- version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -20,7 +20,7 @@ CREATE TABLE rhnPackageCapability
                   CONSTRAINT rhn_pkg_capability_id_pk PRIMARY KEY
                   USING INDEX TABLESPACE [[4m_tbs]],
     name      VARCHAR2(4000) NOT NULL,
-    version   VARCHAR2(64),
+    version   VARCHAR2(512),
     created   DATE
                   DEFAULT (sysdate) NOT NULL,
     modified  DATE
