8fd5eff5cb03e44864c30f3a9c8ccf8891986440
Tomas Lestach <tlestach@redhat.com>
2012-10-23 10:29:27 +0200

    853444 - do not list custom base channel twice in the base channel combo for an activation key

--- java/code/src/com/redhat/rhn/frontend/action/token/ActivationKeyDetailsAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/token/ActivationKeyDetailsAction.java
@@ -284,10 +284,6 @@ public class ActivationKeyDetailsAction extends RhnAction {
             channelWidgets.add(lv(channel.getName(), String.valueOf(channel.getId())));
         }
 
-        List<Channel> customChannels = ChannelFactory.listCustomBaseChannels(user);
-        for (Channel channel : customChannels) {
-            channelWidgets.add(lv(channel.getName(), String.valueOf(channel.getId())));
-        }
         context.getRequest().setAttribute(POSSIBLE_CHANNELS, channelWidgets.toArray());
 
     }
