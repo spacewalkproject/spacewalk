00f2903853d3a63377f3c749ce21e87da3279ffc
Jan Pazdziora <jpazdziora@redhat.com>
2012-10-31 15:08:22 +0100

    Fixing syntax.
    Addressing
    psql:/var/log/spacewalk/schema-upgrade/20121031-094201-script.sql:2414: ERROR:  column "dcm" of relation "rhndistchannelmap" does not exist
    LINE 1: UPDATE rhnDistChannelMap dcm SET dcm.org_id = (SELECT c.org_...
                                             ^

--- schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/146-rhnDistChannelMap-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/146-rhnDistChannelMap-data.sql
@@ -1,1 +1,1 @@
-UPDATE rhnDistChannelMap dcm SET dcm.org_id = (SELECT c.org_id FROM rhnChannel c where c.id = dcm.channel_id);
+UPDATE rhnDistChannelMap dcm SET org_id = (SELECT c.org_id FROM rhnChannel c where c.id = dcm.channel_id);
