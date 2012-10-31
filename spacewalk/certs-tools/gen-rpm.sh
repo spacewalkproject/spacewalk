78b015a8727ba101965a17d232072414bb081be6
Michael Mraka <michael.mraka@redhat.com>
2012-10-31 15:45:11 +0100

    862349 - create rpms compatible with RHEL5
    fixing
    Retrieving http://172.22.xxx.xxx/pub/rhn-org-trusted-ssl-cert-1.0-1.noarch.rpm
    error: Failed dependencies:
           rpmlib(FileDigests) <= 4.6.0-1 is needed by rhn-org-trusted-ssl-cert-1.0-1.noarch
           rpmlib(PayloadIsXz) <= 5.2-1 is needed by rhn-org-trusted-ssl-cert-1.0-1.noarch

--- spacewalk/certs-tools/gen-rpm.sh
+++ spacewalk/certs-tools/gen-rpm.sh
@@ -340,7 +340,12 @@ RPMOPTS="--define \"_topdir $RPM_BUILD_DIR\"\
  --define '_sourcedir   %{_topdir}'\
  --define '_specdir     %{_topdir}'\
  --define '_rpmdir      %{_topdir}'\
- --define '_srcrpmdir   %{_topdir}'"
+ --define '_srcrpmdir   %{_topdir}'\
+ --define '_source_filedigest_algorithm md5'\
+ --define '_binary_filedigest_algorithm md5'\
+ --define '_source_payload nil'\
+ --define '_binary_payload nil'\
+ "
 
 eval "rpmbuild -ta $RPMOPTS --clean $RPM_BUILD_DIR/$TARBALL" || exit 1
 
