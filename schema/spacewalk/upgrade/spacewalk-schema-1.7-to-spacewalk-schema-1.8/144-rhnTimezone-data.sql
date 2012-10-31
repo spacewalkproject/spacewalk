3c5c60f569d8bdcfc0e1d25f52cd5c23e8c4610f
Jan Pazdziora <jpazdziora@redhat.com>
2012-10-31 14:39:17 +0100

    Fixing syntax.
    Addressing
    psql:/var/log/spacewalk/schema-upgrade/20121031-071549-script.sql:2386: ERROR:  missing FROM-clause entry for table "rhn_timezone_id_seq"
    LINE 2: VALUES (rhn_timezone_id_seq.nextval,
                    ^
    (cherry picked from commit a2a9cdaea474e0dfc17fa565323757190779a3df)

--- schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/144-rhnTimezone-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/144-rhnTimezone-data.sql
@@ -1,3 +1,3 @@
 INSERT INTO rhnTimezone (id, olson_name, display_name)
-VALUES (rhn_timezone_id_seq.nextval,
+VALUES (sequence_nextval('rhn_timezone_id_seq'),
         'Africa/Johannesburg', 'South Africa (Johannesburg)');
