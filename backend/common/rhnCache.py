2a94380fb876c8434d3436d0b1afb71fde796cda
Jan Pazdziora <jpazdziora@redhat.com>
2012-11-06 13:42:22 +0100

    The fcntl documentation recommends different pack format.

--- backend/common/rhnCache.py
+++ backend/common/rhnCache.py
@@ -34,9 +34,9 @@ from spacewalk.common.fileutils import makedirs, setPermsPath
 CACHEDIR = "/var/cache/rhn"
 
 # easy structures for locking stuff
-WRLOCK = pack("hhiiii", fcntl.F_WRLCK, 0, 0, 0, 0, 0)
-RDLOCK = pack("hhiiii", fcntl.F_RDLCK, 0, 0, 0, 0, 0)
-UNLOCK = pack("hhiiii", fcntl.F_UNLCK, 0, 0, 0, 0, 0)
+WRLOCK = pack("hhllhh", fcntl.F_WRLCK, 0, 0, 0, 0, 0)
+RDLOCK = pack("hhllhh", fcntl.F_RDLCK, 0, 0, 0, 0, 0)
+UNLOCK = pack("hhllhh", fcntl.F_UNLCK, 0, 0, 0, 0, 0)
 
 def cleanupPath(path):
     """take ~taw/../some/path/$MOUNT_POINT/blah and make it sensible."""
