8a6eb6ae2d076436acc1adb445299efaaafbc434
Jan Pazdziora <jpazdziora@redhat.com>
2012-11-21 10:35:25 +0100

    Revert "removed dead query"
    This reverts part of commit 719a2c75fdb8c31f16011a17aa9923a23abf6d63.
    
    The provisioning_systems_in_set_with_tag mode is still used.

--- web/modules/sniglets/Sniglets/ListView/SystemList.pm
+++ web/modules/sniglets/Sniglets/ListView/SystemList.pm
@@ -62,6 +62,10 @@ sub _register_modes {
 			   -datasource => RHN::DataSource::System->new,
                            -action_callback => \&ssm_remote_command_action_cb);
 
+  Sniglets::ListView::List->add_mode(-mode => "provisioning_systems_in_set_with_tag",
+			   -datasource => RHN::DataSource::System->new,
+			   -action_callback => \&ssm_rollback_by_tag_action_cb);
+
   Sniglets::ListView::List->add_mode(-mode => "visible_to_user",
 			   -datasource => RHN::DataSource::System->new);
 
