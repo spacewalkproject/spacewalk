818f91d4ef5a3db186d1c44fffd675ff4db3524a
Milan Zazrivec <mzazrivec@redhat.com>
2012-11-21 16:17:42 +0100

    877451 - honor yum's "proxy = _none_" settings

--- backend/satellite_tools/repo_plugins/yum_src.py
+++ backend/satellite_tools/repo_plugins/yum_src.py
@@ -130,7 +130,8 @@ class ContentSource(object):
             fileutils.makedirs(pkgdir, user='apache', group='apache')
         repo.pkgdir = pkgdir
 
-        if not repo.proxy and self.proxy_addr is not None:
+        if not (repo.cfg.has_option(self.name, 'proxy') or repo.cfg.has_option('main', 'proxy')) \
+            self.proxy_addr is not None:
             repo.proxy = "http://%s" % self.proxy_addr
             repo.proxy_username = self.proxy_user
             repo.proxy_password = self.proxy_pass
