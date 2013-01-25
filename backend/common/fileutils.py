a1724f3b6c48e825c8d34d2f0cf715d3b2ac6e30
Michael Mraka <michael.mraka@redhat.com>
2013-01-25 11:47:24 +0100

    disable false positive warnings
    fixing
    E1101:175,4:rhn_popen: Instance of 'Popen' has no 'stdin' member
    E1101:182,20:rhn_popen: Instance of 'Popen' has no 'stdout' member
    E1101:182,43:rhn_popen: Instance of 'Popen' has no 'stderr' member
    E1101:188,17:rhn_popen: Instance of 'Popen' has no 'poll' member
    (cherry picked from commit 0edfc1579efbdb8cf6a19a99339368a17d931aa8)
    (cherry picked from commit d34b80f29102c54aad17f9aff4929e1d4ce0aaab)

--- backend/common/fileutils.py
+++ backend/common/fileutils.py
@@ -167,6 +167,7 @@ def rhn_popen(cmd, progressCallback=None, bufferSize=16384, outputLog=None):
     cmd_is_list = type(cmd) in (types.ListType, types.TupleType)
     if cmd_is_list:
         cmd = map(str, cmd)
+    # pylint: disable=E1101
     c = subprocess.Popen(cmd, bufsize=0, stdin=subprocess.PIPE,
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                 close_fds=True, shell=(not cmd_is_list))
