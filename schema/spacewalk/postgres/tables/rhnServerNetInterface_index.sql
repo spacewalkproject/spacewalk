8e2dc3a3bbeb2f090f213903edb71b297f69016d
Tomas Kasparek <tkasparek@redhat.com>
2013-02-20 16:36:18 +0100

    Fixing typo

--- schema/spacewalk/postgres/tables/rhnServerNetInterface_index.sql
+++ schema/spacewalk/postgres/tables/rhnServerNetInterface_index.sql
@@ -1,4 +1,4 @@
--- oracle equivalent source sha185b76b18e46469c02356970e4ea513f91658d73a
+-- oracle equivalent source sha1 85b76b18e46469c02356970e4ea513f91658d73a
 -- function index for rhnServerNetInterface
 
 CREATE UNIQUE INDEX rhn_srv_net_iface_prim_iface
