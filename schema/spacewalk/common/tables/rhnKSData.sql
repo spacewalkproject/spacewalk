ec33a14df57e7bc72b8c9b4d970cacbb27e734d6
Jan Pazdziora <jpazdziora@redhat.com>
2013-02-26 12:01:13 +0100

    Adding missing parentheses.

--- schema/spacewalk/common/tables/rhnKSData.sql
+++ schema/spacewalk/common/tables/rhnKSData.sql
@@ -73,7 +73,7 @@ CREATE TABLE rhnKSData
                         DEFAULT (current_timestamp) NOT NULL,
     modified        timestamp with local time zone
                         DEFAULT (current_timestamp) NOT NULL,
-    update_type     VARCHAR2(7) NOT NULL DEFAULT 'none'
+    update_type     VARCHAR2(7) DEFAULT ('none') NOT NULL
                         CONSTRAINT rhn_ks_update_type
 			    CHECK (update_type in ('all', 'red_hat', 'none')),
     CONSTRAINT rhn_ks_type_ck
