386d1d7c8834893a7e7881488759cc99a6c467ed
Tomas Lestach <tlestach@redhat.com>
2013-03-04 11:25:11 +0100

    introduce SslContentSource class

---
+++ java/code/src/com/redhat/rhn/domain/channel/SslContentSource.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2013 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.domain.channel;
+
+import com.redhat.rhn.domain.kickstart.crypto.SslCryptoKey;
+
+
+/**
+ * SslContentSource
+ * @version $Rev$
+ */
+public class SslContentSource extends ContentSource {
+
+    private  SslCryptoKey caCert;
+    private  SslCryptoKey clientCert;
+    private  SslCryptoKey clientKey;
+
+    /**
+     * @return Returns the caCert.
+     */
+    public SslCryptoKey getCaCert() {
+        return caCert;
+    }
+
+    /**
+     * @param caCertIn The caCert to set.
+     */
+    public void setCaCert(SslCryptoKey caCertIn) {
+        caCert = caCertIn;
+    }
+
+    /**
+     * @return Returns the clientCert.
+     */
+    public SslCryptoKey getClientCert() {
+        return clientCert;
+    }
+
+    /**
+     * @param clientCertIn The clientCert to set.
+     */
+    public void setClientCert(SslCryptoKey clientCertIn) {
+        clientCert = clientCertIn;
+    }
+
+    /**
+     * @return Returns the clientKey.
+     */
+    public SslCryptoKey getClientKey() {
+        return clientKey;
+    }
+
+    /**
+     * @param clientKeyIn The clientKey to set.
+     */
+    public void setClientKey(SslCryptoKey clientKeyIn) {
+        clientKey = clientKeyIn;
+    }
+
+}
