650915ded685bc10cc383fedf3d1ef5c66e95bc0
Tomas Lestach <tlestach@redhat.com>
2013-03-07 18:52:26 +0100

    create CrashNote class and hibernate mapping

---
+++ java/code/src/com/redhat/rhn/domain/server/CrashNote.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2013 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.domain.server;
+
+import com.redhat.rhn.domain.BaseDomainHelper;
+import com.redhat.rhn.domain.user.User;
+
+
+/**
+ * CrashNote
+ * @version $Rev$
+ */
+public class CrashNote extends BaseDomainHelper {
+
+    private Long id;
+    private String subject;
+    private String note;
+    private User creator;
+    private Crash crash;
+
+    /**
+     * @return Returns the id.
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param idIn The id to set.
+     */
+    public void setId(Long idIn) {
+        id = idIn;
+    }
+
+    /**
+     * @return Returns the subject.
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subjectIn The subject to set.
+     */
+    public void setSubject(String subjectIn) {
+        subject = subjectIn;
+    }
+
+    /**
+     * @return Returns the note.
+     */
+    public String getNote() {
+        return note;
+    }
+
+    /**
+     * @param noteIn The note to set.
+     */
+    public void setNote(String noteIn) {
+        note = noteIn;
+    }
+
+    /**
+     * @return Returns the creator.
+     */
+    public User getCreator() {
+        return creator;
+    }
+
+    /**
+     * @param creatorIn The creator to set.
+     */
+    public void setCreator(User creatorIn) {
+        creator = creatorIn;
+    }
+
+    /**
+     * @return Returns the crash.
+     */
+    public Crash getCrash() {
+        return crash;
+    }
+
+    /**
+     * @param crashIn The crash to set.
+     */
+    public void setCrash(Crash crashIn) {
+        crash = crashIn;
+    }
+
+}
