391edc738d7081b6f4baf26c86c4d0c976213c61
Milan Zazrivec <mzazrivec@redhat.com>
2013-03-11 13:53:32 +0100

    fix lengths and names of identifiers

--- schema/spacewalk/common/tables/rhnServerCrashNote.sql
+++ schema/spacewalk/common/tables/rhnServerCrashNote.sql
@@ -20,11 +20,11 @@ CREATE TABLE rhnServerCrashNote
                    CONSTRAINT rhn_server_crash_note_id_pk PRIMARY KEY
                    USING INDEX TABLESPACE [[64k_tbs]],
     creator    NUMBER
-                   CONSTRAINT rhn_server_crash_note_creator_fk
+                   CONSTRAINT rhn_srv_crash_note_creator_fk
                        REFERENCES web_contact (id)
                        ON DELETE SET NULL,
     crash_id  NUMBER NOT NULL
-                   CONSTRAINT rhn_crashrnotes_sid_fk
+                   CONSTRAINT rhn_srv_crash_note_sid_fk
                        REFERENCES rhnServerCrash (id)
                        ON DELETE CASCADE,
     subject    VARCHAR2(80) NOT NULL,
@@ -37,14 +37,14 @@ CREATE TABLE rhnServerCrashNote
 ENABLE ROW MOVEMENT
 ;
 
-CREATE INDEX rhn_server_crash_note_sid_idx
+CREATE INDEX rhn_srv_crash_note_sid_idx
     ON rhnServerCrashNote (crash_id)
     TABLESPACE [[64k_tbs]]
     NOLOGGING;
 
-CREATE INDEX rhn_server_crash_note_creator_idx
+CREATE INDEX rhn_srv_crash_note_creator_idx
     ON rhnServerCrashNote (creator)
     TABLESPACE [[64k_tbs]]
     NOLOGGING;
 
-CREATE SEQUENCE rhn_server_crash_note_id_seq;
+CREATE SEQUENCE rhn_srv_crash_note_id_seq;
