06b87ba0fc31824b536eaf382d18887f9471a072
Michael Mraka <michael.mraka@redhat.com>
2013-03-27 10:51:10 +0100

    918333 - don't strip long path

--- schema/spacewalk/common/tables/rhnErrataFile.sql
+++ schema/spacewalk/common/tables/rhnErrataFile.sql
@@ -27,7 +27,7 @@ CREATE TABLE rhnErrataFile
     checksum_id NUMBER NOT NULL
                    CONSTRAINT rhn_erratafile_chsum_fk
                        REFERENCES rhnChecksum (id),
-    filename   VARCHAR2(1024) NOT NULL,
+    filename   VARCHAR2(4000) NOT NULL,
     created    timestamp with local time zone
                    DEFAULT (current_timestamp) NOT NULL,
     modified   timestamp with local time zone
