bb5e69275754b2aa8e92deec829be664f8b0bfe9
Milan Zazrivec <mzazrivec@redhat.com>
2013-04-05 16:31:34 +0200

    Insert new timezones only if they don't exist

--- schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/144-rhnTimezone-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.7-to-spacewalk-schema-1.8/144-rhnTimezone-data.sql
@@ -1,3 +1,12 @@
-INSERT INTO rhnTimezone (id, olson_name, display_name)
-VALUES (sequence_nextval('rhn_timezone_id_seq'),
-        'Africa/Johannesburg', 'South Africa (Johannesburg)');
+insert into rhnTimezone (id, olson_name, display_name) (
+    select sequence_nextval('rhn_timezone_id_seq'),
+           'Africa/Johannesburg',
+           'South Africa (Johannesburg)'
+      from dual
+     where not exists (
+           select 1
+             from rhnTimezone
+            where olson_name = 'Africa/Johannesburg'
+              and display_name = 'South Africa (Johannesburg)'
+     )
+);
--- schema/spacewalk/upgrade/spacewalk-schema-1.8-to-spacewalk-schema-1.9/004-rhnTimezone.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.8-to-spacewalk-schema-1.9/004-rhnTimezone.sql
@@ -2,8 +2,15 @@ update rhnTimezone
    set display_name = 'Australia (Eastern Daylight)'
  where olson_name = 'Australia/Sydney';
 
-insert into rhnTimezone
-  (id, olson_name, display_name)
-values
-  (sequence_nextval('rhn_timezone_id_seq'),
-   'Australia/Brisbane', 'Australia (Eastern Standard)');
+insert into rhnTimezone (id, olson_name, display_name) (
+    select sequence_nextval('rhn_timezone_id_seq'),
+           'Australia/Brisbane',
+           'Australia (Eastern Standard)'
+      from dual
+     where not exists (
+           select 1
+             from rhnTimezone
+            where olson_name = 'Australia/Brisbane'
+              and display_name = 'Australia (Eastern Standard)'
+     )
+);
