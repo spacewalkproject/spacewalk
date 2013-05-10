af1fdd902a64a6c53477a2025c7003eaad6449ff
Simon Lukasik <slukasik@redhat.com>
2013-05-10 09:02:48 +0200

    Client shall upload full results of the scap.xccdf_eval action

--- client/rhel/spacewalk-oscap/scap.py
+++ client/rhel/spacewalk-oscap/scap.py
@@ -1,11 +1,16 @@
 
 import sys
+import os
 import subprocess
 import xml.sax
 import tempfile
+import shutil
 from base64 import encodestring
 sys.path.append("/usr/share/rhn/")
 from up2date_client import up2dateLog
+from up2date_client import rhnserver
+from up2date_client import up2dateAuth
+from up2date_client import up2dateErrors
 
 __rhnexport__ = [ 'xccdf_eval' ]
 
@@ -15,21 +20,40 @@ def xccdf_eval(args, cache_only=None):
     if cache_only:
         return (0, 'no-ops for caching', {})
 
-    results_file = tempfile.NamedTemporaryFile()
-    params, oscap_err = _process_params(args['params'], results_file.name)
+    results_dir = None
+    if ('id' in args) and ('file_size' in args) and args['file_size'] > 0:
+        results_dir = tempfile.mkdtemp()
+        pwd = os.getcwd()
+        os.chdir(results_dir)
+
+    results_file = tempfile.NamedTemporaryFile(dir=results_dir)
+    params, oscap_err = _process_params(args['params'], results_file.name, results_dir)
+
     oscap_err += _run_oscap(['xccdf', 'eval'] + params + [args['path']])
+    if results_dir:
+        os.chdir(pwd)
 
     if not _assert_xml(results_file.name):
         del(results_file)
+        _cleanup_temp(results_dir)
         return (1, 'oscap tool did not produce valid xml.\n' + oscap_err, {})
 
     ret, resume, xslt_err = _xccdf_resume(results_file.name)
     del(results_file)
     if ret != 0 or resume == '':
+        _cleanup_temp(results_dir)
         return (1, 'Problems with extracting resume:\n' + xslt_err, {})
+
+    try:
+        up_err = _upload_results(results_dir, args)
+    except:
+        # An error during the upload must not prevent scan completion
+        log.log_exception(*sys.exc_info())
+        up_err = "Upload of detailed results failed. Fatal error in Python code occurred"
+    _cleanup_temp(results_dir)
     return (0, 'openscap scan completed', {
         'resume': encodestring(resume),
-        'errors': encodestring(oscap_err + xslt_err)
+        'errors': encodestring(oscap_err + xslt_err + up_err)
         })
 
 def _run_oscap(arguments):
@@ -56,8 +80,10 @@ def _popen(args):
     return subprocess.Popen(args, bufsize=-1, stdin=subprocess.PIPE,
         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
 
-def _process_params(args, filename):
+def _process_params(args, filename, results_dir=None):
     params = ['--results', filename]
+    if results_dir:
+        params += ['--oval-results']
     errors = ''
     if args:
         allowed_args = {
@@ -83,6 +109,39 @@ def _process_params(args, filename):
     if errors:
         errors += '\n'
     return params, errors
+
+def _upload_results(results_dir, args):
+    errors = ''
+    if results_dir:
+        server = rhnserver.RhnServer()
+        # No need to check capabilities. The server supports detailed results
+        # If rhe 'file_size' and 'id' was in supplied in the argument list.
+        systemid = up2dateAuth.getSystemId()
+        for filename in os.listdir(results_dir):
+            path = os.path.join(results_dir, filename)
+            f = open(path, 'r')
+            stat = os.fstat(f.fileno())
+            if stat.st_size < args['file_size']:
+                try:
+                    ret = server.scap.upload_result(systemid, args['id'],
+                         {'filename': filename,
+                          'filecontent': encodestring(f.read()),
+                          'content-encoding': 'base64',
+                         })
+                    if ret and ret['result']:
+                        log.log_debug('The file %s uploaded successfully.' % filename)
+                except up2dateErrors.Error, e:
+                    log.log_exception(*sys.exc_info())
+                    errors += '\nFile "%s" not uploaded. %s' % (filename, e)
+            else:
+                errors += '\nFile "%s" not uploaded. File size (%d B) exceeds the limit.' \
+                       % (filename, stat.st_size)
+            f.close()
+    return errors
+
+def _cleanup_temp(results_dir):
+    if results_dir:
+        shutil.rmtree(results_dir)
 
 def _assert_xml(filename):
     f = open(filename, 'rb')
