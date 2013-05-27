046e1137c7c4b5be5c6f2df45cc419fb297c02d8
Grant Gainey <ggainey@redhat.com>
2013-05-27 11:55:12 -0400

    Make master-to-slave-org mapping be many-to-one instead of many-to-many

--- schema/spacewalk/common/tables/rhnSyncMap.sql
+++
@@ -1,7 +0,0 @@
-create table rhnSyncMap (
-    source_id number not null
-        constraint rhn_sync_orgs_sid_fk references rhnSyncOrgs(id) on delete cascade,
-    target_id number not null
-        constraint web_customer_tid_fk references web_customer(id) on delete cascade,
-    created timestamp with local time zone default (current_timestamp) not null
-);
--- schema/spacewalk/common/tables/rhnSyncOrgs.sql
+++ schema/spacewalk/common/tables/rhnSyncOrgs.sql
@@ -1,9 +1,11 @@
 create table rhnSyncOrgs (
     id number not null constraint rhn_syncorgs_id_pk primary key,
     catalogue_id number not null
-        constraint rhn_syncorgcatalogue_cid_fk references rhnSyncOrgCatalogue(id) on delete cascade,
+        constraint rhn_syncorgs_cid_fk references rhnSyncOrgCatalogue(id) on delete cascade,
     source_org_id number not null,
     source_org_name varchar2(512) not null,
+    target_id number not null
+        constraint rhn_syncorgs_tid_fk references web_customer(id),
     created timestamp with local time zone default (current_timestamp) not null
 );
 
