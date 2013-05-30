1e6218c04e64d49152dd086165e13a7233a78008
Michael Mraka <michael.mraka@redhat.com>
2013-05-30 10:45:54 +0200

    severity may be unknown
    fixing
    unsupported operand type(s) for +: 'NoneType' and 'str'

--- backend/satellite_tools/reposync.py
+++ backend/satellite_tools/reposync.py
@@ -230,7 +230,7 @@ class RepoSync(object):
             e['product']       = notice['release'] or 'Unknown'
             e['description']   = notice['description']
             e['synopsis']      = notice['title'] or notice['update_id']
-            if (notice['type'] == 'security' and
+            if (notice['type'] == 'security' and notice['severity'] and
                 not e['synopsis'].startswith(notice['severity'] + ': ')):
                 e['synopsis'] = notice['severity'] + ': ' + e['synopsis']
             e['topic']         = ' '
