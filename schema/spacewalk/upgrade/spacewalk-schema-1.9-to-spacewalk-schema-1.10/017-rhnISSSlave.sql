d31dde4c5e7716b8549a0aa9eadf85a0b64f1006
Grant Gainey <ggainey@redhat.com>
2013-06-04 09:58:58 -0400

    Fixing whitespace

--- schema/spacewalk/upgrade/spacewalk-schema-1.9-to-spacewalk-schema-1.10/017-rhnISSSlave.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.9-to-spacewalk-schema-1.10/017-rhnISSSlave.sql
@@ -1,13 +1,12 @@
-
 create table rhnISSSlave (
-	id number not null constraint rhn_isss_id_pk primary key,
-	slave varchar2(512) not null constraint rhn_isss_name_uq unique,
-	enabled char(1) not null default 'Y',
-		constraint rhn_isss_enabled_yn check (enabled in ( 'Y', 'N' )),
-	allow_all_orgs char(1) not null default 'Y',
-		constraint rhn_isss_allorgs_yn check (allow_all_orgs in ( 'Y', 'N' )),
-	created timestamp with local time zone default (current_timestamp) not null,
-	modified timestamp with local time zone default (current_timestamp) not null
+    id number not null constraint rhn_isss_id_pk primary key,
+    slave varchar2(512) not null constraint rhn_isss_name_uq unique,
+    enabled char(1) not null default 'Y',
+        constraint rhn_isss_enabled_yn check (enabled in ( 'Y', 'N' )),
+    allow_all_orgs char(1) not null default 'Y',
+        constraint rhn_isss_allorgs_yn check (allow_all_orgs in ( 'Y', 'N' )),
+    created timestamp with local time zone default (current_timestamp) not null,
+    modified timestamp with local time zone default (current_timestamp) not null
 );
 
 create sequence rhn_issslave_seq;
--- schema/spacewalk/upgrade/spacewalk-schema-1.9-to-spacewalk-schema-1.10/018-rhnISSSlaveOrgs.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.9-to-spacewalk-schema-1.10/018-rhnISSSlaveOrgs.sql
@@ -1,9 +1,8 @@
-
 create table rhnISSSlaveOrgs (
-	id number not null constraint rhn_isssorg_id_pk primary key,
-	slave_id number not null constraint rhn_isssorg_sid_fk references rhnISSSlave(id) on delete cascade,
-	org_id number not null constraint rhn_isssorg_oid_fk references web_customer(id) on delete cascade,
-	created timestamp with local time zone default (current_timestamp) not null
+    id number not null constraint rhn_isssorg_id_pk primary key,
+    slave_id number not null constraint rhn_isssorg_sid_fk references rhnISSSlave(id) on delete cascade,
+    org_id number not null constraint rhn_isssorg_oid_fk references web_customer(id) on delete cascade,
+    created timestamp with local time zone default (current_timestamp) not null
 );
 
 create sequence rhn_issslave_org_seq;
