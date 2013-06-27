b9947816810a883fb9d7dd150282557736a8407c
Milan Zazrivec <mzazrivec@redhat.com>
2013-06-27 09:49:13 +0200

    fix sql syntax

--- schema/spacewalk/common/tables/rhnISSSlave.sql
+++ schema/spacewalk/common/tables/rhnISSSlave.sql
@@ -2,7 +2,7 @@
 create table rhnISSSlave (
 	id number not null constraint rhn_isss_id_pk primary key,
 	slave varchar2(512) not null constraint rhn_isss_name_uq unique,
-	enabled char(1) not null default 'Y',
+	enabled char(1) default 'Y' not null
 		constraint rhn_isss_enabled_yn check (enabled in ( 'Y', 'N' )),
 	allow_all_orgs char(1) not null default 'Y',
 		constraint rhn_isss_allorgs_yn check (allow_all_orgs in ( 'Y', 'N' )),
