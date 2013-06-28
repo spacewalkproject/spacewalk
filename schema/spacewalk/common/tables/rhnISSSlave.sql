e07535fcc9a932b2b49f24a09b1f350a1269b890
Dimitar Yordanov <dyordano@redhat.com>
2013-06-28 12:20:16 +0200

    fix sql syntax.

--- schema/spacewalk/common/tables/rhnISSSlave.sql
+++ schema/spacewalk/common/tables/rhnISSSlave.sql
@@ -4,7 +4,7 @@ create table rhnISSSlave (
 	slave varchar2(512) not null constraint rhn_isss_name_uq unique,
 	enabled char(1) default 'Y' not null
 		constraint rhn_isss_enabled_yn check (enabled in ( 'Y', 'N' )),
-	allow_all_orgs char(1) not null default 'Y',
+	allow_all_orgs char(1) default 'Y' not null 
 		constraint rhn_isss_allorgs_yn check (allow_all_orgs in ( 'Y', 'N' )),
 	created timestamp with local time zone default (current_timestamp) not null,
 	modified timestamp with local time zone default (current_timestamp) not null
