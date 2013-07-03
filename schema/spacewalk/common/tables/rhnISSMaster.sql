fb7e32847df91726897146fadb487502042a9890
Stephen Herr <sherr@redhat.com>
2013-07-03 10:01:10 -0400

    977878 - ca cert path field was not long enough

--- schema/spacewalk/common/tables/rhnISSMaster.sql
+++ schema/spacewalk/common/tables/rhnISSMaster.sql
@@ -3,7 +3,7 @@ create table rhnIssMaster (
     label varchar2(256) not null constraint rhn_iss_master_label_uq unique,
     created timestamp with local time zone default (current_timestamp) not null,
     is_current_master char(1) default 'N' not null constraint rhn_issm_master_yn check (is_current_master in ('Y', 'N')),
-    ca_cert varchar2(256)
+    ca_cert varchar2(1024)
 );
 
 create sequence rhn_issmaster_seq;
