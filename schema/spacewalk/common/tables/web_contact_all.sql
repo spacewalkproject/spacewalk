dfd40d8f3eea533efcbef33efe1ac2ff31fc7fd1
Tomas Lestach <tlestach@redhat.com>
2013-07-11 20:28:52 +0200

    web_contact_all may not reference web_customer
    otherwise we're not able to delete org

--- schema/spacewalk/common/tables/web_contact_all.sql
+++ schema/spacewalk/common/tables/web_contact_all.sql
@@ -19,9 +19,7 @@ CREATE TABLE web_contact_all
     id                 NUMBER
                            CONSTRAINT web_contact_all_pk PRIMARY KEY
                            USING INDEX TABLESPACE [[web_index_tablespace_2]],
-    org_id             NUMBER
-                           CONSTRAINT web_contact_all_org_fk
-                               REFERENCES web_customer (id),
+    org_id             NUMBER,
     login              VARCHAR2(64) NOT NULL
 )
 ENABLE ROW MOVEMENT;
