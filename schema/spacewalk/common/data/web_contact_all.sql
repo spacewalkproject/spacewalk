99af3ab6b863a9e518796b8dda801548cb3c2191
Tomas Lestach <tlestach@redhat.com>
2013-07-12 13:28:38 +0200

    populate web_contact_all log users

---
+++ schema/spacewalk/common/data/web_contact_all.sql
@@ -0,0 +1,4 @@
+INSERT INTO web_contact_all (id, org_id, login)
+    VALUES (-1, null, 'SETUP');
+INSERT INTO web_contact_all (id, org_id, login)
+    VALUES (-2, null, 'CLIENT');
--- schema/spacewalk/upgrade/spacewalk-schema-1.9-to-spacewalk-schema-1.10/030-web_contact_all-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-1.9-to-spacewalk-schema-1.10/030-web_contact_all-data.sql
@@ -1,3 +1,6 @@
-
+INSERT INTO web_contact_all (id, org_id, login)
+        VALUES (-1, null, 'SETUP');
+INSERT INTO web_contact_all (id, org_id, login)
+        VALUES (-2, null, 'CLIENT');
 INSERT INTO web_contact_all (id, org_id, login)
     SELECT id, org_id, login FROM web_contact;
