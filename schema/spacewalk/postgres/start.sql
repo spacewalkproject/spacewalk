36b1e13097eebbb8560fdcdbc64ed91b3dbf5dc8
Tomas Lestach <tlestach@redhat.com>
2013-07-16 17:05:32 +0200

    restore search path, that dblink.sql destroys
    addressing spacewalk-schema-diff issue:
    - SET search_path TO logging,public
    + SET search_path TO logging,,public

--- schema/spacewalk/postgres/start.sql
+++ schema/spacewalk/postgres/start.sql
@@ -14,7 +14,11 @@
 -- in this software or its documentation.
 --
 
+create temporary table store_searchpath as select setting from pg_settings where name = 'search_path';
+
 -- The spaces in front of \i are needed to stop blend from expanding
 -- in build time.
    \i /usr/share/pgsql/contrib/dblink.sql
 
+update pg_settings set setting = (select setting from store_searchpath) where name = 'search_path';
+drop table store_searchpath;
