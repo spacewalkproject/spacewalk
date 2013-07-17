db80b5977366f73243ed7014c808c70c97959650
Michael Mraka <michael.mraka@redhat.com>
2013-07-17 15:40:47 +0200

    get install time for packages with arch
    http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=703582

--- client/rhel/rhn-client-tools/src/up2date_client/debUtils.py
+++ client/rhel/rhn-client-tools/src/up2date_client/debUtils.py
@@ -34,12 +34,15 @@ def parseVRE(version):
         release = tmp[-1]
     return version, release, epoch
 
-def installTime(pkg_name):
-    path = '/var/lib/dpkg/info/%s.list' % pkg_name
-    if os.path.isfile(path):
-       return os.path.getmtime(path)
-    else:
-       return None
+def installTime(pkg_name, pkg_arch):
+    dir = '/var/lib/dpkg/info'
+    files = [ '%s.list' % pkg_name,
+              '%s:%s.list' % (pkg_name, pkg_arch) ]
+    for f in files:
+        path = os.path.join(dir,f)
+        if os.path.isfile(path):
+            return os.path.getmtime(path)
+    return None
 
 #FIXME: Using Apt cache might not be an ultimate solution.
 # It could be better to parse /var/lib/dpkg/status manually.
@@ -70,7 +73,7 @@ def getInstalledPackageList(msgCallback = None, progressCallback = None,
             'version': version,
             'release': release,
             'arch': pkg.installed.architecture + '-deb',
-            'installtime': installTime(pkg.name)
+            'installtime': installTime(pkg.name, pkg.installed.architecture)
             }
         pkg_list.append(package)
 
