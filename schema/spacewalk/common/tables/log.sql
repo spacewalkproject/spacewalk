8f6a61b5f120467c97a574d6f8ff6ebebc695c6f
Tomas Lestach <tlestach@redhat.com>
2013-08-06 11:19:57 +0200

    name constraints on the log table

--- schema/spacewalk/common/tables/log.sql
+++ schema/spacewalk/common/tables/log.sql
@@ -16,9 +16,12 @@
 
 CREATE TABLE log
 (
-    id NUMBER NOT NULL PRIMARY KEY,
+    id NUMBER NOT NULL
+        CONSTRAINT log_id_pk PRIMARY KEY,
     stamp TIMESTAMP WITH LOCAL TIME ZONE,
-    user_id NUMBER REFERENCES web_contact_all(id)
+    user_id NUMBER
+        CONSTRAINT log_user_id_fk
+            REFERENCES web_contact_all(id)
 )
 ENABLE ROW MOVEMENT
 ;
