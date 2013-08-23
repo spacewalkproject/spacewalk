683e4debba61b41caadd54cd78d7164b65984f8c
Stephen Herr <sherr@redhat.com>
2013-08-23 15:52:58 -0400

    1000586 - fix checkstyle errors

--- proxy/proxy/broker/rhnBroker.py
+++ proxy/proxy/broker/rhnBroker.py
@@ -76,7 +76,7 @@ class BrokerHandler(SharedHandler):
                     # entry on the client or the hostname of some other
                     # machine (say a load balancer)) then use it
                     hostname = req.headers_in['Host']
-            except:
+            except Exception:
                 # hostname probably didn't exist, fine
                 pass
         if not hostname:
@@ -84,7 +84,7 @@ class BrokerHandler(SharedHandler):
             # ip address
             try:
                 hostname = socket.gethostbyaddr(my_ip_addr)[0]
-            except:
+            except Exception:
                 # unknown host, we don't have a hostname?
                 pass
         if not hostname:
