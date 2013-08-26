17eda3b9ef1f086c6d75dd7bf894c400a93d3272
Dimitar Yordanov <dyordano@redhat.com>
2013-08-26 15:34:43 +0200

    993978 - removing oracle specific code
    as oracle is no longer supported by db-control the oracle specific code can be now safely removed

--- web/modules/dobby/scripts/check-database-space-usage.sh
+++ web/modules/dobby/scripts/check-database-space-usage.sh
@@ -17,32 +17,6 @@
 PATH=/usr/bin:/bin
 export PATH
 
-get_db_backend() {
-   if [ ! -e /usr/bin/spacewalk-cfg-get ]; then
-      if [ x$(grep db_backend /etc/rhn/rhn.conf | awk '{print $3}') = x"postgresql" ]; then
-         echo "postgresql"
-      else
-         echo "oracle"
-      fi
-   else
-      if [ x$(spacewalk-cfg-get db_backend) = x"postgresql" ]; then
-         echo "postgresql"
-      else
-         echo "oracle"
-      fi
-   fi
-}
-
-reportusage() {
-   # both command produce percent as 5th field and first row is header, which is ignored
-   if [ x"$(get_db_backend)" = x"postgresql" ]; then
-      df -hP /var/lib/pgsql/data/
-   else
-      # don't report UNDO usage
-       /sbin/runuser oracle -c "db-control report" | grep -E -v 'UNDO|TEMP'
-   fi
-}
-
 mailitout() {
    local reportusage=$1
 
@@ -52,28 +26,18 @@ mailitout() {
       MAILADDRESS=$(spacewalk-cfg-get traceback_mail)
    fi
 
-   if [ x$(get_db_backend) = x"postgresql" ]; then
-      SUBJECT="Warning - PostgreSQL database mount point is running out of space"
-      BODY="This is a notice to let you know that you have gone over 90% usage of
+   SUBJECT="Warning - PostgreSQL database mount point is running out of space"
+   BODY="This is a notice to let you know that you have gone over 90% usage of
 the mount point where the PostgreSQL database resides. We recommend to be
 proactive and increase the storage before getting to 100% usage."
-   else
-      SUBJECT="Warning - high tablespace usage on Satellite oracle DB"
-
-      BODY="This is a notice to let you know that you have gone over 90% usage in
-one of the Oracle Tablespaces. We recommend to be proactive and increase the
-size of the tablespace  before getting to 100% usage. Please consult
-the Satellite documentation on using db-control to increase the size or
-contact Red Hat Support for assistance."
-   fi
 
    echo -e "$BODY\n\n$reportusage" | mail -s "$SUBJECT" $MAILADDRESS
    exit 0
 }
-#grab the usage numbers from the db-control report output
-REPORTUSAGE=$(reportusage)
+
+REPORTUSAGE=$(df -hP /var/lib/pgsql/data/)
 NUMBERS=$(echo "$REPORTUSAGE" | awk '{if (FNR > 1) {sub("%",""); print $5}}')
-# run db-control and then use awk and sed to get the % numbers
+
 for num in $NUMBERS
    do
    # if number is over 90% then send warning email
