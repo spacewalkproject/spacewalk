spacewalkproject/spacewalk

commit 22f7bef174035bb86f89bbd34f51c8a95984aa84

582510 - disable ks repos without available repodata

java/code/src/com/redhat/rhn/manager/kickstart/KickstartEditCommand.java
@@ -40,7 +40,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -367,14 +366,8 @@ public class KickstartEditCommand extends BaseKickstartCommand {
     public void updateRepos(String[] reposIn) {
         if (ksdata.isRhel5OrGreater()) {
 
-            Map<String, RepoInfo> repos = RepoInfo.getStandardRepos(
-                    ksdata.getKickstartDefaults().getKstree());
-            Set<RepoInfo> selected = new HashSet <RepoInfo>();
-
-            for (int i = 0; i < reposIn.length; i++) {
-                selected.add(repos.get(reposIn[i]));
-            }
-            ksdata.setRepoInfos(selected);
+            ksdata.setRepoInfos(RepoInfo.getStandardRepos(
+                    ksdata.getKickstartDefaults().getKstree()));
             KickstartWizardHelper ksHelper = new KickstartWizardHelper(user);
             ksHelper.processSkipKey(ksdata);
         }

java/code/src/com/redhat/rhn/domain/kickstart/RepoInfo.java
@@ -22,8 +22,8 @@ import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import java.io.File;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -35,6 +35,7 @@ import java.util.regex.Pattern;
 public class RepoInfo {
     private String name;
     private String baseUrl;
+    private boolean available;
 
     private static final Pattern NAME_REGEX =
         Pattern.compile("--name\\s*=\\s*(\\w+)", Pattern.CASE_INSENSITIVE);
@@ -71,6 +72,19 @@ public class RepoInfo {
         RepoInfo info = new RepoInfo();
         info.name  = name;
         info.baseUrl = baseUrl;
+        return info;
+    }
+
+    /**
+     * Create a new repo info object with the given name and base url
+     * @param name the repo name
+     * @param baseUrl the repo URL
+     * @param available is the repo available
+     * @return the Repo info object
+     */
+    public static RepoInfo create(String name, String baseUrl, boolean available) {
+        RepoInfo info = create(name, baseUrl);
+        info.setAvailable(available);
         return info;
     }
 
@@ -131,8 +145,8 @@ public class RepoInfo {
      * @param tree the kickstartable tree
      * @return the standard repos..
      */
-    public static Map<String, RepoInfo> getStandardRepos(KickstartableTree  tree) {
-        Map <String, RepoInfo> map = new LinkedHashMap<String, RepoInfo>();
+    public static List<RepoInfo> getStandardRepos(KickstartableTree  tree) {
+        ArrayList<RepoInfo> repoList = new ArrayList<RepoInfo>();
         String[] repos = {"Cluster", "ClusterStorage", "HighAvailability",
                         "LoadBalancer", "ResilientStorage", "VT", "Workstation",
                         "ScalableFileSystem"};
@@ -140,17 +154,27 @@ public class RepoInfo {
         for (String repo : repos) {
             File file = new File(StringUtil.addPath(tree.getAbsolutePath(), repo));
             if (file.exists()) {
-                addToMap(map, repo);
+                File repodata = new File(file.getPath() +
+                    File.separator + "repodata" +
+                    File.separator + "repomd.xml");
+                repoList.add(create(repo, repo, repodata.exists()));
             }
         }
-        return map;
+        return repoList;
     }
 
-    private static void addToMap(Map <String, RepoInfo> map,  String name) {
-        RepoInfo info = create(name, name);
-        map.put(info.getName(), info);
+    /**
+     * @return the available
+     */
+    public boolean isAvailable() {
+        return available;
     }
-
+    /**
+     * @param availableIn the available to set
+     */
+    public void setAvailable(boolean availableIn) {
+        this.available = availableIn;
+    }
     /**
      * {@inheritDoc}
      */

java/code/src/com/redhat/rhn/frontend/action/kickstart/KickstartSoftwareEditAction.java
@@ -288,8 +288,8 @@ public class KickstartSoftwareEditAction extends BaseKickstartEditAction {
                 !tree.getInstallType().isRhel3() &&
                 !tree.getInstallType().isRhel4()) {
             List <LabelValueEnabledBean> repos = new LinkedList<LabelValueEnabledBean>();
-            for (String name : RepoInfo.getStandardRepos(tree).keySet()) {
-                repos.add(lve(name, name, false));
+            for (RepoInfo repo : RepoInfo.getStandardRepos(tree)) {
+                repos.add(lve(repo.getName(), repo.getName(), !repo.isAvailable()));
             }
             form.set(POSSIBLE_REPOS, repos.toArray(new LabelValueEnabledBean[0]));
             Set<RepoInfo> selected = ksdata.getRepoInfos();

commit 37cc54e56789baa8f8954195028bc2fad5f7eb44

1001551 - fix kickstart repositories selection
this was caused by 22f7bef1

java/code/src/com/redhat/rhn/manager/kickstart/KickstartEditCommand.java
@@ -38,8 +38,11 @@ import org.apache.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -365,9 +368,18 @@ public class KickstartEditCommand extends BaseKickstartCommand {
      */
     public void updateRepos(String[] reposIn) {
         if (ksdata.isRhel5OrGreater()) {
-
-            ksdata.setRepoInfos(RepoInfo.getStandardRepos(
-                    ksdata.getKickstartDefaults().getKstree()));
+            List<RepoInfo> repoList = RepoInfo.getStandardRepos(
+                    ksdata.getKickstartDefaults().getKstree());
+            Map<String, RepoInfo> repoSet = new HashMap<String, RepoInfo>();
+            for (Iterator<RepoInfo> ri = repoList.iterator(); ri.hasNext();) {
+                RepoInfo rInfo = ri.next();
+                repoSet.put(rInfo.getName(), rInfo);
+            }
+            Set<RepoInfo> selected = new HashSet <RepoInfo>();
+            for (int i = 0; i < reposIn.length; i++) {
+                selected.add(repoSet.get(reposIn[i]));
+            }
+            ksdata.setRepoInfos(selected);
             KickstartWizardHelper ksHelper = new KickstartWizardHelper(user);
             ksHelper.processSkipKey(ksdata);
         }