585bb4b6fa9fab68f6d1032efd03c0344b7242a4
Michael Calmer <mc@suse.de>
2013-09-18 13:07:16 +0200

    reactivation keys for a bootstrap entitled system should get enterprise entitlement

--- java/code/src/com/redhat/rhn/domain/token/ActivationKeyFactory.java
+++ java/code/src/com/redhat/rhn/domain/token/ActivationKeyFactory.java
@@ -145,7 +145,8 @@ public class ActivationKeyFactory extends HibernateFactory {
         }
 
         // Set the entitlements equal to what the server has by default
-        if (server != null) {
+        // If the server has the bootstrap entitlement use enterprise entitlement
+        if (server != null && ! server.isBootstrap()) {
             List serverEntitlements = server.getEntitledGroups();
             for (Iterator itr = serverEntitlements.iterator(); itr.hasNext();) {
                 ServerGroup group = (ServerGroup) itr.next();
