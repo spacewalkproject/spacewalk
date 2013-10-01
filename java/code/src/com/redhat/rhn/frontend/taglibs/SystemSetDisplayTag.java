844e0181eb1879ec8a1e74a5f2095e251c8fd9ee
Johannes Renner <jrenner@suse.de>
2013-10-01 17:13:50 +0200

    Use if .. else .. for better readability

--- java/code/src/com/redhat/rhn/frontend/taglibs/SystemSetDisplayTag.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/SystemSetDisplayTag.java
@@ -43,14 +43,27 @@ public class SystemSetDisplayTag extends TagSupport {
 
         try {
             JspWriter out = pageContext.getOut();
-            RhnSet rs = user != null ? RhnSetDecl.SYSTEMS.lookup(user) : null;
-            int size = rs == null ? 0 : rs.size();
+            // Get the selected systems if there is a user
+            RhnSet rs = null;
+            if (user != null) {
+                rs = RhnSetDecl.SYSTEMS.lookup(user);
+            }
+            // The number of systems selected for SSM
+            int size = 0;
+            if (rs != null) {
+                size = rs.size();
+            }
             StringBuilder result = new StringBuilder();
             result.append("<span id=\"spacewalk-ssm-counter\" class=\"badge\">");
             result.append(Integer.toString(size));
             result.append("</span>");
-            String msgKey = size == 1 ? "header.jsp.singleSystemSelected" :
-                    "header.jsp.systemsSelected";
+            String msgKey;
+            if (size == 1) {
+                msgKey = "header.jsp.singleSystemSelected";
+            }
+            else {
+                msgKey = "header.jsp.systemsSelected";
+            }
             result.append(LocalizationService.getInstance().getMessage(msgKey));
             out.println(result);
             return EVAL_PAGE;
