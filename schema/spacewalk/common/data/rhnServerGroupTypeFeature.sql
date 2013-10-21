525dfe50edda0aa1452ba28eeeb027570fcd18c4
Michael Calmer <mc@suse.de>
2013-10-21 14:51:12 +0200

    add system_grouping feature to bootstrap entitlement to be able to use SSM

--- schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
+++ schema/spacewalk/common/data/rhnServerGroupTypeFeature.sql
@@ -151,3 +151,8 @@ insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
 values (lookup_sg_type('bootstrap_entitled'), lookup_feature_type('ftr_kickstart'),
         current_timestamp,current_timestamp);
 
+insert into rhnServerGroupTypeFeature (server_group_type_id, feature_id,
+                                       created, modified)
+values (lookup_sg_type('bootstrap_entitled'), lookup_feature_type('ftr_system_grouping'),
+        current_timestamp,current_timestamp);
+
