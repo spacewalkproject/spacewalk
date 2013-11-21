085e6b0eb88ea827f59f2dee44cbf433f93a09e4
Jan Dobes <jdobes@redhat.com>
2013-11-21 15:12:14 +0100

    1009875 - changing order of operations

--- java/code/src/com/redhat/rhn/manager/org/MigrationManager.java
+++ java/code/src/com/redhat/rhn/manager/org/MigrationManager.java
@@ -125,6 +125,12 @@ public class MigrationManager extends BaseManager {
             throw new PermissionException(RoleFactory.ORG_ADMIN);
         }
 
+        // Unsubscribe from all channels to change channel entitlements
+        UpdateChildChannelsCommand cmd = new UpdateChildChannelsCommand(user, server,
+                new ArrayList());
+        cmd.store();
+        SystemManager.unsubscribeServerFromChannel(server, server.getBaseChannel());
+
         // Remove from all system groups:
         ServerGroupManager manager = ServerGroupManager.getInstance();
         for (ManagedServerGroup group : server.getManagedGroups()) {
@@ -173,12 +179,6 @@ public class MigrationManager extends BaseManager {
                 MonitoringFactory.deleteProbe(probe);
             }
         }
-
-        // Unsubscribe from all channels to change channel entitlements
-        UpdateChildChannelsCommand cmd = new UpdateChildChannelsCommand(user, server,
-                new ArrayList());
-        cmd.store();
-        SystemManager.unsubscribeServerFromChannel(server, server.getBaseChannel());
 
         SystemManager.removeAllServerEntitlements(server.getId());
     }
