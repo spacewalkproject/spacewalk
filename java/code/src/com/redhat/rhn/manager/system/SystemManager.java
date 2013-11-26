4f42f9a50d7be951eb1664c6f1937dc64a799464
Silvio Moioli <smoioli@suse.de>
2013-11-26 11:11:59 +0100

    Bare-metal system search added in Systems list
    Conflicts:
    	java/code/src/com/redhat/rhn/common/db/datasource/xml/System_queries.xml
    	java/code/src/com/redhat/rhn/manager/system/SystemManager.java

--- java/code/src/com/redhat/rhn/manager/system/SystemManager.java
+++ java/code/src/com/redhat/rhn/manager/system/SystemManager.java
@@ -2945,4 +2945,19 @@ public class SystemManager extends BaseManager {
         return m.execute(inParams);
     }
 
+    /**
+     * Returns list bare metal systems visible to user.
+     * @param user Currently logged in user.
+     * @param pc PageControl
+     * @return list of SystemOverviews
+     */
+    public static DataResult<SystemOverview> bootstrapList(User user,
+            PageControl pc) {
+        SelectMode m = ModeFactory.getMode("System_queries", "bootstrap");
+        Map<String, Long> params = new HashMap<String, Long>();
+        params.put("org_id", user.getOrg().getId());
+        params.put("user_id", user.getId());
+        Map<String, Long> elabParams = new HashMap<String, Long>();
+        return makeDataResult(params, elabParams, pc, m);
+    }
 }
