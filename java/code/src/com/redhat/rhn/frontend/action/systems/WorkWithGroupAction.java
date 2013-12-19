fad9b57b373fdcb81457c40251871fe13223f0ac
Michael Mraka <michael.mraka@redhat.com>
2013-12-19 15:44:46 +0100

    work_with_group.pxt rewritten to java

---
+++ java/code/src/com/redhat/rhn/frontend/action/systems/WorkWithGroupAction.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2013 Red Hat, Inc.
+ *
+ * This software is licensed to you under the GNU General Public License,
+ * version 2 (GPLv2). There is NO WARRANTY for this software, express or
+ * implied, including the implied warranties of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+ * along with this software; if not, see
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+ *
+ * Red Hat trademarks are not licensed under GPLv2. No permission is
+ * granted to use or replicate Red Hat trademarks that are incorporated
+ * in this software or its documentation.
+ */
+package com.redhat.rhn.frontend.action.systems;
+
+import com.redhat.rhn.domain.rhnset.RhnSet;
+import com.redhat.rhn.domain.user.User;
+import com.redhat.rhn.domain.server.ManagedServerGroup;
+import com.redhat.rhn.frontend.dto.SystemOverview;
+import com.redhat.rhn.frontend.struts.RequestContext;
+import com.redhat.rhn.frontend.struts.RhnAction;
+import com.redhat.rhn.frontend.struts.RhnHelper;
+import com.redhat.rhn.manager.rhnset.RhnSetDecl;
+import com.redhat.rhn.manager.rhnset.RhnSetManager;
+import com.redhat.rhn.manager.system.SystemManager;
+
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import java.util.Iterator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * WorkWithGroup
+ * @version 1
+ */
+public class WorkWithGroupAction extends RhnAction {
+
+    /** {@inheritDoc} */
+    public ActionForward execute(ActionMapping mapping,
+            ActionForm formIn,
+            HttpServletRequest request,
+            HttpServletResponse response) {
+
+        RequestContext requestContext = new RequestContext(request);
+        ManagedServerGroup serverGroup = requestContext.lookupAndBindServerGroup();
+        User user =  requestContext.getLoggedInUser();
+
+        RhnSet systemSet = RhnSetDecl.SYSTEMS.create(user);
+
+        Iterator systems = SystemManager.systemsInGroup(serverGroup.getId(), null)
+                                .iterator();
+        while (systems.hasNext()) { //for every system in a group
+            Long id = ((SystemOverview)systems.next()).getId();
+            systemSet.addElement(id);
+        }
+
+        RhnSetManager.store(systemSet);
+
+        //response.sendRedirect("/rhn/systems/ssm/ListSystems.do");
+        return mapping.findForward(RhnHelper.DEFAULT_FORWARD);
+    }
+}
