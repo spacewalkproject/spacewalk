7a5f7b839644bb3a933623b2ba1077eea983f031
Tomas Lestach <tlestach@redhat.com>
2014-01-06 10:06:56 +0100

    1048090 - Revert "add package ID to array returned by system.listPackages API call"
    * the patch returned package name id, not package id
    * we actually cannot return package id, since a server may have packages installed that are not available on the Spacewalk server
    
    This reverts commit ceccae97e862eed4bd2ea9470a5974fe59f09bb3.

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -1371,7 +1371,6 @@ public class SystemHandler extends BaseHandler {
      * @xmlrpc.returntype
      *      #array()
      *          #struct("package")
-     *                 #prop("int", "id")
      *                 #prop("string", "name")
      *                 #prop("string", "version")
      *                 #prop("string", "release")
