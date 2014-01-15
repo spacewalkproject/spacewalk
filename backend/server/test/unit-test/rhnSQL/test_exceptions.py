8a97328c592c1dcccbff31ed51a8e8cbb50b0e2f
Flavio Castelli <fcastelli@suse.com>
2014-01-15 18:09:01 +0100

    python test_exceptions.py: fixed broken test
    Call the db connection helper using the right parameters.

--- backend/server/test/unit-test/rhnSQL/test_exceptions.py
+++ backend/server/test/unit-test/rhnSQL/test_exceptions.py
@@ -9,7 +9,24 @@ from spacewalk.server import rhnSQL
 
 class ExceptionsTest(unittest.TestCase):
     def test_failed_connection(self):
-        self.assertRaises(rhnSQL.SQLConnectError, rhnSQL.initDB, 'x/y@z')
+        # Connect to localhost and look for db on a totally bogus port, this
+        # makes the test faster.
+        host     = "localhost"
+        username = "x"
+        password = "y"
+        database = "z"
+        port     = 9000
+
+        self.assertRaises(
+            rhnSQL.SQLConnectError,
+            rhnSQL.initDB,
+            "oracle",
+            host,
+            port,
+            database,
+            username,
+            password
+        )
 
 if __name__ == '__main__':
     unittest.main()
