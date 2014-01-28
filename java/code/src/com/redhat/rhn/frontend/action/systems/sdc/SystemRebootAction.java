295a5ddda301e0abc701d005fc532776bea461d7
Tomas Lestach <tlestach@redhat.com>
2014-01-28 10:25:17 +0100

    remove unnecessary cast

--- java/code/src/com/redhat/rhn/frontend/action/systems/sdc/SystemRebootAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/systems/sdc/SystemRebootAction.java
@@ -52,7 +52,7 @@ public class SystemRebootAction extends RhnAction {
         DynaActionForm form = (DynaActionForm) formIn;
         RequestContext context = new RequestContext(request);
         User user = context.getLoggedInUser();
-        Map<String, Object> params = (Map<String, Object>) makeParamMap(request);
+        Map<String, Object> params = makeParamMap(request);
         String forward = RhnHelper.DEFAULT_FORWARD;
 
         Long sid = context.getRequiredParam(RequestContext.SID);
