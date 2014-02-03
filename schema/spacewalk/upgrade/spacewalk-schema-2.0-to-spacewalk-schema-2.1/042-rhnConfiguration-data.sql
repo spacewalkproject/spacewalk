8c638ed4bc83b62ebe795f8fa174042052829643
Tomas Lestach <tlestach@redhat.com>
2014-02-03 14:07:13 +0100

    rename one and add another rhnConfiguration key

--- schema/spacewalk/upgrade/spacewalk-schema-2.0-to-spacewalk-schema-2.1/042-rhnConfiguration-data.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-2.0-to-spacewalk-schema-2.1/042-rhnConfiguration-data.sql
@@ -1,1 +1,2 @@
-INSERT INTO rhnConfiguration (key, description) VALUES ('orgid_for_ext_authentications', 'Organization id, where externally authenticated users will be created.');
+INSERT INTO rhnConfiguration (key, description) VALUES ('extauth_default_orgid', 'Organization id, where externally authenticated users will be created.');
+INSERT INTO rhnConfiguration (key, description, default_value) VALUES ('extauth_use_orgunit', 'Use Org. Unit IPA setting as organization name to create externally authenticated users in.', 'false');
