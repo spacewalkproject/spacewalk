spacewalkproject/spacewalk

commit f72f426327a1006e215ab5e7cb39b7b3b24da410

patch to handle systems registered with the --nohardware flag

java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -697,10 +697,24 @@ public class SystemHandler extends BaseHandler {
                 m.put("id", server.getId());
                 m.put("name", server.getName());
                 m.put("last_checkin", convertLocalToUtc(server.getLastCheckin()));
-                m.put("ram", server.getRam());
-                m.put("swap", server.getSwap());
-                m.put("cpu_info", server.getCpu());
-                m.put("dmi_info", server.getDmi());
+
+                m.put("ram", new Long(server.getRam()));
+                m.put("swap", new Long(server.getSwap()));
+
+                CPU cpu = server.getCpu();
+                if (cpu == null) {
+                  m.put("cpu_info", new HashMap<String, String>());
+                } else {
+                  m.put("cpu_info", cpu);
+                }
+
+                Dmi dmi = server.getDmi();
+                if (dmi == null) {
+                  m.put("dmi_info", new HashMap<String, String>());
+                } else {
+                  m.put("dmi_info", dmi);
+                }
+
                 m.put("network_devices",
                         new ArrayList<NetworkInterface>(server
                                 .getNetworkInterfaces()));

commit 3e9d084838a1bc23646384040c353aac5fdd3afb

fix checkstyle
f72f426327a1006e215ab5e7cb39b7b3b24da410 caused the trouble

java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -704,14 +704,16 @@ public class SystemHandler extends BaseHandler {
                 CPU cpu = server.getCpu();
                 if (cpu == null) {
                   m.put("cpu_info", new HashMap<String, String>());
-                } else {
+                }
+                else {
                   m.put("cpu_info", cpu);
                 }
 
                 Dmi dmi = server.getDmi();
                 if (dmi == null) {
                   m.put("dmi_info", new HashMap<String, String>());
-                } else {
+                }
+                else {
                   m.put("dmi_info", dmi);
                 }
 