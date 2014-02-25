1405955ff835c9ea4d8667963b36d85140ca8096
Jan Pazdziora <jpazdziora@redhat.com>
2014-02-25 09:39:42 +0100

    1069332 - Use /sbin/ip instead of /sbin/ifconfig.

--- monitoring/SatConfig/cluster/IpAddr.pm
+++ monitoring/SatConfig/cluster/IpAddr.pm
@@ -96,8 +96,8 @@ sub macAddress
 {
 	my $self = shift();
 	my $dev = $self->get_dev;
-	my $mac = `/sbin/ifconfig $dev`;
-	if ($mac =~ /HWaddr (\S+)/) {
+	my $mac = `/sbin/ip -o link`;
+	if ($mac =~ /^\d+:\s*\Q$dev\E:.*?ether (\S*).*/m) {
 		return $1;
 	} else {
 		# NOTE: Kludge for ethernet issue - should be undef
