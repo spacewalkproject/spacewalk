d276b6a34f0243e09a4c835465f9f2e250d3e3b1
Tomas Kasparek <tkasparek@redhat.com>
2014-03-04 15:58:49 +0100

    insert new gpg key only if not exists

--- schema/spacewalk/upgrade/spacewalk-schema-2.0-to-spacewalk-schema-2.1/047-rhnPackageKey-f20.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-2.0-to-spacewalk-schema-2.1/047-rhnPackageKey-f20.sql
@@ -1,6 +1,6 @@
-insert
-  into rhnPackageKey (id, key_id, key_type_id, provider_id)
-values (sequence_nextval('rhn_pkey_id_seq'),
-       '2eb161fa246110c1',
-       lookup_package_key_type('gpg'),
-       lookup_package_provider('Fedora'));
+update rhnPackageKey set provider_id = lookup_package_provider('Fedora')
+ where key_id = '2eb161fa246110c1';
+
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id)
+    (select sequence_nextval('rhn_pkey_id_seq'), '2eb161fa246110c1', lookup_package_key_type('gpg'), lookup_package_provider('Fedora') from dual
+     where not exists (select 1 from rhnPackageKey where key_id = '2eb161fa246110c1'));
