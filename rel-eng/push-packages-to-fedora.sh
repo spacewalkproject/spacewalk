b6bbc14005849313021dfeb34683e4c9edaf7650
Miroslav Suchý <msuchy@redhat.com>
2014-03-04 14:00:45 +0100

    remove packages orphaned in fedora
    (cherry picked from commit 049f9de1fdcd680dc0445ec62d49b69265f3afa8)

--- rel-eng/push-packages-to-fedora.sh
+++ rel-eng/push-packages-to-fedora.sh
@@ -7,7 +7,7 @@ TOP_DIR_FEDORA=$1
 # top dir of spacewalk git checkout
 TOP_DIR_GIT=$2
 
-PACKAGE_LIST="spacewalk-pylint rhnlib rhn-client-tools rhnsd yum-rhn-plugin rhncfg spacewalk-koan rhnpush nocpulse-common perl-Satcon perl-NOCpulse-CLAC perl-NOCpulse-Debug perl-NOCpulse-Gritch perl-NOCpulse-Object perl-NOCpulse-SetID perl-NOCpulse-Utils spacewalk-proxy-html spacewalk-proxy-docs spacecmd spacewalk-backend spacewalk-certs-tools rhn-custom-info spacewalk-config rhnmd spacewalk-branding python-gzipstream spacewalk-admin spacewalk-setup-jabberd"
+PACKAGE_LIST="spacewalk-pylint rhnlib rhn-client-tools rhnsd yum-rhn-plugin rhncfg spacewalk-koan rhnpush perl-Satcon spacewalk-proxy-html spacewalk-proxy-docs spacecmd spacewalk-backend spacewalk-certs-tools rhn-custom-info spacewalk-config rhnmd spacewalk-branding python-gzipstream spacewalk-admin spacewalk-setup-jabberd"
 
 
 if [ -z "$TOP_DIR_FEDORA" -o ! -d "$TOP_DIR_FEDORA" ]; then
