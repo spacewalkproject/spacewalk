8ceaf328390525d72540aad692c5e5a760757ac9
Tomas Lestach <tlestach@redhat.com>
2014-03-20 09:49:34 +0100

    upgrade scripts shall end with semicolon

--- schema/spacewalk/upgrade/spacewalk-schema-2.1-to-spacewalk-schema-2.2/009-rhnOrgConfiguration.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-2.1-to-spacewalk-schema-2.2/009-rhnOrgConfiguration.sql
@@ -1,4 +1,4 @@
 ALTER TABLE rhnOrgConfiguration ADD create_default_sg CHAR(1)
     default('N') not null
     constraint rhn_org_cong_deforg_chk
-    check (create_default_sg in ('Y', 'N'))
+    check (create_default_sg in ('Y', 'N'));
