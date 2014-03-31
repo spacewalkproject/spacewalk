uyuni-project/uyuni

Single system package actions: allow queuing to Action Chains
- refactoring (method extractions) in ActionManager
- addition of ActionChainManager
- edits to struts Actions to handle action chain parameters

Conflicts:
	java/code/src/com/redhat/rhn/manager/action/ActionManager.java

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/InstallConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -74,13 +75,12 @@ public class InstallConfirmSetupAction extends BaseSystemPackagesConfirmAction {
     /**
      * {@inheritDoc}
      */
+    @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-                                                           RequestContext context,
-                                                           List<Map<String, Long>> pkgs,
-                                                           Date earliest) {
-        return ActionManager.schedulePackageInstall(context.getLoggedInUser(),
-                                                    context.lookupAndBindServer(),
-                                                    pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageInstall(context.getLoggedInUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
     }
 
 

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/RemoveConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -53,11 +54,15 @@ public class RemoveConfirmSetupAction extends BaseSystemPackagesConfirmAction {
         return "message.packageremoval";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-            RequestContext context, List<Map<String, Long>> pkgs, Date earliest) {
-        return ActionManager.schedulePackageRemoval(context.getLoggedInUser(),
-                context.lookupAndBindServer(), pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageRemoval(context.getLoggedInUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
     }
 
     @Override

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/UpgradeConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -53,12 +54,15 @@ public class UpgradeConfirmSetupAction extends BaseSystemPackagesConfirmAction {
         return "message.packageupgrade";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-            RequestContext context, List<Map<String, Long>> pkgs, Date earliest) {
-        return ActionManager.schedulePackageUpgrade(context.getLoggedInUser(),
-                                                context.lookupAndBindServer(),
-                                                pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageUpgrade(context.getLoggedInUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
 
     }
 

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/VerifyConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -47,11 +48,15 @@ public class VerifyConfirmSetupAction extends BaseSystemPackagesConfirmAction {
         return "message.packageverify";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-            RequestContext context, List<Map<String, Long>> pkgs, Date earliest) {
-        return ActionManager.schedulePackageVerify(context.getLoggedInUser(),
-                context.lookupAndBindServer(), pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageVerify(context.getLoggedInUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
     }
 
     @Override

spacewalkproject/spacewalk

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/InstallConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -74,13 +75,12 @@ public class InstallConfirmSetupAction extends BaseSystemPackagesConfirmAction {
     /**
      * {@inheritDoc}
      */
+    @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-                                                           RequestContext context,
-                                                           List<Map<String, Long>> pkgs,
-                                                           Date earliest) {
-        return ActionManager.schedulePackageInstall(context.getCurrentUser(),
-                                                    context.lookupAndBindServer(),
-                                                    pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageInstall(context.getCurrentUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
     }
 
 

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/RemoveConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -53,11 +54,15 @@ public class RemoveConfirmSetupAction extends BaseSystemPackagesConfirmAction {
         return "message.packageremoval";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-            RequestContext context, List<Map<String, Long>> pkgs, Date earliest) {
-        return ActionManager.schedulePackageRemoval(context.getCurrentUser(),
-                context.lookupAndBindServer(), pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageRemoval(context.getCurrentUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
     }
 
     @Override

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/UpgradeConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -53,12 +54,15 @@ public class UpgradeConfirmSetupAction extends BaseSystemPackagesConfirmAction {
         return "message.packageupgrade";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-            RequestContext context, List<Map<String, Long>> pkgs, Date earliest) {
-        return ActionManager.schedulePackageUpgrade(context.getCurrentUser(),
-                                                context.lookupAndBindServer(),
-                                                pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageUpgrade(context.getCurrentUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
 
     }
 

java/code/src/com/redhat/rhn/frontend/action/rhnpackage/VerifyConfirmSetupAction.java
@@ -14,9 +14,10 @@
  */
 package com.redhat.rhn.frontend.action.rhnpackage;
 
+import com.redhat.rhn.domain.action.ActionChain;
 import com.redhat.rhn.domain.action.rhnpackage.PackageAction;
 import com.redhat.rhn.frontend.struts.RequestContext;
-import com.redhat.rhn.manager.action.ActionManager;
+import com.redhat.rhn.manager.action.ActionChainManager;
 
 import org.apache.struts.action.ActionForm;
 
@@ -47,11 +48,15 @@ public class VerifyConfirmSetupAction extends BaseSystemPackagesConfirmAction {
         return "message.packageverify";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected PackageAction schedulePackageAction(ActionForm formIn,
-            RequestContext context, List<Map<String, Long>> pkgs, Date earliest) {
-        return ActionManager.schedulePackageVerify(context.getCurrentUser(),
-                context.lookupAndBindServer(), pkgs, earliest);
+        RequestContext context, List<Map<String, Long>> pkgs, Date earliest,
+        ActionChain actionChain) {
+        return ActionChainManager.schedulePackageVerify(context.getCurrentUser(),
+            context.lookupAndBindServer(), pkgs, earliest, actionChain);
     }
 
     @Override