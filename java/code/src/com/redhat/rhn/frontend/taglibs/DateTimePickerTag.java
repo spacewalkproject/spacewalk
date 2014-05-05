5c7f148d1252c00bb1f060c492bd059b76594497
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-05-05 09:11:50 +0200

    checkstyle: unused import

--- java/code/src/com/redhat/rhn/frontend/taglibs/DateTimePickerTag.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/DateTimePickerTag.java
@@ -23,7 +23,6 @@ import java.text.DateFormat;
 import java.text.DateFormatSymbols;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.TimeZone;
 import javax.servlet.jsp.JspException;
 import javax.servlet.jsp.tagext.TagSupport;
 
