21e4ea7c5c2d45d3791bc30e7b04d9b7b58a1b0a
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-05-05 08:39:13 +0200

    Fix bug converting pm times to am when using locales in 24 hour format. See: https://www.redhat.com/archives/spacewalk-list/2014-April/msg00011.html

--- java/code/src/com/redhat/rhn/frontend/taglibs/DateTimePickerTag.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/DateTimePickerTag.java
@@ -259,8 +259,10 @@ public class DateTimePickerTag extends TagSupport {
 
         out.append(createHiddenInput("hour", String.valueOf(data.getHour())).render());
         out.append(createHiddenInput("minute", String.valueOf(data.getMinute())).render());
-        out.append(createHiddenInput("am_pm",
-                String.valueOf((data.getHour() > 12) ? 1 : 0)).render());
+        if (data.isLatin()) {
+            out.append(createHiddenInput("am_pm",
+                    String.valueOf((data.getHour() > 12) ? 1 : 0)).render());
+        }
     }
 
     private HtmlTag createHiddenInput(String type, String value) {
