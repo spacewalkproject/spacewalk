spacewalkproject/spacewalk

commit 51c3e001a7755d016fe4ad40f048370927017f79

remove xhtml tags

java/code/webapp/WEB-INF/pages/common/fragments/kickstart/schedule/advanced.jspf
@@ -3,7 +3,7 @@
 <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
 <%@ taglib uri="http://struts.apache.org/tags-html" prefix="html" %>
 
-<html:xhtml />
+
 <html>
 
   <head>

java/code/webapp/WEB-INF/pages/kickstart/softwareedit.jsp
@@ -3,7 +3,7 @@
 <%@ taglib uri="http://struts.apache.org/tags-bean" prefix="bean" %>
 <%@ taglib uri="http://struts.apache.org/tags-html" prefix="html" %>
 
-<html:html xhtml="true">
+<html:html >
 <head>
 <script language="javascript" type="text/javascript">
 //<!--

commit a4910658eea5e22bffb181655eaf78275a00d850

Form names are only available as name attributes now, not ids.
I think this is caused by 51c3e001a7755d016fe4ad40f048370927017f79.
I think that when struts-taglib renders html:form elements as
html it sticks the name in a "name" attribute, and when rendering
as xhtml it sticks the name in an "id" attribute. But the documentation
is sparse, so it's hard to tell.

java/code/webapp/WEB-INF/pages/common/fragments/kickstart/schedule/advanced.jspf
@@ -11,7 +11,7 @@
 
     <script language="javascript">
 function moveNext() {
-   var form = document.getElementById("kickstartScheduleWizardForm");
+   var form = document.getElementsByName("kickstartScheduleWizardForm")[0];
    form.submit();
 }
 
@@ -38,7 +38,7 @@ function getCheckedValue(radioObj) {
 }
 
 function enableBondStaticIpAddress() {
-    var staticBond = document.getElementById("kickstartScheduleWizardForm").bondStatic
+    var staticBond = document.getElementsByName("kickstartScheduleWizardForm")[0].bondStatic
     if (getCheckedValue(staticBond) == 'true') {
         document.getElementById("bondAddress").disabled = false
         document.getElementById("bondNetmask").disabled = false

java/code/webapp/WEB-INF/pages/kickstart/softwareedit.jsp
@@ -12,12 +12,12 @@ function reloadForm(ctl) {
   submittedFlag.value = "false";
   var changedField = document.getElementById("fieldChanged");
   changedField.value = ctl.id;
-  var form = document.getElementById("kickstartSoftwareForm");
+  var form = document.getElementsByName("kickstartSoftwareForm")[0];
   form.submit();
 }
 
 function toggleKSTree(what) {
-   var form = document.getElementById("kickstartSoftwareForm");
+   var form = document.getElementsByName("kickstartSoftwareForm")[0];
    if(what.checked) {
        form.tree.disabled=1;
    } else {
@@ -26,14 +26,14 @@ function toggleKSTree(what) {
 }
 
 function clickNewestRHTree() {
-   var form = document.getElementById("kickstartSoftwareForm");
+   var form = document.getElementsByName("kickstartSoftwareForm")[0];
    if(form.useNewestRHTree.checked) {
        form.useNewestTree.checked = false;
    }
 }
 
 function clickNewestTree() {
-   var form = document.getElementById("kickstartSoftwareForm");
+   var form = document.getElementsByName("kickstartSoftwareForm")[0];
    if(form.useNewestTree.checked) {
        form.useNewestRHTree.checked = false;
    }