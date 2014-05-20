be36b3832c206c626b4515b99ab5a4119786a5a8
Milan Zazrivec <mzazrivec@redhat.com>
2014-05-20 14:57:03 +0200

    removed pxt pages and related code which had been converted to java

--- web/modules/sniglets/Sniglets/ListView/SystemList.pm
+++ web/modules/sniglets/Sniglets/ListView/SystemList.pm
@@ -163,15 +163,6 @@ sub _register_modes {
   Sniglets::ListView::List->add_mode(-mode => "proxy_path_for_server",
 			   -datasource => RHN::DataSource::System->new);
 
-  Sniglets::ListView::List->add_mode(-mode => "systems_in_channel_family",
-			   -datasource => RHN::DataSource::System->new);
-
-  Sniglets::ListView::List->add_mode(-mode => "regular_systems_in_channel_family",
-			   -datasource => RHN::DataSource::System->new);
-
-  Sniglets::ListView::List->add_mode(-mode => "flex_systems_in_channel_family",
-			   -datasource => RHN::DataSource::System->new);
-
   Sniglets::ListView::List->add_mode(-mode => "systems_potentially_in_channel_family",
 			   -datasource => RHN::DataSource::System->new);
 
