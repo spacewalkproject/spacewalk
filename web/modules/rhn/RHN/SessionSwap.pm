28b323e8d636939d7505504b269eb28a2b34f8f4
Michael Mraka <michael.mraka@redhat.com>
2014-06-02 13:59:52 +0200

    generate_swap_key is unused after encode_data removal

--- web/modules/rhn/RHN/SessionSwap.pm
+++ web/modules/rhn/RHN/SessionSwap.pm
@@ -22,23 +22,6 @@ use Digest::HMAC_SHA1 qw/hmac_sha1_hex/;
 use PXT::Config;
 
 # this is basically a copy of RHN::Session's key stuff
-sub generate_swap_key {
-  my $class = shift;
-  my $id = shift;
-
-  my $chaff = join(":",
-		   PXT::Config->get('session_swap_secret_1'),
-		   PXT::Config->get('session_swap_secret_2'),
-		   $id,
-		   PXT::Config->get('session_swap_secret_3'),
-		   PXT::Config->get('session_swap_secret_4')
-		  );
-
-  my $ret = Digest::MD5::md5_hex($chaff);
-
-  return $ret;
-}
-
 sub rhn_hmac_data {
   my $class = shift;
   my $hmac_data = join("\0", @_);
