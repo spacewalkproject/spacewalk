ab4e4396d11a5a79d6d8742fcefa178b8c34a9f1
Milan Zazrivec <mzazrivec@redhat.com>
2014-06-03 17:53:26 +0200

    custominfo: silence warnings about (unused) wildcard imports

--- spacecmd/src/lib/custominfo.py
+++ spacecmd/src/lib/custominfo.py
@@ -20,6 +20,9 @@
 
 # NOTE: the 'self' variable is an instance of SpacewalkShell
 
+# wildcard import
+# pylint: disable=W0401,W0614
+
 from spacecmd.utils import *
 
 def help_custominfo_createkey(self):
