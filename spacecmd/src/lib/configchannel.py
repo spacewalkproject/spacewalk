383cba69cad87b3f0c78b767cac6e78f9ee089b9
Milan Zazrivec <mzazrivec@redhat.com>
2014-06-03 17:06:06 +0200

    configchannel: silence warnings about invalid function names

--- spacecmd/src/lib/configchannel.py
+++ spacecmd/src/lib/configchannel.py
@@ -27,6 +27,9 @@
 # unused argument
 # pylint: disable=W0613
 
+# invalid function name
+# pylint: disable=C0103
+
 from optparse import Option
 from datetime import datetime
 from time import strftime
