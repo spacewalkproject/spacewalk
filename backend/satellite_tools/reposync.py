45080980b3b71afc08dc9c38565c62fbd64c7836
Michael Mraka <michael.mraka@redhat.com>
2014-06-27 19:55:49 +0200

    fixed Anomalous backslash in string: '\s'.

--- backend/satellite_tools/reposync.py
+++ backend/satellite_tools/reposync.py
@@ -43,7 +43,7 @@ def set_filter_opt(option, opt_str, value, parser):
         f_type = '+'
     else:
         f_type = '-'
-    parser.values.filters.append((f_type, re.split('[,\s]+', value)))
+    parser.values.filters.append((f_type, re.split(r'[,\s]+', value)))
 
 def getChannelRepo():
 
@@ -433,7 +433,7 @@ class RepoSync(object):
                      order by sort_order """)
             h.execute(source_id = source_id)
             filter_data = h.fetchall_dict() or []
-            filters = [(row['flag'], re.split('[,\s]+', row['filter']))
+            filters = [(row['flag'], re.split(r'[,\s]+', row['filter']))
                                                          for row in filter_data]
         else:
             filters = self.filters
