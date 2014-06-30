spacewalkproject/spacewalk

commit 0feb3781d28b774803ea39c98d216595006806b4

fixed Class has no __init__ method

backend/satellite_tools/diskImportLib.py
@@ -31,6 +31,9 @@ from spacewalk.server.importlib import orgImport
 class Backend:
     __backend = None
 
+    def __init__(self):
+        pass
+
     def get_backend(self):
         if self.__backend:
             return self.__backend
@@ -56,6 +59,9 @@ def rpmsPath(obj_id, mountPoint, sources=0):
 class diskImportLibContainer:
     """virtual class - redefines endContainerCallback"""
     importer_class = None
+    def __init__(self):
+        pass
+
     def endContainerCallback(self):
         importer = self.importer_class(self.batch, get_backend())
         importer.run()

backend/satellite_tools/sync_handlers.py
@@ -162,6 +162,9 @@ class ChannelCollection:
 class SyncHandlerContainer:
     collection = None
 
+    def __init__(self):
+        pass
+
     def endItemCallback(self):
         # reference to xmlSource superclass we redefines
         xml_superclass = self.__class__.__bases__[1]

backend/satellite_tools/xmlSource.py
@@ -286,6 +286,9 @@ class BaseItem:
     item_class = None
     tagMap = {}
 
+    def __init__(self):
+        pass
+
     def populate(self, attributes, elements):
         item = self.item_class()
         # Populate the item from the attribute data structure

commit b2384c5fec83975f7cf30f232d5e4111e767e896

fixed ProductNamesContainer instance has no attribute 'tagStack'
caused by broken inheritance introduced by empty __init__
in 0feb3781d28b774803ea39c98d216595006806b4

backend/satellite_tools/diskImportLib.py
@@ -56,12 +56,15 @@ def rpmsPath(obj_id, mountPoint, sources=0):
     return os.path.normpath(template % (mountPoint, hash_object_id(obj_id, 2), obj_id))
 
 
+# pylint: disable=W0232
 class diskImportLibContainer:
     """virtual class - redefines endContainerCallback"""
-    # pylint: disable=E1101
+    # pylint: disable=E1101,E0203,W0201
+    # this class has no __init__ for the purpose
+    # it's used in multiple inheritance mode and inherited classes should
+    # use __init__ from the other base class
+
     importer_class = object
-    def __init__(self):
-        self.batch = None
 
     def endContainerCallback(self):
         importer = self.importer_class(self.batch, get_backend())

backend/satellite_tools/sync_handlers.py
@@ -159,11 +159,13 @@ class ChannelCollection:
         self._shared_state.clear()
         self.__init__()
 
+# pylint: disable=W0232
 class SyncHandlerContainer:
     collection = object
 
-    def __init__(self):
-        pass
+    # this class has no __init__ for the purpose
+    # it's used in multiple inheritance mode and inherited classes should
+    # use __init__ from the other base class
 
     def endItemCallback(self):
         # reference to xmlSource superclass we redefines