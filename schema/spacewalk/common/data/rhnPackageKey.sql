afec386bb19ca553e9e302a8a3a904c1f1a7d3b6
Miroslav Suchý <msuchy@redhat.com>
2014-07-18 15:07:23 +0200

    add Fedora 21 GPG keys

--- schema/spacewalk/common/data/rhnPackageKey.sql
+++ schema/spacewalk/common/data/rhnPackageKey.sql
@@ -60,6 +60,12 @@ insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 -- Fedora 20
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 (sequence_nextval('rhn_pkey_id_seq'), '2eb161fa246110c1', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
+-- Fedora 21
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
+(sequence_nextval('rhn_pkey_id_seq'), '89ad4e8795a43f54', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
+-- Fedora 21 (secondary)
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
+(sequence_nextval('rhn_pkey_id_seq'), '636dea19a0a7badb', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
 
 -- CentOS
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
