e953b2fb6ba6b2d7815bf592e3181cebf035b6fd
Stephen Herr <sherr@redhat.com>
2014-07-28 13:13:31 -0400

    removing monitoring from spacewalk-schema

---
+++ schema/spacewalk/upgrade/spacewalk-schema-2.2-to-spacewalk-schema-2.3/drop_tables.sql
@@ -0,0 +1,8 @@
+drop trigger time_series_purge_mod_trig;
+drop trigger rhn_probe_insert_trigger;
+drop trigger rhn_probe_delete_trigger
+drop trigger rhn_servnet_ipaddr_mon_trig;
+drop table rhn_probe;
+drop table time_series_purge;
+drop view time_series;
+drop table time_series_data;
