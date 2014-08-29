4e4d44af9da256bb996669d269b364e794e153db
Stephen Herr <sherr@redhat.com>
2014-08-29 12:34:38 -0400

    Removing solaris tables

---
+++ schema/spacewalk/upgrade/spacewalk-schema-2.2-to-spacewalk-schema-2.3/006-drop_solaris_tables.sql
@@ -0,0 +1,7 @@
+drop table rhnSolarisPatchSet;
+drop table rhnSolarisPatch;
+drop table rhnSolarisPatchType;
+drop table rhnSolarisPatchedPackage;
+drop table rhnSolarisPackage;
+drop table rhnSolarisPatchSetMembers;
+drop table rhnSolarisPatchPackages;
