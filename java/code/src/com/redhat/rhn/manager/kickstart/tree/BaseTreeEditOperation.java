spacewalkproject/spacewalk

commit 2b443f3199b7d3c3f5687e7cee066591016adc60

packageNamesByCapability: dead code removed

java/code/src/com/redhat/rhn/manager/kickstart/tree/BaseTreeEditOperation.java
@@ -206,17 +206,6 @@ public abstract class BaseTreeEditOperation extends BasePersistOperation {
     }
 
     /**
-     * Get the list of autokickstart package names.
-     * @return List of String package names
-     */
-    public List getAutoKickstartPackageNames() {
-       List retval = PackageManager.
-           packageNamesByCapability(user.getOrg(), KICKSTART_CAPABILITY);
-       replaceLegacyPackageNames(retval);
-       return retval;
-    }
-
-    /**
      * Replace legacy package names with empty string for each PackageListItem
      * in the provided list.
      * @param packageListItems List of PackageListItems to be modified in place.

java/code/src/com/redhat/rhn/manager/rhnpackage/test/PackageManagerTest.java
@@ -411,19 +411,6 @@ public class PackageManagerTest extends BaseTestCaseWithUser {
         assertEquals(0, result);
     }
 
-    // This test only works if you have Channels syched to your sat
-    public void xxxxPackageNamesByCapability() throws Exception {
-
-        /*user.addRole(RoleFactory.ORG_ADMIN);
-        Channel c = ChannelFactoryTest.createTestChannel(user);
-        Package pak = addPackageToChannel(user, TestUtils.randomString(), c);
-        PackageCapability cap = PackageCapabilityTest.createTestCapability();*/
-        DataResult dr = PackageManager.packageNamesByCapability(user.getOrg(),
-                "rhn.kickstart.boot_image");
-        assertNotNull(dr);
-        assertTrue(dr.size() > 0);
-    }
-
     /**
      * Add the up2date package to a system and a channel.  Version
      * should be specified such as "2.9.0"

java/code/src/com/redhat/rhn/common/db/datasource/xml/Package_queries.xml
@@ -362,32 +362,6 @@ ORDER BY UPPER(C.label)
   </query>
 </mode>
 
-<mode name="name_by_provide" class="com.redhat.rhn.frontend.dto.PackageListItem">
-  <query params="org_id, cap_name">
-select * from (
-SELECT DISTINCT PN.name
- FROM rhnPackageName PN,
-      rhnPackage P,
-      rhnPackageProvides PP,
-      rhnPackageCapability PC
-WHERE PC.name = :cap_name
-  AND PC.id = PP.capability_id
-  AND PP.package_id = P.id
-  AND (P.org_id = :org_id OR EXISTS (
-          SELECT  1
-            FROM  rhnChannelPackage CP,
-                  rhnChannelFamilyMembers CFM,
-                  rhnOrgChannelFamilyPermissions CFP
-           WHERE  CFP.org_id = :org_id
-             AND  CFP.channel_family_id = CFM.channel_family_id
-             AND  CFM.CHANNEL_ID = CP.channel_id
-             AND  CP.package_id = P.id))
-  AND P.name_id = PN.id
-) X
-ORDER BY UPPER(X.name)
-  </query>
-</mode>
-
 <mode name="name_by_provide_and_channel" class="com.redhat.rhn.frontend.dto.PackageListItem">
   <query params="channel_id, org_id, cap_name">
 select * from (

java/code/src/com/redhat/rhn/manager/rhnpackage/PackageManager.java
@@ -578,24 +578,6 @@ public class PackageManager extends BaseManager {
     }
 
     /**
-     * Get the list of  Package Names that match the passed in capability string.
-     *
-     * Example:  "rhn.kickstart.boot_image" for the list of auto kickstart rpms
-     * @param org making the request
-     * @param capabilityName to search for
-     * @return DataResult containing *just* the package name strings
-     */
-    public static DataResult packageNamesByCapability(Org org,
-            String capabilityName) {
-        Map<String, Object> params = new HashMap<String, Object>();
-        params.put("org_id", org.getId());
-        params.put("cap_name", capabilityName);
-        SelectMode m = ModeFactory.getMode("Package_queries",
-                "name_by_provide");
-        return m.execute(params);
-    }
-
-    /**
      * Get the list of package names that match the passed in capability and channel.
      *
      * @param org making the request

commit 4ca1353588dd950b3774eabf0bd2ed1603f687a9

checkstyle fix
2b443f3199b7d3c3f5687e7cee066591016adc60 broke it

java/code/src/com/redhat/rhn/manager/kickstart/tree/BaseTreeEditOperation.java
@@ -28,7 +28,6 @@ import com.redhat.rhn.frontend.dto.PackageListItem;
 import com.redhat.rhn.manager.BasePersistOperation;
 import com.redhat.rhn.manager.kickstart.cobbler.CobblerCommand;
 import com.redhat.rhn.manager.kickstart.cobbler.CobblerXMLRPCHelper;
-import com.redhat.rhn.manager.rhnpackage.PackageManager;
 
 import org.cobbler.Distro;
 import org.cobbler.XmlRpcException;

java/code/src/com/redhat/rhn/manager/rhnpackage/test/PackageManagerTest.java
@@ -14,7 +14,6 @@
  */
 package com.redhat.rhn.manager.rhnpackage.test;
 
-import com.redhat.rhn.common.conf.ConfigDefaults;
 import com.redhat.rhn.common.db.datasource.DataResult;
 import com.redhat.rhn.common.db.datasource.ModeFactory;
 import com.redhat.rhn.common.db.datasource.WriteMode;