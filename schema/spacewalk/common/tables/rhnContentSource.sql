a569b5e4cc06aad012f99419daa568cba3837db7
Michael Calmer <mc@suse.de>
2014-10-10 11:49:27 +0200

    increase source_url column size to 2048
    Needed to store an authtoken(max 512) together with the url.
    The max size of an URL in older browsers are 2048 so use this as limit.

--- schema/spacewalk/common/tables/rhnContentSource.sql
+++ schema/spacewalk/common/tables/rhnContentSource.sql
@@ -27,7 +27,7 @@ rhnContentSource
         type_id         number NOT NULL
                         constraint rhn_cs_type_fk
                                 references rhnContentSourceType(id),
-        source_url      varchar2(512) NOT NULL,
+        source_url      varchar2(2048) NOT NULL,
         label           varchar2(64) NOT NULL,
         created         timestamp with local time zone default(current_timestamp) NOT NULL,
         modified        timestamp with local time zone default(current_timestamp) NOT NULL
