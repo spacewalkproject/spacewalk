5b693041c04b54841e03ae7f18b250b5e5924db5
Silvio Moioli <smoioli@suse.de>
2014-11-11 15:24:58 +0100

    CreateChannelCommand imports organized

--- java/code/src/com/redhat/rhn/manager/channel/CreateChannelCommand.java
+++ java/code/src/com/redhat/rhn/manager/channel/CreateChannelCommand.java
@@ -27,6 +27,11 @@ import com.redhat.rhn.frontend.xmlrpc.InvalidParentChannelException;
 
 import org.apache.commons.lang.StringUtils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
