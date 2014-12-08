316484217980e7f7ff3e33df79f6637ee0bc9644
Cynthia Sanchez <csanchez@suse.com>
2014-12-08 14:24:36 +0100

    address tag: removed unnecessary p tag

--- java/code/src/com/redhat/rhn/frontend/taglibs/AddressTag.java
+++ java/code/src/com/redhat/rhn/frontend/taglibs/AddressTag.java
@@ -222,7 +222,6 @@ public class AddressTag extends TagSupport {
                 result.append("<div class=\"alert alert-info\">");
                 result.append(ls.getMessage("address not filled in"));
                 result.append("</div>");
-                result.append("<p>");
                 result.append("<a ")
                       .append("class=\"btn btn-default\"")
                       .append(" href=\"")
@@ -234,7 +233,6 @@ public class AddressTag extends TagSupport {
                       .append("\">")
                       .append(ls.getMessage("Fill in this address"))
                       .append("</a>");
-                result.append("</p>");
             }
 
             out.print(result);
