80573366a187e0e290934ffe8e0207edd3d83276
Stephen Herr <sherr@redhat.com>
2014-12-18 16:18:30 -0500

    Monitoring Panes on YourRHN no longer exist, remove schema references to them

--- schema/spacewalk/common/data/rhnInfoPane.sql
+++ schema/spacewalk/common/data/rhnInfoPane.sql
@@ -14,8 +14,6 @@
 --
 insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'tasks',null);
 insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'critical-systems',null);
-insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'critical-probes','show_monitoring();');
-insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'warning-probes','show_monitoring();');
 insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'system-groups-widget','org_entitlement(sw_mgr_enterprise);');
 insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'latest-errata',null);
 insert into RHNINFOPANE(ID,LABEL,ACL) values (sequence_nextval('rhn_info_pane_id_seq'),'inactive-systems',null);
