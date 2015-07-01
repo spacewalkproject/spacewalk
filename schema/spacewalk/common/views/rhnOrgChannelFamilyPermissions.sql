5b92bde551602c472b327733fbd96fb1ffb469f2
Michael Calmer <mc@suse.de>
2015-07-01 16:36:51 +0200

    schema: remove members from rhnOrgChannelFamilyPermissions and rhnUserChannelFamilyPerms views

--- schema/spacewalk/common/views/rhnOrgChannelFamilyPermissions.sql
+++ schema/spacewalk/common/views/rhnOrgChannelFamilyPermissions.sql
@@ -19,10 +19,6 @@
 create or replace view rhnOrgChannelFamilyPermissions as
 	select	pcf.channel_family_id,
 		u.org_id as org_id,
-		to_number(null, null) as max_members,
-		0 as current_members,
-		to_number(null, null) as fve_max_members,
-		0 as fve_current_members,
 		pcf.created,
 		pcf.modified
 	from	rhnPublicChannelFamily pcf,
@@ -30,10 +26,6 @@ create or replace view rhnOrgChannelFamilyPermissions as
 	union
 	select	channel_family_id,
 		org_id,
-		max_members,
-		current_members,
-		fve_max_members,
-		fve_current_members,
 		created,
 		modified
 	from	rhnPrivateChannelFamily;
