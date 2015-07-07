e3566d42cf8b4a91617f3196cbf8db833944a452
Michael Calmer <mc@suse.de>
2015-07-07 11:45:27 +0200

    schema: drop not_enough_flex_entitlements exception

--- schema/spacewalk/common/data/rhnException.sql
+++ schema/spacewalk/common/data/rhnException.sql
@@ -111,8 +111,5 @@ insert into rhnException values (-20292, 'package_provider_not_found', 'The spec
 
 insert into rhnException values (-20293, 'package_key_type_not_found', 'The specified package key type could not be found.');
 
-insert into rhnException values (-20296, 'not_enough_flex_entitlements', 'You do not have enough entitlements in your org..');
-
-
 commit;
 
