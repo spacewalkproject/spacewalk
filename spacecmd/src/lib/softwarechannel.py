679f335327a12f523b6ad9ee600209a0ded28a2a
Klaas- <Klaas-@users.noreply.github.com>
2015-07-29 14:35:20 +0200

    Fix typo in softwarechannel.py
    missing 'e'

--- spacecmd/src/lib/softwarechannel.py
+++ spacecmd/src/lib/softwarechannel.py
@@ -1525,7 +1525,7 @@ def help_softwarechannel_regenerateneededcache(self):
     print 'softwarechannel_regenerateneededcache: '
     print 'Regenerate the needed errata and package cache for all systems'
     print
-    print 'usage: softwarechannel_regnerateneededcache'
+    print 'usage: softwarechannel_regenerateneededcache'
 
 
 def do_softwarechannel_regenerateneededcache(self, args):
@@ -1539,7 +1539,7 @@ def help_softwarechannel_regenerateyumcache(self):
     print 'softwarechannel_regenerateyumcache: '
     print 'Regenerate the YUM cache for a software channel'
     print
-    print 'usage: softwarechannel_regnerateyumcache <CHANNEL ...>'
+    print 'usage: softwarechannel_regenerateyumcache <CHANNEL ...>'
 
 
 def complete_softwarechannel_regenerateyumcache(self, text, line, beg, end):
