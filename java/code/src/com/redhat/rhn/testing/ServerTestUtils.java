5b6bd8292af3b96cbd2999ebf0107e747e459125
Frantisek Kobzik <fkobzik@suse.de>
2015-08-11 16:36:22 +0200

    Remove unused methods from tests

--- java/code/src/com/redhat/rhn/testing/ServerTestUtils.java
+++ java/code/src/com/redhat/rhn/testing/ServerTestUtils.java
@@ -171,25 +171,6 @@ public class ServerTestUtils {
     }
 
     /**
-    *
-    * Create a sytem with the virtualization platform entitlement.
-    *
-    * @param user to own system
-    * @return Server with guest.
-    * @throws Exception if error
-    */
-   public static Server createVirtPlatformHost(User user) throws Exception {
-       Server s = createTestSystem(user);
-       user.addPermanentRole(RoleFactory.ORG_ADMIN);
-       // Lets give the org/server virt.
-       UserTestUtils.addVirtualizationPlatform(user.getOrg());
-       ServerTestUtils.addVirtualization(user, s);
-       SystemManager.entitleServer(s, EntitlementManager.VIRTUALIZATION_PLATFORM);
-
-       return s;
-   }
-
-    /**
      * Add a new Server as a guest of the passed in Server.
      * @param user adding
      * @param server to add too
