94613c682ac3eef7f6d9dd553c74934404a15606
Silvio Moioli <smoioli@suse.de>
2015-08-12 11:27:20 +0200

    data: remove references to update entitlements

--- schema/spacewalk/common/data/rhnSGTypeBaseAddonCompat.sql
+++ schema/spacewalk/common/data/rhnSGTypeBaseAddonCompat.sql
@@ -21,13 +21,5 @@ insert into rhnSGTypeBaseAddonCompat (base_id, addon_id)
 values (lookup_sg_type('enterprise_entitled'), 
         lookup_sg_type('virtualization_host_platform'));
 
-insert into rhnSGTypeBaseAddonCompat (base_id, addon_id)
-values (lookup_sg_type('sw_mgr_entitled'), 
-        lookup_sg_type('virtualization_host'));
-
-insert into rhnSGTypeBaseAddonCompat (base_id, addon_id)
-values (lookup_sg_type('sw_mgr_entitled'), 
-        lookup_sg_type('virtualization_host_platform'));
-
 commit;
 
