5fab7d09dc1e4bdd71f1104f36566259578aa28f
Duncan Mac-Vicar P <dmacvicar@suse.de>
2016-01-25 11:46:14 +0100

    add Chile to the list of timezones (bsc#959055)
    Chile recently switched to follow UTC-03:00 but without DST so there is no compatible
    timezone to be selected.

---
+++ schema/spacewalk/upgrade/spacewalk-schema-2.4-to-spacewalk-schema-2.5/202-rhnTimezone-data.sql
@@ -0,0 +1,27 @@
+insert into rhnTimezone (id, olson_name, display_name) (
+    select sequence_nextval('rhn_timezone_id_seq'),
+           'America/Santiago',
+           'Chile (Continental)'
+      from dual
+     where not exists (
+           select 1
+             from rhnTimezone
+            where olson_name = 'America/Santiago'
+              and display_name = 'Chile (Continental)'
+     )
+);
+
+insert into rhnTimezone (id, olson_name, display_name) (
+    select sequence_nextval('rhn_timezone_id_seq'),
+           'Pacific/Easter',
+           'Chile (Easter Island)'
+      from dual
+     where not exists (
+           select 1
+             from rhnTimezone
+            where olson_name = 'Pacific/Easter'
+              and display_name = 'Chile (Easter Island)'
+     )
+);
+
+
