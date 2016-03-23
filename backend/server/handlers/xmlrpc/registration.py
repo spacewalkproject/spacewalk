0c5e4a21672826589434afb802069c2c645327e9
Jan Dobes <jdobes@redhat.com>
2016-03-23 18:53:45 +0100

    qemu-kvm guests created on my Fedora 22 have following signature, mark them as virtual
    vendor: SeaBIOS
    manufacturer: QEMU
    product: Standard PC (i440FX + PIIX, 1996)

--- backend/server/handlers/xmlrpc/registration.py
+++ backend/server/handlers/xmlrpc/registration.py
@@ -60,6 +60,8 @@ def parse_smbios(smbios):
 
     if vendor == "QEMU" and uuid is not None:
         return (rhnVirtualization.VirtualizationType.QEMU, uuid)
+    elif manufacturer == 'QEMU' and uuid is not None:
+        return (rhnVirtualization.VirtualizationType.QEMU, uuid)
     elif manufacturer == 'Bochs' and product == 'Bochs' and uuid is not None:
         # Bochs, Bochs is a virtual SUSE KVM machine
         return (rhnVirtualization.VirtualizationType.QEMU, uuid)
