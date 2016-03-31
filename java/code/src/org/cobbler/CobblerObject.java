079617e02322d0e18767a6671a611b1c3d5576fc
Michael Calmer <mc@suse.de>
2016-03-31 12:00:09 +0200

    fix splitting kernel options
    limit -- the result threshold which means how many strings to be returned.

--- java/code/src/org/cobbler/CobblerObject.java
+++ java/code/src/org/cobbler/CobblerObject.java
@@ -393,7 +393,7 @@ public abstract class CobblerObject {
 
         String[] options = StringUtils.split(kernelOpts);
         for (String option : options) {
-            String[] split = option.split("=", 1);
+            String[] split = option.split("=", 2);
             if (split.length == 1) {
                 toRet.put(split[0], new ArrayList<String>());
             }
