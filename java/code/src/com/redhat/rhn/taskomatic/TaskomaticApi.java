c5033d9394b6e0ea2de2cb05c05e758180fac5da
Silvio Moioli <smoioli@suse.de>
2016-04-14 10:09:55 +0200

    TaskomaticApi refactoring: method code formatted

--- java/code/src/com/redhat/rhn/taskomatic/TaskomaticApi.java
+++ java/code/src/com/redhat/rhn/taskomatic/TaskomaticApi.java
@@ -149,8 +149,7 @@ public class TaskomaticApi {
      * @throws TaskomaticApiException if there was an error
      */
     public Date scheduleRepoSync(Channel chan, User user, String cron,
-                                                     Map <String, String>params)
-                                        throws TaskomaticApiException {
+            Map<String, String> params) throws TaskomaticApiException {
         String jobLabel = createRepoSyncScheduleName(chan, user);
 
         Map task = findScheduleByBunchAndLabel("repo-sync-bunch", jobLabel, user);
@@ -162,8 +161,7 @@ public class TaskomaticApi {
         scheduleParams.putAll(params);
 
         return (Date) invoke("tasko.scheduleBunch", user.getOrg().getId(),
-                "repo-sync-bunch", jobLabel , cron,
-                scheduleParams);
+                "repo-sync-bunch", jobLabel, cron, scheduleParams);
     }
 
     /**
