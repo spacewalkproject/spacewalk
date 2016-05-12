5e7dad6cc1dd42efddb5070c50f9fb7c6270d46a
Jan Dobes <jdobes@redhat.com>
2016-05-12 16:41:32 +0200

    import channel families

--- backend/cdn_tools/activation.py
+++ backend/cdn_tools/activation.py
@@ -14,7 +14,12 @@
 
 from spacewalk.satellite_tools import satCerts
 from spacewalk.server import rhnSQL
+from spacewalk.server.importlib.backendOracle import SQLBackend
+from spacewalk.server.importlib.channelImport import ChannelFamilyImport
+from spacewalk.server.importlib.importLib import InvalidChannelFamilyError, ChannelFamily
+from satellite_cert import SatelliteCert
 import constants
+import json
 
 """
 Functions handling activation from RHSM manifest.
@@ -38,4 +43,41 @@ def update_certificates(manifest):
             constants.CLIENT_CERT_PREFIX + creds.get_id(), creds.get_cert(), None)
         satCerts.store_rhnCryptoKey(
             constants.CLIENT_KEY_PREFIX + creds.get_id(), creds.get_key(), None)
+
+def update_channel_families(cert_path):
+    """Insert channel family data into DB"""
+
+    # Satellite 5 certificate
+    with open(cert_path, 'r') as f:
+        cert_content = f.read()
+
+    # Channel families metadata
+    with open(constants.CHANNEL_FAMILY_MAPPING_PATH, 'r') as f:
+        families = json.load(f)
+
+    cert = SatelliteCert()
+    cert.load(cert_content)
+
+    batch = []
+    for cf in cert.channel_families:
+        label = cf.name
+        try:
+            family = families[label]
+            family_object = ChannelFamily()
+            for k in family.keys():
+                family_object[k] = family[k]
+            family_object['label'] = label
+            batch.append(family_object)
+        except KeyError:
+            raise InvalidChannelFamilyError(
+                "ERROR: Channel family '%s' was not found in mapping"
+                % label
+            )
+
+    # Perform import
+    backend = SQLBackend()
+    importer = ChannelFamilyImport(batch, backend)
+    importer.run()
+    backend.commit()
+
 
