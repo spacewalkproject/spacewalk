ecd4e111e253a141e093fec4ee679e0ba6d6726e
Jan Dobes <jdobes@redhat.com>
2016-05-31 11:36:07 +0200

    refactor to class

--- backend/cdn_tools/activation.py
+++ backend/cdn_tools/activation.py
@@ -16,9 +16,10 @@ from spacewalk.satellite_tools import satCerts
 from spacewalk.server import rhnSQL
 from spacewalk.server.importlib.backendOracle import SQLBackend
 from spacewalk.server.importlib.channelImport import ChannelFamilyImport
-from spacewalk.server.importlib.importLib import InvalidChannelFamilyError, ChannelFamily
+from spacewalk.server.importlib.importLib import ChannelFamily
 from satellite_cert import SatelliteCert
 import constants
+from manifest import Manifest
 import json
 
 """
@@ -26,83 +27,90 @@ Functions handling activation from RHSM manifest.
 """
 
 
-def update_certificates(manifest):
-    """Delete and insert certificates needed for syncing from CDN repositories."""
+class Activation(object):
+    """Class inserting channel families and SSL metadata into DB."""
 
-    # Read RHSM cert
-    with open(constants.CA_CERT_PATH, 'r') as f:
-        ca_cert = f.read()
+    def __init__(self, manifest_path, cert_path):
+        rhnSQL.initDB()
+        self.manifest = Manifest(manifest_path)
 
-    # Insert RHSM cert and certs from manifest into DB
-    rhnSQL.initDB()
-    satCerts.store_rhnCryptoKey(
-        constants.CA_CERT_NAME, ca_cert, None)
+        # Satellite 5 certificate
+        with open(cert_path, 'r') as f:
+            self.sat5_cert = SatelliteCert()
+            content = f.read()
+            self.sat5_cert.load(content)
 
-    for entitlement in manifest.get_all_entitlements():
-        creds = entitlement.get_credentials()
+        # Channel families metadata
+        with open(constants.CHANNEL_FAMILY_MAPPING_PATH, 'r') as f:
+            self.families = json.load(f)
+
+        with open(constants.PRODUCT_FAMILY_MAPPING_PATH, 'r') as f:
+            self.products = json.load(f)
+
+        self.families_to_import = []
+
+    def _update_certificates(self):
+        """Delete and insert certificates needed for syncing from CDN repositories."""
+
+        # Read RHSM cert
+        with open(constants.CA_CERT_PATH, 'r') as f:
+            ca_cert = f.read()
+
+        # Insert RHSM cert and certs from manifest into DB
         satCerts.store_rhnCryptoKey(
-            constants.CLIENT_CERT_PREFIX + creds.get_id(), creds.get_cert(), None)
-        satCerts.store_rhnCryptoKey(
-            constants.CLIENT_KEY_PREFIX + creds.get_id(), creds.get_key(), None)
+            constants.CA_CERT_NAME, ca_cert, None)
 
+        for entitlement in self.manifest.get_all_entitlements():
+            creds = entitlement.get_credentials()
+            satCerts.store_rhnCryptoKey(
+                constants.CLIENT_CERT_PREFIX + creds.get_id(), creds.get_cert(), None)
+            satCerts.store_rhnCryptoKey(
+                constants.CLIENT_KEY_PREFIX + creds.get_id(), creds.get_key(), None)
 
-def update_channel_families(manifest, cert_path):
-    """Insert channel family data into DB"""
+    def _update_channel_families(self):
+        """Insert channel family data into DB"""
 
-    # Satellite 5 certificate
-    with open(cert_path, 'r') as f:
-        cert_content = f.read()
+        for entitlement in self.manifest.get_all_entitlements():
+            for product_id in entitlement.get_product_ids():
+                try:
+                    product = self.products[product_id]
+                    self.families_to_import.extend(product['families'])
+                # Some product cannot be mapped into channel families
+                except KeyError:
+                    print("Cannot map product '%s' into channel families" % product_id)
+                    pass
 
-    # Channel families metadata
-    with open(constants.CHANNEL_FAMILY_MAPPING_PATH, 'r') as f:
-        families = json.load(f)
+        self.families_to_import = set(self.families_to_import)
 
-    with open(constants.PRODUCT_FAMILY_MAPPING_PATH, 'r') as f:
-        products = json.load(f)
+        # Debug
+        print("Channel families mapped from products: %d" % len(self.families_to_import))
+        print("Channel families in cert: %d" % len(self.sat5_cert.channel_families))
 
-    mapped_channel_families = []
-    for entitlement in manifest.get_all_entitlements():
-        for product_id in entitlement.get_product_ids():
+        batch = []
+        for cf in self.sat5_cert.channel_families:
+            label = cf.name
+            if label not in self.families_to_import:
+                print("Skipping channel family from certificate, not in the mapping: %s" % label)
+                continue
             try:
-                product = products[product_id]
-                mapped_channel_families.extend(product['families'])
-            # Some product cannot be mapped into channel families
+                family = self.families[label]
+                family_object = ChannelFamily()
+                for k in family.keys():
+                    family_object[k] = family[k]
+                family_object['label'] = label
+                batch.append(family_object)
             except KeyError:
-                print("Cannot map product '%s' into channel families" % product_id)
-                pass
+                print("ERROR: Channel family '%s' was not found in mapping" % label)
+                raise
 
-    mapped_channel_families = set(mapped_channel_families)
+        # Perform import
+        backend = SQLBackend()
+        importer = ChannelFamilyImport(batch, backend)
+        importer.run()
+        backend.commit()
 
-    cert = SatelliteCert()
-    cert.load(cert_content)
-
-    # Debug
-    print("Channel families mapped from products: %d" % len(mapped_channel_families))
-    print("Channel families in cert: %d" % len(cert.channel_families))
-
-    batch = []
-    for cf in cert.channel_families:
-        label = cf.name
-        if label not in mapped_channel_families:
-            print("Skipping channel family from certificate, not in the mapping: %s" % label)
-            continue
-        try:
-            family = families[label]
-            family_object = ChannelFamily()
-            for k in family.keys():
-                family_object[k] = family[k]
-            family_object['label'] = label
-            batch.append(family_object)
-        except KeyError:
-            print("ERROR: Channel family '%s' was not found in mapping" % label)
-            raise
-
-    # Perform import
-    backend = SQLBackend()
-    importer = ChannelFamilyImport(batch, backend)
-    importer.run()
-    backend.commit()
-
-
-
-
+    def run(self):
+        print("Updating certificates...")
+        self._update_certificates()
+        print("Updating channel families...")
+        self._update_channel_families()
