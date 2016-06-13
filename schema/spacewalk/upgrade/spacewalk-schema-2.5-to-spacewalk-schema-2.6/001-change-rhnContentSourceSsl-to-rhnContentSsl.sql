042cf0b3919dbeb8804008518f8dcf4de65e6466
Jan Dobes <jdobes@redhat.com>
2016-06-13 18:16:43 +0200

    sequence is still there

--- schema/spacewalk/upgrade/spacewalk-schema-2.5-to-spacewalk-schema-2.6/001-change-rhnContentSourceSsl-to-rhnContentSsl.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-2.5-to-spacewalk-schema-2.6/001-change-rhnContentSourceSsl-to-rhnContentSsl.sql
@@ -69,3 +69,4 @@ delete from rhnContentSourceSsl;
 drop table rhnContentSourceSsl;
 drop function rhn_csssl_ins_trig_fun();
 drop function rhn_cont_source_ssl_mod_trig_fun();
+drop sequence rhn_contentsourcessl_seq;
