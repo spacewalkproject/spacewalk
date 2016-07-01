3831648d6421cce82c081cb9d36960cb27ad2404
Jakub Jankowski <shasta@toxcorp.com>
2016-07-01 14:40:13 +0200

    spacecmd: user: allow more than one group at a time
    The backend can take a list of groups for the following four operations:
     addAssignedSystemGroups
     addDefaultSystemGroups
     removeAssignedSystemGroups
     removeDefaultSystemGroups
    and spacecmd help text suggests this is also possible with
    user_{add,remove}{default,}group - but because the code checks for
    exactly two arguments, it fails when given more than two.
    
    Fix it by relaxing the check to "less than two args".
    
    Signed-off-by: Jakub Jankowski <shasta@toxcorp.com>

--- spacecmd/src/lib/user.py
+++ spacecmd/src/lib/user.py
@@ -381,7 +381,7 @@ def complete_user_addgroup(self, text, line, beg, end):
 def do_user_addgroup(self, args):
     (args, _options) = parse_arguments(args)
 
-    if len(args) != 2:
+    if len(args) < 2:
         self.help_user_addgroup()
         return
 
@@ -415,7 +415,7 @@ def complete_user_adddefaultgroup(self, text, line, beg, end):
 def do_user_adddefaultgroup(self, args):
     (args, _options) = parse_arguments(args)
 
-    if len(args) != 2:
+    if len(args) < 2:
         self.help_user_adddefaultgroup()
         return
 
@@ -451,7 +451,7 @@ def complete_user_removegroup(self, text, line, beg, end):
 def do_user_removegroup(self, args):
     (args, _options) = parse_arguments(args)
 
-    if len(args) != 2:
+    if len(args) < 2:
         self.help_user_removegroup()
         return
 
@@ -489,7 +489,7 @@ def complete_user_removedefaultgroup(self, text, line, beg, end):
 def do_user_removedefaultgroup(self, args):
     (args, _options) = parse_arguments(args)
 
-    if len(args) != 2:
+    if len(args) < 2:
         self.help_user_removedefaultgroup()
         return
 
