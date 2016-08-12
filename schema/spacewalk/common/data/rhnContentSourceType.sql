d68474f62e36dff01083a8c084c6ad66dbb31bd5
Jan Dobes <jdobes@redhat.com>
2016-08-12 18:34:02 +0200

    enable deb type

--- schema/spacewalk/common/data/rhnContentSourceType.sql
+++ schema/spacewalk/common/data/rhnContentSourceType.sql
@@ -20,3 +20,5 @@ insert into rhnContentSourceType (id, label) values
 (sequence_nextval('rhn_content_source_type_id_seq'), 'yum');
 insert into rhnContentSourceType (id, label) values
 (sequence_nextval('rhn_content_source_type_id_seq'), 'uln');
+insert into rhnContentSourceType (id, label) values
+(sequence_nextval('rhn_content_source_type_id_seq'), 'deb');
