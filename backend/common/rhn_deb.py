d75ac71f940866c3b51bfa22cd8fb1272fede3cf
Jan Dobes <jdobes@redhat.com>
2016-09-02 15:53:57 +0200

    fixing rhnpush
    AttributeError: WsgiRequest instance has no attribute 'tell'

--- backend/common/rhn_deb.py
+++ backend/common/rhn_deb.py
@@ -122,7 +122,7 @@ class DEB_Package(A_Package):
 
     def read_header(self):
         self._stream_copy(self.input_stream, self.header_data)
-        self.header_end = self.input_stream.tell()
+        self.header_end = self.header_data.tell()
         try:
             self.header_data.seek(0, 0)
             self.header = deb_Header(self.header_data)
