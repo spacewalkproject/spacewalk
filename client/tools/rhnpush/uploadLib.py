a20233da9415b28f8d4466ac93d6829873f3a6cb
Gennadii Altukhov <galt@redhat.com>
2016-11-03 17:06:52 +0100

    1390170 - rhnpush is used on a client side as well, so make it compatible with RHEL 5 and Python 2.4

--- client/tools/rhnpush/uploadLib.py
+++ client/tools/rhnpush/uploadLib.py
@@ -754,7 +754,7 @@ def getServer(uri, proxy=None, username=None, password=None, ca_chain=None):
 # pylint: disable=E1123
 def hasChannelChecksumCapability(rpc_server):
     """ check whether server supports getPackageChecksumBySession function"""
-    if 'rpcServerOverride' in inspect.getargspec(rhnserver.RhnServer.__init__).args:
+    if 'rpcServerOverride' in inspect.getargspec(rhnserver.RhnServer.__init__)[0]:
         server = rhnserver.RhnServer(rpcServerOverride=rpc_server)
     else:
         server = rhnserver.RhnServer()
@@ -768,7 +768,7 @@ def exists_getPackageChecksumBySession(rpc_server):
     # unfortunatelly we do not have capability for getPackageChecksumBySession function,
     # but extended_profile in version 2 has been created just 2 months before
     # getPackageChecksumBySession lets use it instead
-    if 'rpcServerOverride' in inspect.getargspec(rhnserver.RhnServer.__init__).args:
+    if 'rpcServerOverride' in inspect.getargspec(rhnserver.RhnServer.__init__)[0]:
         server = rhnserver.RhnServer(rpcServerOverride=rpc_server)
     else:
         server = rhnserver.RhnServer()
