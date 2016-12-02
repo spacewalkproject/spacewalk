spacewalkproject/spacewalk

commit dc7ee6df8e4235799c27064c8b828f2b946bcc3d

Fix of deprecated functions urllib.splittype and urllib.splithost

client/rhel/rhn-client-tools/src/up2date_client/rpcServer.py
@@ -71,14 +71,14 @@ class RetryServer(rpclib.Server):
                 # try a different url
 
                 # use the next serverURL
-                import urllib
-                typ, uri = urllib.splittype(self.serverList.server())
+                parse_res = urlparse.urlsplit(self.serverList.server())
+                typ = parse_res.scheme
+                self._host = parse_res.netloc
+                self._handler = parse_res.path
                 typ = typ.lower()
                 if typ not in ("http", "https"):
                     raise_with_tb(rpclib.InvalidRedirectionError(
                         "Redirected to unsupported protocol %s" % typ))
-
-                self._host, self._handler = urllib.splithost(uri)
                 self._orig_handler = self._handler
                 self._type = typ
                 self._uri = self.serverList.server()

commit af589564670a5c4e0272d1b3a08caf52291173ea

urlsplit back compatibility with python 2.4 fixing bug introduced in dc7ee6d

client/rhel/rhn-client-tools/src/up2date_client/rpcServer.py
@@ -72,9 +72,9 @@ class RetryServer(rpclib.Server):
 
                 # use the next serverURL
                 parse_res = urlparse.urlsplit(self.serverList.server())
-                typ = parse_res.scheme
-                self._host = parse_res.netloc
-                self._handler = parse_res.path
+                typ = parse_res[0] # scheme
+                self._host = parse_res[1] # netloc
+                self._handler = parse_res[2] # path
                 typ = typ.lower()
                 if typ not in ("http", "https"):
                     raise_with_tb(rpclib.InvalidRedirectionError(