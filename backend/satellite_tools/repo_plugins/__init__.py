66e3b2f49a6860db6cebc2dc164db19f5358b22e
Jan Dobes <jdobes@redhat.com>
2016-12-15 17:20:54 +0100

    1409434 - retry download, checksum etc.

--- backend/satellite_tools/repo_plugins/__init__.py
+++ backend/satellite_tools/repo_plugins/__init__.py
@@ -21,11 +21,12 @@ import rpm
 from Queue import Queue, Empty
 from threading import Thread, Lock
 import pycurl
-from urlgrabber.grabber import URLGrabberOptions, PyCurlFileObject
+from urlgrabber.grabber import URLGrabberOptions, PyCurlFileObject, URLGrabError
 from spacewalk.common import rhn_pkg
+from spacewalk.common.checksum import getFileChecksum
 from spacewalk.common.rhnException import rhnFault
 from spacewalk.server import rhnPackageUpload
-from spacewalk.satellite_tools.syncLib import log, log2stderr
+from spacewalk.satellite_tools.syncLib import log, log2
 
 
 class ProgressBarLogger:
@@ -35,7 +36,7 @@ class ProgressBarLogger:
         self.status = 0
         self.lock = Lock()
 
-    def log(self, params=None):
+    def log(self, success, params=None):
         self.lock.acquire()
         self.status += 1
         self._print_progress_bar(self.status, self.total, prefix=self.msg, bar_length=50)
@@ -78,6 +79,10 @@ class PyCurlFileObjectThread(PyCurlFileObject):
         return self.fo
 
 
+class FailedDownloadError(Exception):
+    pass
+
+
 class DownloadThread(Thread):
     def __init__(self, parent):
         Thread.__init__(self)
@@ -85,12 +90,67 @@ class DownloadThread(Thread):
         # pylint: disable=E1101
         self.curl = pycurl.Curl()
 
+    @staticmethod
+    def __is_file_done(local_path=None, file_obj=None, checksum_type=None, checksum=None):
+        if checksum_type and checksum:
+            if local_path and os.path.isfile(local_path):
+                return getFileChecksum(checksum_type, filename=local_path) == checksum
+            elif file_obj:
+                return getFileChecksum(checksum_type, file_obj=file_obj) == checksum
+        if local_path and os.path.isfile(local_path):
+            return True
+        elif file_obj:
+            return True
+        return False
+
+    def __can_retry(self, retry, opts, url, e):
+        retrycode = getattr(e, 'errno', None)
+        code = getattr(e, 'code', None)
+        if retry < (self.parent.retries - 1):
+            # No codes at all or some specified codes
+            # 58, 77 - Couple of curl error codes observed in multithreading on RHEL 7 - probably a bug
+            if (retrycode is None and code is None) or (retrycode in opts.retrycodes or code in [58, 77]):
+                log2(0, 2, "ERROR: Download failed: %s - %s. Retrying..." % (url, sys.exc_info()[1]),
+                     stream=sys.stderr)
+                return True
+
+        log2(0, 1, "ERROR: Download failed: %s - %s." % (url, sys.exc_info()[1]),
+             stream=sys.stderr)
+        return False
+
     def __fetch_url(self, params):
-        (base_urls, remote_relative, local_path, ssl_ca_cert, ssl_cert, ssl_key) = params
+        (base_urls, remote_relative, local_path, ssl_ca_cert, ssl_cert, ssl_key, checksum_type, checksum) = params
+
+        # Skip existing file if exists and matches checksum
+        if not self.parent.force:
+            if self.__is_file_done(local_path=local_path, checksum_type=checksum_type, checksum=checksum):
+                return True
+
         url = base_urls[0] + '/' + remote_relative
         opts = URLGrabberOptions(ssl_ca_cert=ssl_ca_cert, ssl_cert=ssl_cert, ssl_key=ssl_key)
-        fo = PyCurlFileObjectThread(url, local_path, opts, self.curl)
-        fo.close()
+        for retry in range(self.parent.retries):
+            fo = None
+            try:
+                try:
+                    fo = PyCurlFileObjectThread(url, local_path, opts, self.curl)
+                    # Check target file
+                    if not self.__is_file_done(file_obj=fo, checksum_type=checksum_type, checksum=checksum):
+                        raise FailedDownloadError("Target file isn't valid. Checksum should be %s (%s)."
+                                                  % (checksum, checksum_type))
+                    break
+                except (FailedDownloadError, URLGrabError):
+                    e = sys.exc_info()[1]
+                    if not self.__can_retry(retry, opts, url, e):
+                        return False
+
+            finally:
+                if fo:
+                    fo.close()
+                # Delete failed download file
+                elif os.path.isfile(local_path):
+                    os.unlink(local_path)
+
+        return True
 
     def run(self):
         while not self.parent.queue.empty():
@@ -98,33 +158,39 @@ class DownloadThread(Thread):
                 params = self.parent.queue.get(block=False)
             except Empty:
                 break
-            self.__fetch_url(params)
+            success = self.__fetch_url(params)
             if self.parent.log_obj:
                 # log_obj must be thread-safe
-                self.parent.log_obj.log(params=params)
+                self.parent.log_obj.log(success, params=params)
             self.parent.queue.task_done()
 
 
 class ThreadedDownloader:
-    def __init__(self, threads=5, log_obj=None):
+    def __init__(self, threads=5, retries=3, log_obj=None, force=False):
         self.queue = Queue()
         self.threads = threads
+        self.retries = retries
         self.log_obj = log_obj
+        self.force = force
 
     def set_log_obj(self, log_obj):
         self.log_obj = log_obj
+
+    def set_force(self, force):
+        self.force = force
 
     @staticmethod
     def _validate(ssl_ca_cert, ssl_cert, ssl_key):
         for certificate_file in (ssl_ca_cert, ssl_cert, ssl_key):
             if certificate_file and not os.path.isfile(certificate_file):
-                log2stderr(0, "ERROR: Certificate file not found: %s" % certificate_file)
+                log2(0, 0, "ERROR: Certificate file not found: %s" % certificate_file, stream=sys.stderr)
                 return False
         return True
 
-    def add(self, base_urls, remote_relative, local_path, ssl_ca_cert, ssl_cert, ssl_key):
+    def add(self, base_urls, remote_relative, local_path, ssl_ca_cert, ssl_cert, ssl_key, checksum_type, checksum):
         if self._validate(ssl_ca_cert, ssl_cert, ssl_key):
-            self.queue.put((base_urls, remote_relative, local_path, ssl_ca_cert, ssl_cert, ssl_key))
+            self.queue.put((base_urls, remote_relative, local_path,
+                            ssl_ca_cert, ssl_cert, ssl_key, checksum_type, checksum))
 
     def run(self):
         size = self.queue.qsize()
