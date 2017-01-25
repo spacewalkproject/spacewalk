b7a88fb1a7fdb05195a765edaac5fcd7d0574427
Jan Dobes <jdobes@redhat.com>
2017-01-25 18:51:37 +0100

    1401497 - updating repo create/edit page

--- java/code/src/com/redhat/rhn/frontend/action/channel/manage/repo/RepoDetailsAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/channel/manage/repo/RepoDetailsAction.java
@@ -207,8 +207,9 @@ public class RepoDetailsAction extends RhnAction {
         form.set(URL, repo.getSourceUrl());
         form.set(SOURCEID, repo.getId());
         form.set(TYPE, repo.getType().getLabel());
-        if (repo.isSsl()) {
-            SslContentSource sslRepo = (SslContentSource) repo;
+        Set<SslContentSource> repoSslSets = repo.getSslSets();
+        if (!repoSslSets.isEmpty()) {
+            SslContentSource sslRepo = repoSslSets.iterator().next();
             form.set(SSL_CA_CERT, getStringId(sslRepo.getCaCert()));
             form.set(SSL_CLIENT_CERT, getStringId(sslRepo.getClientCert()));
             form.set(SSL_CLIENT_KEY, getStringId(sslRepo.getClientKey()));
@@ -311,11 +312,14 @@ public class RepoDetailsAction extends RhnAction {
         repoCmd.setLabel(label);
         repoCmd.setUrl(url);
         repoCmd.setType(type);
-        repoCmd.setSslCaCertId(parseIdFromForm(form, SSL_CA_CERT));
-        repoCmd.setSslClientCertId(parseIdFromForm(form, SSL_CLIENT_CERT));
-        repoCmd.setSslClientKeyId(parseIdFromForm(form, SSL_CLIENT_KEY));
 
         try {
+            // Add SSL
+            // FIXME: Allow to set multiple SSL sets per custom repo - new page?
+            repoCmd.deleteAllSslSets();
+            repoCmd.addSslSet(parseIdFromForm(form, SSL_CA_CERT),
+                    parseIdFromForm(form, SSL_CLIENT_CERT),
+                    parseIdFromForm(form, SSL_CLIENT_KEY));
             // Process filters
             List<ContentSourceFilter> lresult = processFilters(sfilters);
 
