f81c16d8c1571250a7206a7f7de26fbb48892408
Jan Dobes <jdobes@redhat.com>
2017-01-26 13:32:25 +0100

    1401497 - updating serializers

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/SerializerRegistry.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/SerializerRegistry.java
@@ -107,6 +107,7 @@ public class SerializerRegistry {
         SERIALIZER_CLASSES.add(DistChannelMapSerializer.class);
         SERIALIZER_CLASSES.add(ContentSourceSerializer.class);
         SERIALIZER_CLASSES.add(ContentSourceFilterSerializer.class);
+        SERIALIZER_CLASSES.add(SslContentSourceSerializer.class);
         SERIALIZER_CLASSES.add(XccdfTestResultDtoSerializer.class);
         SERIALIZER_CLASSES.add(XccdfTestResultSerializer.class);
         SERIALIZER_CLASSES.add(XccdfRuleResultDtoSerializer.class);
--- java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/SslContentSourceSerializer.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/serializer/SslContentSourceSerializer.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009--2016 Red Hat, Inc.
+ * Copyright (c) 2017 Red Hat, Inc.
  *
  * This software is licensed to you under the GNU General Public License,
  * version 2 (GPLv2). There is NO WARRANTY for this software, express or
@@ -20,37 +20,30 @@ import java.io.Writer;
 import redstone.xmlrpc.XmlRpcException;
 import redstone.xmlrpc.XmlRpcSerializer;
 
-import com.redhat.rhn.domain.channel.ContentSource;
+import com.redhat.rhn.domain.channel.SslContentSource;
+import com.redhat.rhn.domain.kickstart.crypto.SslCryptoKey;
 import com.redhat.rhn.frontend.xmlrpc.serializer.util.SerializerHelper;
 
 /**
  *
- * ContentSourceSerializer
+ * SslContentSourceSerializer
  * @version $Rev$
  *
  * @xmlrpc.doc
- *  #struct("channel")
- *      #prop("int", "id")
- *      #prop("string", "label")
- *      #prop("string", "sourceUrl")
- *      #prop("string", "type")
- *      #array("sslContentSources")
- *          #struct()
- *              #prop("string", "sslCaDesc")
- *              #prop("string", "sslCertDesc")
- *              #prop("string", "sslKeyDesc")
- *          #struct_end()
- *      #array_end()
+ *  #struct("contentsourcessl")
+ *      #prop("string", "sslCaDesc")
+ *      #prop("string", "sslCertDesc")
+ *      #prop("string", "sslKeyDesc")
  *  #struct_end()
  *
  */
-public class ContentSourceSerializer extends RhnXmlRpcCustomSerializer {
+public class SslContentSourceSerializer extends RhnXmlRpcCustomSerializer {
 
     /**
      * {@inheritDoc}
      */
     public Class getSupportedClass() {
-        return ContentSource.class;
+        return SslContentSource.class;
     }
 
     /**
@@ -59,13 +52,14 @@ public class ContentSourceSerializer extends RhnXmlRpcCustomSerializer {
     protected void doSerialize(Object value, Writer output, XmlRpcSerializer serializer)
         throws XmlRpcException, IOException {
         SerializerHelper helper = new SerializerHelper(serializer);
-        ContentSource repo = (ContentSource) value;
+        SslContentSource sslSet = (SslContentSource) value;
+        SslCryptoKey ca = sslSet.getCaCert();
+        SslCryptoKey cert = sslSet.getClientCert();
+        SslCryptoKey key = sslSet.getClientKey();
 
-        helper.add("id", repo.getId());
-        helper.add("label", repo.getLabel());
-        helper.add("sourceUrl", repo.getSourceUrl());
-        helper.add("type", repo.getType().getLabel());
-        helper.add("sslContentSources", repo.getSslSets());
+        helper.add("sslCaDesc", (ca != null) ? ca.getDescription() : "");
+        helper.add("sslCertDesc", (cert != null) ? cert.getDescription() : "");
+        helper.add("sslKeyDesc", (key != null) ? key.getDescription() : "");
 
         helper.writeTo(output);
     }
