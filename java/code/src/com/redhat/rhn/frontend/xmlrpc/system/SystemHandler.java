07d37ebe93786a2ba4e2b523c538bfc745ecb2b5
Gennadii Altukhov <galt@redhat.com>
2017-02-01 14:22:24 +0100

    1414421 - fix unhandled internal exception: null

--- java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
+++ java/code/src/com/redhat/rhn/frontend/xmlrpc/system/SystemHandler.java
@@ -92,6 +92,7 @@ import com.redhat.rhn.frontend.xmlrpc.InvalidParameterException;
 import com.redhat.rhn.frontend.xmlrpc.InvalidProfileLabelException;
 import com.redhat.rhn.frontend.xmlrpc.InvalidSystemException;
 import com.redhat.rhn.frontend.xmlrpc.MethodInvalidParamException;
+import com.redhat.rhn.frontend.xmlrpc.NoPushClientException;
 import com.redhat.rhn.frontend.xmlrpc.NoSuchActionException;
 import com.redhat.rhn.frontend.xmlrpc.NoSuchCobblerSystemRecordException;
 import com.redhat.rhn.frontend.xmlrpc.NoSuchNetworkInterfaceException;
@@ -5657,7 +5658,15 @@ public class SystemHandler extends BaseHandler {
      */
     public int sendOsaPing(User loggedInUser, Integer serverId) {
         Server server = lookupServer(loggedInUser, serverId);
-        PushClient client = server.getPushClient();
+        if (server == null) {
+            throw new InvalidSystemException();
+        }
+
+        PushClient client = null;
+        client = server.getPushClient();
+        if(client == null) {
+            throw new NoPushClientException();
+        }
         client.setLastPingTime(new Date());
         client.setNextActionTime(null);
         SystemManager.storeServer(server);
