a0631882e1809951e6933235cdb82321eb9c80b5
Tomas Kasparek <tkasparek@redhat.com>
2017-03-10 11:24:52 +0100

    1351992 - remove no longer used package

--- projects/oracle-server-scripts/oracle-home.sh
+++
@@ -1,9 +0,0 @@
-
-PROGRAM_NAME=${BASH_ARGV[0]:-$0}
-OracleVersionShort=$(basename $(cd $(dirname $PROGRAM_NAME) && pwd))
-Oracle=/opt/apps/oracle
-OracleHome=$Oracle/web/product/$OracleVersionShort/db_1
-
-export ORACLE_INIT=$Oracle/admin/$OracleVersionShort/$ORACLE_SID/init.ora
-export ORACLE_HOME=$OracleHome
-echo "Using ORACLE_HOME=$ORACLE_HOME"
