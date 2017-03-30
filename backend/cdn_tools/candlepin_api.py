c09d1b30f6070b897e5cdd816b67e75289ae10ad
Jan Dobes <jdobes@redhat.com>
2017-03-30 16:52:30 +0200

    1397762 - adding handler for calling Candlepin API requests with export manifest support

---
+++ backend/cdn_tools/candlepin_api.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2017 Red Hat, Inc.
+#
+# This software is licensed to you under the GNU General Public License,
+# version 2 (GPLv2). There is NO WARRANTY for this software, express or
+# implied, including the implied warranties of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. You should have received a copy of GPLv2
+# along with this software; if not, see
+# http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt.
+#
+# Red Hat trademarks are not licensed under GPLv2. No permission is
+# granted to use or replicate Red Hat trademarks that are incorporated
+# in this software or its documentation.
+#
+
+import os
+import tempfile
+
+import requests
+
+from spacewalk.cdn_tools.constants import CA_CERT_PATH
+from spacewalk.common.cli import getUsernamePassword
+from spacewalk.common.rhnConfig import CFG
+
+
+class CandlepinApi(object):
+    """Class used to communicate with Candlepin API."""
+
+    def __init__(self, current_manifest=None, username=None, password=None,
+                 http_proxy=None, http_proxy_username=None, http_proxy_password=None):
+        self.base_url = CFG.CANDLEPIN_SERVER_API
+        if self.base_url.startswith('https'):
+            self.protocol = 'https'
+        elif self.base_url.startswith('http'):
+            self.protocol = 'http'
+        else:
+            raise ValueError("Invalid protocol in URL: '%s'" % self.base_url)
+
+        if not self.base_url.endswith('/'):
+            self.base_url += '/'
+
+        self.current_manifest = current_manifest
+
+        # Authentication with upstream consumer certificate or with username and password
+        if self.current_manifest and self.protocol == 'https' and not username:
+            self.username = self.password = None
+        else:
+            print("Candlepin login:")
+            self.username, self.password = getUsernamePassword(username, password)
+
+        self.http_proxy = http_proxy
+        self.http_proxy_username = http_proxy_username
+        self.http_proxy_password = http_proxy_password
+
+    def _get_proxies(self):
+        proxies = {}
+        if self.http_proxy:
+            auth = ""
+            if self.http_proxy_username and self.http_proxy_password:
+                auth = "%s:%s@" % (self.http_proxy_username, self.http_proxy_password)
+            proxy_string = "http://%s%s" % (auth, self.http_proxy)
+            proxies["http"] = proxy_string
+            proxies["https"] = proxy_string
+        return proxies
+
+    def _write_cert(self):
+        cert = None
+        if self.current_manifest:
+            credentials = self.current_manifest.get_consumer_credentials()
+            fd, cert_file = tempfile.mkstemp(prefix="/tmp/cert-")
+            fo = os.fdopen(fd, 'wb')
+            fo.write(credentials.get_cert())
+            fo.flush()
+            fo.close()
+
+            fd, key_file = tempfile.mkstemp(prefix="/tmp/key-")
+            fo = os.fdopen(fd, 'wb')
+            fo.write(credentials.get_key())
+            fo.flush()
+            fo.close()
+            cert = (cert_file, key_file)
+        return cert
+
+    @staticmethod
+    def _delete_cert(cert):
+        if cert is not None:
+            cert_file, key_file = cert
+            if cert_file:
+                os.unlink(cert_file)
+            if key_file:
+                os.unlink(key_file)
+
+    def _call_api(self, url, params):
+        if self.protocol == 'https':
+            verify = CA_CERT_PATH
+        else:
+            verify = False
+
+        if self.username is not None and self.password is not None:
+            response = requests.get(url, params=params, proxies=self._get_proxies(),
+                                    auth=(self.username, self.password), verify=verify)
+        else:
+            cert = self._write_cert()
+            response = requests.get(url, params=params, proxies=self._get_proxies(), verify=verify, cert=cert)
+            self._delete_cert(cert)
+        return response
+
+    def export_manifest(self, uuid=None, ownerid=None, satellite_version=None):
+        """Performs export request to Candlepin API and saves exported manifest to target file.
+           Can take required parameters from current manifest or override them with parameters of this method."""
+        if uuid is None:
+            if self.current_manifest:
+                uuid = self.current_manifest.get_uuid()
+            else:
+                raise ValueError("Uuid is not known.")
+        if ownerid is None:
+            if self.current_manifest:
+                ownerid = self.current_manifest.get_ownerid()
+            else:
+                raise ValueError("Ownerid is not known.")
+        if satellite_version is None:
+            if self.current_manifest:
+                satellite_version = self.current_manifest.get_satellite_version()
+            else:
+                raise ValueError("Satellite version is not known.")
+
+        url = "%s%s/export" % (self.base_url, uuid)
+        params = {"ext": ["ownerid:%s" % ownerid, "version:%s" % satellite_version]}
+
+        print("Download URL: '%s'" % url)
+        print("Download parameters: '%s'" % str(params))
+
+        response = self._call_api(url, params)
+
+        if response.status_code == requests.codes.ok:
+            fd, downloaded_manifest = tempfile.mkstemp(prefix="/tmp/manifest-", suffix=".zip")
+            fo = os.fdopen(fd, 'wb')
+            for chunk in response:
+                fo.write(chunk)
+            fo.flush()
+            fo.close()
+            return downloaded_manifest
+        else:
+            print("Download status code: %s" % response.status_code)
+            print("Message: '%s'" % response.text)
+            return None
