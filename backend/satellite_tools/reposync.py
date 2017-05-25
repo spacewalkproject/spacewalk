555cab4817b1e45448c7a32fcf32a9f69d7294ab
Jan Dobes <jdobes@redhat.com>
2017-05-25 14:25:53 +0200

    1455433 - don't trim channel content if there is major sync error in any repository

--- backend/satellite_tools/reposync.py
+++ backend/satellite_tools/reposync.py
@@ -482,7 +482,7 @@ class RepoSync(object):
                 sync_error = -1
 
         # In strict mode unlink all packages from channel which are not synced from current repositories
-        if self.strict:
+        if self.strict and sync_error == 0:
             channel_packages = rhnSQL.fetchall_dict("""
                 select p.id, ct.label as checksum_type, c.checksum
                 from rhnChannelPackage cp,
