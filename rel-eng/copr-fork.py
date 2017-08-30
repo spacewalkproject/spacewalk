690a9c93330001f8c4ec7da888e8e0c9cfcc916f
Michael Mraka <michael.mraka@redhat.com>
2017-08-30 16:03:44 +0200

    skip packages without git_url

--- rel-eng/copr-fork.py
+++ rel-eng/copr-fork.py
@@ -19,17 +19,22 @@ opts = parser.parse_args()
 dest_owner, dest_project = opts.destination_repo[0].split('/',2)
 myclient = CoprClient.create_from_file_config()
 
+print("Forking project: %s -> %s" % (opts.source_repo[0], opts.destination_repo[0]))
 myclient.fork_project(source=opts.source_repo[0],
                       username=dest_owner, projectname=dest_project, confirm=opts.force)
 
 result = myclient.get_packages_list(ownername=dest_owner, projectname=dest_project)
 
 for pkg in result.packages_list:
+    print(" Updating package: %s" % pkg.data['name'])
     pkg_source = json.loads(pkg.data['source_json'])
-    myclient.edit_package_tito(package_name=pkg.data['name'],
-                               ownername=dest_owner,
-                               projectname=dest_project,
-                               git_url=pkg_source['git_url'],
-                               git_dir=pkg_source['git_dir'],
-                               git_branch=opts.git_branch[0],
-                               webhook_rebuild=pkg.data['webhook_rebuild'])
+    if pkg_source and pkg_source.get('git_url', None):
+        myclient.edit_package_tito(package_name=pkg.data['name'],
+                                   ownername=dest_owner,
+                                   projectname=dest_project,
+                                   git_url=pkg_source['git_url'],
+                                   git_dir=pkg_source['git_dir'],
+                                   git_branch=opts.git_branch[0],
+                                   webhook_rebuild=pkg.data['webhook_rebuild'])
+    else:
+        print("  ERROR: package git url is missing")
