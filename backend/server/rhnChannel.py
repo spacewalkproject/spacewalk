spacewalkproject/spacewalk

commit 79b0ed8e2faaf2fa054ebf6263eb12afa662441c

1494575 - RHEL7 sends also release - drop it as it's not needed

backend/server/rhnChannel.py
@@ -859,7 +859,7 @@ def base_eus_channel_for_ver_rel_arch(version, release, server_arch,
         else:
             parts = 3
 
-        server_rel = '.'.join(release.split('.')[:parts])
+        server_rel = '.'.join(release..split('-')[0].split('.')[:parts])
         channel_rel = '.'.join(db_release.split('.')[:parts])
 
         # XXX we're no longer using the is_default column from the db

commit 4fc79cb933eb1024b061f5c8fb9b1bd669b4073a

Fix typo introduced in 79b0ed8e2f

backend/server/rhnChannel.py
@@ -859,7 +859,7 @@ def base_eus_channel_for_ver_rel_arch(version, release, server_arch,
         else:
             parts = 2
 
-        server_rel = '.'.join(release..split('-')[0].split('.')[:parts])
+        server_rel = '.'.join(release.split('-')[0].split('.')[:parts])
         channel_rel = '.'.join(db_release.split('.')[:parts])
 
         # XXX we're no longer using the is_default column from the db