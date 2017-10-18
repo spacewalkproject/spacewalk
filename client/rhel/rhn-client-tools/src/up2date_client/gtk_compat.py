038149bf5a4fe808c2bde7011e7ff7c014b81f93
Michael Mraka <michael.mraka@redhat.com>
2017-10-18 16:43:32 +0200

    fixed different way of loading glade files in pygtk/gtk3

--- client/rhel/rhn-client-tools/src/up2date_client/gtk_compat.py
+++ client/rhel/rhn-client-tools/src/up2date_client/gtk_compat.py
@@ -16,3 +16,21 @@ except ImportError: # python3 /gtk3 / gi
     from gi.repository import Gtk as gtk
     from gi.repository import GObject as gobject
     GTK3 = True
+
+
+if GTK3:
+    class GladeBuilder(object):
+        def __init__(self):
+            self.builder = gtk.Builder()
+            self.builder.get_widget = self.builder.get_object
+            self.builder.signal_autoconnect = self.builder.connect_signals
+            self.translation_domain = None
+
+        def XML(self, gladefile, widget, domain):
+            self.builder.add_objects_from_file(gladefile, (widget,))
+            if not self.translation_domain:
+                self.builder.set_translation_domain(domain)
+            return self.builder
+
+    gtk.glade = GladeBuilder()
+    gtk.RESPONSE_NONE = gtk.ResponseType.NONE
