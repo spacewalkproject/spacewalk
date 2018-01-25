e17548459fe9c5e10e98b8870dda04000e112616
Tomas Kasparek <tkasparek@redhat.com>
2018-01-25 13:28:21 +0100

    use rhnCompsType table for different types of repo metadata (comps) files

--- schema/spacewalk/common/tables/rhnChannelComps.sql
+++ schema/spacewalk/common/tables/rhnChannelComps.sql
@@ -21,19 +21,25 @@ CREATE TABLE rhnChannelComps
     channel_id         NUMBER NOT NULL
                            CONSTRAINT rhn_channelcomps_cid_fk
                                REFERENCES rhnChannel (id)
-                               ON DELETE CASCADE
-			constraint rhn_channelcomps_cid_uq unique
-			using index tablespace [[2m_tbs]],
+                               ON DELETE CASCADE,
     relative_filename  VARCHAR2(256) NOT NULL,
     last_modified      timestamp with local time zone
                            DEFAULT (current_timestamp) NOT NULL,
     created            timestamp with local time zone
                            DEFAULT (current_timestamp) NOT NULL,
     modified           timestamp with local time zone
-                           DEFAULT (current_timestamp) NOT NULL
+                           DEFAULT (current_timestamp) NOT NULL,
+    comps_type_id      NUMBER NOT NULL
+                          CONSTRAINT rhn_channelcomps_comps_type_fk
+                               REFERENCES rhnCompsType(id)
+
 )
 ENABLE ROW MOVEMENT
 ;
+
+CREATE UNIQUE INDEX rhn_channelcomps_cid_ctype_uq
+    ON rhnChannelComps (channel_id, comps_type_id)
+    TABLESPACE [[2m_tbs]];
 
 CREATE SEQUENCE rhn_channelcomps_id_seq START WITH 101;
 
