de33104c8826eec757bc80276e8529374ff598a5
Jan Dobes <jdobes@redhat.com>
2018-03-23 15:43:12 +0100

    delete container after run

--- rel-eng/docker-builder/test_run.sh
+++ rel-eng/docker-builder/test_run.sh
@@ -2,4 +2,4 @@
 
 GITROOT=$(pwd)/$(git rev-parse --show-cdup)
 
-docker run --privileged=true -v $GITROOT:/git:z -v /tmp/:/out:z -e PACKAGE=backend -e DIST=spacewalk-nightly-el7 spacewalkproject/docker-builder
+docker run --rm --privileged=true -v $GITROOT:/git:z -v /tmp/:/out:z -e PACKAGE=backend -e DIST=spacewalk-nightly-el7 spacewalkproject/docker-builder
