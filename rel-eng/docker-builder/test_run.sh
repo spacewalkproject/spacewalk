a193dde6de002e92936241d64721d253f9e6c638
Jan Dobes <jdobes@redhat.com>
2018-03-23 16:00:34 +0100

    enable on Fedora 27

--- rel-eng/docker-builder/test_run.sh
+++ rel-eng/docker-builder/test_run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+set -e
+
 GITROOT=$(pwd)/$(git rev-parse --show-cdup)
 
 docker run --rm --privileged=true -v $GITROOT:/git:z -v /tmp/:/out:z -e PACKAGE=backend -e DIST=spacewalk-nightly-el7 spacewalkproject/docker-builder
+
+docker run --rm --privileged=true -v $GITROOT:/git:z -v /tmp/:/out:z -e PACKAGE=backend -e DIST=spacewalk-nightly-fedora27 spacewalkproject/docker-builder
