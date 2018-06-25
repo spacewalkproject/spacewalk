9be948a4a01d0dd41baed0c6d0032a2078d336d3
Tomas Kasparek <tkasparek@redhat.com>
2018-06-25 13:04:49 +0200

    1581744 - provide a warning when doing package actions on channels with modules

--- java/code/src/com/redhat/rhn/frontend/action/rhnpackage/BaseSystemPackagesAction.java
+++ java/code/src/com/redhat/rhn/frontend/action/rhnpackage/BaseSystemPackagesAction.java
@@ -16,6 +16,7 @@
 package com.redhat.rhn.frontend.action.rhnpackage;
 
 import com.redhat.rhn.common.db.datasource.DataResult;
+import com.redhat.rhn.domain.channel.Channel;
 import com.redhat.rhn.domain.server.Server;
 import com.redhat.rhn.domain.user.User;
 import com.redhat.rhn.frontend.action.systems.sdc.SdcHelper;
@@ -104,7 +105,16 @@ public abstract class BaseSystemPackagesAction extends RhnAction {
         ListTagHelper.bindSetDeclTo(LIST_NAME, getDecl(sid), request);
         TagHelper.bindElaboratorTo(LIST_NAME, dataSet.getElaborator(), request);
 
-
+        boolean hasModules = false;
+        for (Channel channel : server.getChannels()) {
+            if (channel.getModules() != null) {
+                hasModules = true;
+                break;
+            }
+        }
+        if (hasModules) {
+            addMessage(request, "packagelist.jsp.modulespresent");
+        }
         return mapping.findForward(RhnHelper.DEFAULT_FORWARD);
     }
 
