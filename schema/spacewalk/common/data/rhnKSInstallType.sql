555f16b7ec15c732f6a43bd61013854784572b19
Michael Mraka <michael.mraka@redhat.com>
2018-09-21 11:31:08 +0200

    1624826 - added kickstart distribution data for RHEL 8

--- schema/spacewalk/common/data/rhnKSInstallType.sql
+++ schema/spacewalk/common/data/rhnKSInstallType.sql
@@ -14,6 +14,11 @@
 --
 insert into rhnKSInstallType (id, label, name)
         values (sequence_nextval('rhn_ksinstalltype_id_seq'),
+                'rhel_8','Red Hat Enterprise Linux 8'
+        );
+
+insert into rhnKSInstallType (id, label, name)
+        values (sequence_nextval('rhn_ksinstalltype_id_seq'),
                 'rhel_7','Red Hat Enterprise Linux 7'
         );
 
