ccfeebffc39a28fb42e753a2dc24ab5812314f21
Tomas Kasparek <tkasparek@redhat.com>
2018-10-18 07:44:49 +0200

    1640156 - correctly recognize Nutanix AHV as virtual guest

--- backend/server/handlers/xmlrpc/registration.py
+++ backend/server/handlers/xmlrpc/registration.py
@@ -94,6 +94,8 @@ def parse_smbios(smbios):
         if uuid is None:
             uuid = "flex-guest"
         return (rhnVirtualization.VirtualizationType.VIRTAGE, uuid)
+    elif manufacturer == 'Nutanix' and product == 'AHV' and uuid is not None:
+        return (rhnVirtualization.VirtualizationType.QEMU, uuid)
     else:
         return (None, None)
 
