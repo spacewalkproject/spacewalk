300c6200ae3577322e395f409839a76aff3880fe
Michael Mraka <michael.mraka@redhat.com>
2018-10-19 16:06:49 +0200

    fixing pylint line-too-long

--- backend/satellite_tools/reposync.py
+++ backend/satellite_tools/reposync.py
@@ -1323,7 +1323,8 @@ class RepoSync(object):
             where ce.channel_id = :cid
         """)
         h.execute(cid=self.channel['id'])
-        advisories = dict((row['advisory_name'], row['update_date'].strftime("%Y-%m-%d %H:%M:%S")) for row in h.fetchall_dict() or [])
+        advisories = dict((row['advisory_name'], row['update_date'].strftime("%Y-%m-%d %H:%M:%S"))
+                          for row in h.fetchall_dict() or [])
         return advisories
 
     def list_errata(self):
