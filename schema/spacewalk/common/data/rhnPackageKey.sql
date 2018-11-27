54fc43516043106e68528cc1a414de7b476e38d5
Michael Mraka <michael.mraka@redhat.com>
2018-11-27 09:26:16 +0100

    added Fedora 29 keys
    (cherry picked from commit 559d8b12c7631fed05a45e4d67c4f3411c32cc40)

--- schema/spacewalk/common/data/rhnPackageKey.sql
+++ schema/spacewalk/common/data/rhnPackageKey.sql
@@ -99,6 +99,10 @@ insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 -- Fedora 28
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 (sequence_nextval('rhn_pkey_id_seq'), '9DB62FB1', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
+-- Fedora 29
+insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
+(sequence_nextval('rhn_pkey_id_seq'), '429476B4', lookup_package_key_type('gpg'), lookup_package_provider('Fedora'));
+
 -- CentOS
 insert into rhnPackageKey (id, key_id, key_type_id, provider_id) values
 (sequence_nextval('rhn_pkey_id_seq'), 'a8a447dce8562897', lookup_package_key_type('gpg'), lookup_package_provider('CentOS'));
