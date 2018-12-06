44e16ff00c89e882a7c3c95e920cd8698aa60acb
Michael Mraka <michael.mraka@redhat.com>
2018-12-06 10:14:40 +0100

    fixed failed upgrade on oracle
    (cherry picked from commit b4dec482edc9795a301e3c6e2dbac528e6a12d2a)

--- schema/spacewalk/upgrade/spacewalk-schema-2.8-to-spacewalk-schema-2.9/002-rhnKickstartableTree-kernelOptions.sql
+++ schema/spacewalk/upgrade/spacewalk-schema-2.8-to-spacewalk-schema-2.9/002-rhnKickstartableTree-kernelOptions.sql
@@ -1,3 +1,4 @@
 ALTER TABLE rhnKickstartableTree
-    ADD COLUMN kernel_options       VARCHAR2(256),
-    ADD COLUMN kernel_options_post  VARCHAR2(256);
+    ADD kernel_options       VARCHAR2(256);
+ALTER TABLE rhnKickstartableTree
+    ADD kernel_options_post  VARCHAR2(256);
