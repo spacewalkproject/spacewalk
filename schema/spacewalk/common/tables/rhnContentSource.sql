fdd813d7249bc842c8e1284049d5cca978b9ccaa
Michael Mraka <michael.mraka@redhat.com>
2019-04-04 15:28:06 +0200

    1695614 - allow repo and manifest sources with the same url

--- schema/spacewalk/common/tables/rhnContentSource.sql
+++ schema/spacewalk/common/tables/rhnContentSource.sql
@@ -42,6 +42,7 @@ CREATE UNIQUE INDEX rhn_cs_label_uq
     ON rhnContentSource(org_id, label)
     tablespace [[64k_tbs]];
 CREATE UNIQUE INDEX rhn_cs_repo_uq
-    ON rhnContentSource(org_id, type_id, source_url)
+    ON rhnContentSource(org_id, type_id, source_url,
+                        (case when label like 'manifest_%' then 1 else 0 end))
     tablespace [[64k_tbs]];
 
