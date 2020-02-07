e8972205cc9aadedf8383817c9a763a9b5a04c8e
Michael Mraka <michael.mraka@redhat.com>
2020-02-07 15:06:54 +0100

    Automatic commit of package [python-gzipstream] release [2.10.3-1].
    Created by command:
    
    /usr/bin/tito tag

--- projects/python-gzipstream/setup.py
+++ projects/python-gzipstream/setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 
 setup(name = "gzipstream",
-      version = "2.10.2",
+      version = "2.10.3",
       description = "Streaming zlib (gzip) support for python",
       long_description = """\
 A streaming gzip handler.
